a4fe5f1b1dfcce8086c2a499fb02f112b3c44f22
Priyatam Mudivarti <priyatam@gmail.com>
2006-12-14 01:49:14 +0000

    renamed to ProtocolService to StudyService with Study specific references

---
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/service/StudyService.java
@@ -0,0 +1,12 @@
+package edu.duke.cabig.c3pr.service;
+
+import edu.duke.cabig.c3pr.domain.Study;
+
+/**
+ * @author priyatam
+ * @version 1.0
+ */
+public interface StudyService {
+
+	  public void saveStudy(Study study) throws Exception;
+}
