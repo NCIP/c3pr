139a4ce6ea7ce418d78f0534a05d509d1186d494
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2006-12-20 22:24:19 +0000

    Adding a new domain class (Person)

---
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/Person.java
@@ -0,0 +1,94 @@
+package edu.duke.cabig.c3pr.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author Kulasekaran
+ * @version 1.0
+ */
+
+@MappedSuperclass
+public abstract class Person extends AbstractDomainObject implements Serializable{
+		
+	@Column(name = "FIRST_NAME", length = 20, nullable = false)
+	private String firstName;
+	
+	@Column(name = "LAST_NAME", length = 20, nullable = false)
+	private String lastName;
+	
+	@Column(name = "BIRTH_DATE", nullable = false)
+	private Date birthDate;
+	
+	@Column(name = "SSN", length = 20)
+	private String ssn;
+	
+	@Column(name = "ADMINISTRITATIVE_GENDER_CODE", length = 1)
+	private String administritativeGenderCode;
+	
+	@Column(name = "ETHNIC_GROUP_CODE", length = 20)
+	private String ethnicGroupCode;
+	
+	@Column(name = "RACE_CODE", length = 20)
+	private String raceCode;
+
+	public String getAdministritativeGenderCode() {
+		return administritativeGenderCode;
+	}
+
+	public void setAdministritativeGenderCode(String administritativeGenderCode) {
+		this.administritativeGenderCode = administritativeGenderCode;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getEthnicGroupCode() {
+		return ethnicGroupCode;
+	}
+
+	public void setEthnicGroupCode(String ethnicGroupCode) {
+		this.ethnicGroupCode = ethnicGroupCode;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getRaceCode() {
+		return raceCode;
+	}
+
+	public void setRaceCode(String raceCode) {
+		this.raceCode = raceCode;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+			
+}
