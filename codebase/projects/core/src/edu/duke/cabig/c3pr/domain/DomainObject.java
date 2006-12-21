18677a14b718c4a37626d8291429b09c932bc624
Priyatam Mudivarti <priyatam@gmail.com>
2006-12-21 17:11:04 +0000

    javadocs, implemented default comparator

--- codebase/projects/core/src/edu/duke/cabig/c3pr/domain/DomainObject.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/DomainObject.java
@@ -1,19 +1,22 @@
 package edu.duke.cabig.c3pr.domain;
 
 import java.util.Comparator;
+import edu.nwu.bioinformatics.commons.ComparisonUtils;
 
 /**
+ * Generic Interface for every domain object in C3PR. 
  * @author Priyatam
  */
 public interface DomainObject {
-    public Integer getId();
-    public void setId(Integer id);
-
+    Integer getId();
+    Integer getVersion();
+    
+    void setId(Integer id);
+    void setVersion(Integer version);
+    
     class ById<T extends DomainObject> implements Comparator<T> {
-        public int compare(T o1, T o2) {
-            //return ComparisonUtils.nullSafeCompare(o1.getId(), o2.getId());
-        	//TODO remove
-        	return 1;
-        }
+    	 public int compare(T o1, T o2) {
+             return ComparisonUtils.nullSafeCompare(o1.getId(), o2.getId());
+         }
     }
 }
