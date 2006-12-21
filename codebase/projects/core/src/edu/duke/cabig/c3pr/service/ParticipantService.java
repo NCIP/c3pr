f23c9b0dc0e62200cdbbd280b45ac057b300bb12
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2006-12-21 15:29:33 +0000

    Added new service layer interface (ParticipantService)

---
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/service/ParticipantService.java
@@ -0,0 +1,13 @@
+package edu.duke.cabig.c3pr.service;
+
+import edu.duke.cabig.c3pr.domain.Participant;
+
+/**
+ * @author Kulasekaran
+ * @version 1.0
+ */
+public interface ParticipantService {
+
+	  public void saveParticipant(Participant participant) throws Exception;
+}
+
