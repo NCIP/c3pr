157da0733549abddcc62ddf78c474660e85254cf
Priyatam Mudivarti <priyatam@gmail.com>
2006-12-21 17:11:50 +0000

    javadocs, added equals/hash

--- codebase/projects/core/src/edu/duke/cabig/c3pr/domain/AbstractDomainObject.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/AbstractDomainObject.java
@@ -7,20 +7,18 @@ import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
 /**
+ * Abstract Domain class implementing Domain object
+ * @see edu.duke.cabig.c3pr.domain.DomainObject
  * @author Priyatam
  */
 
 @MappedSuperclass
 public abstract class AbstractDomainObject implements DomainObject {
     
-	@Id @GeneratedValue(generator = "id-generator")	   
-	private Integer id;
-	
-	@Version
-	@Column(name = "VERSION")
+	private Integer id;	
 	private Integer version;
 
-    
+	@Id @GeneratedValue(generator = "id-generator")	   	
     public Integer getId() {
         return id;
     }
@@ -29,7 +27,9 @@ public abstract class AbstractDomainObject implements DomainObject {
         this.id = id;
     }
     
-     public Integer getVersion() {
+    @Version
+	@Column(name = "VERSION")	
+    public Integer getVersion() {
         return version;
     }
 
@@ -51,4 +51,41 @@ public abstract class AbstractDomainObject implements DomainObject {
                 : t1.getId().equals(t2.getId());
         }
     }
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int PRIME = 31;
+		int result = 1;
+		result = PRIME * result + ((id == null) ? 0 : id.hashCode());
+		result = PRIME * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final AbstractDomainObject other = (AbstractDomainObject) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
 }
