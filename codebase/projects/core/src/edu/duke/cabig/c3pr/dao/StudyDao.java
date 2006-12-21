463cc3e81c13b894cd16e1a606c782eeb329b989
Priyatam Mudivarti <priyatam@gmail.com>
2006-12-21 17:07:59 +0000

    added searchByExample(), javadocs

--- codebase/projects/core/src/edu/duke/cabig/c3pr/dao/StudyDao.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/dao/StudyDao.java
@@ -1,27 +1,37 @@
 package edu.duke.cabig.c3pr.dao;
 
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
 import edu.duke.cabig.c3pr.domain.Study;
 
 /**
- * @author Priyatam
+ *  Interface defining methods by which the C3PR Application will access the 
+ *  data store.  All data pertaiing to the Study data aggregate should be 
+ *  accessed via an implementation of this interface.
  *
- *  Interface defining methods by which the C3PR Application will access the data store.  All data
- *  pertaiing to the Study data aggregate should be accessed via an implementation of this
- *  interface.
- *
- *  These methods throw org.springframework.dao.DataAccessException which is a Run-time Exception
- *  that should be handled in one of the calling classes.  See the Spring Framework API for a
- *  hierarchy of the DataAccessException.
-
+ *  These methods throw org.springframework.dao.DataAccessException which is 
+ *  a Run-time Exception that should be handled in one of the calling classes.  
+ *  See the Spring Framework API for a hierarchy of the DataAccessException.
+ *  
+ *  @author Priyatam
  */
 public interface StudyDao extends BaseDao{
-
 
 	/**
 	 * Add a new Study to the data source 
 	 * @param study
 	 * @throws Exception
+	 */	
+	public List<Study> getAll(); 
+	 
+	/**
+	 * Searches based on an example object.	
+	 * @param study
+	 * @return 
+	 * @throws Exception
 	 */
-	public void saveStudy(Study study) throws Exception;
+	public List<Study> searchByExample(Study study) throws DataAccessException;
 	
 }
--- codebase/projects/core/src/edu/duke/cabig/c3pr/dao/hibernate/StudyDaoHibernate.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/dao/hibernate/StudyDaoHibernate.java
@@ -1,12 +1,16 @@
 package edu.duke.cabig.c3pr.dao.hibernate;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Example;
+import org.springframework.dao.DataAccessException;
 import edu.duke.cabig.c3pr.dao.AbstractBaseDao;
 import edu.duke.cabig.c3pr.dao.StudyDao;
 import edu.duke.cabig.c3pr.domain.Study;
 
+
 /**
- * @author Priyatam
- *
  *  Interface defining methods by which the C3PR Application will access the data store.  All data
  *  pertaiing to the Study data aggregate should be accessed via an implementation of this
  *  interface.
@@ -14,17 +18,40 @@ import edu.duke.cabig.c3pr.domain.Study;
  *  These methods throw org.springframework.dao.DataAccessException which is a Run-time Exception
  *  that should be handled in one of the calling classes.  See the Spring Framework API for a
  *  hierarchy of the DataAccessException.
-
+ * 
+ * @author Priyatam
  */
 public class StudyDaoHibernate extends AbstractBaseDao<Study> implements StudyDao {
 	
+	@Override
 	public Class<Study> domainClass() {
-	        return Study.class;
+		return Study.class;
 	 }
-	
-	 public void saveStudy(Study study) throws Exception {
-		 getHibernateTemplate().saveOrUpdate(study);		
-	}
 
+	/*
+	 * Searches based on an example object. Typical usage from your service class: -
+	 * If you want to search based on diseaseCode, monitorCode,
+	 * <li><code>Study study = new Study();</li></code>
+	 * <li>code>study.setDiseaseCode("diseaseCode");</li></code>
+	 * <li>code>study.setDMonitorCode("monitorCode");</li></code>
+	 * <li>code>studyDao.searchByExample(study)</li></code>
+	 * @return list of matching study objects based on your sample study object
+	 * (non-Javadoc)
+	 * @see edu.duke.cabig.c3pr.dao.StudyDao#searchByExample(edu.duke.cabig.c3pr.domain.Study)
+	 */
+	public List<Study> searchByExample(Study study) throws DataAccessException{
+		Session session = getHibernateTemplate().getSessionFactory().openSession();		
+		Example searchCriteria = Example.create(study);
+		return session.createCriteria(Study.class).add(searchCriteria).list();
+	}
+	
+	/*
+	 * Returns all Study objects
+	 * (non-Javadoc)
+	 * @see edu.duke.cabig.c3pr.dao.StudyDao#getAll()
+	 */
+	 public List<Study> getAll() {
+		 return getHibernateTemplate().find("from Study");
+	 }
 
 }
