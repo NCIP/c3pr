4e1b450a27e1c5e5921ec342c9d94e41f1a37604
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2006-12-21 21:16:04 +0000

    removed unnecessary attributes

--- codebase/projects/core/src/edu/duke/cabig/c3pr/domain/Participant.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/Participant.java
@@ -2,7 +2,6 @@ package edu.duke.cabig.c3pr.domain;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -23,61 +22,11 @@ import org.hibernate.annotations.Parameter;
 )
 public class Participant extends Person implements Serializable
 {
-	@Column(name = "CONFIDENTIALITY_INDICATOR", nullable = false)
-	private boolean confidentialityIndicator;
-	
-	@Column(name = "INITIALS", length=1, nullable = false)
-	private char initials;
-	
-	@Column(name = "PAYMENT_METHOD_CODE", length=1, nullable = false)
-	private char paymentMethodCode;
-	
-	@Column(name = "ZIP_CODE", length=10, nullable = false)
-	private String zipCode;
-	
-  	public boolean isConfidentialityIndicator() {
-		return confidentialityIndicator;
-	}
-
-	public void setConfidentialityIndicator(boolean confidentialityIndicator) {
-		this.confidentialityIndicator = confidentialityIndicator;
-	}
-
-	public char getInitials() {
-		return initials;
-	}
-
-	public void setInitials(char initials) {
-		this.initials = initials;
-	}
-
-	public char getPaymentMethodCode() {
-		return paymentMethodCode;
-	}
-
-	public void setPaymentMethodCode(char paymentMethodCode) {
-		this.paymentMethodCode = paymentMethodCode;
-	}
-
-	public String getZipCode() {
-		return zipCode;
-	}
-
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-
 	/**
      * Simple constructor of Participant instances.
      */
     public Participant()
     {
-    }
-    
-	    
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L; 
+    }    
 }
 
