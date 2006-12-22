60ec049a0df564fa3aa0064d47a3c8975e723ad1
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2006-12-22 15:59:09 +0000

    added initBinder method

--- codebase/projects/web/src/edu/duke/cabig/c3pr/web/CreateParticipantController.java
+++ codebase/projects/web/src/edu/duke/cabig/c3pr/web/CreateParticipantController.java
@@ -1,9 +1,14 @@
 package edu.duke.cabig.c3pr.web;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
@@ -16,23 +21,32 @@ import edu.duke.cabig.c3pr.service.ParticipantService;
  * @version 1.0
  * 
  */
-public class CreateParticipantController extends SimpleFormController{
+public class CreateParticipantController extends SimpleFormController {
 
 	private ParticipantService participantService;
-		
-	public CreateParticipantController()
-	{
-		setCommandClass(Participant.class);		
+
+	public CreateParticipantController() {
+		setCommandClass(Participant.class);
 	}
 
-	protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object oCommand, BindException errors) throws Exception {
+	@Override
+	protected void initBinder(HttpServletRequest req,
+			ServletRequestDataBinder binder) throws Exception {
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				new SimpleDateFormat("MM-DD-YYYY"), true));
+	}
+
+	@Override
+	protected ModelAndView onSubmit(HttpServletRequest request,
+			HttpServletResponse response, Object oCommand, BindException errors)
+			throws Exception {
 		Participant participant = (Participant) oCommand;
-    	participantService.saveParticipant(participant);   
-    	ModelAndView modelAndView= new ModelAndView(getSuccessView());
-    	modelAndView.addAllObjects(errors.getModel());
-    	return modelAndView;
-    }
-	
+		participantService.saveParticipant(participant);
+		ModelAndView modelAndView = new ModelAndView(getSuccessView());
+		modelAndView.addAllObjects(errors.getModel());
+		return modelAndView;
+	}
+
 	public ParticipantService getParticipantService() {
 		return participantService;
 	}
