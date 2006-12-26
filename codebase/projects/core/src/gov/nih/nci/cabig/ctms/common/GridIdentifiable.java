cbb309f516a6551aca91f2a42aa4e8dc33218f65
Ram Chilukuri <ram.chilukuri@semanticbits.com>
2006-12-26 23:42:10 +0000

    *** empty log message ***

---
+++ codebase/projects/core/src/gov/nih/nci/cabig/ctms/common/GridIdentifiable.java
@@ -0,0 +1,8 @@
+package gov.nih.nci.cabig.ctms.common;
+
+public interface GridIdentifiable {
+
+    public String getGridIndentifier();
+    
+    public String setGridIdentifier(String gridID);
+}
