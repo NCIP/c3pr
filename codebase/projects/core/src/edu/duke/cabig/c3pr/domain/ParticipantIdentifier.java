4e14417596fba4b46209de65646b9d3617b66b61
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2006-12-28 19:54:18 +0000

    new class

---
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/ParticipantIdentifier.java
@@ -0,0 +1,69 @@
+package edu.duke.cabig.c3pr.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * @author Kulasekaran
+ * @version 1.0 
+ * 
+ */
+
+@Entity 
+@Table (name = "PARTICIPANT_IDENTIFIERS")
+@GenericGenerator (name="id-generator", strategy = "native",
+		parameters = {
+			@Parameter(name="sequence", value="C3PR_GENERAL_SEQ")
+		}
+)
+public class ParticipantIdentifier extends AbstractDomainObject implements Serializable
+{			
+	private String medicalRecordNumber;
+	
+	private Participant participant;
+	
+	private HealthcareSite healthcareSite;
+	
+	public ParticipantIdentifier()
+    {
+    	
+    }    
+    
+    @ManyToOne( cascade = {CascadeType.ALL} )
+	@JoinColumn(name = "HCS_ID", insertable=false, updatable=false)			
+	public HealthcareSite getHealthcareSite() {
+		return healthcareSite;
+	}
+
+	public void setHealthcareSite(HealthcareSite healthcareSite) {
+		this.healthcareSite = healthcareSite;
+	}
+
+	@Column(name = "MEDICAL_RECORD_NUMBER", length = 20, nullable = false)
+	public String getMedicalRecordNumber() {
+		return medicalRecordNumber;
+	}
+
+	public void setMedicalRecordNumber(String medicalRecordNumber) {
+		this.medicalRecordNumber = medicalRecordNumber;
+	}
+	
+	@ManyToOne( cascade = {CascadeType.ALL} )
+	@JoinColumn(name = "PRT_ID", insertable=false, updatable=false)		
+	public Participant getParticipant() {
+		return participant;
+	}
+
+	public void setParticipant(Participant participant) {
+		this.participant = participant;
+	}    
+}
