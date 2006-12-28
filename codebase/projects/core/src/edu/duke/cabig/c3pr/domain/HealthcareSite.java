9d49ef4d9cb5dc28a677808f977f01ec9b8c77ea
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2006-12-28 19:48:42 +0000

    Modified Annotation

--- codebase/projects/core/src/edu/duke/cabig/c3pr/domain/HealthcareSite.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/HealthcareSite.java
@@ -1,25 +1,16 @@
 package edu.duke.cabig.c3pr.domain;
 
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
-import org.hibernate.annotations.Sort;
-import org.hibernate.annotations.SortType;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Transient;
 
 
 /**
  * @author Ram Chilukuri
+ *         Kulasekaran
  */
  
  @Entity
@@ -30,15 +21,18 @@ import javax.persistence.Transient;
      }
  )
 public class HealthcareSite extends Organization {
+		 
     private String nciIdentifier;
+	
     public HealthcareSite() {
     }
 
-    public void setNciIdentifier(String nciIdentifier) {
-        this.nciIdentifier = nciIdentifier;
-    }
-
+    @Column(name = "NCI_INSTITUTE_CODE", length = 20, nullable = false)
     public String getNciIdentifier() {
         return nciIdentifier;
     }
+    
+    public void setNciIdentifier(String nciIdentifier) {
+        this.nciIdentifier = nciIdentifier;
+    }    
 }
--- codebase/projects/core/src/edu/duke/cabig/c3pr/domain/Organization.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/Organization.java
@@ -1,53 +1,53 @@
 package edu.duke.cabig.c3pr.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.JoinColumn;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToOne;
 
-
 /**
  * @author Ram Chilukuri
+ *         Kulasekaran
  */
 
 @MappedSuperclass
 public abstract class Organization extends AbstractDomainObject {
-	
-	@Column(name = "NAME", length = 20, nullable = false)
+		
     private String name;
-	
-	@Column(name = "DESCRIPTION_TEXT", length = 50, nullable = false)
+		
     private String descriptionText;
 
-	@OneToOne(optional=false)
-    @JoinColumn(name="ADDRESS_ID")
     private Address address;
 	
     public Organization() {
     }
 
-    // Bean Methods
+    @Column(name = "NAME", length = 20, nullable = false)
+    public String getName() {
+        return name;
+    }
+    
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
+    @Column(name = "DESCRIPTION_TEXT", length = 50, nullable = false)
+    public String getDescriptionText() {
+        return descriptionText;
     }
 
     public void setDescriptionText(String descriptionText) {
         this.descriptionText = descriptionText;
     }
 
-    public String getDescriptionText() {
-        return descriptionText;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-    
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="ADDRESS_ID")
     public Address getAddress() {
         return address;
+    }
+    
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
