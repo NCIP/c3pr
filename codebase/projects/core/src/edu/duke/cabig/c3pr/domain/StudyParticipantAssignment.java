f0a3704a7f51ee45e3657bdfa82506a4a3a352a5
Priyatam Mudivarti <priyatam@gmail.com>
2007-01-04 22:56:32 +0000

    renamed to STUDY_PARTICIPANT_ASS_ID_SEQ

--- codebase/projects/core/src/edu/duke/cabig/c3pr/domain/StudyParticipantAssignment.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/StudyParticipantAssignment.java
@@ -4,20 +4,17 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.OneToOne;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 
 /**
@@ -28,7 +25,7 @@ import org.hibernate.annotations.CascadeType;
 @Table (name = "STUDY_PARTICIPANT_ASSIGNMENTS")
 @GenericGenerator(name="id-generator", strategy = "native",
     parameters = {
-        @Parameter(name="sequence", value="STUDY_PARTICIPANT_ASSIGNMENTS_ID_seq")
+        @Parameter(name="sequence", value="STUDY_PARTICIPANT_ASS_ID_SEQ")
     }
 )
 public class StudyParticipantAssignment extends AbstractDomainObject {
