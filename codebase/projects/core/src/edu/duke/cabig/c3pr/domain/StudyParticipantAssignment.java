d62b8c6494700e3829d06f26103d56d4a278b58e
kruttik Aggarwal <kruttikagarwal@gmail.com>
2007-01-04 16:15:51 +0000

    *** empty log message ***

--- codebase/projects/core/src/edu/duke/cabig/c3pr/domain/StudyParticipantAssignment.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/StudyParticipantAssignment.java
@@ -1,16 +1,23 @@
 package edu.duke.cabig.c3pr.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 
 /**
@@ -18,20 +25,21 @@ import org.hibernate.annotations.Parameter;
  */
 
 @Entity
-@Table (name = "participant_assignments")
+@Table (name = "STUDY_PARTICIPANT_ASSIGNMENTS")
 @GenericGenerator(name="id-generator", strategy = "native",
     parameters = {
-        @Parameter(name="sequence", value="STUDY_PARTICIPANT_ASSIG_ID_SEQ")
+        @Parameter(name="sequence", value="STUDY_PARTICIPANT_ASSIGNMENTS_ID_seq")
     }
 )
 public class StudyParticipantAssignment extends AbstractDomainObject {
     
     private StudySite studySite;
     private Participant participant;
-    private Date startDateEpoch;
+    private List<ScheduledArm> scheduledArms = new ArrayList<ScheduledArm>();
+    private Date startDate;
     private String studyParticipantIdentifier;
     private String eligibilityWaiverReasonText;
-    private Date informedConsentFormDate;
+    private Date informedConsentSignedDate;
 
     
     ////// BEAN PROPERTIES
@@ -41,7 +49,8 @@ public class StudyParticipantAssignment extends AbstractDomainObject {
     }
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "study_site_id")
+    @JoinColumn(name = "STS_ID")
+    @Cascade (value={CascadeType.ALL})
     public StudySite getStudySite() {
         return studySite;
     }
@@ -51,19 +60,29 @@ public class StudyParticipantAssignment extends AbstractDomainObject {
     }
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "participant_id")
+    @JoinColumn(name = "PRT_ID")
     public Participant getParticipant() {
         return participant;
     }
 
-    public void setStartDateEpoch(Date startDateEpoch) {
-        this.startDateEpoch = startDateEpoch;
-    }
+    @OneToMany (mappedBy = "studyParticipantAssignment")
+    @Cascade (value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })    	    
+	public List<ScheduledArm> getScheduledArms() {
+		return scheduledArms;
+	}
 
-    @Column(name = "first_epoch_stdate")
-    public Date getStartDateEpoch() {
-        return startDateEpoch;
-    }
+	public void setScheduledArms(List<ScheduledArm> scheduledArms) {
+		this.scheduledArms = scheduledArms;
+	}
+
+	public void addScheduledArm(ScheduledArm scheduledArm){
+		scheduledArms.add(scheduledArm);
+	}
+
+	public void removeScheduledArm(ScheduledArm scheduledArm){
+		scheduledArms.remove(scheduledArm);
+	}
+
 
     public void setStudyParticipantIdentifier(String studyParticipantIdentifier) {
         this.studyParticipantIdentifier = studyParticipantIdentifier;
@@ -81,23 +100,32 @@ public class StudyParticipantAssignment extends AbstractDomainObject {
         return eligibilityWaiverReasonText;
     }
 
-    public void setInformedConsentFormDate(Date informedConsentFormDate) {
-        this.informedConsentFormDate = informedConsentFormDate;
-    }
-
-    public Date getInformedConsentFormDate() {
-        return informedConsentFormDate;
-    }
 
     ////// OBJECT METHODS
 
-    public boolean equals(Object o) {
+    public Date getInformedConsentSignedDate() {
+		return informedConsentSignedDate;
+	}
+
+	public void setInformedConsentSignedDate(Date informedConsentSignedDate) {
+		this.informedConsentSignedDate = informedConsentSignedDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         final StudyParticipantAssignment that = (StudyParticipantAssignment) o;
 
-        if (startDateEpoch != null ? !startDateEpoch.equals(that.startDateEpoch) : that.startDateEpoch != null)
+        if (startDate != null ? !startDate.equals(that.startDate) : that.startDate != null)
             return false;
         if (studySite != null ? !studySite.equals(that.studySite) : that.studySite != null)
             return false;
@@ -111,7 +139,7 @@ public class StudyParticipantAssignment extends AbstractDomainObject {
         int result;
         result = (studySite != null ? studySite.hashCode() : 0);
         result = 29 * result + (participant != null ? participant.hashCode() : 0);
-        result = 29 * result + (startDateEpoch != null ? startDateEpoch.hashCode() : 0);
+        result = 29 * result + (startDate != null ? startDate.hashCode() : 0);
         return result;
     }
 
