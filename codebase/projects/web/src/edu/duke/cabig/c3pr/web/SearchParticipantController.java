b30af3915574fddd76c497361c16b8411a2f3690
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-01-05 16:48:02 +0000

    Adding SearchParticipant Controller

---
+++ codebase/projects/web/src/edu/duke/cabig/c3pr/web/SearchParticipantController.java
@@ -0,0 +1,126 @@
+
+package edu.duke.cabig.c3pr.web;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+import edu.duke.cabig.c3pr.domain.Participant;
+import edu.duke.cabig.c3pr.service.ParticipantService;
+
+/**
+ * @author Ramakrishna
+ *
+ */
+
+
+public class SearchParticipantController extends SimpleFormController{
+	
+private static Log log = LogFactory.getLog(SearchParticipantController.class);
+	
+	private ParticipantService participantService;
+	public SearchParticipantController(){
+		setCommandClass(SearchParticipantCommand.class);
+		this.setFormView("particpant_search");
+		this.setSuccessView("participant_search_results");
+	}
+	
+	public ParticipantService getParticipantService() {
+		return participantService;
+	}
+
+	public void setStudyService(ParticipantService participantService) {
+		this.participantService = participantService;
+	}
+	
+	protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object oCommand, BindException errors) throws Exception {
+    	SearchParticipantCommand searchParticipantCommand = (SearchParticipantCommand) oCommand;
+    	Participant participant = new Participant();
+    	String text = searchParticipantCommand.getSearchText();
+    	String type = searchParticipantCommand.getSearchType();
+    	
+    	log.debug("search string = " +text+"; type = "+type);
+    	    	
+    	if ("N".equals(type)){
+    		participant.setLastName(text);
+    	}
+    	    		
+    	List<Participant> participants = participantService.search(participant); 
+    	
+    	Iterator<Participant> participantIter = participants.iterator(); 
+        while(participantIter.hasNext()){
+        	participant = participantIter.next();
+        	System.out.println("Id for participant is "+participant.getId());
+        	System.out.println("LastName of participant is "+participant.getLastName());
+        	System.out.println("FirstName of participant is "+participant.getFirstName());
+        //	System.out.println(" D.O.B of participant is " + participant.getBirthDate());
+        	
+        }
+    	
+    	log.debug("Search results size " +participants.size());
+    	Map map =errors.getModel();
+    	map.put("participants", participants);
+    	ModelAndView modelAndView= new ModelAndView(getSuccessView(), map);
+    	return modelAndView;
+    }
+	
+	 protected Map<String, Object> referenceData(HttpServletRequest httpServletRequest) throws Exception {
+ 		Map<String, Object> refdata = new HashMap<String, Object>();
+ 	
+ 	refdata.put("searchType", getSearchType());
+     return refdata;
+ }
+	 
+	 private List<LOV> getSearchType(){
+			List<LOV> col = new ArrayList<LOV>();
+			LOV lov1 = new LOV("N", "LastName");
+						
+			col.add(lov1);
+	    	    	
+	    	return col;
+		}
+	 
+	 public class LOV {
+			
+			private String code;
+			private String desc;
+			
+			LOV(String code, String desc)
+			{
+				this.code=code;
+				this.desc=desc;
+				
+			}
+			
+			public String getCode() {
+				return code;
+			}
+
+			public void setCode(String code) {
+				this.code = code;
+			}
+			
+			public String getDesc(){
+				return desc;
+			}
+				
+			public void setDesc(String desc){
+				this.desc=desc;
+			}
+		}
+	
+	
+
+}
