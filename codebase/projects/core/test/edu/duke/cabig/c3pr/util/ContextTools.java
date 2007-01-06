16f2317428c39b64d949a728de21f9874326c013
Priyatam Mudivarti <priyatam@gmail.com>
2007-01-06 23:04:33 +0000

    updated context files location : Seems to be a bug in Ant, doesnt load context files with wildchar when run through ant.

--- codebase/projects/core/test/edu/duke/cabig/c3pr/util/ContextTools.java
+++ codebase/projects/core/test/edu/duke/cabig/c3pr/util/ContextTools.java
@@ -9,10 +9,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class ContextTools {
     public static ApplicationContext createDeployedApplicationContext() {
+    	   
     	  return new ClassPathXmlApplicationContext (new String[] {
-			    "classpath:/applicationContext.xml",
-	            "classpath*:/applicationContext-*.xml"
-    }); 
+    			 "classpath*:applicationContext.xml","classpath*:applicationContext-core.xml"	          
+    	  }); 
      }
 
     private ContextTools() { }
