1eabdc5696c1d80d8335d30643c132ab8490cdd2
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-01-06 17:46:32 +0000

    Removed Type and Value attribute from code

--- codebase/projects/core/src/edu/duke/cabig/c3pr/domain/ParticipantIdentifier.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/ParticipantIdentifier.java
@@ -3,7 +3,6 @@ package edu.duke.cabig.c3pr.domain;
 import java.io.Serializable;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -19,43 +18,31 @@ import org.hibernate.annotations.Parameter;
  */
 
 @Entity 
-@Table (name = "PARTICIPANT_IDENTIFIERS")
+@Table (name = "participant_identifiers")
 @GenericGenerator (name="id-generator", strategy = "native",
 		parameters = {
 			@Parameter(name="sequence", value="participant_identifiers_id_seq")
 		}
 )
-public class ParticipantIdentifier extends AbstractDomainObject implements Serializable
+public class ParticipantIdentifier extends AbstractDomainObject
 {			
-//	private String medicalRecordNumber;
-	
-	private Participant participant;
-	private String value;
-	private String typeCode;
-	
+	private String medicalRecordNumber;	
+	private Participant participant;	
 	private HealthcareSite healthcareSite;
-	
-	public String getTypeCode() {
-		return typeCode;
-	}
-
-	public void setTypeCode(String typeCode) {
-		this.typeCode = typeCode;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-
+		
 	public ParticipantIdentifier()
     {
     	
     }    
     
+	public String getMedicalRecordNumber() {
+		return medicalRecordNumber;
+	}
+
+	public void setMedicalRecordNumber(String medicalRecordNumber) {
+		this.medicalRecordNumber = medicalRecordNumber;	
+	}
+	
 	@ManyToOne( cascade = {CascadeType.ALL} )
 	@JoinColumn(name = "HCS_ID")		
 	public HealthcareSite getHealthcareSite() {
@@ -65,14 +52,6 @@ public class ParticipantIdentifier extends AbstractDomainObject implements Seria
 	public void setHealthcareSite(HealthcareSite healthcareSite) {
 		this.healthcareSite = healthcareSite;
 	}
-
-//	public String getMedicalRecordNumber() {
-//		return medicalRecordNumber;
-//	}
-//
-//	public void setMedicalRecordNumber(String medicalRecordNumber) {
-//		this.medicalRecordNumber = medicalRecordNumber;
-//	}
 	
 	@ManyToOne( cascade = {CascadeType.ALL} )
 	@JoinColumn(name = "PRT_ID")		
