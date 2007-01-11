52aca35604ecea2d14d54c332f058d1da9036deb
Priyatam Mudivarti <priyatam@gmail.com>
2007-01-11 17:02:12 +0000

    updated common contextTools for unit testing and main

--- codebase/projects/core/src/edu/duke/cabig/c3pr/utils/ContextTools.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/utils/ContextTools.java
@@ -3,14 +3,28 @@ package edu.duke.cabig.c3pr.utils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+// Note - this classpath pattern has a problem with ant builds. Use below format
+//public class ContextTools {
+//    public static ApplicationContext createDeployedApplicationContext() {
+//        return new ClassPathXmlApplicationContext(new String[] {
+//            "classpath:/applicationContext.xml",
+//            "classpath*:/applicationContext-*.xml"
+//        });
+//    }
+//
+//    private ContextTools() { }
+//}
+
 public class ContextTools {
     public static ApplicationContext createDeployedApplicationContext() {
-        return new ClassPathXmlApplicationContext(new String[] {
-            "classpath:/applicationContext.xml",
-            "classpath*:/applicationContext-*.xml"
-        });
-    }
+    	   
+    	  return new ClassPathXmlApplicationContext (new String[] {
+    			 "classpath*:applicationContext.xml","classpath*:applicationContext-core.xml", 
+    			 "classpath*:applicationContext-configProperties.xml"
+    	  }); 
+     }
 
     private ContextTools() { }
 }
+
 
