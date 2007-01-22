e71fbb396f90e566b821d0e5eff1776fd47a0eef
Priyatam Mudivarti <priyatam@gmail.com>
2007-01-22 16:34:34 +0000

    added

---
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/domain/GridIdentifiable.java
@@ -0,0 +1,17 @@
+package edu.duke.cabig.c3pr.domain;
+/**
+ * Extension Interface from DomainObject with a Grid Identifier
+ * @author Priyatam
+ */
+public interface GridIdentifiable {
+    /**
+     * @return the grid-scoped unique identifier for this object
+     */
+    String getGridId();
+
+    /**
+     * Specify the grid-scoped unique identifier for this object
+     * @param bigId
+     */
+    void setGridId(String bigId);
+}
