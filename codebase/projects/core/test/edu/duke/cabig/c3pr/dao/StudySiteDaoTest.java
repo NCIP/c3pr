bca6cf07d9025ae4f6a380dca06670954eae9ded
Priyatam Mudivarti <priyatam@gmail.com>
2007-01-30 20:41:23 +0000

    added

---
+++ codebase/projects/core/test/edu/duke/cabig/c3pr/dao/StudySiteDaoTest.java
@@ -0,0 +1,35 @@
+package edu.duke.cabig.c3pr.dao;
+
+import static edu.nwu.bioinformatics.commons.testing.CoreTestCase.assertContains;
+
+import java.util.List;
+
+import edu.duke.cabig.c3pr.domain.StudyParticipantAssignment;
+import edu.duke.cabig.c3pr.domain.StudySite;
+import edu.duke.cabig.c3pr.utils.DaoTestCase;
+
+/**
+ * JUnit Tests for StudySiteDao
+ * @author Priyatam
+ * @testType unit
+ */
+public class StudySiteDaoTest extends DaoTestCase {
+    private StudySiteDao dao = (StudySiteDao) getApplicationContext()
+    	.getBean("studySiteDao");
+  
+    /**
+	 * Test for loading a Study by Id 
+	 * @throws Exception
+	 */
+    public void testGetById() throws Exception {
+    	StudySite studySite = dao.getById(1000);
+        assertNotNull("StudySite 1000 not found", studySite);
+    }
+    
+    public void testGetStudyParticipantAssignmentsForStudy() throws Exception {
+    	List<StudyParticipantAssignment> spa = dao.getStudyParticipantAssignmentsForStudy(1000);
+    	assertEquals(2, spa.size());
+        List<Integer> ids = collectIds(spa);
+        assertContains("Wrong study found", ids, 1000);          	    
+    }
+}
