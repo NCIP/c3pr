1584f2b356d7346055bfeeb4c076b998f3abc728
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-01-31 21:57:50 +0000

    Adding Participant Validation Functionality

--- codebase/projects/web/src/edu/duke/cabig/c3pr/web/CreateParticipantController.java
+++ codebase/projects/web/src/edu/duke/cabig/c3pr/web/CreateParticipantController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractWizardFormController;
@@ -21,76 +22,90 @@ import edu.duke.cabig.c3pr.dao.ParticipantDao;
 import edu.duke.cabig.c3pr.domain.Address;
 import edu.duke.cabig.c3pr.domain.Identifier;
 import edu.duke.cabig.c3pr.domain.Participant;
-
+import edu.duke.cabig.c3pr.domain.validator.ParticipantValidator;
 
 /**
- * @author Kulasekaran
+ * @author Kulasekaran,Ramakrishna
  * 
  */
 public class CreateParticipantController extends AbstractWizardFormController {
 
-	private ParticipantDao participantDao;	
-	
+	private ParticipantDao participantDao;
+
+	private ParticipantValidator participantValidator;
+
 	public CreateParticipantController() {
 		setCommandClass(Participant.class);
 	}
 
 	@Override
-	protected Map<String, Object> referenceData(HttpServletRequest httpServletRequest, int page) throws Exception {
-    	// Currently the static data is a hack, once DB design is approved for an LOV this will be
-    	// replaced with LOVDao to get the static data from individual tables
-    	Map<String, Object> refdata = new HashMap<String, Object>();
-    	
-    	if(page==0)
-    	{
-    		refdata.put("administrativeGenderCode", getAdministrativeGenderCodeList());
-    		refdata.put("ethnicGroupCode", getEthnicGroupCodeList());
-    		refdata.put("raceCode", getRaceCodeList());
-    	    refdata.put("source", getSourceList());
-    		refdata.put("searchType", getSearchType());
-    		if(httpServletRequest.getParameter("studySiteId")!=null){
-    			if(!httpServletRequest.getParameter("studySiteId").equals("")){
-    				refdata.put("studySiteId", httpServletRequest.getParameter("studySiteId"));
-    			}
-    		}
-    	}
-    	
-        return refdata;
-    }
-	
-	@Override
-	protected Object formBackingObject(HttpServletRequest request) throws Exception {
-		
-		//FIXME: small hack
-		if(request.getParameter("url")!=null){
-			if(request.getParameter("studySiteId")!=null){
-				setPages(new String[]{"reg_create_patient_study","reg_create_patient_address_study","reg_create_patient_submit_study"});
-			}else{
-				setPages(new String[]{"reg_create_patient","reg_create_patient_address","reg_create_patient_submit"});
+	protected Map<String, Object> referenceData(
+			HttpServletRequest httpServletRequest, int page) throws Exception {
+		// Currently the static data is a hack, once DB design is approved for
+		// an LOV this will be
+		// replaced with LOVDao to get the static data from individual tables
+		Map<String, Object> refdata = new HashMap<String, Object>();
+
+		if (page == 0) {
+			refdata.put("administrativeGenderCode",
+					getAdministrativeGenderCodeList());
+			refdata.put("ethnicGroupCode", getEthnicGroupCodeList());
+			refdata.put("raceCode", getRaceCodeList());
+			refdata.put("source", getSourceList());
+			refdata.put("searchType", getSearchType());
+			if (httpServletRequest.getParameter("studySiteId") != null) {
+				if (!httpServletRequest.getParameter("studySiteId").equals("")) {
+					refdata.put("studySiteId", httpServletRequest
+							.getParameter("studySiteId"));
+				}
 			}
-			request.getSession().setAttribute("url", request.getParameter("url"));
-			request.getSession().setAttribute("studySiteId", request.getParameter("studySiteId"));
-		}else{
-			setPages(new String[]{"participant","participant_address","participant_submit"});
-			request.getSession().removeAttribute("url");	
+		}
+
+		return refdata;
+	}
+
+	@Override
+	protected Object formBackingObject(HttpServletRequest request)
+			throws Exception {
+
+		// FIXME: small hack
+		if (request.getParameter("url") != null) {
+			if (request.getParameter("studySiteId") != null) {
+				setPages(new String[] { "reg_create_patient_study",
+						"reg_create_patient_address_study",
+						"reg_create_patient_submit_study" });
+			} else {
+				setPages(new String[] { "reg_create_patient",
+						"reg_create_patient_address",
+						"reg_create_patient_submit" });
+			}
+			request.getSession().setAttribute("url",
+					request.getParameter("url"));
+			request.getSession().setAttribute("studySiteId",
+					request.getParameter("studySiteId"));
+		} else {
+			setPages(new String[] { "participant", "participant_address",
+					"participant_submit" });
+			request.getSession().removeAttribute("url");
 			request.getSession().removeAttribute("studySiteId");
 		}
-		
-		Participant participant = (Participant) super.formBackingObject(request);
+
+		Participant participant = (Participant) super
+				.formBackingObject(request);
 		for (int i = 0; i < 5; i++) {
-			Identifier temp=new Identifier();
+			Identifier temp = new Identifier();
 			temp.setPrimaryIndicator(false);
 			participant.addIdentifier(temp);
 		}
 		participant.setAddress(new Address());
 		return participant;
 	}
-	
+
 	@Override
 	protected void initBinder(HttpServletRequest req,
 			ServletRequestDataBinder binder) throws Exception {
 		binder.registerCustomEditor(Date.class, new CustomDateEditor(
-				new SimpleDateFormat("MM/dd/yyyy"), true));		
+				new SimpleDateFormat("MM/dd/yyyy"), true));
 	}
 
 	@Override
@@ -98,46 +113,66 @@ public class CreateParticipantController extends AbstractWizardFormController {
 			HttpServletResponse response, Object oCommand, BindException errors)
 			throws Exception {
 		Participant command = (Participant) oCommand;
-		
+
 		Iterator<Identifier> iterator = command.getIdentifiers().iterator();
 
-		while(iterator.hasNext())
-		{
+		while (iterator.hasNext()) {
 			Identifier identifier = iterator.next();
-			if(identifier.getType().trim().length() == 0 || identifier.getValue().trim().length() == 0)
-			{
+			if (identifier.getType().trim().length() == 0
+					|| identifier.getValue().trim().length() == 0) {
 				iterator.remove();
 			}
 		}
-			
+
 		participantDao.save(command);
-		
+
 		ModelAndView modelAndView = null;
-		//FIXME: small hack
+		// FIXME: small hack
 		String url = null;
-		if((url = (String) request.getSession().getAttribute("url")) != null)
-		{
-			url+="?participantId=" + Integer.toString(command.getId());
-			if(request.getSession().getAttribute("studySiteId") != null)
-				url+="&studySiteId="+(String)request.getSession().getAttribute("studySiteId");
+		if ((url = (String) request.getSession().getAttribute("url")) != null) {
+			url += "?participantId=" + Integer.toString(command.getId());
+			if (request.getSession().getAttribute("studySiteId") != null)
+				url += "&studySiteId="
+						+ (String) request.getSession().getAttribute(
+								"studySiteId");
 			response.sendRedirect(url);
 			return null;
 		}
 		response.sendRedirect("/c3pr/SearchAndRegister.do");
 		return null;
-	}	
-	
-	public class LOV {
-		
-		private String code;
-		private String desc;
-		
-		LOV(String code, String desc)
-		{ 
-			this.code=code; 
-			this.desc=desc; 
+	}
+
+	protected void validatePage(Object command, Errors errors, int page) {
+		Participant participant = (Participant) command;
+		switch (page) {
+		case 0: {
+			participantValidator
+					.validateParticipantDetails(participant, errors);
+			// participantValidator.validateIdentifiers(participant, errors);
 		}
-		
+			break;
+		case 1:
+			participantValidator
+					.validateParticipantAddress(participant, errors);
+			break;
+		case 2:
+			//
+			break;
+
+		}
+	}
+
+	public class LOV {
+
+		private String code;
+
+		private String desc;
+
+		LOV(String code, String desc) {
+			this.code = code;
+			this.desc = desc;
+		}
+
 		public String getCode() {
 			return code;
 		}
@@ -145,81 +180,92 @@ public class CreateParticipantController extends AbstractWizardFormController {
 		public void setCode(String code) {
 			this.code = code;
 		}
-		
-		public String getDesc(){
+
+		public String getDesc() {
 			return desc;
 		}
-			
-		public void setDesc(String desc){
-			this.desc=desc;
+
+		public void setDesc(String desc) {
+			this.desc = desc;
 		}
 	}
-	
-	 private List<LOV> getSearchType(){
-			List<LOV> col = new ArrayList<LOV>();
-			LOV lov1 = new LOV("N", "LastName");
-			LOV lov2 = new LOV("MRN", "MRN");
-			
-			col.add(lov1);
-			col.add(lov2);	    	    	
-	    	return col;
-		}
-	
+
+	private List<LOV> getSearchType() {
+		List<LOV> col = new ArrayList<LOV>();
+		LOV lov1 = new LOV("N", "Last Name");
+		LOV lov2 = new LOV("Identifier", "Identifier");
+
+		col.add(lov1);
+		col.add(lov2);
+		return col;
+	}
+
 	private List<LOV> getRaceCodeList() {
 		List<LOV> col = new ArrayList<LOV>();
-		
+
 		col.add(new LOV("-", "--"));
 		col.add(new LOV("Asian", "Asian"));
 		col.add(new LOV("White", "White"));
-		col.add(new LOV("Black or African American", "Black or African American"));
-		col.add(new LOV("American Indian or Alaska Native", "American Indian or Alaska Native"));
-		col.add(new LOV("Native Hawaiian or Pacific Islander", "Native Hawaiian or other Pacific Islander"));
+		col.add(new LOV("Black or African American",
+				"Black or African American"));
+		col.add(new LOV("American Indian or Alaska Native",
+				"American Indian or Alaska Native"));
+		col.add(new LOV("Native Hawaiian or Pacific Islander",
+				"Native Hawaiian or other Pacific Islander"));
 		col.add(new LOV("Not Reported", "Not Reported"));
 		col.add(new LOV("Unknown", "Unknown"));
-		
+
 		return col;
 	}
 
 	private List<LOV> getEthnicGroupCodeList() {
 		List<LOV> col = new ArrayList<LOV>();
-		
+
 		col.add(new LOV("-", "--"));
 		col.add(new LOV("Hispanic or Latino", "Hispanic or Latino"));
 		col.add(new LOV("Non Hispanic or Latino", "Non Hispanic or Latino"));
 		col.add(new LOV("Not Reported", "Not Reported"));
 		col.add(new LOV("Unknown", "Unknown"));
-		
+
 		return col;
 	}
-	
-	private List<LOV> getAdministrativeGenderCodeList() 
-	{
+
+	private List<LOV> getAdministrativeGenderCodeList() {
 		List<LOV> col = new ArrayList<LOV>();
-		
+
 		col.add(new LOV("-", "--"));
 		col.add(new LOV("Male", "Male"));
 		col.add(new LOV("Female", "Female"));
 		col.add(new LOV("Not Reported", "Not Reported"));
 		col.add(new LOV("Unknown", "Unknown"));
-		
+
 		return col;
 	}
 
-	private List<LOV> getSourceList() 
-	{
+	private List<LOV> getSourceList() {
 		List<LOV> col = new ArrayList<LOV>();
-		
-		col.add(new LOV("Duke University Comprehensive Cancer Center", "Duke University Comprehensive Cancer Center"));
-		col.add(new LOV("Warren Grant Magnuson Clinical Center", "Warren Grant Magnuson Clinical Center"));
-		
+
+		col.add(new LOV("Duke University Comprehensive Cancer Center",
+				"Duke University Comprehensive Cancer Center"));
+		col.add(new LOV("Warren Grant Magnuson Clinical Center",
+				"Warren Grant Magnuson Clinical Center"));
+
 		return col;
 	}
-	
+
 	public ParticipantDao getParticipantDao() {
 		return participantDao;
 	}
 
 	public void setParticipantDao(ParticipantDao participantDao) {
 		this.participantDao = participantDao;
-	}	
+	}
+
+	public ParticipantValidator getParticipantValidator() {
+		return participantValidator;
+	}
+
+	public void setParticipantValidator(ParticipantValidator participantValidator) {
+		this.participantValidator = participantValidator;
+	}
 }
