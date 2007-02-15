1cf35bc48d7f08b24b97b15c062b0d7c8c446054
Priyatam Mudivarti <priyatam@gmail.com>
2007-02-15 03:59:59 +0000

    added seperate method for core

--- codebase/projects/core/src/edu/duke/cabig/c3pr/utils/ContextTools.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/utils/ContextTools.java
@@ -4,29 +4,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 // Note - this classpath pattern has a problem with ant builds. Use below format
-//public class ContextTools {
-//    public static ApplicationContext createDeployedApplicationContext() {
-//        return new ClassPathXmlApplicationContext(new String[] {
-//            "classpath:/applicationContext.xml",
-//            "classpath*:/applicationContext-*.xml"
-//        });
-//    }
-//
-//    private ContextTools() { }
-//}
-
+//      "classpath*:/applicationContext-*.xml"
 public class ContextTools {
     public static ApplicationContext createDeployedApplicationContext() {
-    	   
     	  return new ClassPathXmlApplicationContext (new String[] {
-    			 "classpath*:applicationContext.xml",
-    			 "classpath*:applicationContext-core.xml", 
-    			 "classpath*:applicationContext-configProperties-esb.xml",
-    			 "classpath*:applicationContext-configProperties-gridSecurity.xml",    			
+    			 "classpath*:/applicationContext.xml",
+    			 "classpath*:/applicationContext-core.xml", 
+    			 "classpath*:/applicationContext-configProperties.xml",
+    			 "classpath*:/applicationContext-gridSecurity.xml", 
+    			 "classpath*:/applicationContext-esb.xml" 
     	  }); 
      }
+       
+    public static ApplicationContext createDeployedCoreApplicationContext() {
+  	  return new ClassPathXmlApplicationContext (new String[] {
+  			 "classpath*:/applicationContext.xml",
+  			 "classpath*:/applicationContext-core.xml"
+  	  }); 
+   }
 
     private ContextTools() { }
 }
-
-
