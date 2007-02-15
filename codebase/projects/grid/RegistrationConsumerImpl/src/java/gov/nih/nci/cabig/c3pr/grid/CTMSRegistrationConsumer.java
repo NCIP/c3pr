47784528a091904ac7a73c606032dd91e9b17b8c
Manav kher <icechunk@gmail.com>
2007-02-15 21:29:47 +0000

    fixed classpath problem

--- codebase/projects/grid/RegistrationConsumerImpl/src/java/gov/nih/nci/cabig/c3pr/grid/CTMSRegistrationConsumer.java
+++ codebase/projects/grid/RegistrationConsumerImpl/src/java/gov/nih/nci/cabig/c3pr/grid/CTMSRegistrationConsumer.java
@@ -1,31 +1,29 @@
 /**
  * 
  */
-package gov.nih.nci.cabig.caaers.grid;
+package gov.nih.nci.cabig.c3pr.grid;
 
-import java.rmi.RemoteException;
-
-i
 import gov.nih.nci.cabig.ctms.common.RegistrationConsumer;
 import gov.nih.nci.cabig.ctms.grid.ParticipantType;
 import gov.nih.nci.cabig.ctms.grid.RegistrationType;
 import gov.nih.nci.cabig.ctms.stubs.types.InvalidRegistration;
 import gov.nih.nci.cabig.ctms.stubs.types.RegistrationFailed;
 import gov.nih.nci.cagrid.metadata.security.ServiceSecurityMetadata;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.rmi.RemoteException;
 
-public class CTMSSRegistrationConsumer implements RegistrationConsumer {
+
+public class CTMSRegistrationConsumer implements RegistrationConsumer {
 
     private static final Log logger = LogFactory.getLog(CTMSRegistrationConsumer.class);
 
     private ApplicationContext ctx;
 
-    public CaaersRegistrationConsumer() {
+    public CTMSRegistrationConsumer() {
         this.ctx = new ClassPathXmlApplicationContext(
                         new String[] { "classpath*:gov/nih/nci/cabig/ctms/applicationContext-*.xml" });
     }
@@ -38,7 +36,7 @@ public class CTMSSRegistrationConsumer implements RegistrationConsumer {
     public void register(RegistrationType registration) throws RemoteException, InvalidRegistration, RegistrationFailed {
 
 
-        logger.debug("Received registration " + registration.getGridId());
+        logger.debug("Received registration " + registration.getIdentifier  ());
     }
 
     public ServiceSecurityMetadata getServiceSecurityMetadata() throws RemoteException {
