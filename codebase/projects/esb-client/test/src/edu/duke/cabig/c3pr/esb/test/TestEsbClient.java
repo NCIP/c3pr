5bea98c160dd92bee14086d6f1b0df43ea1bb1d4
Manav kher <icechunk@gmail.com>
2007-02-23 21:43:01 +0000

    parameterized

--- codebase/projects/esb-client/test/src/edu/duke/cabig/c3pr/esb/test/TestEsbClient.java
+++ codebase/projects/esb-client/test/src/edu/duke/cabig/c3pr/esb/test/TestEsbClient.java
@@ -13,62 +13,79 @@ import edu.duke.cabig.c3pr.esb.BroadcastException;
 import edu.duke.cabig.c3pr.esb.impl.MessageBroadcastServiceImpl;
 
 public class TestEsbClient {
-//	public static String brokerUrl="tcp://10.10.10.2:61616";
-	public static String brokerUrl="tcp://localhost:61616";
-	public static String sendQueue="registration-message.inputQueue";
-	public static String recvQueue="registration-message.outputQueue";
-	public static long sleep=5000;
-	public static void main(String args[]){
-		MessageBroadcastServiceImpl esbClient=new MessageBroadcastServiceImpl();
-		try {
-			esbClient.setConnectionFactory(new ActiveMQConnectionFactory(brokerUrl));
-			esbClient.setSendQueue(new ActiveMQQueue(sendQueue));
-			esbClient.setRecvQueue(new ActiveMQQueue(recvQueue));
-			esbClient.initialize();
-//            String fileName="resources/create-protocol.xml";
-//			String fileName="resources/person.xml";
-//			String fileName="C:\\SemanticBits\\registration.xml";
-//			String fileName="resources/SampleRegistration2.xml";
-			String fileName= args[1];
-            System.out.println("XML Payload....");
-            String xml="";
-            File f= new File(args[1]);
-            System.out.println(f.getAbsolutePath());
-        	BufferedReader fr=new BufferedReader(new FileReader(f));
-			String temp="";
-			while((temp=fr.readLine())!=null){
-				xml+=temp;
-				System.out.println(temp);
-			}
-     		esbClient.broadcast(xml);
-     		System.out.println("Sleeping for"+sleep);
-     		Thread.sleep(sleep);
-     		System.out.println("out of sleep..");
-			Vector result = esbClient.getBroadcastStatus();
-			if(result!=null){
-				for(int i=0 ; i<result.size();i++){
-					String msg=(String)result.get(i);
-					System.out.println((i+1)+".");
-					System.out.println(msg);
-					System.out.println("--------");
-				}
-			}else{
-				System.out.println("no avalbale result..");
-			}
-				
-		} catch (BroadcastException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally{
-			try {
-				esbClient.close();
-			} catch (BroadcastException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+    //	public static String brokerUrl="tcp://10.10.10.2:61616";
+    public static String brokerUrl="tcp://localhost:61616";
+    public static String sendQueue="registration-message.inputQueue";
+    public static String recvQueue="registration-message.outputQueue";
+    public static long sleep=5000;
+
+    public static void usage(){
+        System.out.println("Usage: ESBClient -file <sample-message> -url <esb-url>");
+    }
+
+    public static void main(String args[]){
+        MessageBroadcastServiceImpl esbClient=new MessageBroadcastServiceImpl();
+        if(args.length > 1){
+            String fileName= args[1];
+
+            if(args[2].indexOf("-url")>-1)
+                brokerUrl = args[3];
+
+            System.out.println("using broker at " + brokerUrl);
+
+            try {
+                esbClient.setConnectionFactory(new ActiveMQConnectionFactory(brokerUrl));
+                esbClient.setSendQueue(new ActiveMQQueue(sendQueue));
+                esbClient.setRecvQueue(new ActiveMQQueue(recvQueue));
+                esbClient.initialize();
+
+
+                System.out.println("XML Payload....");
+                String xml="";
+                File f= new File(fileName);
+                System.out.println(f.getAbsolutePath());
+                BufferedReader fr=new BufferedReader(new FileReader(f));
+                String temp="";
+                while((temp=fr.readLine())!=null){
+                    xml+=temp;
+                    System.out.println(temp);
+                }
+                esbClient.broadcast(xml);
+                System.out.println("Sleeping for"+sleep);
+                Thread.sleep(sleep);
+                System.out.println("out of sleep..");
+                Vector result = esbClient.getBroadcastStatus();
+                if(result!=null){
+                    for(int i=0 ; i<result.size();i++){
+                        String msg=(String)result.get(i);
+                        System.out.println((i+1)+".");
+                        System.out.println(msg);
+                        System.out.println("--------");
+                    }
+                }else{
+                    System.out.println("no avalbale result..");
+                }
+
+
+
+            } catch (BroadcastException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }finally{
+                try {
+                    esbClient.close();
+                } catch (BroadcastException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        else {
+            usage();
+        }
+    }
 }
