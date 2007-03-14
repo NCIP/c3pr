81ddc8fe26711727628980421531d3852dacf061
Priyatam Mudivarti <priyatam@gmail.com>
2007-03-14 22:39:11 +0000

    added

---
+++ codebase/projects/web/src/edu/duke/cabig/c3pr/utils/web/spring/tabbedflow/Flow.java
@@ -0,0 +1,46 @@
+package edu.duke.cabig.c3pr.utils.web.spring.tabbedflow;
+
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class Flow<C> {
+    private String name;
+    private List<Tab<C>> tabs;
+
+    public Flow(String name) {
+        this.name = name;
+        this.tabs = new LinkedList<Tab<C>>();
+    }
+
+    public void addTab(Tab<C> tab) {
+        tab.setNumber(tabs.size());
+        this.tabs.add(tab);
+    }
+
+    public int getTabCount() {
+        return getTabs().size();
+    }
+
+    public Tab<C> getTab(int number) {
+        return getTabs().get(number);
+    }
+
+    public List<Tab<C>> getTabs() {
+        return tabs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    ////// OBJECT METHODS
+
+    public String toString() {
+        return new StringBuilder(getClass().getSimpleName())
+            .append('[').append(getName()).append(']')
+            .toString();
+    }
+}
