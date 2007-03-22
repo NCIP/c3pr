ed2e2a4a7a1f26c1adb6f985cbd99907d0e9e8c5
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-03-22 21:59:05 +0000

    Formatting changes

--- codebase/projects/web/src/edu/duke/cabig/c3pr/web/CreateParticipantController.java
+++ codebase/projects/web/src/edu/duke/cabig/c3pr/web/CreateParticipantController.java
@@ -15,7 +15,6 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractWizardFormController;
 
 import edu.duke.cabig.c3pr.dao.HealthcareSiteDao;
 import edu.duke.cabig.c3pr.dao.ParticipantDao;
@@ -30,89 +29,114 @@ import edu.duke.cabig.c3pr.utils.web.spring.tabbedflow.AbstractTabbedFlowFormCon
 import edu.duke.cabig.c3pr.utils.web.spring.tabbedflow.Flow;
 import edu.duke.cabig.c3pr.utils.web.spring.tabbedflow.Tab;
 
-
 /**
  * @author Kulasekaran, Priyatam
- *
+ * 
  */
-public class CreateParticipantController extends AbstractTabbedFlowFormController<Participant> {
+public class CreateParticipantController extends
+		AbstractTabbedFlowFormController<Participant> {
 
 	private ParticipantDao participantDao;
+
 	protected ConfigurationProperty configurationProperty;
+
 	private ParticipantValidator participantValidator;
+
 	private HealthcareSiteDao healthcareSiteDao;
 
 	public CreateParticipantController() {
 		setCommandClass(Participant.class);
-		Flow<Participant> flow = new Flow<Participant>("Create Participant");               
-        intializeFlows(flow);
+		Flow<Participant> flow = new Flow<Participant>("Create Participant");
+		intializeFlows(flow);
 	}
-	protected void intializeFlows(Flow<Participant> flow)
-	{	   
-		 flow.addTab(new Tab<Participant>(" Subject Information", "Subject Information", "participant/participant") {
-	            public Map<String, Object> referenceData() {
-	           	 	Map <String, List<Lov>> configMap = configurationProperty.getMap();        		        
-	       	  
-	            	Map<String, Object> refdata = new HashMap<String, Object>();
-	        		  	            	
-	            	refdata.put("administrativeGenderCode", configMap.get("administrativeGenderCode"));
-	        		refdata.put("ethnicGroupCode", configMap.get("ethnicGroupCode"));
-	        		refdata.put("raceCode", configMap.get("raceCode"));
-	        	    refdata.put("source", getHealthcareSites());
-	        		refdata.put("searchTypeRefData", configMap.get("participantSearchType"));
-	        		refdata.put("identifiersTypeRefData", configMap.get("participantIdentifiersType"));
-	        		    	  		
-	    	  		return refdata;
-	            }        	
-	        });
-	        flow.addTab(new Tab<Participant>("Address Information", "Address Information", "participant/participant_address"));                 
-	        flow.addTab(new Tab<Participant>("Review and Submit ", "Review and Submit ", "participant/participant_submit"));
-	        setFlow(flow);      
-	       	}
 
-	@Override
-	protected Map<String, Object> referenceData(HttpServletRequest httpServletRequest, int page) throws Exception {
-    	// Currently the static data is a hack, once DB design is approved for an LOV this will be
-    	// replaced with LOVDao to get the static data from individual tables
-    	Map<String, Object> refdata = new HashMap<String, Object>();
-		Map <String, List<Lov>> configMap = configurationProperty.getMap();
+	protected void intializeFlows(Flow<Participant> flow) {
+		flow.addTab(new Tab<Participant>("Subject Information",
+				"Subject Information", "participant/participant") {
+			public Map<String, Object> referenceData() {
+				Map<String, List<Lov>> configMap = configurationProperty
+						.getMap();
 
-    	if(page==0)
-    	{    		
-    		if(httpServletRequest.getParameter("studySiteId")!=null){
-    			if(!httpServletRequest.getParameter("studySiteId").equals("")){
-    				refdata.put("studySiteId", httpServletRequest.getParameter("studySiteId"));
-    			}
-    		}
-    	}
+				Map<String, Object> refdata = new HashMap<String, Object>();
 
-        return refdata;
-    }
+				refdata.put("administrativeGenderCode", configMap
+						.get("administrativeGenderCode"));
+				refdata
+						.put("ethnicGroupCode", configMap
+								.get("ethnicGroupCode"));
+				refdata.put("raceCode", configMap.get("raceCode"));
+				refdata.put("source", getHealthcareSites());
+				refdata.put("searchTypeRefData", configMap
+						.get("participantSearchType"));
+				refdata.put("identifiersTypeRefData", configMap
+						.get("participantIdentifiersType"));
 
-	@Override
-	protected Object formBackingObject(HttpServletRequest request) throws Exception {
-
-		//FIXME: small hack
-		if(request.getParameter("url")!=null){
-			if(request.getParameter("studySiteId")!=null){
-				setPages(new String[]{"registration/reg_create_patient_study","registration/reg_create_patient_address_study","registration/reg_create_patient_submit_study"});
-			}else{
-				setPages(new String[]{"registration/reg_create_patient","registration/reg_create_patient_address","registration/reg_create_patient_submit"});
+				return refdata;
 			}
-			request.getSession().setAttribute("url", request.getParameter("url"));
-			request.getSession().setAttribute("studySiteId", request.getParameter("studySiteId"));
-		}else{
-			setPages(new String[]{"participant/participant","participant/participant_address","participant/participant_submit"});
+		});
+		flow.addTab(new Tab<Participant>("Address Information",
+				"Address Information", "participant/participant_address"));
+		flow.addTab(new Tab<Participant>("Review and Submit ",
+				"Review and Submit ", "participant/participant_submit"));
+		setFlow(flow);
+	}
+
+	@Override
+	protected Map<String, Object> referenceData(
+			HttpServletRequest httpServletRequest, int page) throws Exception {
+		// Currently the static data is a hack, once DB design is approved for
+		// an LOV this will be
+		// replaced with LOVDao to get the static data from individual tables
+		Map<String, Object> refdata = new HashMap<String, Object>();
+		Map<String, List<Lov>> configMap = configurationProperty.getMap();
+
+		if (page == 0) {
+			if (httpServletRequest.getParameter("studySiteId") != null) {
+				if (!httpServletRequest.getParameter("studySiteId").equals("")) {
+					refdata.put("studySiteId", httpServletRequest
+							.getParameter("studySiteId"));
+				}
+			}
+		}
+
+		return refdata;
+	}
+
+	@Override
+	protected Object formBackingObject(HttpServletRequest request)
+			throws Exception {
+
+		// FIXME: small hack
+		if (request.getParameter("url") != null) {
+			if (request.getParameter("studySiteId") != null) {
+				setPages(new String[] {
+						"registration/reg_create_patient_study",
+						"registration/reg_create_patient_address_study",
+						"registration/reg_create_patient_submit_study" });
+			} else {
+				setPages(new String[] { "registration/reg_create_patient",
+						"registration/reg_create_patient_address",
+						"registration/reg_create_patient_submit" });
+			}
+			request.getSession().setAttribute("url",
+					request.getParameter("url"));
+			request.getSession().setAttribute("studySiteId",
+					request.getParameter("studySiteId"));
+		} else {
+			setPages(new String[] { "participant/participant",
+					"participant/participant_address",
+					"participant/participant_submit" });
 			request.getSession().removeAttribute("url");
 			request.getSession().removeAttribute("studySiteId");
 		}
 
-		Participant participant = (Participant) super.formBackingObject(request);
+		Participant participant = (Participant) super
+				.formBackingObject(request);
 		for (int i = 0; i < 5; i++) {
-			Identifier temp=new Identifier();
+			Identifier temp = new Identifier();
 			temp.setPrimaryIndicator(false);
 			participant.addIdentifier(temp);
-			}
+		}
 		participant.setAddress(new Address());
 		return participant;
 	}
@@ -132,11 +156,10 @@ public class CreateParticipantController extends AbstractTabbedFlowFormControlle
 
 		Iterator<Identifier> iterator = command.getIdentifiers().iterator();
 
-		while(iterator.hasNext())
-		{
+		while (iterator.hasNext()) {
 			Identifier identifier = iterator.next();
-			if(identifier.getType().trim().length() == 0 || identifier.getValue().trim().length() == 0)
-			{
+			if (identifier.getType().trim().length() == 0
+					|| identifier.getValue().trim().length() == 0) {
 				iterator.remove();
 			}
 		}
@@ -144,13 +167,14 @@ public class CreateParticipantController extends AbstractTabbedFlowFormControlle
 		participantDao.save(command);
 
 		ModelAndView modelAndView = null;
-		//FIXME: small hack
+		// FIXME: small hack
 		String url = null;
-		if((url = (String) request.getSession().getAttribute("url")) != null)
-		{
-			url+="?participantId=" + Integer.toString(command.getId());
-			if(request.getSession().getAttribute("studySiteId") != null)
-				url+="&studySiteId="+(String)request.getSession().getAttribute("studySiteId");
+		if ((url = (String) request.getSession().getAttribute("url")) != null) {
+			url += "?participantId=" + Integer.toString(command.getId());
+			if (request.getSession().getAttribute("studySiteId") != null)
+				url += "&studySiteId="
+						+ (String) request.getSession().getAttribute(
+								"studySiteId");
 			response.sendRedirect(url);
 			return null;
 		}
@@ -158,7 +182,8 @@ public class CreateParticipantController extends AbstractTabbedFlowFormControlle
 		return null;
 	}
 
-	protected void validatePage(Object command, Errors errors, int page, boolean finish) {
+	protected void validatePage(Object command, Errors errors, int page,
+			boolean finish) {
 		Participant participant = (Participant) command;
 		switch (page) {
 		case 0: {
@@ -178,16 +203,16 @@ public class CreateParticipantController extends AbstractTabbedFlowFormControlle
 		}
 	}
 
-	protected List<HealthcareSite> getHealthcareSites()
-	{
-  		return healthcareSiteDao.getAll();
+	protected List<HealthcareSite> getHealthcareSites() {
+		return healthcareSiteDao.getAll();
 	}
 
 	public ConfigurationProperty getConfigurationProperty() {
 		return configurationProperty;
 	}
 
-	public void setConfigurationProperty(ConfigurationProperty configurationProperty) {
+	public void setConfigurationProperty(
+			ConfigurationProperty configurationProperty) {
 		this.configurationProperty = configurationProperty;
 	}
 
@@ -203,7 +228,8 @@ public class CreateParticipantController extends AbstractTabbedFlowFormControlle
 		return participantValidator;
 	}
 
-	public void setParticipantValidator(ParticipantValidator participantValidator) {
+	public void setParticipantValidator(
+			ParticipantValidator participantValidator) {
 		this.participantValidator = participantValidator;
 	}
 
