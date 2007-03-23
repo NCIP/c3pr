a9aca61d6675fe358ffbcc2f67a15b428ac3556c
Priyatam Mudivarti <priyatam@gmail.com>
2007-03-23 15:12:28 +0000

    added findBySubname and other methods for pattern matching

--- codebase/projects/core/src/edu/duke/cabig/c3pr/dao/AbstractBaseDao.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/dao/AbstractBaseDao.java
@@ -1,5 +1,11 @@
 package edu.duke.cabig.c3pr.dao;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.Criteria;
 import org.hibernate.FetchMode;
 import org.hibernate.criterion.Criterion;
@@ -17,6 +23,9 @@ import edu.nwu.bioinformatics.commons.CollectionUtils;
  */
 public abstract class AbstractBaseDao<T extends DomainObject> extends HibernateDaoSupport
 	implements BaseDao{   
+	
+	private static Log log = LogFactory.getLog(HealthcareSiteInvestigatorDao.class);
+	
 		  
 	/**
 	 * Get Object by Id (based on domain class)
@@ -75,4 +84,80 @@ public abstract class AbstractBaseDao<T extends DomainObject> extends HibernateD
         Object[] params = {gridId};
         return (T) CollectionUtils.firstElement(getHibernateTemplate().find(query.toString(), params));
     }
+    
+    /**
+     * A variation of {@link #findBySubname} that does not allow for extra conditions
+     */
+    protected List<T> findBySubname(
+        String[] subnames, List<String> substringMatchProperties, List<String> exactMatchProperties
+    ) {
+        return findBySubname(subnames, null, null, substringMatchProperties, exactMatchProperties);
+    }
+
+    /**
+     * A query builder for use by subclass DAOs.  It makes it easy to match a fragment of a name
+     * or identifier against multiple properties.  This is intended for use in implementing
+     * user-friendly dynamic searches; e.g., autocompleters.
+     *
+     * @param subnames the name fragments to search on
+     * @param extraConditions custom HQL conditions with which to constrain the fragment matches
+     * @param extraParameters parameters for the custom conditions
+     * @param substringMatchProperties a list of properties of the implementing object which should
+     *          be matched as case-insensitive substrings
+     * @param exactMatchProperties a list of properties which should be matched as case-insensitive
+     *          full strings
+     * @return a list of matching domain object instances
+     */
+    @SuppressWarnings("unchecked")
+    protected List<T> findBySubname(
+        String[] subnames, String extraConditions, List<Object> extraParameters,
+        List<String> substringMatchProperties, List<String> exactMatchProperties
+    ) {
+        StringBuilder query = new StringBuilder("from ")
+            .append(domainClass().getName()).append(" o where ");
+        if (extraConditions != null) query.append(extraConditions).append(" and ");
+        List<Object> params = new LinkedList<Object>();
+        if (extraParameters != null) params.addAll(extraParameters);
+
+        for (int i = 0; i < subnames.length; i++) {
+            buildSubnameQuery(subnames[i], query, params,
+                substringMatchProperties, exactMatchProperties);
+            if (i < subnames.length - 1) query.append(" and ");
+        }
+                
+        log.debug("query string = " +query);        
+        return getHibernateTemplate().find(query.toString(), params.toArray());
+    }
+
+    protected void buildSubnameQuery(
+        String subname, StringBuilder query, List<Object> params,
+        List<String> substringMatchProperties, List<String> exactMatchProperties
+    ) {
+        query.append('(');
+        if (hasAny(substringMatchProperties)) {
+            for (Iterator<String> it = substringMatchProperties.iterator(); it.hasNext();) {
+                String prop = it.next();
+                query.append("LOWER(o.").append(prop).append(") LIKE ?");
+                params.add('%' + subname.toLowerCase() + '%');
+                if (it.hasNext()) query.append(" or ");
+            }
+            if (hasAny(exactMatchProperties)) {
+                query.append(" or ");
+            }
+        }
+        if (hasAny(exactMatchProperties)) {
+            for (Iterator<String> it = exactMatchProperties.iterator(); it.hasNext();) {
+                String prop = it.next();
+                query.append("LOWER(o.").append(prop).append(") = ?");
+                params.add(subname.toLowerCase());
+                if (it.hasNext()) query.append(" or ");
+            }
+        }
+        query.append(')');
+    }
+
+    private boolean hasAny(List<String> properties) {
+        return properties != null && properties.size() > 0;
+    }
+
 }
