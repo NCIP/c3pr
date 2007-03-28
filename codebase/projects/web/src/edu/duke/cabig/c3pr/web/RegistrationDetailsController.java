e8588872269d6801fd0702c0ba0084621d360279
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-03-28 15:28:28 +0000

    changed to Tabbed Flow Controller

--- codebase/projects/web/src/edu/duke/cabig/c3pr/web/RegistrationDetailsController.java
+++ codebase/projects/web/src/edu/duke/cabig/c3pr/web/RegistrationDetailsController.java
@@ -13,6 +13,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractWizardFormController;
@@ -22,68 +23,259 @@ import edu.duke.cabig.c3pr.dao.ParticipantDao;
 import edu.duke.cabig.c3pr.dao.StudyParticipantAssignmentDao;
 import edu.duke.cabig.c3pr.dao.StudySiteDao;
 import edu.duke.cabig.c3pr.domain.HealthcareSite;
+import edu.duke.cabig.c3pr.domain.Identifier;
 import edu.duke.cabig.c3pr.domain.Participant;
 import edu.duke.cabig.c3pr.domain.StudyParticipantAssignment;
 import edu.duke.cabig.c3pr.utils.ConfigurationProperty;
 import edu.duke.cabig.c3pr.utils.Lov;
 import edu.duke.cabig.c3pr.utils.web.propertyeditors.CustomDaoEditor;
+import edu.duke.cabig.c3pr.utils.web.spring.tabbedflow.Flow;
+import edu.duke.cabig.c3pr.utils.web.spring.tabbedflow.AbstractTabbedFlowFormController;
+import edu.duke.cabig.c3pr.utils.web.spring.tabbedflow.Tab;
 
 /**
  * @author Ramakrishna
- *
+ * 
  */
 
-public class RegistrationDetailsController extends AbstractWizardFormController{
-	
-	private static Log log = LogFactory.getLog(RegistrationDetailsController.class);
+public class RegistrationDetailsController extends
+		AbstractTabbedFlowFormController {
+
+	private static Log log = LogFactory
+			.getLog(RegistrationDetailsController.class);
 
 	private ParticipantDao participantDao;
-	
+
 	private StudyParticipantAssignmentDao registrationDao;
 
 	private HealthcareSiteDao healthcareSiteDao;
-	
+
 	private StudySiteDao studySiteDao;
 
 	protected ConfigurationProperty configurationProperty;
 
 	public RegistrationDetailsController() {
 		setCommandClass(StudyParticipantAssignment.class);
+		Flow<StudyParticipantAssignment> flow = new Flow<StudyParticipantAssignment>(
+				"Registration Management");
+		intializeFlows(flow);
+	}
+
+	protected void intializeFlows(Flow<StudyParticipantAssignment> flow) {
+		flow.addTab(new Tab<StudyParticipantAssignment>("Details", "Details",
+				"registration/reg_details_study_participant") {
+			public Map<String, Object> referenceData() {
+				Map<String, List<Lov>> configMap = configurationProperty
+						.getMap();
+
+				Map<String, Object> refdata = new HashMap<String, Object>();
+
+				refdata.put("administrativeGenderCode", configMap
+						.get("administrativeGenderCode"));
+				refdata
+						.put("ethnicGroupCode", configMap
+								.get("ethnicGroupCode"));
+				refdata.put("raceCode", configMap.get("raceCode"));
+				refdata.put("source", healthcareSiteDao.getAll());
+				refdata.put("searchTypeRefData", configMap
+						.get("participantSearchType"));
+				refdata.put("identifiersTypeRefData", configMap
+						.get("participantIdentifiersType"));
+				;
+
+				return refdata;
+			}
+		});
+		flow.addTab(new Tab<StudyParticipantAssignment>("Identifiers",
+				"Identifiers", "registration/reg_details_identifiers") {
+			public Map<String, Object> referenceData() {
+				Map<String, List<Lov>> configMap = configurationProperty
+				.getMap();
+
+		Map<String, Object> refdata = new HashMap<String, Object>();
+
+		refdata.put("administrativeGenderCode", configMap
+				.get("administrativeGenderCode"));
+		refdata
+				.put("ethnicGroupCode", configMap
+						.get("ethnicGroupCode"));
+		refdata.put("raceCode", configMap.get("raceCode"));
+		refdata.put("source", healthcareSiteDao.getAll());
+		refdata.put("searchTypeRefData", configMap
+				.get("participantSearchType"));
+		refdata.put("identifiersTypeRefData", configMap
+				.get("participantIdentifiersType"));
+		;
+
+				return refdata;
+			}
+		});
+		flow.addTab(new Tab<StudyParticipantAssignment>("Eligibility",
+				"Eligibility", "registration/reg_details_eligibility") {
+			public Map<String, Object> referenceData() {
+				Map<String, List<Lov>> configMap = configurationProperty
+				.getMap();
+
+		Map<String, Object> refdata = new HashMap<String, Object>();
+
+		refdata.put("administrativeGenderCode", configMap
+				.get("administrativeGenderCode"));
+		refdata
+				.put("ethnicGroupCode", configMap
+						.get("ethnicGroupCode"));
+		refdata.put("raceCode", configMap.get("raceCode"));
+		refdata.put("source", healthcareSiteDao.getAll());
+		refdata.put("searchTypeRefData", configMap
+				.get("participantSearchType"));
+		refdata.put("identifiersTypeRefData", configMap
+				.get("participantIdentifiersType"));
+		;
+
+				return refdata;
+			}
+		});
+		flow.addTab(new Tab<StudyParticipantAssignment>("Stratification",
+				"Stratification", "registration/reg_details_stratification") {
+			public Map<String, Object> referenceData() {
+				Map<String, List<Lov>> configMap = configurationProperty
+				.getMap();
+
+		Map<String, Object> refdata = new HashMap<String, Object>();
+
+		refdata.put("administrativeGenderCode", configMap
+				.get("administrativeGenderCode"));
+		refdata
+				.put("ethnicGroupCode", configMap
+						.get("ethnicGroupCode"));
+		refdata.put("raceCode", configMap.get("raceCode"));
+		refdata.put("source", healthcareSiteDao.getAll());
+		refdata.put("searchTypeRefData", configMap
+				.get("participantSearchType"));
+		refdata.put("identifiersTypeRefData", configMap
+				.get("participantIdentifiersType"));
+		;
+
+				return refdata;
+			}
+
+		});
+		flow.addTab(new Tab<StudyParticipantAssignment>("Randomization",
+				"Randomization", "registration/reg_details_randomization") {
+			public Map<String, Object> referenceData() {
+				Map<String, List<Lov>> configMap = configurationProperty
+				.getMap();
+
+		Map<String, Object> refdata = new HashMap<String, Object>();
+
+		refdata.put("administrativeGenderCode", configMap
+				.get("administrativeGenderCode"));
+		refdata
+				.put("ethnicGroupCode", configMap
+						.get("ethnicGroupCode"));
+		refdata.put("raceCode", configMap.get("raceCode"));
+		refdata.put("source", healthcareSiteDao.getAll());
+		refdata.put("searchTypeRefData", configMap
+				.get("participantSearchType"));
+		refdata.put("identifiersTypeRefData", configMap
+				.get("participantIdentifiersType"));
+		;
+
+				return refdata;
+			}
+
+		});
+		flow.addTab(new Tab<StudyParticipantAssignment>("Status", "Status",
+				"registration/reg_details_status") {
+			public Map<String, Object> referenceData() {
+				Map<String, List<Lov>> configMap = configurationProperty
+				.getMap();
+
+		Map<String, Object> refdata = new HashMap<String, Object>();
+
+		refdata.put("administrativeGenderCode", configMap
+				.get("administrativeGenderCode"));
+		refdata
+				.put("ethnicGroupCode", configMap
+						.get("ethnicGroupCode"));
+		refdata.put("raceCode", configMap.get("raceCode"));
+		refdata.put("source", healthcareSiteDao.getAll());
+		refdata.put("searchTypeRefData", configMap
+				.get("participantSearchType"));
+		refdata.put("identifiersTypeRefData", configMap
+				.get("participantIdentifiersType"));
+		;
+
+				return refdata;
+			}
+
+		});
+		setFlow(flow);
 	}
 
 	@Override
-	protected ModelAndView processFinish(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, BindException arg3) throws Exception {
+	protected ModelAndView processFinish(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, BindException arg3)
+			throws Exception {
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see org.springframework.web.servlet.mvc.AbstractFormController#formBackingObject(javax.servlet.http.HttpServletRequest)
 	 */
 	@Override
 	protected Object formBackingObject(HttpServletRequest request)
 			throws Exception {
 		StudyParticipantAssignment registration = null;
-		System.out.println(" registration...id is " + request
-					.getParameter("registrationId"));
-		if ((request.getParameter("registrationId") != null) &&(request.getParameter("registrationId") != "") ) {
+		System.out.println(" registration...id is "
+				+ request.getParameter("registrationId"));
+		if ((request.getParameter("registrationId") != null)
+				&& (request.getParameter("registrationId") != "")) {
 			System.out.println(" Request URl  is:"
 					+ request.getRequestURL().toString());
-			System.out.println( " RegistrationId is: " + Integer.parseInt(request
-					.getParameter("registrationId")));
-			System.out.println(" registration Dao is :" + registrationDao.toString());
+			System.out.println(" RegistrationId is: "
+					+ Integer.parseInt(request.getParameter("registrationId")));
+			System.out.println(" registration Dao is :"
+					+ registrationDao.toString());
 			registration = registrationDao.getById(Integer.parseInt(request
-					.getParameter("registrationId")),true);
+					.getParameter("registrationId")), true);
 			System.out.println(" Registration ID is:" + registration.getId());
-			}
-		
-		else return new StudyParticipantAssignment();
-		
+		}
+
+		else
+			return new StudyParticipantAssignment();
 		return registration;
 	}
-	
+	@Override
+	protected void postProcessPage(HttpServletRequest request, Object oCommand,
+			Errors errors, int page) throws Exception {
+		StudyParticipantAssignment registration = (StudyParticipantAssignment) oCommand;
+		if (page == 1) {
+			handleIdentifierAction(registration,
+					request.getParameter("_action"), request
+							.getParameter("_selected"));
+		}
+
+		if (("update").equals(request.getParameter("_update"))) {
+			registrationDao.save(registration);
+		}
+	}
+	private void handleIdentifierAction(StudyParticipantAssignment registration, String action,
+			String selected) {
+		if ("addIdentifier".equals(action)) {
+			log.debug("Requested Add Identifier");
+			Identifier id = new Identifier();
+			id.setSource("Duke");
+			id.setValue("<enter value>");
+			registration.addIdentifier(id);
+		} else if ("removeIdentifier".equals(action)) {
+			log.debug("Requested Remove Identifier");
+			registration.getIdentifiers().remove(Integer.parseInt(selected));
+		}
+	}
 	@Override
 	protected Map<String, Object> referenceData(
 			HttpServletRequest httpServletRequest, int page) throws Exception {
@@ -93,11 +285,15 @@ public class RegistrationDetailsController extends AbstractWizardFormController{
 		Map<String, Object> refdata = new HashMap<String, Object>();
 		Map<String, List<Lov>> configMap = configurationProperty.getMap();
 		refdata.put("source", healthcareSiteDao.getAll());
-		refdata.put("searchTypeRefData", configMap.get("participantSearchType"));
-		refdata.put("identifiersTypeRefData", configMap.get("participantIdentifiersType"));
+		refdata
+				.put("searchTypeRefData", configMap
+						.get("participantSearchType"));
+		refdata.put("identifiersTypeRefData", configMap
+				.get("participantIdentifiersType"));
 
 		return refdata;
 	}
+
 	@Override
 	protected void initBinder(HttpServletRequest req,
 			ServletRequestDataBinder binder) throws Exception {
@@ -111,7 +307,8 @@ public class RegistrationDetailsController extends AbstractWizardFormController{
 		return configurationProperty;
 	}
 
-	public void setConfigurationProperty(ConfigurationProperty configurationProperty) {
+	public void setConfigurationProperty(
+			ConfigurationProperty configurationProperty) {
 		this.configurationProperty = configurationProperty;
 	}
 
@@ -146,6 +343,5 @@ public class RegistrationDetailsController extends AbstractWizardFormController{
 	public void setStudySiteDao(StudySiteDao studySiteDao) {
 		this.studySiteDao = studySiteDao;
 	}
-
 
 }
