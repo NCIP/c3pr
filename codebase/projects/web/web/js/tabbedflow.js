7b0f373ed51ca28f90d151e231a56078e0545495
Priyatam Mudivarti <priyatam@gmail.com>
2007-04-10 15:35:55 +0000

    added check to disable _targetPage in edit workflows

--- codebase/projects/web/web/js/tabbedflow.js
+++ codebase/projects/web/web/js/tabbedflow.js
@@ -19,7 +19,8 @@ Event.observe(window, "load", function() {
     if ($("flow-update")) Event.observe("flow-update", "click", function(click) {
         Event.stop(click);
         $("command")._action.value='update';
-        document.getElementById('command').targetPage.name='_noname';
+        // $("targetPage").name='_noname';
+        document.getElementById('targetPage').name='_noname';
         $("command").submit(); // command is the default ID for a form created with form:form
     })
 })
