0100ae6961814f30c0900d03b37dd6af2dd4e8ed
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-04-19 23:05:20 +0000

    added getBySubNames method for ajax search

--- codebase/projects/core/src/edu/duke/cabig/c3pr/dao/StudyParticipantAssignmentDao.java
+++ codebase/projects/core/src/edu/duke/cabig/c3pr/dao/StudyParticipantAssignmentDao.java
@@ -1,68 +1,99 @@
 package edu.duke.cabig.c3pr.dao;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Example;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.dao.DataAccessException;
 
 import edu.duke.cabig.c3pr.domain.Identifier;
 import edu.duke.cabig.c3pr.domain.Participant;
+import edu.duke.cabig.c3pr.domain.Study;
 import edu.duke.cabig.c3pr.domain.StudyParticipantAssignment;
+import edu.duke.cabig.c3pr.domain.StudySite;
+import edu.emory.mathcs.backport.java.util.Collections;
 
-public class StudyParticipantAssignmentDao extends AbstractBaseDao<StudyParticipantAssignment> {
-    public StudyParticipantAssignmentDao() {
-    }
-    
-    @Override
-    public Class<StudyParticipantAssignment> domainClass() {
-            return StudyParticipantAssignment.class;
-     }
-    
-    /**
-	 * /* Searches based on an example object. Typical usage from your service
-	 * class: - If you want to search based on diseaseCode, monitorCode,
-	 * <li><code>Participant participant = new Participant();</li></code>
-	 * <li>code>participant.setLastName("last_namee");</li>
-	 * </code>
-	 * <li>code>participantDao.searchByExample(study)</li>
-	 * </code>
-	 * 
-	 * @return list of matching participant objects based on your sample
-	 *         participant object
-	 * @param participant
+public class StudyParticipantAssignmentDao extends
+		AbstractBaseDao<StudyParticipantAssignment> {
+
+	private static final List<String> SUBSTRING_MATCH_PROPERTIES = Arrays
+			.asList("treatingPhysician");
+
+	private static final List<String> EXACT_MATCH_PROPERTIES = Collections
+			.emptyList();
+
+	public StudyParticipantAssignmentDao() {
+	}
+
+	@Override
+	public Class<StudyParticipantAssignment> domainClass() {
+		return StudyParticipantAssignment.class;
+	}
+
+	/**
+	 *  * @return list of matching registration objects based on your sample
+	 *         registration object
+	 * @param registration
 	 * @return
 	 */
-	public List<StudyParticipantAssignment> searchByExample(StudyParticipantAssignment registration, boolean isWildCard) {
-		Example example = Example.create(registration).excludeZeroes().ignoreCase();
-		Criteria registrationCriteria = getSession().createCriteria(
-			StudyParticipantAssignment.class);
+	public List<StudyParticipantAssignment> searchByExample(
+			StudyParticipantAssignment registration, boolean isWildCard) {
+		
+		Example example = Example.create(registration).excludeZeroes()
+				.ignoreCase();
+		Criteria registrationCriteria = getHibernateTemplate().getSessionFactory().getCurrentSession().createCriteria(StudyParticipantAssignment.class);
 		if (isWildCard) {
 			example.excludeProperty("doNotUse").enableLike(MatchMode.ANYWHERE);
 			registrationCriteria.add(example);
 			if (registration.getIdentifiers().size() > 0) {
 				registrationCriteria.createCriteria("identifiers").add(
-					Restrictions.like("value", registration.getIdentifiers()
-					.get(0).getValue()+ "%"));
-			}
+						Restrictions.like("value", registration
+								.getIdentifiers().get(0).getValue()
+								+ "%"));
+			}else
+				if (registration.getParticipant()!=null) {
+						registrationCriteria.createCriteria("participant").add(
+							Restrictions.like("lastName", registration.getParticipant().getLastName()+ "%"));
+																
+				}
+				else
+					if (registration.getStudySite()!=null) {
+						registrationCriteria.createCriteria("studySite.study").add(
+								Restrictions.like("shortTitleText", registration.getStudySite().getStudy().getShortTitleText()+ "%"));
+																	
+					}
 			return registrationCriteria.list();
 
 		}
 		return registrationCriteria.add(example).list();
 
 	}
-    
-    public StudyParticipantAssignment getById(int id, boolean withIdentifiers) {
+		
+		public List<StudyParticipantAssignment> getAll() throws DataAccessException {
+			return getHibernateTemplate().find("from StudyParticipantAssignmet");
+		}
+	public List<StudyParticipantAssignment> searchByExample(StudyParticipantAssignment registration) {
+		return searchByExample(registration, true);
+	}
 
-		StudyParticipantAssignment registration = (StudyParticipantAssignment) getHibernateTemplate().get(
-				domainClass(), id);
+	public StudyParticipantAssignment getById(int id, boolean withIdentifiers) {
+
+		StudyParticipantAssignment registration = (StudyParticipantAssignment) getHibernateTemplate()
+				.get(domainClass(), id);
 		if (withIdentifiers) {
 			List<Identifier> identifiers = registration.getIdentifiers();
 			int size = identifiers.size();
 		}
-
-		return registration ;
+		return registration;
 
 	}
+
+	public List<StudyParticipantAssignment> getBySubnames(String[] subnames) {
+		return findBySubname(subnames, SUBSTRING_MATCH_PROPERTIES,
+				EXACT_MATCH_PROPERTIES);
+	}
+
 }
