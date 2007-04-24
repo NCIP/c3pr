c3d8a4094692a817ebbf9dee190a3df186666467
Manav kher <icechunk@gmail.com>
2007-04-24 20:14:35 +0000

    acegi-csm

---
+++ codebase/projects/web/src/edu/duke/cabig/c3pr/web/security/TaskPrivilegeAndObjectIdGenerator.java
@@ -0,0 +1,22 @@
+package edu.duke.cabig.c3pr.web.security;
+
+import edu.duke.cabig.c3pr.utils.web.navigation.Task;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kherm
+ * Date: Apr 24, 2007
+ * Time: 11:28:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaskPrivilegeAndObjectIdGenerator  extends AbstractPrivilegeAndObjectIdGenerator {
+
+        protected String getKeyValue(Object object){
+            return ((Task)object).getUrl();
+        }
+
+        protected boolean supports(Object object){
+            return object instanceof Task;
+        }
+
+}
