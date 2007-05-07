34762c0cbb05b5a20b279d2c0308dceb5a04c092
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-05-07 19:13:14 +0000

    updated

--- codebase/projects/web/web/js/validation.js
+++ codebase/projects/web/web/js/validation.js
@@ -70,7 +70,7 @@ validateForm=function(submit){
 var submitPostProcess
 prepareField=function(element){
 	ERROR_MSG_REQUIRED="required"
-	ERROR_MSG_PATTERN="Incorrect fromat"
+	ERROR_MSG_PATTERN="Incorrect format"
 	ERROR_MSG_MINLENGTH="too short"
 	ERROR_MSG_MAXLENGTH="too long"	
 	ERROR_MSG_PHONE="The second phone number is not valid"	
