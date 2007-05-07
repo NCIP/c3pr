7d506fab1af210894dc4a1f17245219e761ec99e
kruttik Aggarwal <kruttikagarwal@gmail.com>
2007-05-07 18:44:53 +0000

    added help comments

--- codebase/projects/web/web/js/autocomplete.js
+++ codebase/projects/web/web/js/autocomplete.js
@@ -1,3 +1,56 @@
+// ===================================================================
+// Author: Kruttik Aggarwal <kruttikagarwal@@gmail.com>
+// 
+//
+// NOTICE: You may use this code for any purpose, commercial or
+// private, without any further permission from the author. You may
+// remove this notice from your final code if you wish.
+//
+// You may *NOT* re-distribute this code in any way except through its
+// use. That means, you can include it in your product, or your web
+// site, or any other form where the code is actually being used. You
+// may not put the plain javascript up on your site for download or
+// include it in your javascript libraries for download. 
+// If you wish to share this code with others, please just point them
+// to the URL instead.
+// Please DO NOT link directly to my .js files from your site. Copy
+// the files to your server and use them there. Thank you.
+// ===================================================================
+
+// ------------------------------------------------------------------
+// This library assists in building auto completers
+// The class names are case sensitive
+// example usage
+// Build your own autocompleter object
+// var diseaseSiteAutocompleterProps = {
+//	  basename: "<some base name>",
+//    
+//    /*This function populates the dropp downs*/
+//	  populator: function(autocompleter, text) {
+//			        <Some DWR Bean>.<Some Method>([[arg1],[arg2]...], function(returnedData) {
+//																    	autocompleter.setChoices(returnedData)
+//																	   })
+//			    },
+//    /*This function returns a string from each object withing the returned objects*/
+//    valueSelector: function(obj) {
+//						return obj.name
+//			    	},
+//    /*This function is called after user select a element from the auto completer*/
+//    afterUpdateElement: function(inputElement, selectedElement, selectedChoice) {
+//    								hiddenField=diseaseSiteAutocompleterProps.basename+"-hidden"
+//	    							$(hiddenField).value=selectedChoice.id;
+//	    							ajaxDiseaseSite=selectedChoice.name;
+//								},
+// }
+//
+// After writing your class for autocompleter add this class to the autocompleters array. for e.g
+//    autoCompleters.push(<My Auto Completer Object>);
+//
+// Thats all you need to do. The rest will be handled by the library.
+// Note: Make sure that that the input fields match the <basename>-<option> syntax
+//       for autocompleter to work properly
+
+
 var autoCompleters=new Array()
 Event.observe(window, "load", function() {
 	autoCompleters.length>0?registerAutoCompleters() : null
@@ -20,6 +73,8 @@ function acCreate(mode) {
 														$(mode.inputElement()).value = ""
 												    })
 }
+
+/* Abstract Implementation of an autocompleter object*/
 var AbstractAutocompleterProps = Class.create();
 var AbstractAutocompleterProps = {
 	basename: "autoCompleter",
