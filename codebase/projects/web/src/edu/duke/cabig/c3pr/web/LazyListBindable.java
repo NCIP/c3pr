c816611c680508ca3bf2ac009d961aab72c43ab0
kruttik Aggarwal <kruttikagarwal@gmail.com>
2007-05-18 14:27:48 +0000

    deleted

--- codebase/projects/web/src/edu/duke/cabig/c3pr/web/LazyListBindable.java
+++
@@ -1,9 +0,0 @@
-package edu.duke.cabig.c3pr.web;
-
-import javax.servlet.http.HttpServletRequest;
-
-import edu.duke.cabig.c3pr.web.beans.HttpServletLazyCollectionInitializer;
-
-public interface LazyListBindable {
-	public void initLazyListBinder(HttpServletRequest request, HttpServletLazyCollectionInitializer lazyCollectionInitializer) throws Exception ;
-}
