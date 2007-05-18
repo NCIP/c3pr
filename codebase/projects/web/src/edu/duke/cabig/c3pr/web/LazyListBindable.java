ddef8d438b38f356c26d7d57e06bb7bc13c8e64a
kruttik Aggarwal <kruttikagarwal@gmail.com>
2007-05-18 14:14:35 +0000

    interface for lazy list binding

---
+++ codebase/projects/web/src/edu/duke/cabig/c3pr/web/LazyListBindable.java
@@ -0,0 +1,9 @@
+package edu.duke.cabig.c3pr.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import edu.duke.cabig.c3pr.web.beans.HttpServletLazyCollectionInitializer;
+
+public interface LazyListBindable {
+	public void initLazyListBinder(HttpServletRequest request, HttpServletLazyCollectionInitializer lazyCollectionInitializer) throws Exception ;
+}
