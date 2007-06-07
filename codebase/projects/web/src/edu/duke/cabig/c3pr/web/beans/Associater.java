863b19c08bbe3e44d1d538b46a84dd224ae58142
Manav kher <icechunk@gmail.com>
2007-06-07 19:41:27 +0000

    *** empty log message ***

--- codebase/projects/web/src/edu/duke/cabig/c3pr/web/beans/Associater.java
+++
@@ -1,7 +0,0 @@
-package edu.duke.cabig.c3pr.web.beans;
-
-import java.lang.reflect.Method;
-
-public interface Associater {
-	public void associate(Object entity1, Object entity2, Method method) throws Exception;
-}
