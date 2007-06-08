db6f1b98f0e846332e7a332e0d25e9d2115a435a
Manav kher <icechunk@gmail.com>
2007-06-08 05:47:25 +0000

    test cases

---
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/xml/StudyHibernateObjectMarshallingTest.java
@@ -0,0 +1,50 @@
+package edu.duke.cabig.c3pr.xml;
+
+import edu.duke.cabig.c3pr.dao.StudyDao;
+import edu.duke.cabig.c3pr.domain.Study;
+import edu.duke.cabig.c3pr.utils.DaoTestCase;
+import gov.nih.nci.common.exception.XMLUtilityException;
+
+import java.io.StringReader;
+
+/**
+ * Will test marshalling of a hibernate object.
+ *
+ * @testType unit
+ *
+ * Created by IntelliJ IDEA.
+ * User: kherm
+ * Date: Jun 8, 2007
+ * Time: 12:49:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StudyHibernateObjectMarshallingTest extends DaoTestCase {
+    private StudyDao dao = (StudyDao) getApplicationContext().getBean("studyDao");
+    XmlMarshaller marshaller;
+
+
+
+
+    public void testMarshallStudy(){
+        try {
+            Study study = dao.getById(1000);
+            marshaller = new XmlMarshaller();
+
+            String studyXML = marshaller.toXML(study);
+            assertNotNull(studyXML);
+
+            // make sure no hibernate proxy (garbage) is being serialized           
+            assertFalse(studyXML.indexOf("proxy")>-1);
+
+        } catch (XMLUtilityException e) {
+            fail(e.getMessage());
+        }
+
+
+    }
+
+    // override default dataset filename
+    protected String getClassNameWithoutPackage() {
+        return "StudyDataset";
+    }
+}
