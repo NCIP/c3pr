81fa60500d7f4e516103d6bfec14ab36c318c5b3
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-07-02 11:44:41 +0000

    added mapping to study

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/ExclusionEligibilityCriteria.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/ExclusionEligibilityCriteria.java
@@ -2,13 +2,25 @@ package edu.duke.cabig.c3pr.domain;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 /**
  * @author Kruttik
  */
 @Entity
-@DiscriminatorValue(value="E") 
-public class ExclusionEligibilityCriteria extends EligibilityCriteria
-{
-	 
+@DiscriminatorValue(value = "E")
+public class ExclusionEligibilityCriteria extends EligibilityCriteria {
+private Study study;
+	
+	@ManyToOne
+	@JoinColumn(name = "stu_id", updatable = false)
+	public Study getStudy() {
+		return study;
+	}
+
+	public void setStudy(Study study) {
+		this.study = study;
+	}
+
 }
