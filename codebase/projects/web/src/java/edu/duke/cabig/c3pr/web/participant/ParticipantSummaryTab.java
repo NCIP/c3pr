c4197b45d92d0efff0b4394457146d184ea3348d
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-07-02 20:27:44 +0000

    added summary tab

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/participant/ParticipantSummaryTab.java
@@ -0,0 +1,9 @@
+package edu.duke.cabig.c3pr.web.participant;
+
+public class ParticipantSummaryTab extends ParticipantTab{
+	
+	public ParticipantSummaryTab() {
+		super("Summary", "Summary", "participant/participant_summary_view");
+	}
+
+}
