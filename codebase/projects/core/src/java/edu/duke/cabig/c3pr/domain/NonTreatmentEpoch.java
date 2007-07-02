be5e09c5561e00d1fdf03106928bd16c4ead0a71
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-07-02 11:41:26 +0000

    adding new class NonTreatment Epoch

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/NonTreatmentEpoch.java
@@ -0,0 +1,43 @@
+package edu.duke.cabig.c3pr.domain;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+import org.hibernate.annotations.Where;
+
+@Entity(name="NonTreatmentEpoch")
+@DiscriminatorValue(value="NON-TREATMENT")
+//@Where(clause="type='NON-TREATMENT'")
+public class NonTreatmentEpoch extends Epoch{
+	private int accrualCeiling;
+	private String accrualIndicator;
+	private String reservationIndicator;
+	private String enrollmentIndicator;
+	
+	public int getAccrualCeiling() {
+		return accrualCeiling;
+	}
+	public void setAccrualCeiling(int accrualCeiling) {
+		this.accrualCeiling = accrualCeiling;
+	}
+	public String getAccrualIndicator() {
+		return accrualIndicator;
+	}
+	public void setAccrualIndicator(String accrualIndicator) {
+		this.accrualIndicator = accrualIndicator;
+	}
+	public String getEnrollmentIndicator() {
+		return enrollmentIndicator;
+	}
+	public void setEnrollmentIndicator(String enrollmentIndicator) {
+		this.enrollmentIndicator = enrollmentIndicator;
+	}
+	public String getReservationIndicator() {
+		return reservationIndicator;
+	}
+	public void setReservationIndicator(String reservationIndicator) {
+		this.reservationIndicator = reservationIndicator;
+	}
+	
+}
