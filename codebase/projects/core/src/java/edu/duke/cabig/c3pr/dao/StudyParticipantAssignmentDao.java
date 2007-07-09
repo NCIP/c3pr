4399ded46d485d98bbc0d2105a49f6541ab85525
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2007-07-09 15:50:04 +0000

    updated version with the change from Coordinating Center Identifier to Identifier.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/StudyParticipantAssignmentDao.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/StudyParticipantAssignmentDao.java
@@ -45,59 +45,50 @@ public class StudyParticipantAssignmentDao extends
 	 */
 	public List<StudyParticipantAssignment> advancedSearch(StudyParticipantAssignment registration, Date startDate, Date endDate, String ccId) {
 
-//		Example example = Example.create(registration).excludeZeroes()
-//				.ignoreCase();
+
 		Criteria registrationCriteria = getHibernateTemplate()
 				.getSessionFactory().getCurrentSession().createCriteria(
 						StudyParticipantAssignment.class);
-//		if (isWildCard) {
-			//example.excludeProperty("doNotUse").enableLike(MatchMode.ANYWHERE);
-			//registrationCriteria.add(example);
-//			if (registration.getIdentifiers().size() > 0) {
-//				registrationCriteria.createCriteria("identifiers").add(
-//						Restrictions.like("value", registration
-//								.getIdentifiers().get(0).getValue()
-//								+ "%"));				
-//			}
-			Criteria studySiteCriteria = registrationCriteria.createCriteria("studySite");
-			Criteria participantCriteria = registrationCriteria.createCriteria("participant");
-			Criteria studyCriteria = studySiteCriteria.createCriteria("study");
-			Criteria siteCriteria = studySiteCriteria.createCriteria("site");
-			Criteria identifiersCriteria = studyCriteria.createCriteria("identifiersInternal");
-			
-			//Study Criteria
-			if(registration.getStudySite().getStudy().getShortTitleText() != null && !registration.getStudySite().getStudy().getShortTitleText().equals("")){
-				studyCriteria.add(Expression.like("shortTitleText", "%"+registration.getStudySite().getStudy().getShortTitleText()+"%"));
-			}
-			
-			//Site criteria
-			if(registration.getStudySite().getSite().getName() != null && !registration.getStudySite().getSite().getName().equals("")){
-				siteCriteria.add(Expression.like("name", "%"+registration.getStudySite().getSite().getName()+"%"));
-			}
-			if(registration.getStudySite().getSite().getNciInstituteCode() != null && !registration.getStudySite().getSite().getNciInstituteCode().equals("")){
-				siteCriteria.add(Expression.like("nciInstituteCode", "%"+registration.getStudySite().getSite().getNciInstituteCode()+"%"));
-			}
-						
-			//registration criteria
-			if(startDate != null && endDate != null){
-				registrationCriteria.add(Expression.between("informedConsentSignedDate", startDate, endDate));
-			}			
-			
-			//participant/subject criteria
-			if(registration.getParticipant().getBirthDate() != null){
-				participantCriteria.add(Expression.eq("birthDate", registration.getParticipant().getBirthDate()));
-			}
-			if(registration.getParticipant().getRaceCode() != null && !registration.getParticipant().getRaceCode().equals("")){
-				participantCriteria.add(Expression.eq("raceCode", registration.getParticipant().getRaceCode()));
-			}
-			
-			if(ccId != null && !ccId.equals("")){
-				identifiersCriteria.add(Expression.eq("type","Coordinating Center Identifier"));
-				identifiersCriteria.add(Expression.eq("value", ccId));
-			}
-			registrationCriteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY); 
-			registrationCriteria.addOrder(Order.asc("id"));
-			return registrationCriteria.list();
+
+		Criteria studySiteCriteria = registrationCriteria.createCriteria("studySite");
+		Criteria participantCriteria = registrationCriteria.createCriteria("participant");
+		Criteria studyCriteria = studySiteCriteria.createCriteria("study");
+		Criteria siteCriteria = studySiteCriteria.createCriteria("site");
+		Criteria identifiersCriteria = studyCriteria.createCriteria("identifiersInternal");
+		
+		//Study Criteria
+		if(registration.getStudySite().getStudy().getShortTitleText() != null && !registration.getStudySite().getStudy().getShortTitleText().equals("")){
+			studyCriteria.add(Expression.like("shortTitleText", "%"+registration.getStudySite().getStudy().getShortTitleText()+"%"));
+		}
+		
+		//Site criteria
+		if(registration.getStudySite().getSite().getName() != null && !registration.getStudySite().getSite().getName().equals("")){
+			siteCriteria.add(Expression.like("name", "%"+registration.getStudySite().getSite().getName()+"%"));
+		}
+		if(registration.getStudySite().getSite().getNciInstituteCode() != null && !registration.getStudySite().getSite().getNciInstituteCode().equals("")){
+			siteCriteria.add(Expression.like("nciInstituteCode", "%"+registration.getStudySite().getSite().getNciInstituteCode()+"%"));
+		}
+					
+		//registration criteria
+		if(startDate != null && endDate != null){
+			registrationCriteria.add(Expression.between("informedConsentSignedDate", startDate, endDate));
+		}			
+		
+		//participant/subject criteria
+		if(registration.getParticipant().getBirthDate() != null){
+			participantCriteria.add(Expression.eq("birthDate", registration.getParticipant().getBirthDate()));
+		}
+		if(registration.getParticipant().getRaceCode() != null && !registration.getParticipant().getRaceCode().equals("")){
+			participantCriteria.add(Expression.eq("raceCode", registration.getParticipant().getRaceCode()));
+		}
+		
+		if(ccId != null && !ccId.equals("")){
+//				identifiersCriteria.add(Expression.eq("type","Coordinating Center Identifier"));
+			identifiersCriteria.add(Expression.eq("value", ccId));
+		}
+		registrationCriteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY); 
+		registrationCriteria.addOrder(Order.asc("id"));
+		return registrationCriteria.list();
 
 	}
 
