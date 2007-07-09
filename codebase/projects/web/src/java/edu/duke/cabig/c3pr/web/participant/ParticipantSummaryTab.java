6f0ba520ea349796d7dd7b0b25b6a49830964d45
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-07-09 16:08:06 +0000

    added registrations ref data

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/participant/ParticipantSummaryTab.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/participant/ParticipantSummaryTab.java
@@ -1,9 +1,32 @@
 package edu.duke.cabig.c3pr.web.participant;
 
+import java.util.Map;
+
+import edu.duke.cabig.c3pr.dao.ParticipantDao;
+import edu.duke.cabig.c3pr.domain.Participant;
+import edu.duke.cabig.c3pr.domain.Study;
+
 public class ParticipantSummaryTab extends ParticipantTab{
+	 private ParticipantDao participantDao;
 	
 	public ParticipantSummaryTab() {
-		super("Summary", "Summary", "participant/participant_summary_view");
+		super("Summary&Registrations", "Summary & Registrations", "participant/participant_summary_view");
+	}
+	
+	@Override
+    public Map<String, Object> referenceData(Participant participant) {
+        Map<String, Object> refdata = super.referenceData(participant);
+        refdata.put("participantAssignments", this.getParticipantDao().getById(participant.getId()).getStudyParticipantAssignments());
+
+        return refdata;
+    }
+
+	public ParticipantDao getParticipantDao() {
+		return participantDao;
+	}
+
+	public void setParticipantDao(ParticipantDao participantDao) {
+		this.participantDao = participantDao;
 	}
 
 }
