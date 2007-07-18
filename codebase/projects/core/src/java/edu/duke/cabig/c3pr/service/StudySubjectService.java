35e06ac1899d0ff904c21f59576a8b036f77ae37
kruttik Aggarwal <kruttikagarwal@gmail.com>
2007-07-18 20:02:46 +0000

    created new service

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/StudySubjectService.java
@@ -0,0 +1,22 @@
+package edu.duke.cabig.c3pr.service;
+
+import java.util.List;
+
+import edu.duke.cabig.c3pr.domain.Participant;
+import edu.duke.cabig.c3pr.domain.StudySubject;
+
+/**
+ * @author Kulasekaran,Ramakrishna
+ * @version 1.0
+ */
+public interface StudySubjectService {	  
+	  
+	  /**
+		 * Search using a sample. Populate a Participant object
+		 * @param  Participant object
+		 * @return List of Participant objects based on the sample participant object
+		 * @throws Runtime exception 
+		 */
+	  public void createRegistration (StudySubject studySubject);
+}
+
