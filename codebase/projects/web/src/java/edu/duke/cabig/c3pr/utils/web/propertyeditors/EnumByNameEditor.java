3aa27caa3172075842b9600a87a9f5112e58e32a
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-07-25 03:24:35 +0000

    adding property editor for enum types

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/propertyeditors/EnumByNameEditor.java
@@ -0,0 +1,36 @@
+package edu.duke.cabig.c3pr.utils.web.propertyeditors;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.beans.PropertyEditorSupport;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class EnumByNameEditor<E extends Enum<E>> extends PropertyEditorSupport {
+    Class<E> enumClass;
+
+    public EnumByNameEditor(Class<E> enumClass) {
+        this.enumClass = enumClass;
+    }
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        if (text == null || StringUtils.isBlank(text)) {
+            setValue(null);
+        } else {
+            setValue(Enum.valueOf(enumClass, text));
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public String getAsText() {
+        Enum<E> v = (Enum<E>) getValue();
+        if (v == null) {
+            return null;
+        } else {
+            return v.name();
+        }
+    }
+}
