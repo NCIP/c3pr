5d71ecb93d127a7893c81eff5e7d056d9bafce66
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-07-31 22:25:50 +0000

    added getBySubnames

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/HealthcareSiteDao.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/HealthcareSiteDao.java
@@ -1,14 +1,21 @@
 package edu.duke.cabig.c3pr.dao;
 
+import java.util.Arrays;
 import java.util.List;
 
 import edu.duke.cabig.c3pr.domain.HealthcareSite;
+import edu.duke.cabig.c3pr.domain.Participant;
+import edu.emory.mathcs.backport.java.util.Collections;
 
 /**
  * @author Ramakrishna
  * @version 1.0
  */
 public class HealthcareSiteDao extends GridIdentifiableDao<HealthcareSite> {
+	
+	private List<String> SUBSTRING_MATCH_PROPERTIES = Arrays.asList("name");
+
+	private List<String> EXACT_MATCH_PROPERTIES = Collections.emptyList();
 	
 	public Class<HealthcareSite> domainClass() {
 	        return HealthcareSite.class;
@@ -22,6 +29,14 @@ public class HealthcareSiteDao extends GridIdentifiableDao<HealthcareSite> {
 	 public List<HealthcareSite> getAll() {
 		 return getHibernateTemplate().find("from HealthcareSite");
 	 }
+	 
+	 public List<HealthcareSite> getBySubnames(String[] subnames) {
+
+			SUBSTRING_MATCH_PROPERTIES = Arrays.asList("name");
+				
+			return findBySubname(subnames, SUBSTRING_MATCH_PROPERTIES,
+					EXACT_MATCH_PROPERTIES);
+		}
 	 
 }
 
