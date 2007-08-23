5c70fd50701082e8d85e28982f8f40f0cd690e97
Manav kher <icechunk@gmail.com>
2007-08-23 22:46:44 +0000

    added identifier validation

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/Organization.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/Organization.java
@@ -23,43 +23,36 @@ import org.hibernate.annotations.CascadeType;
  */
 @MappedSuperclass
 public abstract class Organization extends AbstractMutableDomainObject {
-		
+
     private String name;
-		
+
     private String descriptionText;
 
     private Address address;
-    
-    private String trimmedName;
-    
-    private List<StudyOrganization> studyOrganizations = new ArrayList<StudyOrganization>();
-    
-    @OneToMany(mappedBy = "healthcareSite", fetch = FetchType.LAZY)
-	@Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
-    public List<StudyOrganization> getStudyOrganizations() {
-		return studyOrganizations;
-	}
 
-	public void setStudyOrganizations(List<StudyOrganization> studyOrganizations) {
-		this.studyOrganizations = studyOrganizations;
-	}
-	
+    private String trimmedName;
+
+    private List<StudyOrganization> studyOrganizations = new ArrayList<StudyOrganization>();
+    private List<OrganizationAssignedIdentifier> identifiers = new ArrayList<OrganizationAssignedIdentifier>();
+
+
+
     public Organization() {
-    	address = new Address();
+        address = new Address();
     }
-    
+
     public Organization(boolean initialise) {
-    	address = new Address();
-    }       
-    
+        address = new Address();
+    }
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public String getDescriptionText() {
         return descriptionText;
     }
@@ -67,21 +60,41 @@ public abstract class Organization extends AbstractMutableDomainObject {
     public void setDescriptionText(String descriptionText) {
         this.descriptionText = descriptionText;
     }
-    
+
+    @OneToMany(mappedBy = "healthcareSite", fetch = FetchType.LAZY)
+    @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
+    public List<StudyOrganization> getStudyOrganizations() {
+        return studyOrganizations;
+    }
+
+    public void setStudyOrganizations(List<StudyOrganization> studyOrganizations) {
+        this.studyOrganizations = studyOrganizations;
+    }
+
+    @OneToMany(mappedBy = "healthcareSite", fetch = FetchType.LAZY)
+    @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
+    public List<OrganizationAssignedIdentifier> getIdentifiers() {
+        return identifiers;
+    }
+
+    public void setIdentifiers(List<OrganizationAssignedIdentifier> identifiers) {
+        this.identifiers = identifiers;
+    }
+
     @OneToOne(cascade={javax.persistence.CascadeType.ALL}, optional=false)
     @JoinColumn(name="ADDRESS_ID" ,nullable=false)
     public Address getAddress() {
         return address;
     }
 
-    
+
     public void setAddress(Address address) {
         this.address = address;
     }
 
     @Transient
-	public String getTrimmedName() {		
-		return StringUtils.getTrimmedText(name, 25);
-	}
+    public String getTrimmedName() {
+        return StringUtils.getTrimmedText(name, 25);
+    }
 
 }
--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/validator/IdentifierValidator.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/validator/IdentifierValidator.java
@@ -5,16 +5,28 @@ import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import edu.duke.cabig.c3pr.domain.Identifier;
+import edu.duke.cabig.c3pr.domain.OrganizationAssignedIdentifier;
 
 public class IdentifierValidator implements Validator{
 
-	public boolean supports(Class clazz) {
-		return Identifier.class.isAssignableFrom(clazz);
-	}
-	
-	public void validate(Object target, Errors errors) {
-		ValidationUtils.rejectIfEmpty(errors, "source", "required",	"required field");
-		ValidationUtils.rejectIfEmpty(errors, "type", "required", "required field");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "value", "required", "required field");
-	}	
+
+    public boolean supports(Class clazz) {
+        return Identifier.class.isAssignableFrom(clazz);
+    }
+
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmpty(errors, "source", "required",	"required field");
+        ValidationUtils.rejectIfEmpty(errors, "type", "required", "required field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "value", "required", "required field");
+
+        if(target instanceof OrganizationAssignedIdentifier){
+            OrganizationAssignedIdentifier id = (OrganizationAssignedIdentifier) target;
+            if(id.getType().equalsIgnoreCase("protocol identity identifier"))
+                for(OrganizationAssignedIdentifier loadedId : id.getHealthcareSite().getIdentifiers()){
+                    if (loadedId.getValue().equals(id.getValue())){
+                        errors.rejectValue("value","Duplicate Value for Identifier");
+                    }
+                }
+        }
+    }
 }
