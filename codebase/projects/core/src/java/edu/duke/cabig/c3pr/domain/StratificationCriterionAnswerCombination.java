c4c056558c24b1992b7b02de07cef305baf379aa
Manav kher <icechunk@gmail.com>
2007-08-29 19:04:39 +0000

    refactor table name

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/StratificationCriterionAnswerCombination.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/StratificationCriterionAnswerCombination.java
@@ -2,27 +2,22 @@ package edu.duke.cabig.c3pr.domain;
 
 
 import gov.nih.nci.cabig.ctms.domain.AbstractMutableDomainObject;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
 
 
 /**
  * @author Vinay Gangoli
  */
 @Entity
-@Table (name = "stratification_criterion_answer_combination")
+@Table (name = "strat_criterion_answer")
 @GenericGenerator(name="id-generator", strategy = "native",
     parameters = {
-        @Parameter(name="sequence", value="STRATIFICATION_CRITERION_ANSWER_COMBINATION_ID_SEQ")
+        @Parameter(name="sequence", value="STRAT_CRIT_ANSWER_ID_SEQ")
     }
 )
 public class StratificationCriterionAnswerCombination extends AbstractMutableDomainObject {
