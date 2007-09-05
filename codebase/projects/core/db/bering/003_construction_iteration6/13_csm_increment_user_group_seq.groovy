2883697d25ae52a3a1a1a7a13ad23af096e11a9d
Manav kher <icechunk@gmail.com>
2007-09-05 14:59:50 +0000

    renamed

--- codebase/projects/core/db/bering/003_construction_iteration6/13_csm_increment_user_group_seq.groovy
+++
@@ -1,9 +0,0 @@
-class CSMIncrememntUserGroupSeq extends edu.northwestern.bioinformatics.bering.Migration {
-	void up() {
-		execute("ALTER SEQUENCE csm_user_grou_user_group_i_seq restart with 4");
-	}
-
-	void down(){
-
-	}
-}
