452ac78c00ad83ce7aa314a433566990fdda1722
Manav kher <icechunk@gmail.com>
2007-09-10 22:57:45 +0000

    update for oracle

--- codebase/projects/core/db/bering/003_construction_iteration6/013_csm_increment_user_group_seq.groovy
+++ codebase/projects/core/db/bering/003_construction_iteration6/013_csm_increment_user_group_seq.groovy
@@ -4,9 +4,8 @@ class CSMIncrememntUserGroupSeq extends edu.northwestern.bioinformatics.bering.M
 		    execute("ALTER SEQUENCE csm_user_grou_user_group_i_seq restart with 4");
 		}
          if (databaseMatches('oracle')) {
-            execute("DROP SEQUENCE csm_user_grou_user_group_i_seq increment by 1
-                      start with 1 NOMAXVALUE minvalue 1 nocycle nocache noorder;");
-            execute("CREATE SEQUENCE csm_user_grou_user_group_i_seq start with 4");
+            execute("DROP SEQUENCE csm_user_grou_user_group_i_seq");
+            execute("CREATE SEQUENCE csm_user_grou_user_group_i_seq start with 4 increment by 1 NOMAXVALUE minvalue 1 nocycle nocache noorder");
          }
 
 
--- codebase/projects/core/db/bering/004_construction_iteration7/001_csm_increment_group_sequence.groovy
+++ codebase/projects/core/db/bering/004_construction_iteration7/001_csm_increment_group_sequence.groovy
@@ -1,7 +1,13 @@
-
 class CSMIncrememntGroupSeq extends edu.northwestern.bioinformatics.bering.Migration {
+
 	void up() {
-		execute("ALTER SEQUENCE CSM_GROUP_GROUP_ID_SEQ restart with 4");
+         if (databaseMatches('postgres')) {
+		    execute("ALTER SEQUENCE CSM_GROUP_GROUP_ID_SEQ restart with 4");
+		}
+         if (databaseMatches('oracle')) {
+            execute("DROP SEQUENCE CSM_GROUP_GROUP_ID_SEQ");
+            execute("CREATE SEQUENCE CSM_GROUP_GROUP_ID_SEQ start with 4 increment by 1 NOMAXVALUE minvalue 1 nocycle nocache noorder");
+         }
 	}
 
 	void down(){
