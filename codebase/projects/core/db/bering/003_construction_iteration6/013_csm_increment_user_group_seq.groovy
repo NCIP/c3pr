e40a1381b5d5bce6b7d23f4730f8ab2919475ba5
Manav kher <icechunk@gmail.com>
2007-09-10 22:48:13 +0000

    update for oracle

--- codebase/projects/core/db/bering/003_construction_iteration6/013_csm_increment_user_group_seq.groovy
+++ codebase/projects/core/db/bering/003_construction_iteration6/013_csm_increment_user_group_seq.groovy
@@ -1,6 +1,15 @@
 class CSMIncrememntUserGroupSeq extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-		execute("ALTER SEQUENCE csm_user_grou_user_group_i_seq restart with 4");
+         if (databaseMatches('postgres')) {
+		    execute("ALTER SEQUENCE csm_user_grou_user_group_i_seq restart with 4");
+		}
+         if (databaseMatches('oracle')) {
+            execute("DROP SEQUENCE csm_user_grou_user_group_i_seq increment by 1
+                      start with 1 NOMAXVALUE minvalue 1 nocycle nocache noorder;");
+            execute("CREATE SEQUENCE csm_user_grou_user_group_i_seq start with 4");
+         }
+
+
 	}
 
 	void down(){
