b6373cff380e02352ec18593cd8aa615d40e8022
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-09-19 21:27:27 +0000

    adding enum class to capture coordinating center study status

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/CoordinatingCenterStudyStatus.java
@@ -0,0 +1,36 @@
+package edu.duke.cabig.c3pr.domain;
+
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.getByClassAndCode;
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.register;
+import static gov.nih.nci.cabig.ctms.domain.EnumHelper.sentenceCasedName;
+import gov.nih.nci.cabig.ctms.domain.CodedEnum;
+
+public enum CoordinatingCenterStudyStatus  implements CodedEnum<String> {
+	ACTIVE("Active"), PENDING("Pending"), AMENDMENT_PENDING("Amendment Pending"), 
+	CLOSED_TO_ACCRUAL_AND_TREATMENT("Closed To Accrual And Treatment"),CLOSED_TO_ACCRUAL("Closed To Accrual"),
+	TEMPORARILY_CLOSED_TO_ACCRUAL_AND_TREATMENT("Temporarily Closed To Accrual And Treatment"),
+	TEMPORARILY_CLOSED_TO_ACCRUAL("Temporarily Closed To Accrual") ;
+
+	private String code;
+
+	private CoordinatingCenterStudyStatus(String code) {
+		this.code = code;
+		register(this);
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getDisplayName() {
+		return sentenceCasedName(this);
+	}
+
+	public String getName() {
+		return name();
+	}
+
+	public static CoordinatingCenterStudyStatus getByCode(String code) {
+		return getByClassAndCode(CoordinatingCenterStudyStatus.class, code);
+	}
+}
