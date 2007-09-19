6457fd4b68d8c5962a1080df5313c1ca3c53d5a3
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-09-19 20:31:12 +0000

    removed status, status_code columns in studies and studyOrganizations and added data_entry_status, coordinating_center_study_status and site_study_status respectively

---
+++ codebase/projects/core/db/bering/004_construction_iteration7/011_refactor_study_and_study_sites_modify_status_columns.groovy
@@ -0,0 +1,25 @@
+class RefactorStudyAndStudySitesModifyStatusColumns extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+    	addColumn('STUDIES','DATA_ENTRY_STATUS','string');
+    	setNullable('STUDIES','DATA_ENTRY_STATUS',false);
+	   	addColumn('STUDIES','COORDINATING_CENTER_STUDY_STATUS','string');
+	   	setNullable('STUDIES','COORDINATING_CENTER_STUDY_STATUS',false);
+    	addColumn('STUDY_ORGANIZATIONS','SITE_STUDY_STATUS','string');
+    	dropColumn('STUDIES','STATUS');
+    	dropColumn('STUDY_ORGANIZATIONS','STATUS_CODE');
+    }
+
+    void down() {
+    	addColumn('STUDY_ORGANIZATIONS','STATUS_CODE','string');
+    	setNullable('STUDY_ORGANIZATIONS','STATUS_CODE',false);
+	   	addColumn('STUDIES','STATUS','string');
+	   	setNullable('STUDIES','STATUS',false);
+	   	
+	    dropColumn('STUDY_ORGANIZATIONS','SITE_STUDY_STATUS');
+    	setNullable('STUDIES','COORDINATING_CENTER_STUDY_STATUS',true);
+	   	dropColumn('STUDIES','COORDINATING_CENTER_STUDY_STATUS');
+	   	setNullable('STUDIES','DATA_ENTRY_STATUS',true);
+    	dropColumn('STUDIES','DATA_ENTRY_STATUS')
+    	
+    }
+}
