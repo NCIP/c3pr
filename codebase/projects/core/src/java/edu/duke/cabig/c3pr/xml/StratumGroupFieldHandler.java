3f6e6272e62c10ebb986a837fd166c7c23c0cf26
Manav kher <icechunk@gmail.com>
2007-09-19 21:29:31 +0000

    new registration schema

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/xml/StratumGroupFieldHandler.java
@@ -0,0 +1,49 @@
+package edu.duke.cabig.c3pr.xml;
+
+import org.exolab.castor.mapping.FieldHandler;
+import org.exolab.castor.mapping.ValidityException;
+import org.apache.log4j.Logger;
+import edu.duke.cabig.c3pr.domain.StratumGroup;
+import edu.duke.cabig.c3pr.domain.StudySubject;
+import edu.duke.cabig.c3pr.exception.C3PRBaseException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kherm
+ * Date: Sep 19, 2007
+ * Time: 3:29:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StratumGroupFieldHandler implements FieldHandler {
+
+    Logger log = Logger.getLogger(StratumGroupFieldHandler.class);
+
+    public Object getValue(Object object) throws IllegalStateException {
+        StudySubject registration = (StudySubject)object;
+        try {
+            return registration.getStratumGroup().toString();
+        } catch (Exception e) {
+            log.warn(e);
+        }
+        return null;
+    }
+
+    public void setValue(Object object, Object object1) throws IllegalStateException, IllegalArgumentException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void resetValue(Object object) throws IllegalStateException, IllegalArgumentException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * @deprecated
+     */
+    public void checkValidity(Object object) throws ValidityException, IllegalStateException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Object newInstance(Object object) throws IllegalStateException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
