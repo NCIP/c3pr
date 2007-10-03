c3c642b2349276c2a01ada418b4275623a7da547
Manav kher <icechunk@gmail.com>
2007-10-03 19:16:34 +0000

    added new site coord role

---
+++ codebase/projects/core/db/bering/004_construction_iteration7/017_csm_authorization_site_coordinator.groovy
@@ -0,0 +1,26 @@
+class CSMAddSiteCoordinator extends edu.northwestern.bioinformatics.bering.Migration {
+	void up() {
+		insert('csm_group', [ group_id: -1005, group_name: 'site_coordinator', group_desc: 'Site coordinator group',  application_id: 1 ], primaryKey: false)
+        //create on study
+		insert('csm_user_group_role_pg',[USER_GROUP_ROLE_PG_ID: -1005, GROUP_ID: -1005, ROLE_ID: 4, PROTECTION_GROUP_ID: 2],primaryKey: false)
+        //read on study
+		insert('csm_user_group_role_pg',[USER_GROUP_ROLE_PG_ID: -1006, GROUP_ID: -1005, ROLE_ID: 6, PROTECTION_GROUP_ID: 2],primaryKey: false)
+
+        //read on Participant
+		insert('csm_user_group_role_pg',[USER_GROUP_ROLE_PG_ID: -1007, GROUP_ID: -1005, ROLE_ID: 3, PROTECTION_GROUP_ID: 1],primaryKey: false)
+
+        //read on Registration
+		insert('csm_user_group_role_pg',[USER_GROUP_ROLE_PG_ID: -1008, GROUP_ID: -1005, ROLE_ID: 9, PROTECTION_GROUP_ID: 3],primaryKey: false)
+
+        //read on research staff
+		insert('csm_user_group_role_pg',[USER_GROUP_ROLE_PG_ID: -1009, GROUP_ID: -1005, ROLE_ID: 12, PROTECTION_GROUP_ID: 6],primaryKey: false)
+
+        //read on research staff
+		insert('csm_user_group_role_pg',[USER_GROUP_ROLE_PG_ID: -1010, GROUP_ID: -1005, ROLE_ID: 15, PROTECTION_GROUP_ID: 5],primaryKey: false)
+
+	}
+
+	void down(){
+
+	}
+}
