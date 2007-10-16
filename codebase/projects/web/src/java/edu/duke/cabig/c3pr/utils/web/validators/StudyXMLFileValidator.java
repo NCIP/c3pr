664524feb95ba9529da3cddbc3ad0909b0f16663
Manav kher <icechunk@gmail.com>
2007-10-16 22:08:51 +0000

    validation errors for xml import

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/validators/StudyXMLFileValidator.java
@@ -0,0 +1,45 @@
+package edu.duke.cabig.c3pr.utils.web.validators;
+
+import edu.duke.cabig.c3pr.exception.XMLValidationException;
+import edu.duke.cabig.c3pr.web.beans.StudyXMLFileBean;
+import edu.duke.cabig.c3pr.xml.XMLParser;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+/**
+ * Will validate the xml import with the schema
+ * 
+ * Created by IntelliJ IDEA.
+ * User: kherm
+ * Date: Oct 16, 2007
+ * Time: 2:46:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StudyXMLFileValidator implements Validator {
+
+    public XMLParser xmlParser;
+
+    public boolean supports(Class aClass) {
+        return aClass.isAssignableFrom(StudyXMLFileBean.class);
+    }
+
+    public void validate(Object object, Errors errors) {
+        StudyXMLFileBean xmlBean = (StudyXMLFileBean) object;
+        try {
+            xmlParser.validate(xmlBean.getFile());
+        } catch (XMLValidationException e) {
+            errors.reject("XML is Invalid " + e.getMessage());
+        }
+    }
+
+
+    public XMLParser getXmlParser() {
+        return xmlParser;
+    }
+
+    public void setXmlParser(XMLParser xmlParser) {
+        this.xmlParser = xmlParser;
+    }
+}
+
+
