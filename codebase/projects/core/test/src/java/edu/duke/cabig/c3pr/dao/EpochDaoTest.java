bf248fbfbda4f9d8452d8ad6c648f5f502f28755
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2007-10-17 21:37:13 +0000

    Test case for the retired_indicator cascade

--- codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/dao/EpochDaoTest.java
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/dao/EpochDaoTest.java
@@ -2,14 +2,20 @@ package edu.duke.cabig.c3pr.dao;
 
 import static edu.nwu.bioinformatics.commons.testing.CoreTestCase.assertContains;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.duke.cabig.c3pr.domain.Arm;
+import edu.duke.cabig.c3pr.domain.BookRandomization;
+import edu.duke.cabig.c3pr.domain.BookRandomizationEntry;
 import edu.duke.cabig.c3pr.domain.EligibilityCriteria;
 import edu.duke.cabig.c3pr.domain.Epoch;
 import edu.duke.cabig.c3pr.domain.ExclusionEligibilityCriteria;
 import edu.duke.cabig.c3pr.domain.InclusionEligibilityCriteria;
 import edu.duke.cabig.c3pr.domain.StratificationCriterion;
+import edu.duke.cabig.c3pr.domain.StratificationCriterionAnswerCombination;
+import edu.duke.cabig.c3pr.domain.StratificationCriterionPermissibleAnswer;
+import edu.duke.cabig.c3pr.domain.StratumGroup;
 import edu.duke.cabig.c3pr.domain.Study;
 import edu.duke.cabig.c3pr.domain.TreatmentEpoch;
 import edu.duke.cabig.c3pr.utils.ContextDaoTestCase;
@@ -30,7 +36,110 @@ public class EpochDaoTest extends ContextDaoTestCase<EpochDao> {
 	private EpochDao epochDao = (EpochDao) getApplicationContext().getBean(
 			"epochDao");
 	
+	public void testRetiredIndicator() throws Exception{
+		Integer savedId;
+		{
+			Study study = new Study();
+			study.setShortTitleText("ShortTitleText");
+			study.setLongTitleText("LongTitleText");
+			study.setPhaseCode("PhaseCode");
+			study.setCoordinatingCenterStudyStatus(CoordinatingCenterStudyStatus.ACTIVE);
+			study.setDataEntryStatus(StudyDataEntryStatus.COMPLETE);
+			study.setTargetAccrualNumber(150);
+			study.setType("Type");
+			study.setMultiInstitutionIndicator(Boolean.TRUE);
+			
+			TreatmentEpoch epoch1 = new TreatmentEpoch();
+			Arm armA = new Arm();
+			armA.setName("A");			
+			armA.setTreatmentEpoch(epoch1);
+			armA.setName("Arm name");
+			ArrayList<Arm> aList = new ArrayList<Arm>();
+			aList.add(armA);
+			epoch1.getArms().addAll(aList);
+			epoch1.setName("epoch1");
+			epoch1.setStudy(study);
+			BookRandomization bRandomization = new BookRandomization();
+			BookRandomizationEntry bre = new BookRandomizationEntry();
+			bre.setPosition(10);
+			
+			if (epoch1 != null) {
+				List<Arm> armList = epoch1.getArms();
+				for (Arm individualArm : armList) {
+					if (individualArm.getName().equals("Arm name")) {
+						bre.setArm(individualArm);
+					}
+				}
+			}
+			addStratumGroupToEpoch(epoch1);
+			bre.setStratumGroup(epoch1.getStratumGroups().get(0));
+
+			List<BookRandomizationEntry> breList = new ArrayList<BookRandomizationEntry>();
+			breList.add(bre);
+			bRandomization.getBookRandomizationEntry().addAll(breList);
+			epoch1.setRandomization(bRandomization);
+
+			InclusionEligibilityCriteria incCrit = new InclusionEligibilityCriteria();
+			incCrit.setQuestionText("questionText");
+			incCrit.setQuestionNumber(1);
+			epoch1.getInclusionEligibilityCriteria().add(incCrit);
+
+			ExclusionEligibilityCriteria excCrit = new ExclusionEligibilityCriteria();
+			excCrit.setQuestionText("questionText for exc");
+			excCrit.setQuestionNumber(1);
+			epoch1.getExclusionEligibilityCriteria().add(excCrit);
+
+			study.getTreatmentEpochs().add(epoch1);
+			study.getTreatmentEpochs().get(0).setRetiredIndicatorAsTrue();
+			//intentionally undoing the retired flag of the epoch so that it may be retrieved.
+			//for the test to clear...the retrieved study must not contain its children
+			study.getTreatmentEpochs().get(0).setRetiredIndicatorAsFalse();
+			studyDao.save(study);
+			savedId = study.getId();
+		}
+
+		interruptSession();
+		{
+			Study loadedStudy = studyDao.getById(savedId);
+			TreatmentEpoch te = loadedStudy.getTreatmentEpochs().get(0);
+			assertEquals(1, loadedStudy.getTreatmentEpochs().size());
+			assertEquals(0, te.getArms().size());
+			assertEquals(0, te.getInclusionEligibilityCriteria().size());
+			assertEquals(0, te.getEligibilityCriteria().size());
+			assertEquals(0, te.getStratificationCriteria().size());
+			assertEquals(0, te.getStratumGroups().size());			
+		}
+	}
 	
+	public void addStratumGroupToEpoch(TreatmentEpoch epoch1) {
+		StratificationCriterion sc = new StratificationCriterion();
+		sc.setQuestionText("will I work?");
+		StratificationCriterionPermissibleAnswer scpa = new StratificationCriterionPermissibleAnswer();
+		scpa.setPermissibleAnswer("lets find out");
+		ArrayList scpaList = new ArrayList();
+		scpaList.add(scpa);
+		sc.setPermissibleAnswers(scpaList);
+		ArrayList scList = new ArrayList();
+		scList.add(sc);
+		epoch1.setStratificationCriteria(scList);
+
+		StratificationCriterionAnswerCombination scac = new StratificationCriterionAnswerCombination();
+		scac.setStratificationCriterion(sc);
+		scac.setStratificationCriterionPermissibleAnswer(scpa);
+		List<StratificationCriterionAnswerCombination> scacList = new ArrayList<StratificationCriterionAnswerCombination>();
+		scacList.add(scac);
+
+		StratumGroup stratumGroup = new StratumGroup();
+		stratumGroup.getStratificationCriterionAnswerCombination().addAll(
+				scacList);
+		stratumGroup.setCurrentPosition(1);
+		stratumGroup.setStratumGroupNumber(2);
+		ArrayList<StratumGroup> sgList = new ArrayList<StratumGroup>();
+		sgList.add(stratumGroup);
+
+		epoch1.getStratumGroups().addAll(sgList);
+	}
+
 
 	public void testSaveTreatmentEpochWithEligibilityCriteria()
 			throws Exception {
