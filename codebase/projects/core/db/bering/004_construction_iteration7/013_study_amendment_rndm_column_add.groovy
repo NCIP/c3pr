991a1f2f24eb6adfe9119c8b5a71c474e0f1b239
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-10-18 16:20:27 +0000

    updated column name randomization_changed_indicator to rndm_changed_indicator

--- codebase/projects/core/db/bering/004_construction_iteration7/013_study_amendment_rndm_column_add.groovy
+++ codebase/projects/core/db/bering/004_construction_iteration7/013_study_amendment_rndm_column_add.groovy
@@ -1,11 +1,11 @@
 class StudyAmendmentRndmColumnAdd extends edu.northwestern.bioinformatics.bering.Migration {
     
     void up() {
-   		addColumn('study_amendments', 'randomization_changed_indicator', 'boolean');   		
+   		addColumn('study_amendments', 'rndm_changed_indicator', 'boolean');   		
    	 }
 
     void down() {
-    	dropColumn('study_amendments', 'randomization_changed_indicator'); 		
+    	dropColumn('study_amendments', 'rndm_changed_indicator'); 		
 	}
 	
 }
