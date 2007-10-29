00e2d71581690ad0e0861829e8b43c08397e73ac
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-10-29 14:18:56 +0000

    adding tab for participant registrations' page

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/participant/ParticipantRegistrationsTab.java
@@ -0,0 +1,32 @@
+package edu.duke.cabig.c3pr.web.participant;
+
+import java.util.Map;
+
+import edu.duke.cabig.c3pr.dao.ParticipantDao;
+import edu.duke.cabig.c3pr.domain.Participant;
+import edu.duke.cabig.c3pr.domain.Study;
+
+public class ParticipantRegistrationsTab extends ParticipantTab{
+	 private ParticipantDao participantDao;
+	
+	public ParticipantRegistrationsTab() {
+		super("Registrations", "Registrations", "participant/participant_registrations");
+	}
+	
+	@Override
+    public Map<String, Object> referenceData(Participant participant) {
+        Map<String, Object> refdata = super.referenceData(participant);
+        refdata.put("participantAssignments", this.getParticipantDao().getById(participant.getId()).getStudySubjects());
+
+        return refdata;
+    }
+
+	public ParticipantDao getParticipantDao() {
+		return participantDao;
+	}
+
+	public void setParticipantDao(ParticipantDao participantDao) {
+		this.participantDao = participantDao;
+	}
+
+}
