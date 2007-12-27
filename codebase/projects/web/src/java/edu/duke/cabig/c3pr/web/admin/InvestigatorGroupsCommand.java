a4573fc3d5d6128ad771c21e3be13a186a9a4375
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2007-12-27 16:12:54 +0000

    adding command class for investigator groups controller

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/admin/InvestigatorGroupsCommand.java
@@ -0,0 +1,16 @@
+package edu.duke.cabig.c3pr.web.admin;
+
+import edu.duke.cabig.c3pr.domain.HealthcareSite;
+
+public class InvestigatorGroupsCommand {
+	private HealthcareSite healthcareSite;
+
+	public HealthcareSite getHealthcareSite() {
+		return healthcareSite;
+	}
+
+	public void setHealthcareSite(HealthcareSite healthcareSite) {
+		this.healthcareSite = healthcareSite;
+	}
+
+}
