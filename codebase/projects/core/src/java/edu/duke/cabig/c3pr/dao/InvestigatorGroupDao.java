9d91af4304ab0c1f7de3b684b929e266a32dae73
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-01-02 20:59:25 +0000

    added function for retrieving site affiliations

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/InvestigatorGroupDao.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/InvestigatorGroupDao.java
@@ -2,30 +2,31 @@ package edu.duke.cabig.c3pr.dao;
 
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.dao.DataAccessException;
 
+import edu.duke.cabig.c3pr.domain.DiseaseTerm;
 import edu.duke.cabig.c3pr.domain.InvestigatorGroup;
+import edu.duke.cabig.c3pr.domain.SiteInvestigatorGroupAffiliation;
+
 
 /**
- * @author Priyatam
+ * @author Ramakrishna
  */
 public class InvestigatorGroupDao extends GridIdentifiableDao<InvestigatorGroup> {
 
-    private static Log log = LogFactory.getLog(InvestigatorGroupDao.class);
-
-
-    public Class<InvestigatorGroup> domainClass() {
+	public Class<InvestigatorGroup> domainClass() {
         return InvestigatorGroup.class;
     }
 
-    /**
-     * Get All InvestigatorGroups
-     * @throws DataAccessException
-     */
+	@SuppressWarnings("unchecked")
     public List<InvestigatorGroup> getAll() throws DataAccessException {
         return getHibernateTemplate().find("from InvestigatorGroup");
     }
-	   
+    
+    @SuppressWarnings("unchecked")
+    public List<SiteInvestigatorGroupAffiliation> getAffiliationsByGroupId(Integer parentId) throws DataAccessException {
+        return getHibernateTemplate().find("from SiteInvestigatorGroupAffiliation siga where siga.investigatorGroup.id =?",
+        		new Object[] { parentId });
+    }
+    
 }
