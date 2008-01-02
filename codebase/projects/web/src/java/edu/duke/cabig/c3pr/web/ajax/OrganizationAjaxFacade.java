1a74f33d34438d59ab86194f2885b96c88372cb9
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-01-02 21:00:47 +0000

    updated

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/ajax/OrganizationAjaxFacade.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/ajax/OrganizationAjaxFacade.java
@@ -13,134 +13,178 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.HttpSessionRequiredException;
-import org.springframework.web.servlet.ModelAndView;
 
 import edu.duke.cabig.c3pr.dao.HealthcareSiteDao;
 import edu.duke.cabig.c3pr.dao.HealthcareSiteInvestigatorDao;
-import edu.duke.cabig.c3pr.dao.ParticipantDao;
+import edu.duke.cabig.c3pr.dao.InvestigatorGroupDao;
+import edu.duke.cabig.c3pr.dao.OrganizationDao;
 import edu.duke.cabig.c3pr.domain.HealthcareSite;
 import edu.duke.cabig.c3pr.domain.HealthcareSiteInvestigator;
+import edu.duke.cabig.c3pr.domain.Investigator;
 import edu.duke.cabig.c3pr.domain.InvestigatorGroup;
-import edu.duke.cabig.c3pr.domain.Study;
+import edu.duke.cabig.c3pr.domain.SiteInvestigatorGroupAffiliation;
 
-	/**
-	 * @author Ramakrishna
-	 */
-	public class OrganizationAjaxFacade {
-	    private HealthcareSiteDao healthcareSiteDao;
-	    private HealthcareSiteInvestigatorDao healthcareSiteInvestigatorDao;
+/**
+ * @author Ramakrishna
+ */
+public class OrganizationAjaxFacade {
+	private HealthcareSiteDao healthcareSiteDao;
 
-	    private static Log log = LogFactory.getLog(OrganizationAjaxFacade.class);
+	private HealthcareSiteInvestigatorDao healthcareSiteInvestigatorDao;
 
-	    @SuppressWarnings("unchecked")
-	    private <T> T buildReduced(T src, List<String> properties) {
-	        T dst = null;
-	        try {
-	            // it doesn't seem like this cast should be necessary
-	            dst = (T) src.getClass().newInstance();
-	        } catch (InstantiationException e) {
-	            throw new RuntimeException("Failed to instantiate " + src.getClass().getName(), e);
-	        } catch (IllegalAccessException e) {
-	            throw new RuntimeException("Failed to instantiate " + src.getClass().getName(), e);
-	        }
+	private OrganizationDao organizationDao;
 
-	        BeanWrapper source = new BeanWrapperImpl(src);
-	        BeanWrapper destination = new BeanWrapperImpl(dst);
-	        for (String property : properties) {
-	            destination.setPropertyValue(
-	                    property,
-	                    source.getPropertyValue(property)
-	            );
-	        }
-	        return dst;
-	    }
+	private InvestigatorGroupDao investigatorGroupDao;
 
-	   public List<HealthcareSite> matchHealthcareSites(String text) throws Exception {
-	    	
-	    	 List<HealthcareSite> healthcareSites = healthcareSiteDao.getBySubnames(extractSubnames(text));
+	private static Log log = LogFactory.getLog(OrganizationAjaxFacade.class);
 
-			List<HealthcareSite> reducedHealthcareSites = new ArrayList<HealthcareSite>(
-					healthcareSites.size());
-			for (HealthcareSite healthcareSite : healthcareSites) {
-				reducedHealthcareSites.add(buildReduced(healthcareSite, Arrays.asList(
-						"id", "name","nciInstituteCode")));
+	@SuppressWarnings("unchecked")
+	private <T> T buildReduced(T src, List<String> properties) {
+		T dst = null;
+		try {
+			// it doesn't seem like this cast should be necessary
+			dst = (T) src.getClass().newInstance();
+		} catch (InstantiationException e) {
+			throw new RuntimeException("Failed to instantiate "
+					+ src.getClass().getName(), e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException("Failed to instantiate "
+					+ src.getClass().getName(), e);
+		}
+
+		BeanWrapper source = new BeanWrapperImpl(src);
+		BeanWrapper destination = new BeanWrapperImpl(dst);
+		for (String property : properties) {
+			destination.setPropertyValue(property, source
+					.getPropertyValue(property));
+		}
+		return dst;
+	}
+
+	public List<HealthcareSite> matchHealthcareSites(String text)
+			throws Exception {
+
+		List<HealthcareSite> healthcareSites = healthcareSiteDao
+				.getBySubnames(extractSubnames(text));
+
+		List<HealthcareSite> reducedHealthcareSites = new ArrayList<HealthcareSite>(
+				healthcareSites.size());
+		for (HealthcareSite healthcareSite : healthcareSites) {
+			reducedHealthcareSites.add(buildReduced(healthcareSite, Arrays
+					.asList("id", "name", "nciInstituteCode")));
+		}
+		return reducedHealthcareSites;
+
+	}
+
+	public List<HealthcareSiteInvestigator> matchOrganizationInvestigatorsGivenOrganizationId(
+			String text, int organizationId, HttpServletRequest request)
+			throws Exception {
+		List<HealthcareSiteInvestigator> inv = healthcareSiteInvestigatorDao
+				.getBySubnames(extractSubnames(text), organizationId);
+		List<HealthcareSiteInvestigator> reducedInv = new ArrayList<HealthcareSiteInvestigator>(
+				inv.size());
+		for (HealthcareSiteInvestigator hcInv : inv) {
+			HealthcareSiteInvestigator temp;
+			temp = buildReduced(hcInv, Arrays.asList("id"));
+			temp.setInvestigator(buildReduced(hcInv.getInvestigator(), Arrays
+					.asList("firstName", "lastName", "maidenName")));
+			reducedInv.add(temp);
+
+		}
+
+		return reducedInv;
+	}
+
+	public List<InvestigatorGroup> getInvestigatorGroups(int organizationId,
+			HttpServletRequest request) throws Exception {
+
+		HealthcareSite healthcareSite = organizationDao.getById(organizationId);
+		if (healthcareSite.getInvestigatorGroups() != null
+				&& healthcareSite.getInvestigatorGroups().size() > 0) {
+
+			List<InvestigatorGroup> reducedInvestigatorGroups = new ArrayList<InvestigatorGroup>();
+			for (InvestigatorGroup investigatorGroup : healthcareSite
+					.getInvestigatorGroups()) {
+				reducedInvestigatorGroups.add(buildReduced(investigatorGroup,
+						Arrays.asList("id", "name", "descriptionText",
+								"startDate", "endDate")));
 			}
-			return reducedHealthcareSites;
-
+			return  reducedInvestigatorGroups;
 		}
-	   public List<HealthcareSiteInvestigator> matchStudyOrganizationInvestigatorsGivenOrganizationId(
-				String text, int organizationId, HttpServletRequest request)
-				throws Exception {
-			List<HealthcareSiteInvestigator> inv = healthcareSiteInvestigatorDao
-					.getBySubnames(extractSubnames(text), organizationId);
-			List<HealthcareSiteInvestigator> reducedInv = new ArrayList<HealthcareSiteInvestigator>(
-					inv.size());
-			for (HealthcareSiteInvestigator hcInv : inv) {
-				// creating a new temp HSI and calling build reduced twice as
-				// Arrays.aslist doesnt understand
-				// the dot operator (in other words...something like inv.firstName
-				// doesnt work)
-				// Also calling build reduced with specific params instead of the
-				// whole HSI object to prevent
-				// hibernate from retrieving every nested object.
-				HealthcareSiteInvestigator temp;
-				temp = buildReduced(hcInv, Arrays.asList("id"));
-				temp.setInvestigator(buildReduced(hcInv.getInvestigator(), Arrays
-						.asList("firstName", "lastName", "maidenName")));
-				reducedInv.add(temp);
+		return null;
+	}
 
-			}
+	public List<Investigator> getAffiliatedInvestigatorsForGroup(int groupId,
+			HttpServletRequest request) throws Exception {
 
-			return reducedInv;
+		List<Investigator> reducedInvestigators = new ArrayList<Investigator>();
+		for (SiteInvestigatorGroupAffiliation siteInvestigatorGroupAffiliation : investigatorGroupDao
+				.getAffiliationsByGroupId(groupId)) {
+			reducedInvestigators.add(buildReduced(
+					siteInvestigatorGroupAffiliation
+							.getHealthcareSiteInvestigator().getInvestigator(),
+					Arrays.asList("lastName","firstName", "id")));
 		}
-	  
-	   public List<InvestigatorGroup> getInvestigatorGroups(int organizationId,HttpServletRequest request)
-				throws Exception {
-		   
-		 		HealthcareSite healthcareSite = healthcareSiteDao.getById(organizationId);
-		 		if (healthcareSite.getInvestigatorGroups()!=null &&healthcareSite.getInvestigatorGroups().size()>0){
-		 			return healthcareSite.getInvestigatorGroups();
-		 		}
-		 		return null;
+		return reducedInvestigators;
+	}
+
+	private final Object getCommandOnly(HttpServletRequest request)
+			throws Exception {
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			throw new HttpSessionRequiredException(
+					"Must have session when trying to bind (in session-form mode)");
 		}
+		String formAttrName = getFormSessionAttributeName();
+		Object sessionFormObject = session.getAttribute(formAttrName);
 
-	    private final Object getCommandOnly(HttpServletRequest request) throws Exception {
-	        HttpSession session = request.getSession(false);
-	        if (session == null) {
-	            throw new HttpSessionRequiredException("Must have session when trying to bind (in session-form mode)");
-	        }
-	        String formAttrName = getFormSessionAttributeName();
-	        Object sessionFormObject = session.getAttribute(formAttrName);
+		return sessionFormObject;
+	}
 
-	        return sessionFormObject;
-	    }
-	    
-	    ////// CONFIGURATION
+	// //// CONFIGURATION
 
-	    @Required
-	    private String getFormSessionAttributeName() {
-	        return "edu.duke.cabig.c3pr.web.admin.CreateInvestigatorGroupsController.FORM.command";
-	    }
+	@Required
+	private String getFormSessionAttributeName() {
+		return "edu.duke.cabig.c3pr.web.admin.CreateInvestigatorGroupsController.FORM.command";
+	}
 
-	    private String[] extractSubnames(String text) {
-	        return text.split("\\s+");
-	    }
+	private String[] extractSubnames(String text) {
+		return text.split("\\s+");
+	}
 
-		public HealthcareSiteDao getHealthcareSiteDao() {
-			return healthcareSiteDao;
-		}
+	public HealthcareSiteDao getHealthcareSiteDao() {
+		return healthcareSiteDao;
+	}
 
-		public void setHealthcareSiteDao(HealthcareSiteDao healthcareSiteDao) {
-			this.healthcareSiteDao = healthcareSiteDao;
-		}
+	public void setHealthcareSiteDao(HealthcareSiteDao healthcareSiteDao) {
+		this.healthcareSiteDao = healthcareSiteDao;
+	}
 
-		public HealthcareSiteInvestigatorDao getHealthcareSiteInvestigatorDao() {
-			return healthcareSiteInvestigatorDao;
-		}
+	public HealthcareSiteInvestigatorDao getHealthcareSiteInvestigatorDao() {
+		return healthcareSiteInvestigatorDao;
+	}
 
-		public void setHealthcareSiteInvestigatorDao(
-				HealthcareSiteInvestigatorDao healthcareSiteInvestigatorDao) {
-			this.healthcareSiteInvestigatorDao = healthcareSiteInvestigatorDao;
-		}
+	public void setHealthcareSiteInvestigatorDao(
+			HealthcareSiteInvestigatorDao healthcareSiteInvestigatorDao) {
+		this.healthcareSiteInvestigatorDao = healthcareSiteInvestigatorDao;
+	}
+
+	public OrganizationDao getOrganizationDao() {
+		return organizationDao;
+	}
+
+	public void setOrganizationDao(OrganizationDao organizationDao) {
+		this.organizationDao = organizationDao;
+	}
+
+	public InvestigatorGroupDao getInvestigatorGroupDao() {
+		return investigatorGroupDao;
+	}
+
+	public void setInvestigatorGroupDao(
+			InvestigatorGroupDao investigatorGroupDao) {
+		this.investigatorGroupDao = investigatorGroupDao;
+	}
 
 }
