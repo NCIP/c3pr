cdd64bf2b530e173298efb7fc3167d5ae9d8f034
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-01-22 22:46:38 +0000

    adding scripts for loading ctep organizations and indexing organizations table

---
+++ codebase/projects/core/db/bering/008_transition_iteration1/009_add_index_organizations.groovy
@@ -0,0 +1,9 @@
+class AddIndexOrganizations extends edu.northwestern.bioinformatics.bering.Migration {
+	void up() {
+		execute("create index organizations_idx on organizations(name,nci_institute_code)");
+	}
+
+	void down(){
+				execute("drop index organizations_idx");
+	}
+}
