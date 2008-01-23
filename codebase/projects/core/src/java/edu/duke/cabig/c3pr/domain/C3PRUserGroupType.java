6a2b15e0067e07867cdaaee30cb311d4373db78f
Manav kher <icechunk@gmail.com>
2008-01-23 22:41:45 +0000

    added register to constructor

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/C3PRUserGroupType.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/C3PRUserGroupType.java
@@ -2,6 +2,7 @@ package edu.duke.cabig.c3pr.domain;
 
 import gov.nih.nci.cabig.ctms.domain.CodedEnum;
 import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.getByClassAndCode;
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.register;
 import static gov.nih.nci.cabig.ctms.domain.EnumHelper.sentenceCasedName;
 
 /**
@@ -13,18 +14,17 @@ import static gov.nih.nci.cabig.ctms.domain.EnumHelper.sentenceCasedName;
  */
 public enum C3PRUserGroupType implements CodedEnum<String> {
 
-    C3PR_ADMIN ("c3pr_admin"),
+    C3PR_ADMIN("c3pr_admin"),
     STUDY_COORDINATOR("study_coordinator"),
     REGISTRAR("registrar"),
-    SITE_COORDINATOR("site_coordinator")
-
-    ;
+    SITE_COORDINATOR("site_coordinator");
 
     private String code;
 
 
-    C3PRUserGroupType(String code) {
+    private C3PRUserGroupType(String code) {
         this.code = code;
+        register(this);
     }
 
 
@@ -32,7 +32,7 @@ public enum C3PRUserGroupType implements CodedEnum<String> {
         return code;
     }
 
-      public static C3PRUserGroupType getByCode(String code) {
+    public static C3PRUserGroupType getByCode(String code) {
         return getByClassAndCode(C3PRUserGroupType.class, code);
     }
 
