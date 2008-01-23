7f34fe1e98830cb4c936bb3dc703e6e56071402b
kruttik Aggarwal <kruttikagarwal@gmail.com>
2008-01-23 17:32:11 +0000

    added default c3pr url

--- codebase/projects/core/db/bering/008_transition_iteration1/010_add_hostedMode_config_properties.groovy
+++ codebase/projects/core/db/bering/008_transition_iteration1/010_add_hostedMode_config_properties.groovy
@@ -1,6 +1,7 @@
 class ConfigDefaultProperties extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
 		insert('configuration', [ key:"hostedMode", value:"true" ], primaryKey: false)
+		insert('configuration', [ key:"c3pr.webapp.url", value:"https://cbvapp-d1017.nci.nih.gov:28443/c3pr" ], primaryKey: false)
 	}
 
 	void down(){
