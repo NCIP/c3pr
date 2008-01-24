6465a2f050a3a78db6c91aac52307e84e1bbea30
Manav kher <icechunk@gmail.com>
2008-01-24 22:33:21 +0000

    fixed psc link

--- codebase/projects/core/db/bering/007_construction_iteration10/005_config_default_properties.groovy
+++ codebase/projects/core/db/bering/007_construction_iteration10/005_config_default_properties.groovy
@@ -3,7 +3,7 @@ class ConfigDefaultProperties extends edu.northwestern.bioinformatics.bering.Mig
 	    //clear out existing configuration
 	    execute("delete from configuration");
 
-		insert('configuration', [ key:"pscBaseUrl", value:"https://cbvapp-d1017.nci.nih.gov:28443/psc/pages/schedule" ], primaryKey: false)
+		insert('configuration', [ key:"pscBaseUrl", value:"https://cbvapp-d1017.nci.nih.gov:28443/psc/pages/cal/schedule" ], primaryKey: false)
 		insert('configuration', [ key:"caaersBaseUrl", value:"https://cbvapp-d1017.nci.nih.gov:28443/caaers/pages/ae/list" ], primaryKey: false)
 		insert('configuration', [ key:"c3dViewerBaseUrl", value:"https://octrials-train.nci.nih.gov/opa45/rdclaunch.htm" ], primaryKey: false)
 		insert('configuration', [ key:"esbUrl", value:"https://cbvapp-d1017.nci.nih.gov:28445/wsrf/cagrid/CaXchangeRequestProcessor" ], primaryKey: false)
