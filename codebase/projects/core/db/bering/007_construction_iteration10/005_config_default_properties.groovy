d8fc70c2c65e5509cad59e3435771806d815044d
Manav kher <icechunk@gmail.com>
2008-01-28 23:54:30 +0000

    authorization on by default

--- codebase/projects/core/db/bering/007_construction_iteration10/005_config_default_properties.groovy
+++ codebase/projects/core/db/bering/007_construction_iteration10/005_config_default_properties.groovy
@@ -7,7 +7,7 @@ class ConfigDefaultProperties extends edu.northwestern.bioinformatics.bering.Mig
 		insert('configuration', [ key:"caaersBaseUrl", value:"https://cbvapp-d1017.nci.nih.gov:28443/caaers/pages/ae/list" ], primaryKey: false)
 		insert('configuration', [ key:"c3dViewerBaseUrl", value:"https://octrials-train.nci.nih.gov/opa45/rdclaunch.htm" ], primaryKey: false)
 		insert('configuration', [ key:"esbUrl", value:"https://cbvapp-d1017.nci.nih.gov:28445/wsrf/cagrid/CaXchangeRequestProcessor" ], primaryKey: false)
-		insert('configuration', [ key:"authorizationSwitch", value:"false" ], primaryKey: false)
+		insert('configuration', [ key:"authorizationSwitch", value:"true" ], primaryKey: false)
 		insert('configuration', [ key:"esbEnable", value:"false" ], primaryKey: false)
 		insert('configuration', [ key:"outgoingMailServer", value:"smtp.comcast.net" ], primaryKey: false)
 		insert('configuration', [ key:"outgoingMailServerPort", value:"25" ], primaryKey: false)
