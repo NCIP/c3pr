e0beb1edc2bc401638a76cdc2488d55f7faa71f5
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2008-02-04 22:27:57 +0000

    Added the reg id columna dn hyperlinked it to the reg page

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/ajax/RegistrationLinkDisplayCell.java
@@ -0,0 +1,24 @@
+package edu.duke.cabig.c3pr.web.ajax;
+
+import org.extremecomponents.table.bean.Column;
+import org.extremecomponents.table.cell.AbstractCell;
+import org.extremecomponents.table.core.TableModel;
+
+import edu.duke.cabig.c3pr.domain.StudySubject;
+
+public class RegistrationLinkDisplayCell extends AbstractCell {
+
+	 
+    @Override
+    protected String getCellValue(final TableModel model, final Column column) {
+    	StudySubject ss = (StudySubject) model.getCurrentRowBean();
+          String cellValue = column.getValueAsString();
+          String link = model.getContext().getContextPath() + "/pages/registration/manageRegistration?registrationId=";
+          if (ss != null) {
+                cellValue = "<a href=\"" + link + ss.getId() + "\">" + cellValue + "</a>";
+          }
+          return cellValue;
+    }
+
+}
+
