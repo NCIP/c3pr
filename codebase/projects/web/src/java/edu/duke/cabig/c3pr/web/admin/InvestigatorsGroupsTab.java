e3616821c5bcbe1ba1bb12ce5358eb438f023eb3
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-02-05 21:40:04 +0000

    updated with inplace edit functionality

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/admin/InvestigatorsGroupsTab.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/admin/InvestigatorsGroupsTab.java
@@ -1,6 +1,10 @@
 package edu.duke.cabig.c3pr.web.admin;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,6 +13,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.WebUtils;
 
 import edu.duke.cabig.c3pr.dao.HealthcareSiteDao;
 import edu.duke.cabig.c3pr.domain.HealthcareSite;
@@ -18,7 +23,7 @@ import edu.duke.cabig.c3pr.utils.web.spring.tabbedflow.InPlaceEditableTab;
  * @author Ramakrishna
  */
 public class InvestigatorsGroupsTab<C> extends InPlaceEditableTab<C>{
-
+	
 	public InvestigatorsGroupsTab() {
 		super("Investigator Groups","Investigator Groups","admin/investigator_groups_create");
 	}
@@ -34,43 +39,31 @@ public class InvestigatorsGroupsTab<C> extends InPlaceEditableTab<C>{
 	
 	private HealthcareSiteDao healthcareSiteDao;
 	
-	public ModelAndView getInvestigatorGroups(HttpServletRequest request,
-			Object commandObj, Errors error) {
-
-		InvestigatorGroupsCommand investigatorGroupsCommand = (InvestigatorGroupsCommand) commandObj;
-		HashMap map = new HashMap();
-		if (request.getParameter("organizationId") != null) {
-			int organizationId = Integer.parseInt(request
-					.getParameter("organizationId"));
-			HealthcareSite healthcareSite = healthcareSiteDao
-					.getById(organizationId);
-			investigatorGroupsCommand.setHealthcareSite(healthcareSite);
-			map.put("command", investigatorGroupsCommand);
-			ModelAndView  mv = new ModelAndView("admin/show_investigator_groups", map);
-			return mv;
-		}else {
-			return new ModelAndView("", map);
-		}
+	@Override
+	protected ModelAndView postProcessInPlaceEditing(HttpServletRequest request, C command, String property, String value) throws Exception {
+		InvestigatorGroupsCommand investigatorGroupsCommand = (InvestigatorGroupsCommand)command;
+		Map<String, String> map = new HashMap<String, String>();
+        String retValue = "";
+        int groupIndex =0;
+        if(WebUtils.hasSubmitParameter(request, "groupIndex")){
+        	groupIndex = Integer.parseInt(request.getParameter("groupIndex"));
+        }
+		if (property.startsWith("changedSiteAffiliationEndDate")) {
+			 int siteAffiliationIndex = Integer.parseInt(property.split("_")[1]);
+			 Date currentEndDate = investigatorGroupsCommand.getHealthcareSite().getInvestigatorGroups().get(groupIndex).getSiteInvestigatorGroupAffiliations().get(siteAffiliationIndex).getEndDate();
+            try {
+                Date targetEndDate = new SimpleDateFormat("MM/dd/yyyy").parse(value);
+                investigatorGroupsCommand.getHealthcareSite().getInvestigatorGroups().get(groupIndex).getSiteInvestigatorGroupAffiliations().get(siteAffiliationIndex).setEndDate(targetEndDate);
+                retValue += investigatorGroupsCommand.getHealthcareSite().getInvestigatorGroups().get(groupIndex).getSiteInvestigatorGroupAffiliations().get(siteAffiliationIndex).getEndDateStr();
+            } catch (ParseException e) {
+                  e.printStackTrace();
+                  retValue +=currentEndDate;
+            }
+        }
+		map.put(getFreeTextModelName(), retValue);
+        return new ModelAndView("", map);
 	}
 	
-	public ModelAndView getSiteAffiliations(HttpServletRequest request,
-			Object commandObj, Errors error) {
-
-		InvestigatorGroupsCommand investigatorGroupsCommand = (InvestigatorGroupsCommand) commandObj;
-		HashMap map = new HashMap();
-		if (request.getParameter("selected_site") != null) {
-			int siteId = Integer.parseInt("selected_site");
-			if(investigatorGroupsCommand.getHealthcareSite()!=null && investigatorGroupsCommand.getHealthcareSite().getInvestigatorGroups()!=null &&
-					investigatorGroupsCommand.getHealthcareSite().getInvestigatorGroups().get(siteId)!=null){
-				map.put("siteAffiliations", investigatorGroupsCommand.getHealthcareSite().getInvestigatorGroups().get(siteId).getSiteInvestigatorGroupAffiliations());
-			}
-			ModelAndView  mv = new ModelAndView("admin/show_investigator_affiliations", map);
-			
-			return mv;
-		}else {
-			return new ModelAndView("", map);
-		}
-	}
 
 	public HealthcareSiteDao getHealthcareSiteDao() {
 		return healthcareSiteDao;
