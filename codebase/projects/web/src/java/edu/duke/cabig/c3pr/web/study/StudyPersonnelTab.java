d8141cd96d745726c9bdc80e5a88b03fad9a0714
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-03-06 05:50:24 +0000

    updated for getting research staff roles for saved ones

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/study/StudyPersonnelTab.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/study/StudyPersonnelTab.java
@@ -1,13 +1,22 @@
 package edu.duke.cabig.c3pr.web.study;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.validation.Errors;
 
+
+import edu.duke.cabig.c3pr.domain.C3PRUserGroupType;
 import edu.duke.cabig.c3pr.domain.Study;
+import edu.duke.cabig.c3pr.domain.StudyOrganization;
 import edu.duke.cabig.c3pr.domain.validator.StudyValidator;
+import edu.duke.cabig.c3pr.exception.C3PRBaseException;
+import edu.duke.cabig.c3pr.service.PersonnelService;
+import edu.duke.cabig.c3pr.utils.StringUtils;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,18 +28,45 @@ import edu.duke.cabig.c3pr.domain.validator.StudyValidator;
 class StudyPersonnelTab extends StudyTab {
 	
 	private StudyValidator studyValidator;
+	private PersonnelService personnelService;
 
-    public StudyPersonnelTab() {
+    public void setPersonnelService(PersonnelService personnelService) {
+		this.personnelService = personnelService;
+	}
+
+	public StudyPersonnelTab() {
         this(false);
     }
 
     public StudyPersonnelTab(boolean editMode){
         super("Personnel", "Personnel", editMode?"study/study_personnel_edit":"study/study_personnel");
     }
+    
 
     @Override
-    public Map<String, Object> referenceData(Study study) {
+    public Map referenceData(HttpServletRequest request, Study study) {
         Map<String, Object> refdata = super.referenceData(study);    //To change body of overridden methods use File | Settings | File Templates.
+        for(int i=0; i< study.getStudyOrganizations().size(); i ++) {
+        	for (int j=0; j < study.getStudyOrganizations().get(i).getStudyPersonnel().size();j++){
+        		try {
+        			List<String> groupRoles = new ArrayList<String>();
+        			Iterator<C3PRUserGroupType> groupIterator =personnelService.getGroups(study.getStudyOrganizations().get(i).getStudyPersonnel().get(j).getResearchStaff()).iterator();
+        			C3PRUserGroupType userGroup;
+        			while(groupIterator.hasNext()){
+        				userGroup = groupIterator.next();
+        				groupRoles.add(userGroup.getDisplayName());
+        			}
+					refdata.put("studyOrganizations["+i+"].studyPersonnel["+j+"].roleData",groupRoles);
+				} catch (C3PRBaseException e) {
+					e.printStackTrace();
+				}
+        	}
+        }
+        
+        if((request.getParameter("_selectedSite")==null) || (request.getParameter("_selectedSite")!=null && StringUtils.isBlank(request.getParameter("_selectedSite").toString()))){
+        	refdata.put("selectedSite", 0);
+        }
+        
         addConfigMapToRefdata(refdata, "studyPersonnelRoleRefData");
         addConfigMapToRefdata(refdata, "studyPersonnelStatusRefData");
 
