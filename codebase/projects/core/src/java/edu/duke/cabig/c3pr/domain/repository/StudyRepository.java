7a72ee595d49ee2b82d1959735b3261cdff37855
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-04-11 16:24:29 +0000

    study service related refactoring changes

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/repository/StudyRepository.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/repository/StudyRepository.java
@@ -1,6 +1,10 @@
 package edu.duke.cabig.c3pr.domain.repository;
 
+import java.util.List;
+
+import edu.duke.cabig.c3pr.domain.OrganizationAssignedIdentifier;
 import edu.duke.cabig.c3pr.domain.Study;
+import edu.duke.cabig.c3pr.exception.C3PRCodedException;
 import edu.duke.cabig.c3pr.exception.StudyValidationException;
 
 public interface StudyRepository {
@@ -8,4 +12,6 @@ public interface StudyRepository {
 	public void buildAndSave(Study study) throws Exception;
 	
 	public void validate(Study study) throws StudyValidationException;
+	
+	public List<Study> searchByCoOrdinatingCenterId(OrganizationAssignedIdentifier identifier) throws C3PRCodedException;
 }
