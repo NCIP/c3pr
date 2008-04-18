ceb0a03e2628ad58bcdd1bd91ee0373f3bb92a9c
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-04-18 15:09:21 +0000

    Changes made for implementation of multiple races

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RaceCode.java
@@ -0,0 +1,41 @@
+package edu.duke.cabig.c3pr.domain;
+
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.getByClassAndCode;
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.register;
+import static gov.nih.nci.cabig.ctms.domain.EnumHelper.sentenceCasedName;
+import gov.nih.nci.cabig.ctms.domain.CodedEnum;
+
+public enum RaceCode implements CodedEnum<String> {
+	
+	Asian("Asian"), 
+	White("White"),
+	Black_or_African_American("Black or African American"),
+	American_Indian_or_Alaska_Native("American Indian or Alaska Native"),
+	Native_Hawaiin_or_Pacific_Islander("Native Hawaiin or Pacific Islander"),
+	Not_Reported("Not Reported"),
+	Unknown("Unknown");
+	
+	private String raceCode;
+
+    private RaceCode(String code) {
+        this.raceCode = code;
+        register(this);
+    }
+
+    public String getCode() {
+        return raceCode;
+    }
+
+    public String getDisplayName() {
+        return sentenceCasedName(this);
+    }
+
+    public String getName() {
+        return name();
+    }
+
+    public static RaceCode getByCode(String code) {
+        return getByClassAndCode(RaceCode.class, code);
+    }
+
+}
