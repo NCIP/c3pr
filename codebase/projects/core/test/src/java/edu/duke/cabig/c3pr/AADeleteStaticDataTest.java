639ff1c864d477d387cd30aa0eb758aea3f20ff2
kruttik Aggarwal <kruttikagarwal@gmail.com>
2008-04-18 22:29:29 +0000

    dummy test case to delete static data

---
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/AADeleteStaticDataTest.java
@@ -0,0 +1,10 @@
+package edu.duke.cabig.c3pr;
+
+import edu.duke.cabig.c3pr.utils.DaoTestCase;
+
+public class AADeleteStaticDataTest extends DaoTestCase{
+
+    public void testDelete(){
+        
+    }
+}
