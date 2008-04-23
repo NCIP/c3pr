d26929cd3dc20bccb76a52b60099210c805eafc4
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-04-23 14:14:21 +0000

    updated for XML import errors

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/StudyXMLImporterService.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/StudyXMLImporterService.java
@@ -1,5 +1,6 @@
 package edu.duke.cabig.c3pr.service;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.List;
 
@@ -8,15 +9,16 @@ import org.springframework.transaction.annotation.Transactional;
 import edu.duke.cabig.c3pr.domain.Study;
 import edu.duke.cabig.c3pr.exception.C3PRBaseException;
 import edu.duke.cabig.c3pr.exception.C3PRBaseRuntimeException;
+import edu.duke.cabig.c3pr.exception.C3PRCodedException;
 
 /**
  * User: kherm
  * 
  * @author kherm manav.kher@semanticbits.com
  */
-@Transactional(readOnly = false, rollbackFor = C3PRBaseException.class, noRollbackFor = C3PRBaseRuntimeException.class)
+@Transactional(readOnly = false, rollbackFor = C3PRCodedException.class, noRollbackFor = C3PRBaseRuntimeException.class)
 public interface StudyXMLImporterService {
-    List<Study> importStudies(InputStream xmlStream) throws C3PRBaseRuntimeException;
+    List<Study> importStudies(InputStream xmlStream,File importXMLResult) throws C3PRCodedException;
 
 //    public void importStudy(Study study) throws Exception;
 //    void validate(Study study) throws StudyValidationException;
