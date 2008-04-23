c5528c1948ac0eee58e03c5c2bc8f0c621a3ad98
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-04-23 14:12:46 +0000

    updated for XML import errors

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/impl/StudyXMLImporterServiceImpl.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/impl/StudyXMLImporterServiceImpl.java
@@ -1,5 +1,7 @@
 package edu.duke.cabig.c3pr.service.impl;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
@@ -13,7 +15,15 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.acegisecurity.AccessDeniedException;
 import org.apache.log4j.Logger;
-import org.w3c.dom.Document;
+import org.jdom.Comment;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.springframework.context.MessageSource;
+import org.springframework.transaction.annotation.Transactional;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -21,6 +31,8 @@ import edu.duke.cabig.c3pr.dao.StudyDao;
 import edu.duke.cabig.c3pr.domain.Study;
 import edu.duke.cabig.c3pr.domain.repository.StudyRepository;
 import edu.duke.cabig.c3pr.exception.C3PRBaseRuntimeException;
+import edu.duke.cabig.c3pr.exception.C3PRCodedException;
+import edu.duke.cabig.c3pr.exception.C3PRExceptionHelper;
 import edu.duke.cabig.c3pr.xml.XmlMarshaller;
 import gov.nih.nci.common.exception.XMLUtilityException;
 
@@ -39,6 +51,10 @@ public class StudyXMLImporterServiceImpl implements
     private StudyRepository studyRepository;
 
     private XmlMarshaller marshaller;
+    
+    private C3PRExceptionHelper exceptionHelper;
+
+    private MessageSource c3prErrorMessages;
 
     private Logger log = Logger.getLogger(StudyXMLImporterServiceImpl.class.getName());
 
@@ -51,62 +67,50 @@ public class StudyXMLImporterServiceImpl implements
      * @return
      * @throws Exception
      */
-    public List<Study> importStudies(InputStream xmlStream) throws C3PRBaseRuntimeException {
+    @Transactional
+    public List<Study> importStudies(InputStream xmlStream,File importXMLResult) throws C3PRCodedException {
         List<Study> studyList = null;
+        org.jdom.Document document = null;
         try {
             studyList = new ArrayList<Study>();
-            Document studyDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(
-                            xmlStream);
-            NodeList studies = studyDoc.getElementsByTagName("study");
+          
+            document = new SAXBuilder().build(xmlStream);
+            
+            List<Element> studies = document.getRootElement().getChildren("study",Namespace.getNamespace("gme://ccts.cabig/1.0/gov.nih.nci.cabig.ccts.domain"));
 
-            for (int i = 0; i < studies.getLength(); i++) {
-                Node studyNode = studies.item(i);
-                if (studyNode.getNodeType() == Node.ELEMENT_NODE) {
-                    StringWriter sw = new StringWriter();
-                    StreamResult sr = new StreamResult(sw);
-                    TransformerFactory.newInstance().newTransformer().transform(
-                                    new DOMSource(studyNode), sr);
-
+            for (int i = 0; i < studies.size(); i++) {
+            	Element studyNode = studies.get(i);
+            	
                     Study study = null;
                     try {
-                        study = (Study) marshaller.fromXML(new StringReader(sr.getWriter()
-                                        .toString()));
-                    }
-                    catch (XMLUtilityException e) {
-                        // ignore but log it. Cannot handle this in the UI
-                        log.error("Error marshalling Study during import");
-                    }
-
-                    try {
+                        study = (Study) marshaller.fromXML(new StringReader(new XMLOutputter().outputString(studyNode)));
                         studyRepository.validate(study);
 
                         log.debug("Saving study with grid ID" + study.getGridId());
 
                         studyRepository.buildAndSave(study);
-                        // once saved retreive persisted study
-                        studyList.add(studyDao.getByGridId(study.getGridId()));
+                        // once saved retrieve persisted study
+                        studyList.add(studyDao.getById(study.getId()));
+                        studyNode.addContent(new Comment("Successfull Import"));
                     }
-                    catch (AccessDeniedException e) {
-                        // if user cannot save a study then no point continuing
-                        throw e;
-                    }
+                  
                     catch (Exception e) {
                         // ignore any other problem and continue to import
-                        study.setImportErrorString(e.getMessage());
-                        studyList.add(study);
+                    	e.printStackTrace();                        
                         log.error(e.getMessage());
+                        studyNode.addContent(new Comment("Error while importing: " + e.getMessage()));
+                        
                     }
-                }
             }
+            new XMLOutputter(Format.getPrettyFormat()).output(document, new FileWriter(
+                    importXMLResult));
         }
         catch (Exception e) {
-            throw new C3PRBaseRuntimeException("Could not import study", e);
+        	 throw this.exceptionHelper.getException(
+                     getCode("C3PR.EXCEPTION.REGISTRATION.IMPORT.ERROR_UNMARSHALLING"), e);
         }
         return studyList;
     }
-
-
-
 
     // setters for spring
 
@@ -126,8 +130,19 @@ public class StudyXMLImporterServiceImpl implements
 		return studyRepository;
 	}
 
-
 	public void setStudyRepository(StudyRepository studyRepository) {
 		this.studyRepository = studyRepository;
 	}
+
+	public void setExceptionHelper(C3PRExceptionHelper exceptionHelper) {
+		this.exceptionHelper = exceptionHelper;
+	}
+
+	public void setC3prErrorMessages(MessageSource errorMessages) {
+		c3prErrorMessages = errorMessages;
+	}
+	
+	private int getCode(String errortypeString) {
+        return Integer.parseInt(this.c3prErrorMessages.getMessage(errortypeString, null, null));
+    }
 }
