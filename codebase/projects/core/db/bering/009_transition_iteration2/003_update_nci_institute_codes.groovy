957b33727ca2204ecff0904a6ae4be59de9939ca
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-05-01 22:49:28 +0000

    corrected spelling

--- codebase/projects/core/db/bering/009_transition_iteration2/003_update_nci_institute_codes.groovy
+++ codebase/projects/core/db/bering/009_transition_iteration2/003_update_nci_institute_codes.groovy
@@ -10,10 +10,10 @@ class UpdateNciInstituteCodes extends edu.northwestern.bioinformatics.bering.Mig
 			execute("update csm_protection_element set protection_element_name='edu.duke.cabig.c3pr.domain.HealthcareSite.WAKE',object_id='edu.duke.cabig.c3pr.domain.HealthcareSite.WAKE' where protection_element_name='edu.duke.cabig.c3pr.domain.HealthcareSite.Wake'");
 			execute("update csm_protection_group set  protection_group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.WAKE' where protection_group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.Wake'");
 			
-			execute("update organizations set nci_institute_code='unkown or does not exist' where nci_institute_code='Warren'");
-			execute("update csm_group set group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.unkown or does not exist' where group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.Warren'");
-			execute("update csm_protection_element set protection_element_name='edu.duke.cabig.c3pr.domain.HealthcareSite.unkown or does not exist',object_id='edu.duke.cabig.c3pr.domain.HealthcareSite.unkown or does not exist' where protection_element_name='edu.duke.cabig.c3pr.domain.HealthcareSite.Warren'");
-			execute("update csm_protection_group set  protection_group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.unkown or does not exist' where protection_group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.Warren'");
+			execute("update organizations set nci_institute_code='unknown or does not exist' where nci_institute_code='Warren'");
+			execute("update csm_group set group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.unknown or does not exist' where group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.Warren'");
+			execute("update csm_protection_element set protection_element_name='edu.duke.cabig.c3pr.domain.HealthcareSite.unknown or does not exist',object_id='edu.duke.cabig.c3pr.domain.HealthcareSite.unknown or does not exist' where protection_element_name='edu.duke.cabig.c3pr.domain.HealthcareSite.Warren'");
+			execute("update csm_protection_group set  protection_group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.unknown or does not exist' where protection_group_name='edu.duke.cabig.c3pr.domain.HealthcareSite.Warren'");
 	}
 
 	void down(){
