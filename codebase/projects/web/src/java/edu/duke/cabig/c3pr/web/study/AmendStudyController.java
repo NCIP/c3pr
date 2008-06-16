cc9793594b503b23cd54f098252ee969b8964263
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2008-06-16 20:42:34 +0000

    Added "isAdmin" to refData to enable softDeletes and editable screens for admin login. Changed the Row manager onLoadInitialize() to check for isAdmin before disabling the screen.

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/study/AmendStudyController.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/study/AmendStudyController.java
@@ -48,13 +48,13 @@ public class AmendStudyController extends StudyController<Study> {
         SecurityContext context = SecurityContextHolder.getContext();
         Authentication auth = context.getAuthentication();
         GrantedAuthority[] groups = auth.getAuthorities();
-        boolean isAdmin = false;
+        String isAdmin = "false";
         for (GrantedAuthority ga : groups) {
             if (ga.getAuthority().endsWith("admin")) {
-                isAdmin = true;
+                isAdmin = "true";
             }
         }
-
+        request.setAttribute("isAdmin", isAdmin);
         return super.referenceData(request, arg1);
     }
 
