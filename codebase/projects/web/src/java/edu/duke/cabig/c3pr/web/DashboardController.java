93484efd9b12b64e7c48aeefc02362562572be1d
Ion Olaru <ion.olaru@gmail.com>
2008-06-19 20:48:43 +0000

    CPR-60

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/DashboardController.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/DashboardController.java
@@ -2,11 +2,23 @@ package edu.duke.cabig.c3pr.web;
 
 import org.springframework.web.servlet.mvc.ParameterizableViewController;
 import org.springframework.web.servlet.ModelAndView;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.Authentication;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.duke.cabig.c3pr.tools.Configuration;
+import edu.duke.cabig.c3pr.utils.web.PropertyWrapper;
+import edu.duke.cabig.c3pr.domain.repository.CSMUserRepository;
+
+import java.util.*;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * User: ion
@@ -15,16 +27,55 @@ import edu.duke.cabig.c3pr.tools.Configuration;
  */
 
 public class DashboardController extends ParameterizableViewController {
-//    private Configuration configuration;
+
+    protected static final Log log = LogFactory.getLog(DashboardController.class);
+    private Configuration configuration;
+    private String filename;
+    
+    CSMUserRepository csmUserRepository;
+
+    public void setCsmUserRepository(CSMUserRepository csmUserRepository) {
+        this.csmUserRepository = csmUserRepository;
+    }
 
     protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-//        httpServletRequest.setAttribute("skinName", configuration.getMap().get("skinPath").toString());
+        httpServletRequest.setAttribute("skinName", configuration.getMap().get("skinPath").toString());
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication auth = context.getAuthentication();
+        GrantedAuthority[] groups = auth.getAuthorities();
+
+        filename = "default.links.properties";
+
+        for (GrantedAuthority ga : groups) {
+            System.out.println("ga: " + ga.getAuthority());
+            if (DashboardController.class.getClassLoader().getResource(ga.getAuthority() + ".links.properties") != null) {
+                filename = ga.getAuthority() + ".links.properties";
+                log.debug("Found rolebased links file: " + filename);
+                break;
+            }
+        }
+
+//        filename = "default.links.property";
+        Properties p = new Properties();
+        System.out.println("Filename: " + filename);                         
+        try {
+            p.load(DashboardController.class.getClassLoader().getResourceAsStream(filename));
+            log.debug("The links file has " + p.keySet().size() + " elements.");
+            httpServletRequest.setAttribute("links", new PropertyWrapper(p));
+        } catch (IOException e) {
+            log.error("Error while trying to read the property file: [" + filename + "]");
+        }
+
+/*
+        List l = new ArrayList(p.keySet());
+        Collections.sort(l);
+*/
+
         return super.handleRequestInternal(httpServletRequest, httpServletResponse);
     }
 
-/*
     public void setConfiguration(Configuration configuration) {
         this.configuration = configuration;
     }
-*/
 }
--- codebase/projects/web/web/WEB-INF/views/dashboard.jsp
+++ codebase/projects/web/web/WEB-INF/views/dashboard.jsp
@@ -1,12 +1,60 @@
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="chrome" tagdir="/WEB-INF/tags/chrome" %>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
 <%--
-  Created by IntelliJ IDEA.
   User: ion
   Date: Jun 11, 2008
   Time: 10:54:45 AM
-  To change this template use File | Settings | File Templates.
 --%>
+
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head><title>Simple jsp page</title></head>
-  <body>Place your content here</body>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<div id="main">
+<table width="100%" border="0">
+<tr>
+    <td valign="top" width="33%" style="background:url(../../images/chrome/li_item.jpg);">
+        <chrome:box title="Frequently Used Shortcuts" autopad="true">
+
+                <table width=100% cellspacing="0" cellpadding="0" border="0">
+                <tr><td>
+                    <c:forEach var="k" items="${links.keys}" varStatus="status">
+                        <img src="<c:url value="/images/chrome/li_item.jpg" />">&nbsp;&nbsp;<a href="<c:url value="/${k}" />">${links.p[k]}</a><br/>
+                        <c:if test="${status.count == fn:length(links.keys) / 2}"><td></c:if>
+                    </c:forEach>
+                </tr>
+                </table>
+            
+        </chrome:box>
+        <chrome:box title="C3PR Notifications" autopad="true">
+            _CONTENT_
+        </chrome:box>
+        <chrome:box title="C3PR Development Notes" autopad="true">
+            _CONTENT_
+        </chrome:box>
+    </td>
+    <td valign="top">
+        <chrome:box title="Incomplete Registrations - Most Recent" autopad="true">
+            _CONTENT_
+        </chrome:box>
+        <chrome:box title="Pending Studies - Most Recent" autopad="true">
+            _CONTENT_
+        </chrome:box>
+        <chrome:box title="Most Active Studies" autopad="true">
+            _CONTENT_
+        </chrome:box>
+    </td>
+</tr>
+</table>
+</div>
+
+</body>
 </html>
