614a1840f0c19f64361bd683a02b805395ded943
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-07-11 21:04:24 +0000

    C3PR-21

---
+++ codebase/projects/core/db/bering/011_elobration_iteration1/016_rename_epochs_type_and_drop_scheduled_epochs_dtype.groovy
@@ -0,0 +1,12 @@
+class RenameEpochsTypeAndDropScheduledEpochsDtype extends edu.northwestern.bioinformatics.bering.Migration {
+	void up() {
+		renameColumn('epochs', 'type', 'display_role');
+		dropColumn('scheduled_epochs','TYPE');
+	}
+
+	void down(){
+	renameColumn('epochs', 'type', 'display_role');
+	addColumn('scheduled_epochs','TYPE',"string");
+
+	}
+}
