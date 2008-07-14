1446c38c00f42b7694ad8c8dfe41dbbb0704223c
Himanshu Gupta <himanshu.iit@gmail.com>
2008-07-14 19:59:23 +0000

    CPR-37 Companion Protocol

---
+++ codebase/projects/core/db/bering/011_elobration_iteration1/018_companion_protocol.groovy
@@ -0,0 +1,32 @@
+class companionProtocol extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+    	addColumn("studies", "standalone_indicator", "boolean", defaultValue : true)
+    	addColumn("studies", "companion_indicator", "boolean", defaultValue : false)
+    	addColumn("study_subjects", "comp_assoc_id", "integer", nullable : true)
+    	addColumn("study_subjects", "parent_stu_sub_id", "integer", nullable : true)
+    	
+    	createTable("comp_stu_associations") { t ->
+            t.addColumn("parent_study_id", "integer", nullable: false)
+            t.addColumn("companion_study_id", "integer", nullable: false)
+            t.addColumn("mandatory_indicator", "boolean")
+            t.addColumn("version", "integer", defaultValue: 0, nullable: false)
+			t.addColumn("grid_id", "string")
+			t.addColumn("retired_indicator", 'string' );
+        }
+		
+		execute("ALTER TABLE STUDY_SUBJECTS ADD CONSTRAINT FK_STU_SUB_PARENT FOREIGN KEY (parent_stu_sub_id) REFERENCES STUDY_SUBJECTS (ID)");
+		execute("ALTER TABLE STUDY_SUBJECTS ADD CONSTRAINT FK_STU_SUB_COMP_ASSOC FOREIGN KEY (comp_assoc_id) REFERENCES comp_stu_associations (ID)");
+		execute("ALTER TABLE comp_stu_associations ADD CONSTRAINT FK_STU_PARENT FOREIGN KEY (parent_study_id) REFERENCES STUDIES (ID)");
+		execute("ALTER TABLE comp_stu_associations ADD CONSTRAINT FK_STU_COMPA FOREIGN KEY (companion_study_id) REFERENCES STUDIES (ID)");
+    }
+  
+    void down() {
+    	dropColumn("studies", "companion_indicator")
+    	dropColumn("studies", "standalone_indicator")
+    	dropTable("comp_stu_associations")
+    	dropColumn("study_subjects", "comp_assoc_id")
+    	dropColumn("study_subjects", "parent_stu_sub_id")
+    	execute(" ALTER TABLE STUDY_SUBJECTS drop CONSTRAINT FK_STU_SUB_PARENT")
+    	execute(" ALTER TABLE STUDY_SUBJECTS drop CONSTRAINT FK_STU_SUB_COMP_ASSOC")
+    }
+}
