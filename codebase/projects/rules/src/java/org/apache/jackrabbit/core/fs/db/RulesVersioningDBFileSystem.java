46082ccdc43ee96e691f5c172a3e83894df0e689
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2008-07-22 21:14:44 +0000

    externalized datasource.props values

--- codebase/projects/rules/src/java/org/apache/jackrabbit/core/fs/db/RulesVersioningDBFileSystem.java
+++ codebase/projects/rules/src/java/org/apache/jackrabbit/core/fs/db/RulesVersioningDBFileSystem.java
@@ -14,9 +14,9 @@ public class RulesVersioningDBFileSystem extends DbFileSystem {
     public RulesVersioningDBFileSystem() {
 
 
-       /* Properties props = new Properties();
+        Properties props = new Properties();
         try{
-        	props.load(this.getClass().getClassLoader().getResourceAsStream("datasource.properties"));
+        	props.load(this.getClass().getClassLoader().getResourceAsStream("context/datasource.properties"));
         }catch(IOException ioe){
         	logger.error(ioe.getMessage());
         }
@@ -24,16 +24,17 @@ public class RulesVersioningDBFileSystem extends DbFileSystem {
         this.driver = props.getProperty("datasource.driver");
         this.password = props.getProperty("datasource.password");
         this.user = props.getProperty("datasource.username");
-        this.schema = props.getProperty("datasource.schema");
-        this.schemaObjectPrefix = "rep_ver_";
-        this.url = props.getProperty("datasource.url");*/
-        
-        this.driver = "org.postgresql.Driver";
-        this.password = "postgres";
-        this.user = "postgres";
         this.schema = "postgresql";
         this.schemaObjectPrefix = "rep_ver_";
-        this.url = "jdbc:postgresql://localhost:5432/c3pr_svn";
+        this.url = props.getProperty("datasource.url");
+        
+    	/*this.driver = driverClassName; //"org.postgresql.Driver";
+        this.password = password;//"postgres";
+        this.user = username;//"postgres";
+        this.schema = "postgresql";
+        this.schemaObjectPrefix = "rep_ver_";
+        this.url = url;//"jdbc:postgresql://localhost:5432/c3pr_svn";
+*/
     }
 
 }
