48f95d53a0576173e6153356b181ed7923453e15
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-07-25 15:14:38 +0000

    C3PR-21:added stratification indicator, treatment indicator and data migration scripts

---
+++ codebase/projects/core/db/bering/012_elobration_iteration4/003_add_stratification_indicator_to_epoch.groovy
@@ -0,0 +1,14 @@
+class addStratificationIndicatorToEpoch extends edu.northwestern.bioinformatics.bering.Migration {
+    	void up() {
+        addColumn('epochs','stratification_indicator','boolean');
+       	if (databaseMatches('postgresql')){
+        	execute("update epochs set stratification_indicator='false'");
+        	execute("update epochs set stratification_indicator='true' from epochs ep inner join studies on ep.stu_id=studies.id where studies.stratification_indicator ='true' and (epochs.display_role='TREATMENT'or epochs.display_role='Treatment'or epochs.display_role='Generic' )");
+        }
+    }
+
+    void down() {
+       dropColumn('epochs','stratification_indicator');
+    }
+}
+
---
+++ codebase/projects/core/db/bering/012_elobration_iteration4/004_convert_epoch_display_role_to_treatment_indicator.groovy
@@ -0,0 +1,20 @@
+class convertEpochDisplayRoleToTreatmentIndicator extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+       addColumn('epochs','treatment_indicator','boolean');
+       if (databaseMatches('postgresql')){
+	       execute("update epochs set treatment_indicator='true' where display_role='Treatment' or display_role='TREATMENT' or display_role='Generic'");
+	       execute("update epochs set treatment_indicator='false' where display_role='Non-Treatment' or display_role='NonTreatment'");
+       }
+        dropColumn('epochs','display_role');
+    }
+
+    void down() {
+       addColumn('epochs','display_role','string');
+       if (databaseMatches('postgresql')){
+	       execute("update epochs set display_role='TREATMENT' where treatment_indicator='true'");
+	       execute("update epochs set display_role='NON-TREATMENT' where treatment_indicator='false'");
+       }
+       dropColumn('epochs','treatment_indicator');
+       
+    }
+}
