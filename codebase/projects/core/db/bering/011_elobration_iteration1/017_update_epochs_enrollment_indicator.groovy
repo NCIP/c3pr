1ec4e51d1aea7e794720d508cb4d38e3550c601d
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-07-25 15:25:35 +0000

    C3PR-21:updated

--- codebase/projects/core/db/bering/011_elobration_iteration1/017_update_epochs_enrollment_indicator.groovy
+++ codebase/projects/core/db/bering/011_elobration_iteration1/017_update_epochs_enrollment_indicator.groovy
@@ -1,6 +1,6 @@
 class UpdateEpochsEnrollmentIndicator extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-		execute("update epochs set enrollment_indicator='true' where display_role='Treatment'");
+		execute("update epochs set enrollment_indicator='true' where display_role='Treatment'or display_role='TREATMENT' ");
 	}
 
 	void down(){
