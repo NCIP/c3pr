fd40865d930580e13f2fe225d859250de67278dd
kruttik Aggarwal <kruttikagarwal@gmail.com>
2008-07-30 18:50:45 +0000

    updated

--- codebase/projects/core/db/bering/011_elobration_iteration1/014_sitename_config.groovy
+++ codebase/projects/core/db/bering/011_elobration_iteration1/014_sitename_config.groovy
@@ -1,6 +1,6 @@
 class AddMultisiteConfigDefaultProperty extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-		insert('configuration', [ key:"siteName", value:"<SITE NAME>" ], primaryKey: false)
+		insert('configuration', [ key:"siteName", value:"<SITE BANNER URL>" ], primaryKey: false)
 	}
 
 	void down(){
