e58b4e0a8a0e098d31977f5b1d175f9bcbf6df44
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2008-07-31 00:20:20 +0000

    changed due to notifications

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/Participant.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/Participant.java
@@ -2,7 +2,6 @@ package edu.duke.cabig.c3pr.domain;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.StringTokenizer;
