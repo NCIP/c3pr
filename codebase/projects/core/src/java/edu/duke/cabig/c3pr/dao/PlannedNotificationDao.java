cd91b4965f344162555ff698ca41609fff1b974d
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2008-08-05 01:50:25 +0000

    changed due to notifications

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/PlannedNotificationDao.java
@@ -0,0 +1,28 @@
+package edu.duke.cabig.c3pr.dao;
+
+import java.util.List;
+
+import edu.duke.cabig.c3pr.domain.PlannedNotification;
+
+/**
+ * Hibernate implementation of ArmDao
+ * 
+ * @see edu.duke.cabig.c3pr.dao.PlannedNotificationDao
+ * @author Priyatam
+ */
+public class PlannedNotificationDao extends GridIdentifiableDao<PlannedNotification> {
+
+    @Override
+    public Class<PlannedNotification> domainClass() {
+        return PlannedNotification.class;
+    }
+
+    /*
+     * Returns all Arm objects (non-Javadoc)
+     * 
+     * @see edu.duke.cabig.c3pr.dao.Arm#getAll()
+     */
+    public List<PlannedNotification> getAll() {
+        return getHibernateTemplate().find("from PlannedNotification");
+    }
+}
