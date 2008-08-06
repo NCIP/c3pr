5e8a9a57c20b2c58bc1fc44a5b3b6ac57b2e36ce
Himanshu Gupta <himanshu.iit@gmail.com>
2008-08-06 19:32:21 +0000

    CPR-37 Companion Protocol - Study Flow - Access and Authorization code

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/study/ConfirmStudyController.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/study/ConfirmStudyController.java
@@ -1,33 +1,54 @@
 package edu.duke.cabig.c3pr.web.study;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.acegisecurity.Authentication;
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.BaseCommandController;
 
 import edu.duke.cabig.c3pr.dao.StudyDao;
 import edu.duke.cabig.c3pr.domain.Study;
+import edu.duke.cabig.c3pr.utils.web.navigation.Task;
 
 public class ConfirmStudyController extends BaseCommandController {
-	
+
 	protected StudyDao studyDao;
+	private edu.duke.cabig.c3pr.utils.web.navigation.Task editTask;
 
-    public ConfirmStudyController() {
-    }
-    
+	public ConfirmStudyController() {
+	}
 
-    public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    	Study study = studyDao.getById(Integer.parseInt(request.getParameter("studyId")));
-    	HashMap<String, Study> map = new HashMap<String, Study>() ;
-        if (study != null) {
-        	map.put("command", study);
-        }
-        ModelAndView mav = new ModelAndView("study/study_confirmation", map);
-        return mav;
-    }
+
+	public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication auth = context.getAuthentication();
+		GrantedAuthority[] groups = auth.getAuthorities();
+		boolean isRegistrarOnly = true;
+		for (GrantedAuthority ga : groups) {
+			if (ga.getAuthority().endsWith("admin") || ga.getAuthority().endsWith("ordinator")) {
+				isRegistrarOnly = false;
+			}
+		}
+		request.setAttribute("isRegistrar", isRegistrarOnly);
+		request.setAttribute("editAuthorizationTask", editTask);
+		
+		Study study = studyDao.getById(Integer.parseInt(request.getParameter("studyId")));
+		HashMap<String, Study> map = new HashMap<String, Study>() ;
+		if (study != null) {
+			map.put("command", study);
+		}
+		ModelAndView mav = new ModelAndView("study/study_confirmation", map);
+		return mav;
+	}
 
 
 	public StudyDao getStudyDao() {
@@ -38,4 +59,13 @@ public class ConfirmStudyController extends BaseCommandController {
 	public void setStudyDao(StudyDao studyDao) {
 		this.studyDao = studyDao;
 	}
+
+	public Task getEditTask() {
+		return editTask;
+	}
+
+	public void setEditTask(Task editTask) {
+		this.editTask = editTask;
+	}
+
 }
