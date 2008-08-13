c60cf95adbc9ab845b0b9ec474337a7b252d59c5
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-08-13 22:14:04 +0000

    updated for Oracle

--- codebase/projects/core/db/bering/011_elobration_iteration1/017_update_epochs_enrollment_indicator.groovy
+++ codebase/projects/core/db/bering/011_elobration_iteration1/017_update_epochs_enrollment_indicator.groovy
@@ -1,6 +1,12 @@
 class UpdateEpochsEnrollmentIndicator extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
+	if (databaseMatches('postgresql')){
 		execute("update epochs set enrollment_indicator='true' where display_role='Treatment'or display_role='TREATMENT' ");
+		}
+		
+	if (databaseMatches('oracle')){
+		execute("update epochs set enrollment_indicator='yes' where display_role='Treatment'or display_role='TREATMENT' ");
+		}
 	}
 
 	void down(){
