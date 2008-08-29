fa8e20f807de3612446f819d101633640216177a
Himanshu Gupta <himanshu.iit@gmail.com>
2008-08-29 15:36:57 +0000

    groovy

--- codebase/projects/core/db/bering/013_elobration_iteration5/004_default_companion_study_column.groovy
+++ codebase/projects/core/db/bering/013_elobration_iteration5/004_default_companion_study_column.groovy
@@ -2,17 +2,17 @@ class defaultCompanionStudyColumns extends edu.northwestern.bioinformatics.berin
     void up() {
         if (databaseMatches('oracle')) {
         	execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT 1")
-    		execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT 0")
+    		execute("ALTER TABLE STUDIES ALTER COLUMN companion_indicator SET DEFAULT 0")
     		execute("update studies set standalone_indicator='1' where standalone_indicator IS NULL ")
         	execute("update studies set companion_indicator='0' where companion_indicator IS NULL ")
         } else if (databaseMatches('postgresql')){
         	execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT true")
-    		execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT false")
+    		execute("ALTER TABLE STUDIES ALTER COLUMN companion_indicator SET DEFAULT false")
     		execute("update studies set standalone_indicator='true' where standalone_indicator IS NULL ")
         	execute("update studies set companion_indicator='false' where companion_indicator IS NULL ")
         } else {
         	execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT true")
-    		execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT false")
+    		execute("ALTER TABLE STUDIES ALTER COLUMN companion_indicator SET DEFAULT false")
     		execute("update studies set standalone_indicator='true' where standalone_indicator IS NULL ")
         	execute("update studies set companion_indicator='false' where companion_indicator IS NULL ")
         }
