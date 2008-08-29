f782c1699589d0a6066ab1c613e7e350f961c9c6
kruttik Aggarwal <kruttikagarwal@gmail.com>
2008-08-29 13:43:40 +0000

    temporarily checked to run ccts build

--- codebase/projects/core/db/bering/012_elobration_iteration4/008_add_window_name_config_default_property.groovy
+++ codebase/projects/core/db/bering/012_elobration_iteration4/008_add_window_name_config_default_property.groovy
@@ -1,8 +1,8 @@
 class AddMultisiteConfigDefaultProperty extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-		insert('configuration', [ key:"psc_window_name", value:"_psc" ], primaryKey: false)
-		insert('configuration', [ key:"caaers_window_name", value:"_caaers" ], primaryKey: false)
-		insert('configuration', [ key:"c3d_window_name", value:"_c3d" ], primaryKey: false)
+		//insert('configuration', [ key:"psc_window_name", value:"_psc" ], primaryKey: false)
+		//insert('configuration', [ key:"caaers_window_name", value:"_caaers" ], primaryKey: false)
+		//insert('configuration', [ key:"c3d_window_name", value:"_c3d" ], primaryKey: false)
 	}
 
 	void down(){
