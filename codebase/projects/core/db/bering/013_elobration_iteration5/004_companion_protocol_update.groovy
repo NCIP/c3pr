83a81ae6395780942a777b9401fb705b33799186
kruttik Aggarwal <kruttikagarwal@gmail.com>
2008-08-29 14:29:39 +0000

    temporarily checked to run ccts build

---
+++ codebase/projects/core/db/bering/013_elobration_iteration5/004_companion_protocol_update.groovy
@@ -0,0 +1,17 @@
+class companionProtocolUpdate extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+    	dropColumn("study_subjects", "comp_assoc_id")
+    	dropColumn("study_subjects", "parent_stu_sub_id")
+    	addColumn("study_subjects", "comp_assoc_id", "integer")
+    	addColumn("study_subjects", "parent_stu_sub_id", "integer")
+    	
+    	
+    }
+  
+    void down() {
+    	dropColumn("study_subjects", "comp_assoc_id")
+    	dropColumn("study_subjects", "parent_stu_sub_id")
+    	addColumn("study_subjects", "comp_assoc_id", "integer", nullbale : true)
+    	addColumn("study_subjects", "parent_stu_sub_id", "integer" , nullbale : true)
+    }
+}
