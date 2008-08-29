d9ed599ac96037021ea90e6eedb1ac82ce092af2
Himanshu Gupta <himanshu.iit@gmail.com>
2008-08-29 15:35:59 +0000

    Incomplete - task 1: New Task

--- codebase/projects/core/db/bering/013_elobration_iteration5/004_default_companion_study_column.groovy
+++ codebase/projects/core/db/bering/013_elobration_iteration5/004_default_companion_study_column.groovy
@@ -1,14 +1,20 @@
 class defaultCompanionStudyColumns extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         if (databaseMatches('oracle')) {
-        	execute("alter table STUDIES modify standalone_indicator default 1")
-    		execute("alter table STUDIES modify standalone_indicator default 0")
+        	execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT 1")
+    		execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT 0")
+    		execute("update studies set standalone_indicator='1' where standalone_indicator IS NULL ")
+        	execute("update studies set companion_indicator='0' where companion_indicator IS NULL ")
         } else if (databaseMatches('postgresql')){
-        	execute("alter table STUDIES modify standalone_indicator default 'true'")
-    		execute("alter table STUDIES modify standalone_indicator default 'false'")
+        	execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT true")
+    		execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT false")
+    		execute("update studies set standalone_indicator='true' where standalone_indicator IS NULL ")
+        	execute("update studies set companion_indicator='false' where companion_indicator IS NULL ")
         } else {
-        	execute("alter table STUDIES modify standalone_indicator default 'true'")
-    		execute("alter table STUDIES modify standalone_indicator default 'false'")
+        	execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT true")
+    		execute("ALTER TABLE STUDIES ALTER COLUMN standalone_indicator SET DEFAULT false")
+    		execute("update studies set standalone_indicator='true' where standalone_indicator IS NULL ")
+        	execute("update studies set companion_indicator='false' where companion_indicator IS NULL ")
         }
     }
     
