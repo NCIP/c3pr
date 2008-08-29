0fdce3d861543e4e3d7bc0f7f97ff4bc5226c083
Himanshu Gupta <himanshu.iit@gmail.com>
2008-08-29 15:27:06 +0000

    groovy

--- codebase/projects/core/db/bering/013_elobration_iteration5/004_default_companion_study_column.groovy
+++ codebase/projects/core/db/bering/013_elobration_iteration5/004_default_companion_study_column.groovy
@@ -1,28 +1,19 @@
 class defaultCompanionStudyColumns extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         if (databaseMatches('oracle')) {
-        	execute("update studies set standalone_indicator='1' where standalone_indicator = 'null'" )
-        	execute("update studies set companion_indicator='0' where companion_indicator = 'null' ")
+        	execute("alter table STUDIES modify standalone_indicator default 1")
+    		execute("alter table STUDIES modify standalone_indicator default 0")
         } else if (databaseMatches('postgresql')){
-        	execute("update studies set standalone_indicator='true' where standalone_indicator = 'null'" )
-        	execute("update studies set companion_indicator='false' where companion_indicator = 'null' ")
+        	execute("alter table STUDIES modify standalone_indicator default 'true'")
+    		execute("alter table STUDIES modify standalone_indicator default 'false'")
         } else {
-        	execute("update studies set standalone_indicator='true' where standalone_indicator = 'null' ")
-        	execute("update studies set companion_indicator='false' where companion_indicator = 'null' ")
+        	execute("alter table STUDIES modify standalone_indicator default 'true'")
+    		execute("alter table STUDIES modify standalone_indicator default 'false'")
         }
     }
     
     void down() {
-        if (databaseMatches('oracle')) {
-        	execute("update studies set standalone_indicator= null where standalone_indicator = '1' ")
-        	execute("update studies set companion_indicator= null where companion_indicator = '0' ")
-        } else if (databaseMatches('postgresql')){
-        	execute("update studies set standalone_indicator=null  where standalone_indicator = 'true' ")
-        	execute("update studies set companion_indicator=null  where companion_indicator = 'false' ")
-        } else {
-        	execute("update studies set standalone_indicator=null  where standalone_indicator = 'true' ")
-        	execute("update studies set companion_indicator=null  where companion_indicator = 'false' ")
-        }
+       
     }
 }
 
