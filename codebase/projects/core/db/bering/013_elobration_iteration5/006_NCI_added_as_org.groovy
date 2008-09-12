05dfb6f72554541cdfa11db368c9b5ddf9ac5643
Himanshu Gupta <himanshu.iit@gmail.com>
2008-09-12 17:37:27 +0000

    ui changes

--- codebase/projects/core/db/bering/013_elobration_iteration5/006_NCI_added_as_org.groovy
+++ codebase/projects/core/db/bering/013_elobration_iteration5/006_NCI_added_as_org.groovy
@@ -1,9 +1,11 @@
 class LocalNCIInstituteCode extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
+		insert('addresses', [ id: 16999, city: "Rockville", state_code: "MD", country_code: "USA",version:0], primaryKey: false)
 		insert('organizations', [ id: 16999, nci_institute_code: "NCI", name: "National Cancer Institute" ,address_id: 16999,version:0], primaryKey: false)
 	}
 
 	void down(){
         	execute("delete from organizations where id IN (16999)");
+        	execute("delete from addresses where id IN (16999)");
 	}
 }
