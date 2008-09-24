fdd0c95e778d12c262832e761bdd965fd950f363
kruttik Aggarwal <kruttikagarwal@gmail.com>
2008-09-24 15:54:19 +0000

    added password encryter test

---
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/security/UserPasswordTestCase.java
@@ -0,0 +1,25 @@
+package edu.duke.cabig.c3pr.security;
+
+import org.apache.fop.fo.properties.SrcMaker;
+
+import gov.nih.nci.security.util.StringEncrypter;
+import junit.framework.TestCase;
+
+public class UserPasswordTestCase extends TestCase {
+    
+    public void testEncryptPassword() throws Exception{
+        String password="Ccts123!";
+        String encryptPassword=new StringEncrypter().encrypt(password);
+        assertEquals("Wrong encrypted value", "56nit3oKigG8j2uyHEABIQ==", encryptPassword);
+    }
+    
+    public void testDecryptPassword() throws Exception{
+        String encryptPassword="56nit3oKigG8j2uyHEABIQ==!";
+        String password=new StringEncrypter().decrypt(encryptPassword);
+        assertEquals("Wrong encrypted value", "Ccts123!", password);
+    }
+    
+    public void testPassword() throws Exception{
+        System.out.println(new StringEncrypter().encrypt("p@ssw0rd"));
+    }
+}
