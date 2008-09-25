f54d3bde5515f906efe0fbca2891bfaa459c32a4
Shilpa Alluru <shilpaalluru@yahoo.com>
2008-09-25 14:37:06 +0000

    Fixed drop command for pg

--- codebase/projects/core/db/bering/013_elobration_iteration5/014_companion_notification_oracle_fix.groovy
+++ codebase/projects/core/db/bering/013_elobration_iteration5/014_companion_notification_oracle_fix.groovy
@@ -1,9 +1,11 @@
 class CompanionNotificationOracleFix extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-		
-		ALTER TABLE prt_org_associations DROP column id;
+		if(databaseMatches('postgres')){
+			ALTER TABLE prt_org_associations DROP id;
+		}
 		
 		if (databaseMatches('oracle')) {
+			ALTER TABLE prt_org_associations DROP column id;
 	   		execute("rename SEQ_COMP_STU_ASSOCIATIONS_ID to COMP_STU_ASSOCIATIONS_ID_SEQ");
 	   		execute("rename SEQ_STU_SUB_ASSOCIATIONS_ID to STU_SUB_ASSOCIATIONS_ID_SEQ");
 	   		execute("rename SEQ_SCHLD_NOTFNS_ID to SCHLD_NOTFNS_ID_SEQ");
