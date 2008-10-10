cdb1ca4e9af5a0091aec1757ca7471269e7da920
Himanshu Gupta <himanshu.iit@gmail.com>
2008-10-10 02:57:28 +0000

    command object change

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/study/StudyWrapper.java
@@ -0,0 +1,17 @@
+package edu.duke.cabig.c3pr.web.study;
+
+import edu.duke.cabig.c3pr.domain.Study;
+
+public class StudyWrapper {
+	
+	private Study study ;
+
+	public Study getStudy() {
+		return study;
+	}
+
+	public void setStudy(Study study) {
+		this.study = study;
+	}
+
+}
