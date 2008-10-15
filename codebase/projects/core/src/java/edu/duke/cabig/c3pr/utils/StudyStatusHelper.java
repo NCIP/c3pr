54bbeb6d17c92877370f90e51f6fd6e93810061b
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-10-15 17:45:41 +0000

    cpr-309 adding helper file to set study status

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/StudyStatusHelper.java
@@ -0,0 +1,33 @@
+package edu.duke.cabig.c3pr.utils;
+
+import edu.duke.cabig.c3pr.domain.CoordinatingCenterStudyStatus;
+import edu.duke.cabig.c3pr.domain.Study;
+import edu.duke.cabig.c3pr.exception.C3PRCodedException;
+
+public class StudyStatusHelper {
+	
+	public void setStatus(Study study,CoordinatingCenterStudyStatus targetStatus) throws C3PRCodedException{
+		
+		// TODO Should be using Switch Statement to do this
+		
+		if (targetStatus.equals(CoordinatingCenterStudyStatus.ACTIVE)){
+			study.open();
+		} else if (targetStatus.equals(CoordinatingCenterStudyStatus.OPEN)){
+			study.open();
+		} else if (targetStatus.equals(CoordinatingCenterStudyStatus.PENDING)){
+			study.putInPending();
+		} else if (targetStatus.equals(CoordinatingCenterStudyStatus.AMENDMENT_PENDING)){
+			study.putInAmendmentPending();
+		} else if (targetStatus.equals(CoordinatingCenterStudyStatus.TEMPORARILY_CLOSED_TO_ACCRUAL)){
+			study.temporarilyCloseToAccrual();
+		} else if (targetStatus.equals(CoordinatingCenterStudyStatus.TEMPORARILY_CLOSED_TO_ACCRUAL_AND_TREATMENT)){
+			study.temporarilyCloseToAccrualAndTreatment();
+		} else if (targetStatus.equals(CoordinatingCenterStudyStatus.CLOSED_TO_ACCRUAL)){
+			study.closeToAccrual();
+		} else if (targetStatus.equals(CoordinatingCenterStudyStatus.CLOSED_TO_ACCRUAL_AND_TREATMENT)){
+			study.closeToAccrualAndTreatment();
+		}
+		
+	}
+
+}
