e8329c6122f3c0d2eb7a9b7b70b4ebc883f965c3
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-11-04 16:33:04 +0000

    added new class for returning errors with exception

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/exception/C3PRBaseErrorsException.java
@@ -0,0 +1,35 @@
+package edu.duke.cabig.c3pr.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.duke.cabig.c3pr.domain.Error;
+
+public class C3PRBaseErrorsException extends C3PRBaseRuntimeException {
+	
+	private List<Error> errors = new ArrayList<Error>();
+
+	public C3PRBaseErrorsException(String string, List<Error> errors) {
+		super(string);
+		this.errors = errors;
+	}
+
+	public List<Error> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(List<Error> errors) {
+		this.errors = errors;
+	}
+
+	public C3PRBaseErrorsException(String string) {
+		super(string);
+		// TODO Auto-generated constructor stub
+	}
+
+	public C3PRBaseErrorsException(String string, Throwable throwable) {
+		super(string, throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+}
