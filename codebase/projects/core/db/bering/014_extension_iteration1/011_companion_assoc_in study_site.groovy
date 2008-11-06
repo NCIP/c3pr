72996745f8ffd21c3198a052b40db1913be292ba
Himanshu Gupta <himanshu.iit@gmail.com>
2008-11-06 14:47:25 +0000

    groovy fix

--- codebase/projects/core/db/bering/014_extension_iteration1/011_companion_assoc_in study_site.groovy
+++ codebase/projects/core/db/bering/014_extension_iteration1/011_companion_assoc_in study_site.groovy
@@ -1,9 +1,9 @@
 class CompanionStudySiteAssociation extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-			addColumn('STUDY_ORGANIZATIONS','comp_assoc_id', 'integer')			
+			addColumn("STUDY_ORGANIZATIONS","comp_assoc_id", "integer")			
 	}
 
 	void down(){
-	        dropColumn('STUDY_ORGANIZATIONS','comp_assoc_id')
+	        dropColumn("STUDY_ORGANIZATIONS","comp_assoc_id")
 	}
 }
