bce2aa244800a24d1ce69fe60159acd0b6ade3cb
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2008-11-13 21:39:00 +0000

    overrode hashcode and equals

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/AbstractMutableDeletableDomainObject.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/AbstractMutableDeletableDomainObject.java
@@ -8,7 +8,34 @@ import javax.persistence.Transient;
 @MappedSuperclass
 public class AbstractMutableDeletableDomainObject extends AbstractMutableDomainObject {
 
-    private String retiredIndicator = "false";
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((retiredIndicator == null) ? 0 : retiredIndicator.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractMutableDeletableDomainObject other = (AbstractMutableDeletableDomainObject) obj;
+		if (retiredIndicator == null) {
+			if (other.retiredIndicator != null)
+				return false;
+		} else if (!retiredIndicator.equals(other.retiredIndicator))
+			return false;
+		return true;
+	}
+
+	private String retiredIndicator = "false";
 
     public String getRetiredIndicator() {
         return retiredIndicator;
