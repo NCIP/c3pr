c4c877e3d880c8a0a80f836c837c3c17e0b5466e
kruttik Aggarwal <kruttikagarwal@gmail.com>
2008-12-31 19:08:24 +0000

    remove hash from identifier value

--- codebase/projects/core/db/bering/016_extension_iteration3/008_update_identifiers_replace_hash.groovy
+++ codebase/projects/core/db/bering/016_extension_iteration3/008_update_identifiers_replace_hash.groovy
@@ -1,15 +1,15 @@
 class UpdateCsmAuthorizationNamespace extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
 	if (databaseMatches('postgres')) {
-			execute("update identifiers set value=translate(value,'#','');
+			execute("update identifiers set value=translate(value,'#','')");
 		 }
 	 
 	 if (databaseMatches('oracle')) {
-			execute("update identifiers set value = replace(value,'#');
+			execute("update identifiers set value = replace(value,'#')");
 	 	}
 	 	
 	 if (databaseMatches('sqlserver')) {
-			execute("update identifiers set value = replace(value,'#','');
+			execute("update identifiers set value = replace(value,'#','')");
 	 	}
 	}
 
