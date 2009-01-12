067792dc1b5d3b3eaf8c18f196f702535cb63a87
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-01-12 21:34:16 +0000

    updated the dao

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/ResearchStaffDao.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/ResearchStaffDao.java
@@ -24,45 +24,79 @@ import edu.duke.cabig.c3pr.dao.query.ResearchStaffQuery;
 import edu.duke.cabig.c3pr.domain.ResearchStaff;
 
 /**
- * Hibernate implementation of StudySiteDao
+ * Hibernate implementation of ResearchStaffDao.
  * 
- * @see edu.duke.cabig.c3pr.dao.StudySiteDao
+ * @see edu.duke.cabig.c3pr.dao.ResearchStaffDao
  * @author Priyatam
  */
 public class ResearchStaffDao extends GridIdentifiableDao<ResearchStaff> {
 
+    /** The log. */
     private static Log log = LogFactory.getLog(InvestigatorDao.class);
 
+    /** The Constant SUBSTRING_MATCH_PROPERTIES. */
     private static final List<String> SUBSTRING_MATCH_PROPERTIES = Arrays.asList("firstName","lastName");
     
+    /** The Constant SUBNAME_SUBEMAIL_MATCH_PROPERTIES. */
     private static final List<String> SUBNAME_SUBEMAIL_MATCH_PROPERTIES = Arrays.asList("firstName","lastName","contactMechanisms.value");
 
+    /** The Constant EXACT_MATCH_PROPERTIES. */
     private static final List<String> EXACT_MATCH_PROPERTIES = Collections.emptyList();
 
+    /** The Constant EXTRA_PARAMS. */
     private static final List<Object> EXTRA_PARAMS = Collections.emptyList();
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.dao.C3PRBaseDao#domainClass()
+     */
     @Override
     public Class<ResearchStaff> domainClass() {
         return ResearchStaff.class;
     }
 
-    /*
-     * Returns all ResearchStaff objects
+    /**
+     * Gets  all ResearchStaff.
+     * 
+     * @return all ResearchStaff
      */
     public List<ResearchStaff> getAll() {
         return getHibernateTemplate().find("from ResearchStaff");
     }
 
+    /**
+     * Gets the by subnames.
+     * 
+     * @param subnames the subnames
+     * @param healthcareSite the healthcare site
+     * 
+     * @return the by subnames
+     */
     public List<ResearchStaff> getBySubnames(String[] subnames, int healthcareSite) {
         return findBySubname(subnames, "o.healthcareSite.id = '" + healthcareSite + "'",
                         EXTRA_PARAMS, SUBSTRING_MATCH_PROPERTIES, EXACT_MATCH_PROPERTIES);
     }
     
+    /**
+     * Gets the by sub name and sub email.
+     * 
+     * @param subnames the subnames
+     * @param nciInstituteCode the nci institute code
+     * 
+     * @return the by sub name and sub email
+     */
     public List<ResearchStaff> getBySubNameAndSubEmail(String[] subnames, String nciInstituteCode) {
         return findBySubname(subnames, "o.healthcareSite.nciInstituteCode = '" + nciInstituteCode + "'",
                         EXTRA_PARAMS, SUBNAME_SUBEMAIL_MATCH_PROPERTIES, EXACT_MATCH_PROPERTIES);
     }
 
+    /**
+     * Search by example.
+     * 
+     * @param staff the staff
+     * @param isWildCard the is wild card
+     * 
+     * @return the list< research staff>
+     */
     public List<ResearchStaff> searchByExample(ResearchStaff staff, boolean isWildCard) {
         List<ResearchStaff> result = new ArrayList<ResearchStaff>();
 
@@ -79,16 +113,13 @@ public class ResearchStaffDao extends GridIdentifiableDao<ResearchStaff> {
                 criteria.add(example);
                 if (staff.getHealthcareSite() != null) {
                     criteria.createCriteria("healthcareSite").add(
-                                    Restrictions.ilike("name", "%"
-                                                    + staff.getHealthcareSite().getName() + "%"));
+                               Restrictions.ilike("name", "%" + staff.getHealthcareSite().getName() + "%"));
                 }
-
                 result = criteria.list();
             }
             else {
                 result = criteria.add(example).list();
             }
-
         }
         catch (DataAccessResourceFailureException e) {
             log.error(e.getMessage());
@@ -102,23 +133,14 @@ public class ResearchStaffDao extends GridIdentifiableDao<ResearchStaff> {
         return result;
     }
 
-    public ResearchStaff getByNciIdentifier(String nciIdentifier) {
-        ResearchStaff result = null;
-
-        ResearchStaff staff = new ResearchStaff();
-        staff.setNciIdentifier(nciIdentifier);
-
-        try {
-            result = searchByExample(staff, false).get(0);
-        }
-        catch (Exception e) {
-            log.debug("User with nciIdentifier " + nciIdentifier
-                            + " does not exist. Returning null");
-        }
-
-        return result;
-    }
     
+    /**
+     * Search research staff.
+     * 
+     * @param query the query
+     * 
+     * @return the list< research staff>
+     */
     @SuppressWarnings( { "unchecked" })
     public List<ResearchStaff> searchResearchStaff(final ResearchStaffQuery query) {
         String queryString = query.getQueryString();
@@ -140,8 +162,36 @@ public class ResearchStaffDao extends GridIdentifiableDao<ResearchStaff> {
         });
     }
     
-    /*
-     * Created for the notifications use case.
+    
+    /**
+     * Gets the by nci identifier.
+     * 
+     * @param nciIdentifier the nci identifier
+     * 
+     * @return the by nci identifier
+     */
+    public ResearchStaff getByNciIdentifier(String nciIdentifier) {
+        ResearchStaff result = null;
+        ResearchStaff staff = new ResearchStaff();
+        staff.setNciIdentifier(nciIdentifier);
+
+        try {
+            result = searchByExample(staff, false).get(0);
+        }
+        catch (Exception e) {
+            log.debug("User with nciIdentifier " + nciIdentifier
+                            + " does not exist. Returning null");
+        }
+        return result;
+    }
+    
+    
+    /**
+     * Gets the by email address. Created for the notifications use case.
+     * 
+     * @param emailAddress the email address
+     * 
+     * @return the ResearchStaff List
      */
     public List<ResearchStaff> getByEmailAddress(String emailAddress) {
         return getHibernateTemplate().find("from ResearchStaff rs where rs.contactMechanisms.value = '" +emailAddress+ "'");
