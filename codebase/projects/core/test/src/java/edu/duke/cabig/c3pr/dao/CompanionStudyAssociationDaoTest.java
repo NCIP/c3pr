593d1747769ad92f6743bece5ffcf104cefb4531
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-01-14 23:21:00 +0000

    adding test case class for CompanionStudyAssociation

---
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/dao/CompanionStudyAssociationDaoTest.java
@@ -0,0 +1,57 @@
+package edu.duke.cabig.c3pr.dao;
+
+import static edu.duke.cabig.c3pr.C3PRUseCase.ADD_DISEASE;
+import static edu.duke.cabig.c3pr.C3PRUseCase.ADD_STRATIFICATION;
+import static edu.duke.cabig.c3pr.C3PRUseCase.CREATE_STUDY;
+import static edu.duke.cabig.c3pr.C3PRUseCase.CREATE_STUDY_INVESTIGATOR;
+import static edu.duke.cabig.c3pr.C3PRUseCase.SEARCH_STUDY;
+import static edu.duke.cabig.c3pr.C3PRUseCase.UPDATE_STUDY;
+import static edu.duke.cabig.c3pr.C3PRUseCase.VERIFY_SUBJECT;
+import edu.duke.cabig.c3pr.C3PRUseCases;
+import edu.duke.cabig.c3pr.domain.CompanionStudyAssociation;
+import edu.duke.cabig.c3pr.utils.ContextDaoTestCase;
+import edu.duke.cabig.c3pr.utils.DaoTestCase;
+
+/**
+ * JUnit Tests for CompanionStudyAssociationDao.
+ * 
+ * @author Ramakrishna
+ * @testType unit
+ */
+
+public class CompanionStudyAssociationDaoTest extends ContextDaoTestCase<StudyDao> {
+    
+    /** The dao. */
+    private CompanionStudyAssociationDao companionStudyAssociationDao;
+    private StudyDao studyDao;
+
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.utils.DaoTestCase#setUp()
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        companionStudyAssociationDao = (CompanionStudyAssociationDao) getApplicationContext().getBean("companionStudyAssociationDao");
+        studyDao = (StudyDao) getApplicationContext().getBean("studyDao");
+    }
+    
+    public void testDomainClass() throws Exception{
+    	assertEquals("Wrong domain class",CompanionStudyAssociation.class,companionStudyAssociationDao.domainClass());
+    }
+
+    public void testSave() throws Exception{
+    	CompanionStudyAssociation companionStudyAssociation =new CompanionStudyAssociation();
+    	companionStudyAssociation.setMandatoryIndicator(false);
+    	companionStudyAssociation.setCompanionStudy(studyDao.getById(1001));
+    	companionStudyAssociation.setParentStudy(studyDao.getById(1000));
+    	assertNull("companionStudyAssociation should not have an Id",companionStudyAssociation.getId());
+    	companionStudyAssociationDao.save(companionStudyAssociation);
+    	
+    	Integer savedId = companionStudyAssociation.getId();
+    	CompanionStudyAssociation loadedCompanionStudyAssociation = companionStudyAssociationDao.getById(savedId);
+    	
+    	assertEquals("Parent study wrong or not found","short_title_text",loadedCompanionStudyAssociation.getParentStudy().getShortTitleText());
+    	assertEquals("Companion study wrong or not found","short_title_text2",loadedCompanionStudyAssociation.getCompanionStudy().getShortTitleText());
+    }
+  
+}
