fb209c9aa5608e12aafd2f318cdbd2baa80750a5
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-01-14 19:25:27 +0000

    new format

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/C3PRAuthorizationDao.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/C3PRAuthorizationDao.java
@@ -21,31 +21,24 @@ public class C3PRAuthorizationDao extends DIAuthorizationDao {
 	private SessionFactory sessionFactory;
 	
 	@Override
-	public User getUser(String loginName) {
+	public User getUser(String loginName) 
+	{
 		 	User user = null ;
 	        User search = new User();
 	        search.setLoginName(loginName);
 	        Example example = Example.create(search).ignoreCase();
 	        Session s = HibernateSessionFactoryHelper.getAuditSession(sessionFactory);
 	        List list = s.createCriteria(User.class).add(example ).list();
-	        if(list.size() != 0)
-	        {
+	        if(list.size() != 0){
 	            user = (User)list.get(0);
 	        }
-	        try
-	        {
+	        try{
 	            user = (User)performEncrytionDecryption(user, false);
-	        }
-	        catch(gov.nih.nci.security.util.StringEncrypter.EncryptionException e)
-	        {
+	        }catch(gov.nih.nci.security.util.StringEncrypter.EncryptionException e){
 	            
-	        }
-	        try
-	        {
+	        }try{
 	            s.close();
-	        }
-	        catch(Exception ex2)
-	        {
+	        }catch(Exception ex2){
 	            if(log.isDebugEnabled())
 	            {
 	                log.debug((new StringBuilder()).append("Authorization|||getUser|Failure|Error in Closing Session |").append(ex2.getMessage()).toString());
@@ -54,41 +47,33 @@ public class C3PRAuthorizationDao extends DIAuthorizationDao {
 	        return user;
 	}
 	
-	   private Object performEncrytionDecryption(Object obj, boolean encrypt) throws gov.nih.nci.security.util.StringEncrypter.EncryptionException
+	private Object performEncrytionDecryption(Object obj, boolean encrypt) 
+					throws gov.nih.nci.security.util.StringEncrypter.EncryptionException
     {
-        if(obj instanceof User)
-        {
+        if(obj instanceof User){
             User user = (User)obj;
-            if(isEncryptionEnabled && StringUtilities.initTrimmedString(user.getPassword()).length() > 0)
-            {
+            if(isEncryptionEnabled && StringUtilities.initTrimmedString(user.getPassword()).length() > 0){
                 StringEncrypter stringEncrypter = new StringEncrypter();
-                if(encrypt)
-                {
+                if(encrypt){
                     user.setPassword(stringEncrypter.encrypt(user.getPassword().trim()));
-                } else
-                {
+                } else {
                     user.setPassword(stringEncrypter.decrypt(user.getPassword().trim()));
                 }
             }
             return user;
         }
-        if(obj instanceof Application)
-        {
+        if(obj instanceof Application){
             Application application = (Application)obj;
-            if(isEncryptionEnabled && StringUtilities.initTrimmedString(application.getDatabasePassword()).length() > 0)
-            {
+            if(isEncryptionEnabled && StringUtilities.initTrimmedString(application.getDatabasePassword()).length() > 0){
                 StringEncrypter stringEncrypter = new StringEncrypter();
-                if(encrypt)
-                {
+                if(encrypt){
                     application.setDatabasePassword(stringEncrypter.encrypt(application.getDatabasePassword().trim()));
-                } else
-                {
+                } else {
                     application.setDatabasePassword(stringEncrypter.decrypt(application.getDatabasePassword().trim()));
                 }
             }
             return application;
-        } else
-        {
+        } else {
             return obj;
         }
     }
