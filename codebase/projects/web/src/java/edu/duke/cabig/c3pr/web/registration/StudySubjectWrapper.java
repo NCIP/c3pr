dddcbfaa3bbec433ef01d401ccf032fec115e4ae
Himanshu Gupta <himanshu.iit@gmail.com>
2009-01-27 16:10:23 +0000

    CPR-501

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/registration/StudySubjectWrapper.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/registration/StudySubjectWrapper.java
@@ -24,14 +24,15 @@ public class StudySubjectWrapper {
 	}
 	
 	public Boolean getShouldReserve(){
-		if(!this.studySubject.getDataEntryStatus())
-			return null;
+		if(!this.studySubject.getDataEntryStatus()){
+			return false;
+		}
 		return(this.studySubject.getScheduledEpoch().getEpoch().getReservationIndicator()&& (this.studySubject.getScheduledEpoch().getScEpochWorkflowStatus()==ScheduledEpochWorkFlowStatus.PENDING) &&(this.studySubject.getRegWorkflowStatus()==RegistrationWorkFlowStatus.PENDING));
 	}
 	
 	public Boolean getShouldRegister(){
 		if(!this.studySubject.getDataEntryStatus()){
-			return null;
+			return false;
 		}
 		
 		if(this.studySubject.getParentStudySubject()!=null && this.studySubject.getParentStudySubject().getRegWorkflowStatus()== RegistrationWorkFlowStatus.ENROLLED && this.studySubject.getMatchingCompanionStudyAssociation(studySubject) != null && !this.studySubject.getMatchingCompanionStudyAssociation(studySubject).getMandatoryIndicator()){
@@ -47,14 +48,14 @@ public class StudySubjectWrapper {
 	
 	public Boolean getShouldEnroll(){
 		if(!this.studySubject.getDataEntryStatus()){
-			return null;
+			return false;
 		}
 		return(!this.studySubject.getWorkPendingOnMandatoryCompanionRegistrations() && this.studySubject.getScheduledEpoch().getEpoch().getEnrollmentIndicator());
 	}
 	
 	public Boolean getShouldRandomize(){
 		if(!this.studySubject.getDataEntryStatus()){
-			return null;
+			return false;
 		}
 		return(this.getStudySubject().getScheduledEpoch().getRequiresRandomization());
 	}
