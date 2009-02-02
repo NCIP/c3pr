5982c6990f076e4df80d4139197be70a73188f20
Himanshu Gupta <himanshu.iit@gmail.com>
2009-02-02 22:24:17 +0000

    registration refactoring

--- codebase/projects/core/db/bering/017_extension_iteration4/002_consent_version_migration.groovy
+++ codebase/projects/core/db/bering/017_extension_iteration4/002_consent_version_migration.groovy
@@ -1,8 +1,9 @@
 class ConsentVersionMigration extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-        execute("insert into consent_history ('stu_sub_id','consent_version','consent_signed_date') select 'id','informed_consent_version','informed_consent_signed_date' from study_subjects")
+        execute('insert into "consent_history" ("stu_sub_id","consent_version","consent_signed_date") select "id","informed_consent_version","informed_consent_signed_date" from study_subjects')
     }
 
     void down() {
+    	execute('insert into "study_subjects" ("informed_consent_version","informed_consent_signed_date") select "consent_version","consent_signed_date" from consent_history where id = "stu_sub_id"')
     }
 }
