f7943410be0710478513563e6c24f15e75808d92
Himanshu Gupta <himanshu.iit@gmail.com>
2009-02-03 15:23:34 +0000

    registration refactoring

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/registration/TakeSubjectOffStudyController.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/registration/TakeSubjectOffStudyController.java
@@ -1,36 +1,23 @@
-/**
- * 
- */
 package edu.duke.cabig.c3pr.web.registration;
 
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
 import edu.duke.cabig.c3pr.dao.StudySubjectDao;
 import edu.duke.cabig.c3pr.domain.StudySubject;
 import edu.duke.cabig.c3pr.utils.web.ControllerTools;
 
-/**
- * @author Himanshu
- *
- */
-public class UpdateConsentVersionController extends SimpleFormController {
+public class TakeSubjectOffStudyController extends SimpleFormController {
 	
-	 private static Log log = LogFactory.getLog(UpdateConsentVersionController.class);
 	 private StudySubjectDao studySubjectDao;
-
-
+	 
 	@Override
-	protected StudySubjectWrapper formBackingObject(HttpServletRequest request) throws Exception {
+	protected Object formBackingObject(HttpServletRequest request)
+			throws Exception {
 		StudySubjectWrapper wrapper = new StudySubjectWrapper();
 		String registartionId = request.getParameter("registartionId")  ; 
 		if (registartionId != null) {
@@ -39,18 +26,19 @@ public class UpdateConsentVersionController extends SimpleFormController {
 		}
 		return wrapper;
 	}
+
+	public StudySubjectDao getStudySubjectDao() {
+		return studySubjectDao;
+	}
+
+	public void setStudySubjectDao(StudySubjectDao studySubjectDao) {
+		this.studySubjectDao = studySubjectDao;
+	}
 	
 	@Override
 	protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
 		super.initBinder(request, binder);
 		binder.registerCustomEditor(Date.class, ControllerTools.getDateEditor(false));
 	}
-	
-	public void setStudySubjectDao(StudySubjectDao studySubjectDao) {
-		this.studySubjectDao = studySubjectDao;
-	}
 
-	public StudySubjectDao getStudySubjectDao() {
-		return studySubjectDao;
-	}
 }
