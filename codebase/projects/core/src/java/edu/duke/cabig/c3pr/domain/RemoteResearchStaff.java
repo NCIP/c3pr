2b1383e237f57287017737436304ae9c247ea900
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-02-05 23:02:37 +0000

    added import of RemoteResearchStaffResolver

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RemoteResearchStaff.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RemoteResearchStaff.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 
 import com.semanticbits.coppa.domain.annotations.RemoteEntity;
 import com.semanticbits.coppa.domain.annotations.RemoteUniqueId;
-import com.semanticbits.coppa.infrastructure.service.RemoteResolver;
+import edu.duke.cabig.c3pr.infrastructure.RemoteResearchStaffResolver;
 
 @RemoteEntity(entityResolver=RemoteResearchStaffResolver.class)
 @Entity
