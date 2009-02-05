9851ad3c91ec399fabe931a65a201ec10ec7fe17
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-02-05 17:37:06 +0000

    corrected dtype for existing LocalResearchStaff Personnel in research_staff

--- codebase/projects/core/db/bering/017_extension_iteration4/005_update_column_dtype_research_staff.groovy
+++ codebase/projects/core/db/bering/017_extension_iteration4/005_update_column_dtype_research_staff.groovy
@@ -1,9 +1,9 @@
 class UpdateColumnsDtypeResearchStaff extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-       	execute("update research_staff set dtype='local'");
+       	execute("update research_staff set dtype='Local' where dtype='local'");
     }
   
     void down() {
-    	execute("update research_staff set dtype=''");
+    	execute("update research_staff set dtype='local' where dtype='Local'");
     }
 }
