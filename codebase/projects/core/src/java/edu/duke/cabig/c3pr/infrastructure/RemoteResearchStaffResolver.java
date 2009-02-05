70e993b6ee420500e355d090f5ce7028011689d9
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-02-05 23:21:44 +0000

    COPPA related development

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/infrastructure/RemoteResearchStaffResolver.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/infrastructure/RemoteResearchStaffResolver.java
@@ -1,8 +1,13 @@
 package edu.duke.cabig.c3pr.infrastructure;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.semanticbits.coppa.infrastructure.service.RemoteResolver;
+import com.semanticbits.coppasimulator.domain.ResearchStaffDTO;
+import com.semanticbits.coppasimulator.service.ResearchStaffService;
+
+import edu.duke.cabig.c3pr.domain.RemoteResearchStaff;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -10,12 +15,21 @@ import com.semanticbits.coppa.infrastructure.service.RemoteResolver;
  */
 public class RemoteResearchStaffResolver implements RemoteResolver{
 
+	ResearchStaffService researchStaffService;
+	
 	/* (non-Javadoc)
 	 * @see com.semanticbits.coppa.infrastructure.service.RemoteResolver#find(java.lang.Object)
 	 */
 	public List<Object> find(Object arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		RemoteResearchStaff remoteResearchStaff = (RemoteResearchStaff) arg0;
+		
+		ResearchStaffDTO researchStaffDTO = researchStaffService.getClinicalResearchStaffPerson("LPage@nci.org");
+		List<ResearchStaffDTO> researchStaffDTOList  = new ArrayList<ResearchStaffDTO>();
+		researchStaffDTOList.add(researchStaffDTO);
+		
+		List<Object> researchStaffList = convertToResearchStaff(researchStaffDTOList);
+		
+		return researchStaffList;
 	}
 
 	/* (non-Javadoc)
@@ -26,4 +40,32 @@ public class RemoteResearchStaffResolver implements RemoteResolver{
 		return null;
 	}
 
+	
+	private List<Object> convertToResearchStaff(List<ResearchStaffDTO> researchStaffDTOList){
+		List<Object> researchStaffList = new ArrayList<Object>();
+		RemoteResearchStaff remoteResearchStaff;
+		for(ResearchStaffDTO researchStaffDTO: researchStaffDTOList){
+			remoteResearchStaff = new RemoteResearchStaff();
+			remoteResearchStaff.setFirstName(researchStaffDTO.getFirstName());
+			remoteResearchStaff.setLastName(researchStaffDTO.getLastName());
+			remoteResearchStaff.setNciIdentifier(researchStaffDTO.getNciIdentifier());
+			
+			researchStaffList.add(remoteResearchStaff);
+		}
+		
+		return researchStaffList;
+	}
+	
+	public ResearchStaffService getResearchStaffService() {
+		return researchStaffService;
+	}
+
+	public void setResearchStaffService(ResearchStaffService researchStaffService) {
+		this.researchStaffService = researchStaffService;
+	}
+
+
 }
+
+
+
