8b03caf0fe3595ed371527e5f5ceff285b9a8e24
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-02-05 23:01:25 +0000

    moved RemoteResearchStaffResolver from domain infrastructure

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/infrastructure/RemoteResearchStaffResolver.java
@@ -0,0 +1,29 @@
+package edu.duke.cabig.c3pr.infrastructure;
+
+import java.util.List;
+
+import com.semanticbits.coppa.infrastructure.service.RemoteResolver;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class RemoteResearchStaffResolver.
+ */
+public class RemoteResearchStaffResolver implements RemoteResolver{
+
+	/* (non-Javadoc)
+	 * @see com.semanticbits.coppa.infrastructure.service.RemoteResolver#find(java.lang.Object)
+	 */
+	public List<Object> find(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.semanticbits.coppa.infrastructure.service.RemoteResolver#getRemoteEntityByUniqueId(java.lang.String)
+	 */
+	public Object getRemoteEntityByUniqueId(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
