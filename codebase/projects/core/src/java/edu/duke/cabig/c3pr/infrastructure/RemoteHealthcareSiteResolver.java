bbe681995469a62015cb1e2b7a0336aadb3185d2
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-02-09 20:46:24 +0000

    adding Remote healthcare site class

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/infrastructure/RemoteHealthcareSiteResolver.java
@@ -0,0 +1,83 @@
+package edu.duke.cabig.c3pr.infrastructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.semanticbits.coppa.infrastructure.service.RemoteResolver;
+import com.semanticbits.coppasimulator.domain.OrganizationDTO;
+import com.semanticbits.coppasimulator.domain.ResearchStaffDTO;
+import com.semanticbits.coppasimulator.service.OrganizationService;
+import com.semanticbits.coppasimulator.service.ResearchStaffService;
+
+import edu.duke.cabig.c3pr.domain.RemoteHealthcareSite;
+import edu.duke.cabig.c3pr.domain.RemoteResearchStaff;
+
+/**
+ * The Class RemoteResearchStaffResolver.
+ */
+public class RemoteHealthcareSiteResolver implements RemoteResolver{
+
+//	ResearchStaffService researchStaffService;
+	
+	OrganizationService organizationService;
+	
+	public OrganizationService getOrganizationService() {
+		return organizationService;
+	}
+
+	public void setOrganizationService(OrganizationService organizationService) {
+		this.organizationService = organizationService;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.semanticbits.coppa.infrastructure.service.RemoteResolver#find(java.lang.Object)
+	 */
+	public List<Object> find(Object arg0) {
+//		RemoteResearchStaff remoteResearchStaff = (RemoteResearchStaff) arg0;
+		
+		RemoteHealthcareSite remoteHealthcareSite = (RemoteHealthcareSite) arg0;
+		
+	//	ResearchStaffDTO researchStaffDTO = researchStaffService.getClinicalResearchStaffPerson("LPage@nci.org");
+		OrganizationDTO organizationDTO = null;
+		try {
+			organizationDTO = organizationService.getOrganization("RM-TST-ID");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<OrganizationDTO> organizationDTOList  = new ArrayList<OrganizationDTO>();
+		organizationDTOList.add(organizationDTO);
+		
+		List<Object> organizationList = convertToOrganization(organizationDTOList);
+		
+		return organizationList;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.semanticbits.coppa.infrastructure.service.RemoteResolver#getRemoteEntityByUniqueId(java.lang.String)
+	 */
+	public Object getRemoteEntityByUniqueId(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	private List<Object> convertToOrganization(List<OrganizationDTO> organizationDTOList){
+		List<Object> remoteOrganizations = new ArrayList<Object>();
+		RemoteHealthcareSite remoteOrganization;
+		
+		for(OrganizationDTO organizationDTO: organizationDTOList){
+			remoteOrganization = new RemoteHealthcareSite();
+			remoteOrganization.setName(organizationDTO.getName());
+			remoteOrganization.setNciInstituteCode(organizationDTO.getNciIdentifier());
+			remoteOrganizations.add(remoteOrganization);
+		}
+		
+		return remoteOrganizations;
+	}
+	
+
+}
+
+
+
