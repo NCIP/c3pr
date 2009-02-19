08aa97073ffd99ccccd1a02332fa87dd4bdc7c86
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-02-19 15:35:22 +0000

    integration with mock COPPA services

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/infrastructure/RemoteHealthcareSiteResolver.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/infrastructure/RemoteHealthcareSiteResolver.java
@@ -1,78 +1,137 @@
 package edu.duke.cabig.c3pr.infrastructure;
 
+import edu.duke.cabig.c3pr.domain.Address;
+import edu.duke.cabig.c3pr.domain.Organization;
+import edu.duke.cabig.c3pr.domain.RemoteHealthcareSite;
+import gov.nih.nci.coppa.iso.AddressPartType;
+import gov.nih.nci.coppa.iso.Adxp;
+import gov.nih.nci.coppa.iso.Enxp;
+import gov.nih.nci.coppa.iso.Ii;
+import gov.nih.nci.services.correlation.IdentifiedOrganizationCorrelationServiceRemote;
+import gov.nih.nci.services.correlation.IdentifiedOrganizationDTO;
+import gov.nih.nci.services.organization.OrganizationDTO;
+import gov.nih.nci.services.organization.OrganizationEntityServiceRemote;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.semanticbits.coppa.infrastructure.service.RemoteResolver;
-import com.semanticbits.coppasimulator.domain.OrganizationDTO;
-import com.semanticbits.coppasimulator.domain.ResearchStaffDTO;
-import com.semanticbits.coppasimulator.service.OrganizationService;
-import com.semanticbits.coppasimulator.service.ResearchStaffService;
 
-import edu.duke.cabig.c3pr.domain.RemoteHealthcareSite;
-import edu.duke.cabig.c3pr.domain.RemoteResearchStaff;
-
+// TODO: Auto-generated Javadoc
 /**
- * The Class RemoteResearchStaffResolver.
+ * The Class RemoteHealthcareSiteResolver.
  */
 public class RemoteHealthcareSiteResolver implements RemoteResolver{
 
-//	ResearchStaffService researchStaffService;
+	/** The organization entity service remote. */
+	private OrganizationEntityServiceRemote organizationEntityServiceRemote;
 	
-	OrganizationService organizationService;
+	/** The identified organization correlation service remote. */
+	private IdentifiedOrganizationCorrelationServiceRemote identifiedOrganizationCorrelationServiceRemote;
+
 	
-	public OrganizationService getOrganizationService() {
-		return organizationService;
-	}
-
-	public void setOrganizationService(OrganizationService organizationService) {
-		this.organizationService = organizationService;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.semanticbits.coppa.infrastructure.service.RemoteResolver#find(java.lang.Object)
+	/**
+	 * Convert to organization.
+	 * 
+	 * @param organizationDto the organization dto
+	 * 
+	 * @return the list< object>
 	 */
-	public List<Object> find(Object arg0) {
-		RemoteHealthcareSite remoteHealthcareSite = (RemoteHealthcareSite) arg0;
-		
-		List<OrganizationDTO> organizationDTOList  = new ArrayList<OrganizationDTO>();
-		
+	
+	
+	public RemoteHealthcareSite populateRemoteOrganization(OrganizationDTO organizationDto){
+		IdentifiedOrganizationDTO identifiedOrganizationDTO = null;
 		try {
-			organizationDTOList = organizationService.search(new OrganizationDTO());
+			identifiedOrganizationDTO = identifiedOrganizationCorrelationServiceRemote.getCorrelation(organizationDto.getIdentifier());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		RemoteHealthcareSite remoteHealthcareSite = new RemoteHealthcareSite();
+		Iterator<Enxp> enxpItr = organizationDto.getName().getPart().iterator();
+		Enxp next = enxpItr.next();
+		List<Adxp> postalAddress = organizationDto.getPostalAddress().getPart();
+		Iterator<Adxp> adxpItr = postalAddress.iterator();
+		String city = "";
+		String country = "";
+		while(adxpItr.hasNext()){
+			Adxp adXp = adxpItr.next();
+			if(AddressPartType.CTY.equals(adXp.getType())){
+				city = adXp.getValue();
+			}
+
+			if(AddressPartType.CNT.equals(adXp.getType())){
+				country = adXp.getCode();
+			}
+		}
 		
-		List<Object> organizationList = convertToOrganization(organizationDTOList);
-		
-		return organizationList;
+		Address remoteOrgAddress = new Address();
+
+		remoteHealthcareSite.setName(next.getValue().toString());
+		remoteOrgAddress.setCity(city);
+		remoteOrgAddress.setCountryCode(country);	
+		remoteHealthcareSite.setAddress(remoteOrgAddress);
+		remoteHealthcareSite.setNciInstituteCode(identifiedOrganizationDTO.getAssignedId().getExtension());	
+		remoteHealthcareSite.setExternalId(organizationDto.getIdentifier().getExtension());
+		return remoteHealthcareSite;
 	}
 	
-
+	
 	/* (non-Javadoc)
 	 * @see com.semanticbits.coppa.infrastructure.service.RemoteResolver#getRemoteEntityByUniqueId(java.lang.String)
 	 */
-	public Object getRemoteEntityByUniqueId(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object getRemoteEntityByUniqueId(String externalId) {
+		OrganizationDTO organizationDto = null;
+		Ii ii = new Ii();
+		ii.setExtension(externalId);
+		try {
+			organizationDto = organizationEntityServiceRemote.getOrganization(ii);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return populateRemoteOrganization(organizationDto);
 	}
-
 	
-	private List<Object> convertToOrganization(List<OrganizationDTO> organizationDTOList){
-		List<Object> remoteOrganizations = new ArrayList<Object>();
-		RemoteHealthcareSite remoteOrganization;
+	/* (non-Javadoc)
+	 * @see com.semanticbits.coppa.infrastructure.service.RemoteResolver#find(java.lang.Object)
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Object> find(Object example) {	
+		Organization remoteOrgExample = (RemoteHealthcareSite)example;
+//		 build Organization DTO based on remoteOrgExample
+		OrganizationDTO dtoExample = new OrganizationDTO();
+		List<OrganizationDTO> organizationDTOs = organizationEntityServiceRemote.search(dtoExample);
 		
-		for(OrganizationDTO organizationDTO: organizationDTOList){
-			remoteOrganization = new RemoteHealthcareSite();
-			remoteOrganization.setName(organizationDTO.getName());
-			remoteOrganization.setNciInstituteCode(organizationDTO.getNciIdentifier());
+		
+		List<Object> remoteOrganizations = new ArrayList<Object>();
+		for (OrganizationDTO organizationDTO:organizationDTOs) {
+			Organization remoteOrganization = populateRemoteOrganization(organizationDTO);
 			remoteOrganizations.add(remoteOrganization);
 		}
-		
 		return remoteOrganizations;
 	}
 	
+	/**
+	 * Sets the identified organization correlation service remote.
+	 * 
+	 * @param identifiedOrganizationCorrelationServiceRemote the new identified organization correlation service remote
+	 */
+	public void setIdentifiedOrganizationCorrelationServiceRemote(
+			IdentifiedOrganizationCorrelationServiceRemote identifiedOrganizationCorrelationServiceRemote) {
+		this.identifiedOrganizationCorrelationServiceRemote = identifiedOrganizationCorrelationServiceRemote;
+	}
+
+	/**
+	 * Sets the organization entity service remote.
+	 * 
+	 * @param organizationEntityServiceRemote the new organization entity service remote
+	 */
+	public void setOrganizationEntityServiceRemote(
+			OrganizationEntityServiceRemote organizationEntityServiceRemote) {
+		this.organizationEntityServiceRemote = organizationEntityServiceRemote;
+	}
 
 }
 
