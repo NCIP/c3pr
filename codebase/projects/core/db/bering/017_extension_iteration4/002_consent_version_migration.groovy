106db85f74cbf3508862bceb4f552cc58f11795d
Himanshu Gupta <himanshu.iit@gmail.com>
2009-03-02 16:48:00 +0000

    groovy fix

--- codebase/projects/core/db/bering/017_extension_iteration4/002_consent_version_migration.groovy
+++ codebase/projects/core/db/bering/017_extension_iteration4/002_consent_version_migration.groovy
@@ -5,10 +5,14 @@ class ConsentVersionMigration extends edu.northwestern.bioinformatics.bering.Mig
         }
 	if (databaseMatches('oracle')) {
 	   		execute("insert into consent_history (stu_sub_id,consent_version,consent_signed_date) select id,informed_consent_version,informed_consent_signed_date from study_subjects");
+	   		execute("rename CONSENT_HISTORY_ID_SEQ to SEQ_CONSENT_HISTORY_ID")
 	    }
     }
 
     void down() {
-    
+    	if (databaseMatches('oracle')) {
+	   		execute("rename SEQ_CONSENT_HISTORY_ID to CONSENT_HISTORY_ID_SEQ")
+	    }
+    }
     }
 }
