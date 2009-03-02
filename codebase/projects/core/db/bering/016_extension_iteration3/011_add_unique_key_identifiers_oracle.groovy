bcf0203b65e1a5d30b0d572b05ff65ef7bc499d8
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-03-02 15:50:49 +0000

    the constraint required is not added so removed it

--- codebase/projects/core/db/bering/016_extension_iteration3/011_add_unique_key_identifiers_oracle.groovy
+++ codebase/projects/core/db/bering/016_extension_iteration3/011_add_unique_key_identifiers_oracle.groovy
@@ -2,13 +2,11 @@ class AddUniqueKeyIdentifiersOracle extends edu.northwestern.bioinformatics.beri
     void up() {
 
         if (databaseMatches('oracle')) {
-	    	execute("alter table identifiers add constraint uk_type_source_target unique(stu_id,prt_id,spa_id,type,hcs_id,system_name)")
 	 	}     
     }
   
     void down() {
     	if (databaseMatches('oracle')) {
-	    	execute("alter table identifiers drop constraint uk_type_source_target")
 	 	}   
     }
 }
