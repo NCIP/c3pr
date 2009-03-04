550a2e7bb99df71e42c13c5be38480ec927dc312
Himanshu Gupta <himanshu.iit@gmail.com>
2009-03-04 16:20:49 +0000

    removed groovy

--- codebase/projects/core/db/bering/017_extension_iteration4/015_SiteBannerDefaultImageUrl_config.groovy
+++
@@ -1,6 +0,0 @@
-class SiteBannerDefaultImageUrl extends edu.northwestern.bioinformatics.bering.Migration {
-	void up() {
-			insert('configuration', [ prop:"siteName", value:"http://www.cancer.gov/images/banner_nci_logo_1.gif" ], primaryKey: false)
-	}
-
-}
