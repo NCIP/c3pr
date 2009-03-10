d6eacdd67ec032417d7867d07cb771ab313d3b8c
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-03-10 19:46:44 +0000

    CPR-601: Coordinating center can now see all sites. but sites can only see themselves

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/accesscontrol/StudySiteAuthorizationCheck.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/accesscontrol/StudySiteAuthorizationCheck.java
@@ -17,16 +17,29 @@ public class StudySiteAuthorizationCheck extends DelegatingObjectPrivilegeCSMAut
    @Override
 	public boolean checkAuthorization(Authentication authentication, String privilege,
 			Object object) {
-	   StudySite studySite = (StudySite) object;
 	   
-	   StudyOrganization studyCoordinatingCenter = studySite.getStudy().getStudyCoordinatingCenter();
+	   boolean authorize = false;
+	   StudyOrganization studyOrganization = null;
+	   StudyOrganization studyCoordinatingCenter = null;
+	   if(object instanceof StudyOrganization){
+		   studyOrganization = (StudyOrganization)object;
+		   studyCoordinatingCenter = studyOrganization.getStudy().getStudyCoordinatingCenter(); 
+	   }
+	   
+	   StudySite studySite = null;
+	   if(object instanceof StudySite){
+		   studySite = (StudySite) object;
+		   studyCoordinatingCenter = studySite.getStudy().getStudyCoordinatingCenter();
+	   }
 	    
-	   boolean authorize = super.checkAuthorization(authentication, privilege,studyCoordinatingCenter);
 	   //super.checkAuthorization will enforce that the logged in user only sees his organization.
 	   //however if the logged in user is a Coordinating center for the study being viewed the he 
 	   //shud be able to see all sites
-	   return authorize?authorize:super.checkAuthorization(authentication,privilege, object);
+	   if(studyCoordinatingCenter != null){
+		   authorize = super.checkAuthorization(authentication, privilege,studyCoordinatingCenter);
+	   }
 	   
+	   return authorize?authorize:super.checkAuthorization(authentication,privilege, object);
 	}
 }
 
