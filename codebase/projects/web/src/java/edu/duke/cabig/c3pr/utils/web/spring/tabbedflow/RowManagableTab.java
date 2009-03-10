40146bf8d090f1f70f551b66c689cf2479c19f21
kruttik Aggarwal <kruttikagarwal@gmail.com>
2009-03-10 18:39:56 +0000

    CPR-533

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/spring/tabbedflow/RowManagableTab.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/spring/tabbedflow/RowManagableTab.java
@@ -1,5 +1,6 @@
 package edu.duke.cabig.c3pr.utils.web.spring.tabbedflow;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -61,35 +62,8 @@ public abstract class RowManagableTab<C> extends ReflexiveAjaxableTab<C> {
 //        List col = (List) new DefaultObjectPropertyReader(command, listPath)
 //                        .getPropertyValueFromPath();
         List col=(List)new BeanWrapperImpl(command).getPropertyValue(listPath);
-        Integer index = null;
-
         String deletionIdStr = request.getParameter(getDeleteHashCodeParamName());
-        if (deletionIdStr.startsWith("ID#")) {
-            deletionIdStr = deletionIdStr.substring(3);
-            int id = Integer.parseInt(deletionIdStr);
-            for (int i = 0; i < col.size(); i++) {
-                AbstractMutableDomainObject amdo;
-                if (col.get(i) instanceof AbstractMutableDomainObject) {
-                    amdo = (AbstractMutableDomainObject) col.get(i);
-                    if (amdo.getId() == id) {
-                        index = i;
-                        break;
-                    }
-                }
-            }
-        }
-        else if (deletionIdStr.startsWith("HC#")) {
-            deletionIdStr = deletionIdStr.substring(3);
-            int hashCode = Integer.parseInt(deletionIdStr);
-            for (int i = 0; i < col.size(); i++) {
-                if (col.get(i).hashCode() == hashCode) {
-                	if(col.get(i) instanceof AbstractMutableDeletableDomainObject && ((AbstractMutableDeletableDomainObject)col.get(i)).getId()==null){
-	                    index = i;
-	                    break;
-                	}
-                }
-            }
-        }
+        Integer index = getIndex(col, deletionIdStr);
         Map<String, String> map = new HashMap<String, String>();
         if (index == null) {
             map.put(AjaxableUtils.getFreeTextModelName(), "Unmatched hashCode/Id");
@@ -111,4 +85,51 @@ public abstract class RowManagableTab<C> extends ReflexiveAjaxableTab<C> {
 
         return new ModelAndView("", map);
     }
+    
+    private Integer getIndex(List collection, String deletionString){
+    	if (deletionString.startsWith("ID#")) {
+    		return getIndexBasedOnID(collection, Integer.parseInt(deletionString.substring(3)));
+        }
+        else if (deletionString.startsWith("HC#")) {
+        	return getIndexBasedOnHash(collection, Integer.parseInt(deletionString.substring(3)));
+        }
+    	return null;
+    }
+    
+    private Integer getIndexBasedOnID(List collection, int id){
+    	Integer index=null;
+    	for (int i = 0; i < collection.size(); i++) {
+            AbstractMutableDomainObject amdo;
+            if (collection.get(i) instanceof AbstractMutableDomainObject) {
+                amdo = (AbstractMutableDomainObject) collection.get(i);
+                if (amdo.getId() == id) {
+                    index= i;
+                    break;
+                }
+            }
+        }
+    	//check for duplicate. If found return the one which doesn't have id.
+    	if(index!=null){
+    		Integer hashBasedIndex=getIndexBasedOnHash(collection, collection.get(index).hashCode());
+    		if(hashBasedIndex!=null){
+    			return hashBasedIndex;
+    		}else{
+    			return index;
+    		}
+    	}
+    		
+    	return null;
+    }
+    
+    private Integer getIndexBasedOnHash(List collection, int hash){
+    	for (int i = 0; i < collection.size(); i++) {
+            if (collection.get(i).hashCode() == hash) {
+            	if(collection.get(i) instanceof AbstractMutableDeletableDomainObject && ((AbstractMutableDeletableDomainObject)collection.get(i)).getId()==null){
+                    return i;
+            	}
+            }
+        }
+    	return null;
+    }
+    
 }
