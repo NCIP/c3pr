c3808e9a6c27eab436b42fbb2f86751b430a9e11
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-03-19 15:34:48 +0000

    increased the column width of versions in research_staff table

---
+++ codebase/projects/core/db/bering/018_extension_iteration5/003_alter_research_staff_increase_version_column_size_postgres.groovy
@@ -0,0 +1,11 @@
+class alterResearchStaffIncreaseVersionColumnSizePostgres extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+	   		if (databaseMatches('postgres')) {
+	   			execute("alter table research_staff alter column version TYPE bigint;");
+ 	    	}
+    }
+
+    void down() {
+     
+    }
+}
