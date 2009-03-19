3184ab74aec3608d9bcef68e7ef9ffba42411d2b
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-03-19 15:35:16 +0000

    corrected syntax

--- codebase/projects/core/db/bering/018_extension_iteration5/003_alter_research_staff_increase_version_column_size_postgres.groovy
+++ codebase/projects/core/db/bering/018_extension_iteration5/003_alter_research_staff_increase_version_column_size_postgres.groovy
@@ -1,7 +1,7 @@
 class alterResearchStaffIncreaseVersionColumnSizePostgres extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
 	   		if (databaseMatches('postgres')) {
-	   			execute("alter table research_staff alter column version TYPE bigint;");
+	   			execute("alter table research_staff alter column version TYPE bigint");
  	    	}
     }
 
