1b79f329a50c2e8c4bffb896703ddb8c0fd36356
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-04-07 21:53:54 +0000

    Merged the ser/deser content with the broadcast methods.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/infrastructure/RemoteHealthcareSiteResolver.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/infrastructure/RemoteHealthcareSiteResolver.java
@@ -3,6 +3,13 @@ package edu.duke.cabig.c3pr.infrastructure;
 import edu.duke.cabig.c3pr.domain.Address;
 import edu.duke.cabig.c3pr.domain.RemoteHealthcareSite;
 import gov.nih.nci.cagrid.common.Utils;
+import edu.duke.cabig.c3pr.esb.CCTSMessageBroadcaster;
+import edu.duke.cabig.c3pr.esb.Metadata;
+import edu.duke.cabig.c3pr.esb.OperationNameEnum;
+import edu.duke.cabig.c3pr.esb.ServiceTypeEnum;
+import edu.duke.cabig.c3pr.esb.impl.CaXchangeMessageBroadcasterImpl;
+import edu.duke.cabig.c3pr.exception.C3PRCodedException;
+import edu.duke.cabig.c3pr.exception.C3PRExceptionHelper;
 import gov.nih.nci.coppa.iso.AddressPartType;
 import gov.nih.nci.coppa.iso.Adxp;
 import gov.nih.nci.coppa.iso.Enxp;
@@ -24,6 +31,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.xml.namespace.QName;
+import org.springframework.context.MessageSource;
 
 import org.iso._21090.AD;
 import org.iso._21090.CD;
@@ -31,9 +39,9 @@ import org.iso._21090.DSETTEL;
 
 import com.semanticbits.coppa.infrastructure.service.RemoteResolver;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class RemoteHealthcareSiteResolver.
+ * @author Vinay Gangoli, Ramakrishna Gundala
  */
 public class RemoteHealthcareSiteResolver implements RemoteResolver{
 
@@ -42,7 +50,16 @@ public class RemoteHealthcareSiteResolver implements RemoteResolver{
 	
 	/** The identified organization correlation service remote. */
 	private IdentifiedOrganizationCorrelationServiceRemote identifiedOrganizationCorrelationServiceRemote;
+	
+	/** The message broadcaster. */
+	private edu.duke.cabig.c3pr.esb.CCTSMessageBroadcaster coppaMessageBroadcaster;
+	
+	/** The exception helper. */
+	protected C3PRExceptionHelper exceptionHelper;
 
+	/** The c3pr error messages. */
+	private MessageSource c3prErrorMessages;
+    
 	
 	/**
 	 * Convert to organization.
@@ -178,6 +195,47 @@ public class RemoteHealthcareSiteResolver implements RemoteResolver{
 	}
 	
 	/**
+	 * Broadcast organization search.
+	 * 
+	 * @param healthcareSiteXml the healthcare site xml
+	 * @return the string
+	 * @throws C3PRCodedException the c3 pr coded exception
+	 */
+	public String broadcastOrganizationSearch(String healthcareSiteXml) throws C3PRCodedException {
+		try {
+            //build metadata with operation name and the external Id and pass it to the broadcast method.
+            Metadata mData = new Metadata(OperationNameEnum.search.getName(), "extId", ServiceTypeEnum.ORGANIZATION.getName());
+            return coppaMessageBroadcaster.broadcastCoppaMessage(healthcareSiteXml, mData);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw this.exceptionHelper.getException(
+                            getCode("C3PR.EXCEPTION.ORGANIZATION.SEARCH.BROADCAST.SEND_ERROR"), e);
+        }
+	}
+	
+	/**
+	 * Broadcast organization search. overloaded method.
+	 * 
+	 * @param healthcareSiteXml the healthcare site xml
+	 * @return the string
+	 * @throws C3PRCodedException the c3 pr coded exception
+	 */
+	public String broadcastOrganizationSearch(CCTSMessageBroadcaster coppaMessageBroadcaster, String healthcareSiteXml) throws C3PRCodedException {
+		try {
+            //build metadata with operation name and the external Id and pass it to the broadcast method.
+            Metadata mData = new Metadata(OperationNameEnum.search.getName(), "extId", ServiceTypeEnum.ORGANIZATION.getName());
+            return coppaMessageBroadcaster.broadcastCoppaMessage(healthcareSiteXml, mData);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw this.exceptionHelper.getException(
+                            getCode("C3PR.EXCEPTION.ORGANIZATION.SEARCH.BROADCAST.SEND_ERROR"), e);
+        }
+	}
+	
+	
+	/**
 	 * Sets the identified organization correlation service remote.
 	 * 
 	 * @param identifiedOrganizationCorrelationServiceRemote the new identified organization correlation service remote
@@ -195,6 +253,47 @@ public class RemoteHealthcareSiteResolver implements RemoteResolver{
 	public void setOrganizationEntityServiceRemote(
 			OrganizationEntityServiceRemote organizationEntityServiceRemote) {
 		this.organizationEntityServiceRemote = organizationEntityServiceRemote;
+	}
+
+
+    /**
+     * Gets the error code which is used to retrieve the exception message.
+     * 
+     * @param errortypeString the errortype string
+     * @return the code
+     */
+    public int getCode(String errortypeString) {
+        return Integer.parseInt(this.c3prErrorMessages.getMessage(errortypeString, null, null));
+    }
+    
+	public C3PRExceptionHelper getExceptionHelper() {
+		return exceptionHelper;
+	}
+
+
+	public void setExceptionHelper(C3PRExceptionHelper exceptionHelper) {
+		this.exceptionHelper = exceptionHelper;
+	}
+
+
+	public MessageSource getC3prErrorMessages() {
+		return c3prErrorMessages;
+	}
+
+
+	public void setC3prErrorMessages(MessageSource errorMessages) {
+		c3prErrorMessages = errorMessages;
+	}
+
+
+	public edu.duke.cabig.c3pr.esb.CCTSMessageBroadcaster getMessageBroadcaster() {
+		return coppaMessageBroadcaster;
+	}
+
+
+	public void setMessageBroadcaster(
+			edu.duke.cabig.c3pr.esb.CCTSMessageBroadcaster coppaMessageBroadcaster) {
+		this.coppaMessageBroadcaster = coppaMessageBroadcaster;
 	}
 
 }
