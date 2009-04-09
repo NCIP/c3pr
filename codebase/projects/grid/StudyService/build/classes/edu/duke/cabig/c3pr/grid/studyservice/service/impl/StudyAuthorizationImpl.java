3bd4bcc6ad8aca3142a04ffc22b194cbfa8e62b9
kruttik Aggarwal <kruttikagarwal@gmail.com>
2009-04-09 21:00:47 +0000

    adding the new code

---
+++ codebase/projects/grid/StudyService/build/classes/edu/duke/cabig/c3pr/grid/studyservice/service/impl/StudyAuthorizationImpl.java
@@ -0,0 +1,70 @@
+package edu.duke.cabig.c3pr.grid.studyservice.service.impl;
+
+public class StudyAuthorizationImpl implements StudyAuthorizationI {
+
+	public void authorizeActivateStudySite(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeAmendStudy(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeApproveStudySiteForActivation(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeCloseStudy(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeCloseStudySite(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeCreateStudy(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeGetMultipleResourceProperties(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeGetResourceProperty(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeGetServiceSecurityMetadata(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeOpenStudy(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeQueryResourceProperties(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeUpdateStudySiteProtocolVersion(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+	public void authorizeUpdateStudyStatus(String callerIdentity) {
+		System.out.println("Default Authorized");
+
+	}
+
+}
---
+++ codebase/projects/grid/StudyService/build/stubs-StudyService/src/edu/duke/cabig/c3pr/grid/studyservice/stubs/UpdateStudyStatusRequest.java
@@ -0,0 +1,118 @@
+/**
+ * UpdateStudyStatusRequest.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC2 Apr 28, 2006 (12:42:00 EDT) WSDL2Java emitter.
+ */
+
+package edu.duke.cabig.c3pr.grid.studyservice.stubs;
+
+public class UpdateStudyStatusRequest  implements java.io.Serializable {
+    private edu.duke.cabig.c3pr.grid.studyservice.stubs.UpdateStudyStatusRequestMessage message;
+
+    public UpdateStudyStatusRequest() {
+    }
+
+    public UpdateStudyStatusRequest(
+           edu.duke.cabig.c3pr.grid.studyservice.stubs.UpdateStudyStatusRequestMessage message) {
+           this.message = message;
+    }
+
+
+    /**
+     * Gets the message value for this UpdateStudyStatusRequest.
+     * 
+     * @return message
+     */
+    public edu.duke.cabig.c3pr.grid.studyservice.stubs.UpdateStudyStatusRequestMessage getMessage() {
+        return message;
+    }
+
+
+    /**
+     * Sets the message value for this UpdateStudyStatusRequest.
+     * 
+     * @param message
+     */
+    public void setMessage(edu.duke.cabig.c3pr.grid.studyservice.stubs.UpdateStudyStatusRequestMessage message) {
+        this.message = message;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof UpdateStudyStatusRequest)) return false;
+        UpdateStudyStatusRequest other = (UpdateStudyStatusRequest) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.message==null && other.getMessage()==null) || 
+             (this.message!=null &&
+              this.message.equals(other.getMessage())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getMessage() != null) {
+            _hashCode += getMessage().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(UpdateStudyStatusRequest.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://studyservice.grid.c3pr.cabig.duke.edu/StudyService", ">UpdateStudyStatusRequest"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("message");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://studyservice.grid.c3pr.cabig.duke.edu/StudyService", "message"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://studyservice.grid.c3pr.cabig.duke.edu/StudyService", ">>UpdateStudyStatusRequest>message"));
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
