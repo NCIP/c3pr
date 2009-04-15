4265ed308f9005a7df2c4104854f66f08f637817
kruttik Aggarwal <kruttikagarwal@gmail.com>
2009-04-15 18:03:21 +0000

    added new grid service

---
+++ codebase/projects/grid/RegistrationService/src/edu/duke/cabig/c3pr/grid/registrationservice/service/impl/EchoRegistrationServiceImpl.java
@@ -0,0 +1,55 @@
+package edu.duke.cabig.c3pr.grid.registrationservice.service.impl;
+
+import java.rmi.RemoteException;
+
+import javax.xml.namespace.QName;
+
+import org.oasis.wsrf.properties.GetMultipleResourcePropertiesResponse;
+import org.oasis.wsrf.properties.GetMultipleResourceProperties_Element;
+import org.oasis.wsrf.properties.GetResourcePropertyResponse;
+import org.oasis.wsrf.properties.QueryResourcePropertiesResponse;
+import org.oasis.wsrf.properties.QueryResourceProperties_Element;
+
+import edu.duke.cabig.c3pr.grid.registrationservice.common.RegistrationServiceI;
+import gov.nih.nci.cabig.ccts.domain.IdentifierType;
+import gov.nih.nci.cabig.ccts.domain.Message;
+import gov.nih.nci.cabig.ccts.domain.Registration;
+import gov.nih.nci.cabig.ccts.domain.ScheduledEpochType;
+
+public class EchoRegistrationServiceImpl implements RegistrationServiceI{
+
+    public Message enroll(Message message) throws RemoteException {
+        System.out.println("recieved message..");
+        return null;
+    }
+
+    public GetMultipleResourcePropertiesResponse getMultipleResourceProperties(
+                    GetMultipleResourceProperties_Element params) throws RemoteException {
+        return null;
+    }
+
+    public GetResourcePropertyResponse getResourceProperty(QName params) throws RemoteException {
+        return null;
+    }
+
+    public void offStudy(Message message) throws RemoteException {
+        System.out.println("recieved message..");
+    }
+
+    public QueryResourcePropertiesResponse queryResourceProperties(
+                    QueryResourceProperties_Element params) throws RemoteException {
+        return null;
+    }
+
+    public Message transfer(Message message) throws RemoteException {
+        System.out.println("recieved message..");
+        return null;
+    }
+
+	public Message getRegistrations(Message message) throws RemoteException {
+		System.out.println("recieved message..");
+		return null;
+	}
+
+    
+}
