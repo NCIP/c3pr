3c15c524c3cc853ad74bc75c8eb3ca399f3ff22d
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-05-12 19:28:00 +0000

    Domain layer test coverage work.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/StratificationCriterion.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/StratificationCriterion.java
@@ -21,7 +21,9 @@ import edu.duke.cabig.c3pr.utils.StringUtils;
 import gov.nih.nci.cabig.ctms.collections.LazyListHelper;
 
 /**
- * @author Priyatam
+ * The Class StratificationCriterion.
+ * 
+ * @author Vinay Gangoli
  */
 @Entity
 @Table(name = "strat_criteria")
@@ -29,12 +31,18 @@ import gov.nih.nci.cabig.ctms.collections.LazyListHelper;
 public class StratificationCriterion extends AbstractMutableDeletableDomainObject implements
                 Comparable<StratificationCriterion> {
 
+    /** The lazy list helper. */
     private LazyListHelper lazyListHelper;
 
+    /** The question number. */
     private Integer questionNumber = new Integer(0);
 
+    /** The question text. */
     private String questionText;
 
+    /**
+     * Instantiates a new stratification criterion.
+     */
     public StratificationCriterion() {
         lazyListHelper = new LazyListHelper();
         this.questionNumber = new Integer(0);
@@ -43,8 +51,9 @@ public class StratificationCriterion extends AbstractMutableDeletableDomainObjec
                                         StratificationCriterionPermissibleAnswer.class));
     }
 
-    // / LOGIC
-
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.AbstractMutableDeletableDomainObject#setRetiredIndicatorAsTrue()
+     */
     @Override
     @Transient
     public void setRetiredIndicatorAsTrue() {
@@ -58,41 +67,82 @@ public class StratificationCriterion extends AbstractMutableDeletableDomainObjec
         }
     }
 
+    /**
+     * Adds the permissible answer.
+     * 
+     * @param answer the answer
+     */
     public void addPermissibleAnswer(StratificationCriterionPermissibleAnswer answer) {
         getPermissibleAnswers().add(answer);
-        // answer.setStratificationCriterion(this);
     }
 
+    /**
+     * Removes the permissible answer.
+     * 
+     * @param answer the answer
+     */
     public void removePermissibleAnswer(StratificationCriterionPermissibleAnswer answer) {
         getPermissibleAnswers().remove(answer);
     }
 
+    /**
+     * Gets the trimmed question text.
+     * 
+     * @return the trimmed question text
+     */
     @Transient
     public String getTrimmedQuestionText() {
         return StringUtils.getTrimmedText(questionText, 40);
     }
 
-    // / BEAN PROPERTIES
+    /* (non-Javadoc)
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
     public int compareTo(StratificationCriterion o) {
         return 0;
     }
 
+    /**
+     * Gets the question number.
+     * 
+     * @return the question number
+     */
     public Integer getQuestionNumber() {
         return questionNumber;
     }
 
+    /**
+     * Sets the question number.
+     * 
+     * @param questionNumber the new question number
+     */
     public void setQuestionNumber(Integer questionNumber) {
         this.questionNumber = questionNumber;
     }
 
+    /**
+     * Gets the question text.
+     * 
+     * @return the question text
+     */
     public String getQuestionText() {
         return questionText;
     }
 
+    /**
+     * Sets the question text.
+     * 
+     * @param questionText the new question text
+     */
     public void setQuestionText(String questionText) {
         this.questionText = questionText;
     }
 
+    /**
+     * Gets the permissible answers internal.
+     * 
+     * @return the permissible answers internal
+     */
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "str_cri_id", nullable = false)
     @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
@@ -101,23 +151,41 @@ public class StratificationCriterion extends AbstractMutableDeletableDomainObjec
         return lazyListHelper.getInternalList(StratificationCriterionPermissibleAnswer.class);
     }
 
+    /**
+     * Sets the permissible answers.
+     * 
+     * @param permissibleAnswers the new permissible answers
+     */
     public void setPermissibleAnswers(
                     List<StratificationCriterionPermissibleAnswer> permissibleAnswers) {
         lazyListHelper.setInternalList(StratificationCriterionPermissibleAnswer.class,
                         permissibleAnswers);
     }
 
+    /**
+     * Gets the permissible answers.
+     * 
+     * @return the permissible answers
+     */
     @Transient
     public List<StratificationCriterionPermissibleAnswer> getPermissibleAnswers() {
         return lazyListHelper.getLazyList(StratificationCriterionPermissibleAnswer.class);
     }
 
+    /**
+     * Sets the permissible answers internal.
+     * 
+     * @param permissibleAnswers the new permissible answers internal
+     */
     public void setPermissibleAnswersInternal(
                     List<StratificationCriterionPermissibleAnswer> permissibleAnswers) {
         lazyListHelper.setInternalList(StratificationCriterionPermissibleAnswer.class,
                         permissibleAnswers);
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.AbstractMutableDeletableDomainObject#hashCode()
+     */
     @Override
     public int hashCode() {
         final int PRIME = 31;
@@ -130,6 +198,9 @@ public class StratificationCriterion extends AbstractMutableDeletableDomainObjec
         return result;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.AbstractMutableDeletableDomainObject#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
