60b1f192d39522b0d0fc05843472a3d29713c4ac
Himanshu Gupta <himanshu.iit@gmail.com>
2009-05-13 21:54:13 +0000

    test case coverage improvement

--- codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/UserTestCase.java
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/UserTestCase.java
@@ -1,5 +1,8 @@
 package edu.duke.cabig.c3pr.domain;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import edu.duke.cabig.c3pr.AbstractTestCase;
 import edu.duke.cabig.c3pr.utils.UserCreationHelper;
 
@@ -18,15 +21,31 @@ public class UserTestCase extends AbstractTestCase{
 	}
 	
 	public void testResetToken(){
-		
+		Timestamp timestamp = new Timestamp(200);
+		user.setTokenTime(timestamp);
+		user.resetToken();
+		assertNotEquals("after reset token time, time stamp should be differnt then earlier", timestamp, user.getTokenTime());
 	}
 	
 	public void testGetPasswordAge(){
-		
+        Timestamp timestamp = new Timestamp(new Date().getTime() - 1000);
+		user.setPasswordLastSet(timestamp);
+		System.out.println("######## : " + user.getPasswordAge());
+		assertEquals("password age is 1000 miliseconds", 1000, user.getPasswordAge());
 	}
 	
 	public void testAddPasswordToHistory(){
-		
+		assertEquals("password history size is 0", 0, user.getPasswordHistory().size());
+		user.addPasswordToHistory("testpasswd", 1)	;
+		assertEquals("password history size is 1", 1, user.getPasswordHistory().size());
+	}
+	
+	public void testAddPasswordToHistory1(){
+		assertEquals("password history size is 0", 0, user.getPasswordHistory().size());
+		user.addPasswordToHistory("testpasswd", 1)	;
+		user.addPasswordToHistory("testpasswd2", 1)	;
+		assertEquals("password history size is 1", 1, user.getPasswordHistory().size());
+		assertEquals("last stored password should be testpasswd2", "testpasswd2", user.getPasswordHistory().get(user.getPasswordHistory().size() -1));
 	}
 	
 	public void testGetLastFirst(){
@@ -36,4 +55,5 @@ public class UserTestCase extends AbstractTestCase{
 	public void testGetFullName(){
 		assertEquals("getLastFirst should evaluate to John Scott","John Scott",user.getFullName());
 	}
+	
 }
