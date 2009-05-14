ebae39857578ed9b8fbdd896e7fc39c36a9fc497
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-05-14 19:44:43 +0000

    Domain layer test coverage work.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/UserBasedRecipient.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/UserBasedRecipient.java
@@ -8,19 +8,30 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-
+/**
+ * The Class UserBasedRecipient. 
+ * This is used by the notification framework to send notifications to recipients who are users in 
+ * the C3PR system. Hence this class has links to the staff and investigators
+ */
 @Entity
 @DiscriminatorValue(value = "ER")
 public class UserBasedRecipient extends Recipient {
 
+    /** The research staff. */
     private ResearchStaff researchStaff;
+    
+    /** The investigator. */
     private Investigator investigator;
     
+    /** The email address. */
     private String emailAddress;
     
 
+	/**
+	 * Gets the investigator.
+	 * 
+	 * @return the investigator
+	 */
 	@ManyToOne
     @JoinColumn(name = "investigators_id")
 //    @Cascade(value = { CascadeType.LOCK })
@@ -28,10 +39,20 @@ public class UserBasedRecipient extends Recipient {
 		return investigator;
 	}
 
+	/**
+	 * Sets the investigator.
+	 * 
+	 * @param investigator the new investigator
+	 */
 	public void setInvestigator(Investigator investigator) {
 		this.investigator = investigator;
 	}
 
+	/**
+	 * Gets the research staff.
+	 * 
+	 * @return the research staff
+	 */
 	@ManyToOne
     @JoinColumn(name = "research_staff_id")
 //    @Cascade(value = { CascadeType.LOCK })
@@ -39,10 +60,29 @@ public class UserBasedRecipient extends Recipient {
 		return researchStaff;
 	}
 
+	/**
+	 * Sets the research staff.
+	 * 
+	 * @param researchStaff the new research staff
+	 */
 	public void setResearchStaff(ResearchStaff researchStaff) {
 		this.researchStaff = researchStaff;
 	}
+	
+    /**
+     * Sets the email address.
+     * 
+     * @param emailAddress the new email address
+     */
+    public void setEmailAddress(String emailAddress){
+    	this.emailAddress = emailAddress;
+    }
     
+    /**
+     * Gets the email address.
+     * 
+     * @return the email address
+     */
     @Transient
 	public String getEmailAddress(){		
     	if(this.emailAddress != null && this.emailAddress!= "" ){
@@ -67,13 +107,13 @@ public class UserBasedRecipient extends Recipient {
 		return "";
 	}
     
-    public void setEmailAddress(String emailAddress){
-    	this.emailAddress = emailAddress;
-    }
-    
+    /**
+     * Gets the full name.
+     * 
+     * @return the full name
+     */
     @Transient
 	public String getFullName(){		
- 
 		String fullName = "";		
 		if(researchStaff != null){
 			fullName = researchStaff.getFirstName() +" "+ researchStaff.getLastName();
@@ -85,6 +125,9 @@ public class UserBasedRecipient extends Recipient {
 		return fullName;
 	}
 	   
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.AbstractMutableDeletableDomainObject#setRetiredIndicatorAsTrue()
+     */
     @Override
     @Transient
     public void setRetiredIndicatorAsTrue() {
---
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/UserBasedRecipientTest.java
@@ -0,0 +1,74 @@
+package edu.duke.cabig.c3pr.domain;
+
+import edu.duke.cabig.c3pr.AbstractTestCase;
+
+/**
+ * The Class UserBasedRecipientTest.
+ */
+public class UserBasedRecipientTest extends AbstractTestCase{
+	
+	/** The Constant INV. */
+	public static final String INV = "inv@example.com";
+	
+	/** The Constant STAFF. */
+	public static final String STAFF = "staff@example.com";
+	
+	/**
+	 * Test get email address for Investigator.
+	 */
+	public void testGetEmailAddressForInv(){
+		
+		UserBasedRecipient userBasedRecipient = getUserBasedRecipientForInv();
+		String emails = userBasedRecipient.getEmailAddress();
+		assertTrue(emails.equals(INV));
+	}
+	
+	/**
+	 * Test get email address for staff.
+	 */
+	public void testGetEmailAddressForStaff(){
+		
+		UserBasedRecipient userBasedRecipient = getUserBasedRecipientForStaff();
+		String emails = userBasedRecipient.getEmailAddress();
+		assertTrue(emails.equals(STAFF));
+	}
+
+	/**
+	 * Gets the user based recipient for staff.
+	 * 
+	 * @return the user based recipient for staff
+	 */
+	private UserBasedRecipient getUserBasedRecipientForStaff() {
+		ResearchStaff researchStaff = new LocalResearchStaff();
+		ContactMechanism contactMechanism = new ContactMechanism();
+		contactMechanism.setType(ContactMechanismType.EMAIL);
+		contactMechanism.setValue(STAFF);
+		researchStaff.getContactMechanisms().add(contactMechanism);
+		
+		
+		UserBasedRecipient userBasedRecipient = new UserBasedRecipient();
+		userBasedRecipient.setResearchStaff(researchStaff);
+		
+		return userBasedRecipient;
+	}
+	
+	/**
+	 * Gets the user based recipient for inv.
+	 * 
+	 * @return the user based recipient for inv
+	 */
+	private UserBasedRecipient getUserBasedRecipientForInv() {
+		
+		Investigator investigator = new LocalInvestigator();
+		ContactMechanism contactMechanismInv = new ContactMechanism();
+		contactMechanismInv.setType(ContactMechanismType.EMAIL);
+		contactMechanismInv.setValue(INV);
+		investigator.getContactMechanisms().add(contactMechanismInv);
+		
+		UserBasedRecipient userBasedRecipient = new UserBasedRecipient();
+		userBasedRecipient.setInvestigator(investigator);
+		
+		return userBasedRecipient;
+	}
+}
+
