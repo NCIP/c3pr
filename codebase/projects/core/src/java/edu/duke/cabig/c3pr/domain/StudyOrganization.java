b5147df67cf998b751e6be1e5a5fb5e811b19368
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-05-15 20:34:35 +0000

    Domain layer test coverage work.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/StudyOrganization.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/StudyOrganization.java
@@ -21,6 +21,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Where;
 
+import edu.duke.cabig.c3pr.constants.InvestigatorStatusCodeEnum;
 import edu.duke.cabig.c3pr.constants.APIName;
 import edu.duke.cabig.c3pr.constants.ServiceName;
 import edu.duke.cabig.c3pr.constants.WorkFlowStatusType;
@@ -30,10 +31,9 @@ import edu.duke.cabig.c3pr.domain.factory.ParameterizedBiDirectionalInstantiateF
 import gov.nih.nci.cabig.ctms.collections.LazyListHelper;
 
 /**
- * This class encapsulates all types of organizations associated with a Study
+ * This class encapsulates all types of organizations associated with a Study.
  * 
  * @author Ram Chilukuri
- * 
  */
 @Entity
 @Table(name = "study_organizations")
@@ -42,27 +42,46 @@ import gov.nih.nci.cabig.ctms.collections.LazyListHelper;
 @GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "STUDY_ORGANIZATIONS_ID_SEQ") })
 public abstract class StudyOrganization extends InteroperableAbstractMutableDeletableDomainObject implements CustomFieldAuthorable{
 
+    /** The study. */
     protected Study study;
 
+    /** The healthcare site. */
     private HealthcareSite healthcareSite;
 
+    /** The lazy list helper. */
     private LazyListHelper lazyListHelper;
 
     // TODO move into Command Object
+    /** The study investigator ids. */
     private String[] studyInvestigatorIds;
     
+    /** The study personnel ids. */
     private String[] studyPersonnelIds;
     
+    /** The hosted mode. */
     private Boolean hostedMode=true;
 
+    /**
+     * Gets the hosted mode.
+     * 
+     * @return the hosted mode
+     */
     public Boolean getHostedMode() {
         return hostedMode;
     }
 
+    /**
+     * Sets the hosted mode.
+     * 
+     * @param hostedMode the new hosted mode
+     */
     public void setHostedMode(Boolean hostedMode) {
         this.hostedMode = hostedMode;
     }
 
+    /**
+     * Instantiates a new study organization.
+     */
     public StudyOrganization() {
         lazyListHelper = new LazyListHelper();
         lazyListHelper.add(StudyInvestigator.class,
@@ -75,16 +94,31 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         lazyListHelper.add(CustomFieldDefinition.class,new ParameterizedBiDirectionalInstantiateFactory<CustomFieldDefinition>(CustomFieldDefinition.class, this));
     }
 
+    /**
+     * Adds the study personnel.
+     * 
+     * @param studyPersonnel the study personnel
+     */
     public void addStudyPersonnel(StudyPersonnel studyPersonnel) {
         getStudyPersonnel().add(studyPersonnel);
         studyPersonnel.setStudyOrganization(this);
     }
 
+    /**
+     * Adds the study investigator.
+     * 
+     * @param studyInvestigator the study investigator
+     */
     public void addStudyInvestigator(StudyInvestigator studyInvestigator) {
         getStudyInvestigators().add(studyInvestigator);
         studyInvestigator.setStudyOrganization(this);
     }
 
+    /**
+     * Gets the study investigators internal.
+     * 
+     * @return the study investigators internal
+     */
     @OneToMany(mappedBy = "studyOrganization")
     @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     @Where(clause = "retired_indicator = 'false'")
@@ -92,29 +126,54 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return lazyListHelper.getInternalList(StudyInvestigator.class);
     }
 
+    /**
+     * Sets the study investigators internal.
+     * 
+     * @param studyInvestigators the new study investigators internal
+     */
     public void setStudyInvestigatorsInternal(List<StudyInvestigator> studyInvestigators) {
         lazyListHelper.setInternalList(StudyInvestigator.class, studyInvestigators);
     }
 
+    /**
+     * Gets the study investigators.
+     * 
+     * @return the study investigators
+     */
     @Transient
     public List<StudyInvestigator> getStudyInvestigators() {
         return lazyListHelper.getLazyList(StudyInvestigator.class);
     }
 
+    /**
+     * Gets the active study investigators.
+     * 
+     * @return the active study investigators
+     */
     @Transient
     public List<StudyInvestigator> getActiveStudyInvestigators() {
         List<StudyInvestigator> list = getStudyInvestigators();
         List<StudyInvestigator> retList = new ArrayList<StudyInvestigator>();
         for (StudyInvestigator s : list) {
-            if (s.getStatusCode().equalsIgnoreCase("Active")) retList.add(s);
+            if (s.getStatusCode().equals(InvestigatorStatusCodeEnum.AC)) retList.add(s);
         }
         return retList;
     }
 
+    /**
+     * Sets the study investigators.
+     * 
+     * @param studyInvestigators the new study investigators
+     */
     public void setStudyInvestigators(List<StudyInvestigator> studyInvestigators) {
         lazyListHelper.setInternalList(StudyInvestigator.class, studyInvestigators);
     }
 
+    /**
+     * Gets the study personnel internal.
+     * 
+     * @return the study personnel internal
+     */
     @OneToMany(mappedBy = "studyOrganization")
     @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     @Where(clause = "retired_indicator = 'false'")
@@ -122,19 +181,39 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return lazyListHelper.getInternalList(StudyPersonnel.class);
     }
 
+    /**
+     * Sets the study personnel internal.
+     * 
+     * @param studyPersonnel the new study personnel internal
+     */
     public void setStudyPersonnelInternal(List<StudyPersonnel> studyPersonnel) {
         lazyListHelper.setInternalList(StudyPersonnel.class, studyPersonnel);
     }
 
+    /**
+     * Gets the study personnel.
+     * 
+     * @return the study personnel
+     */
     @Transient
     public List<StudyPersonnel> getStudyPersonnel() {
         return lazyListHelper.getLazyList(StudyPersonnel.class);
     }
 
+    /**
+     * Sets the study personnel.
+     * 
+     * @param studyPersonnel the new study personnel
+     */
     public void setStudyPersonnel(List<StudyPersonnel> studyPersonnel) {
         lazyListHelper.setInternalList(StudyPersonnel.class, studyPersonnel);
     }
 
+    /**
+     * Gets the healthcare site.
+     * 
+     * @return the healthcare site
+     */
     @ManyToOne
     @JoinColumn(name = "hcs_id", nullable = false)
     @Cascade(value = { CascadeType.LOCK })
@@ -142,10 +221,18 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return healthcareSite;
     }
 
+    /**
+     * Sets the healthcare site.
+     * 
+     * @param site the new healthcare site
+     */
     public void setHealthcareSite(HealthcareSite site) {
         this.healthcareSite = site;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.AbstractMutableDeletableDomainObject#hashCode()
+     */
     @Override
     public int hashCode() {
         final int PRIME = 31;
@@ -155,6 +242,9 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return result;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.AbstractMutableDeletableDomainObject#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -172,6 +262,11 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return true;
     }
 
+    /**
+     * Gets the study.
+     * 
+     * @return the study
+     */
     @ManyToOne
     @JoinColumn(name = "study_id", nullable = false)
     @Cascade( { CascadeType.LOCK })
@@ -179,34 +274,67 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return study;
     }
 
+    /**
+     * Sets the study.
+     * 
+     * @param study the new study
+     */
     public void setStudy(Study study) {
         this.study = study;
     }
 
+    /**
+     * Gets the study investigator ids.
+     * 
+     * @return the study investigator ids
+     */
     @Transient
     public String[] getStudyInvestigatorIds() {
         return studyInvestigatorIds;
     }
     
+    /**
+     * Gets the study personnel ids.
+     * 
+     * @return the study personnel ids
+     */
     @Transient
     public String[] getStudyPersonnelIds() {
         return studyPersonnelIds;
     }
     
+    /**
+     * Sets the study personnel ids.
+     * 
+     * @param studyPersonnelIds the new study personnel ids
+     */
     public void setStudyPersonnelIds(String[] studyPersonnelIds) {
         this.studyPersonnelIds = studyPersonnelIds;
     }
 
+    /**
+     * Sets the study investigator ids.
+     * 
+     * @param studyInvestigatorIds the new study investigator ids
+     */
     public void setStudyInvestigatorIds(String[] studyInvestigatorIds) {
         this.studyInvestigatorIds = studyInvestigatorIds;
     }
     
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.InteroperableAbstractMutableDeletableDomainObject#getEndpoints()
+     */
     @OneToMany(mappedBy="studyOrganization")
     @Cascade(value = { CascadeType.DELETE, CascadeType.DELETE_ORPHAN })
     public List<EndPoint> getEndpoints() {
         return endpoints;
     }
     
+    /**
+     * Gets the study endpoints.
+     * 
+     * @return the study endpoints
+     */
     @Transient
     public List<EndPoint> getStudyEndpoints() {
     	List<EndPoint> studyEndpoints=new ArrayList<EndPoint>();
@@ -217,6 +345,11 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return studyEndpoints;
     }
     
+    /**
+     * Gets the registration endpoints.
+     * 
+     * @return the registration endpoints
+     */
     @Transient
     public List<EndPoint> getRegistrationEndpoints() {
     	List<EndPoint> regEndpoints=new ArrayList<EndPoint>();
@@ -227,6 +360,11 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return regEndpoints;
     }
     
+    /**
+     * Gets the last attempted registration endpoint.
+     * 
+     * @return the last attempted registration endpoint
+     */
     @Transient
     public EndPoint getLastAttemptedRegistrationEndpoint(){
     	List<EndPoint> reList=getRegistrationEndpoints();
@@ -235,6 +373,13 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
     	return reList.get(0);
     }
     
+    /**
+     * If study investigator exists.
+     * 
+     * @param healthcareInvestigator the healthcare investigator
+     * 
+     * @return true, if successful
+     */
     @Transient
     public boolean ifStudyInvestigatorExists(HealthcareSiteInvestigator healthcareInvestigator){
     	for (StudyInvestigator studyInvestigator:getStudyInvestigators()){
@@ -245,6 +390,13 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
     	return false;
     }
     
+    /**
+     * Checks if is successfull send.
+     * 
+     * @param apiName the api name
+     * 
+     * @return true, if is successfull send
+     */
     @Transient
     public boolean isSuccessfullSend(APIName apiName){
         for(EndPoint endPoint:getEndpoints()){
@@ -254,6 +406,11 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
         return false;
     }
     
+    /**
+     * Gets the checks if is coordinating center.
+     * 
+     * @return the checks if is coordinating center
+     */
     @Transient
     public boolean getIsCoordinatingCenter(){
         return this.study.isCoOrdinatingCenter(this.getHealthcareSite().getNciInstituteCode());
@@ -262,21 +419,39 @@ public abstract class StudyOrganization extends InteroperableAbstractMutableDele
     
     
     
+    /**
+     * Gets the custom field definitions internal.
+     * 
+     * @return the custom field definitions internal
+     */
     @OneToMany(mappedBy = "studyOrganization", fetch = FetchType.LAZY)
 	@Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
 	public List<CustomFieldDefinition> getCustomFieldDefinitionsInternal() {
 		return lazyListHelper.getInternalList(CustomFieldDefinition.class);
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.duke.cabig.c3pr.domain.customfield.CustomFieldAuthorable#getCustomFieldDefinitions()
+	 */
 	@Transient
 	public List<CustomFieldDefinition> getCustomFieldDefinitions() {
 		return lazyListHelper.getLazyList(CustomFieldDefinition.class);
 	}
 
+	/**
+	 * Sets the custom field definitions internal.
+	 * 
+	 * @param customFieldDefinitions the new custom field definitions internal
+	 */
 	public void setCustomFieldDefinitionsInternal(List<CustomFieldDefinition> customFieldDefinitions) {
 		lazyListHelper.setInternalList(CustomFieldDefinition.class,customFieldDefinitions);
 	}
 
+	/**
+	 * Adds the custom field definition.
+	 * 
+	 * @param customFieldDefinition the custom field definition
+	 */
 	public void addCustomFieldDefinition(CustomFieldDefinition customFieldDefinition) {
 		this.getCustomFieldDefinitions().add(customFieldDefinition);
 		customFieldDefinition.setStudyOrganization(this);
