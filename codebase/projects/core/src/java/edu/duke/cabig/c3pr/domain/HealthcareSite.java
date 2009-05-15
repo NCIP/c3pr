43106efa4666aa182a69ddc72aa673af4ec0be1c
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-05-15 16:47:41 +0000

    added comments for java docs

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/HealthcareSite.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/HealthcareSite.java
@@ -20,7 +20,10 @@ import org.hibernate.annotations.Where;
 
 import gov.nih.nci.cabig.ctms.collections.LazyListHelper;
 
+// TODO: Auto-generated Javadoc
 /**
+ * The Class HealthcareSite.
+ * 
  * @author Priyatam
  * @author Kulasekaran
  * 
@@ -33,22 +36,36 @@ import gov.nih.nci.cabig.ctms.collections.LazyListHelper;
 @Where(clause = "retired_indicator  = 'false'")
 public abstract class HealthcareSite extends Organization implements Comparable<HealthcareSite> {
 
+    /** The nci institute code. */
     private String nciInstituteCode;
 
+    /** The healthcare site investigators. */
     private List<HealthcareSiteInvestigator> healthcareSiteInvestigators = new ArrayList<HealthcareSiteInvestigator>();
 
+    /** The research staffs. */
     private List<ResearchStaff> researchStaffs = new ArrayList<ResearchStaff>();
     
+    /** The participants. */
     private List<Participant> participants = new ArrayList<Participant>();
     
+    /** The external organizations. */
     protected List<HealthcareSite> externalOrganizations = new ArrayList<HealthcareSite>();
 
+    /**
+     * Sets the external organizations.
+     * 
+     * @param externalOrganizations the new external organizations
+     */
     public void setExternalOrganizations(List<HealthcareSite> externalOrganizations) {
 		this.externalOrganizations = externalOrganizations;
 	}
 
+	/** The lazy list helper. */
 	private LazyListHelper lazyListHelper;
     
+    /**
+     * Instantiates a new healthcare site.
+     */
     public HealthcareSite() {
         lazyListHelper = new LazyListHelper();
         lazyListHelper.add(InvestigatorGroup.class,
@@ -56,72 +73,123 @@ public abstract class HealthcareSite extends Organization implements Comparable<
                                         InvestigatorGroup.class, this, "HealthcareSite", new Class[] { HealthcareSite.class }));
     }
 
+    /**
+     * Adds the healthcare site investigator.
+     * 
+     * @param hcsi the hcsi
+     */
     public void addHealthcareSiteInvestigator(HealthcareSiteInvestigator hcsi) {
         healthcareSiteInvestigators.add(hcsi);
         hcsi.setHealthcareSite(this);
     }
 
+    /**
+     * Adds the investigator group.
+     * 
+     * @param invGroup the inv group
+     */
     public void addInvestigatorGroup(InvestigatorGroup invGroup) {
         this.getInvestigatorGroups().add(invGroup);
     }
 
+    /**
+     * Removes the healthcare site investigator.
+     * 
+     * @param hcsi the hcsi
+     */
     public void removeHealthcareSiteInvestigator(HealthcareSiteInvestigator hcsi) {
         healthcareSiteInvestigators.remove(hcsi);
     }
 
-    @Transient
-    public int getUnsavedInvestigatorGroupsLastIndex() {
-        if ((this.getInvestigatorGroups().size() > 0)
-                        && (this.getInvestigatorGroups().get(
-                                        this.getInvestigatorGroups().size() - 1).getName()) == null) {
-            return this.getInvestigatorGroups().size() - 1;
-        }
-        return -1;
-    }
-
+    /**
+     * Gets the healthcare site investigators.
+     * 
+     * @return the healthcare site investigators
+     */
     @OneToMany(mappedBy = "healthcareSite", fetch = FetchType.LAZY)
     @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     public List<HealthcareSiteInvestigator> getHealthcareSiteInvestigators() {
         return healthcareSiteInvestigators;
     }
 
+    /**
+     * Sets the healthcare site investigators.
+     * 
+     * @param healthcareSiteInvestigators the new healthcare site investigators
+     */
     public void setHealthcareSiteInvestigators(
                     List<HealthcareSiteInvestigator> healthcareSiteInvestigators) {
         this.healthcareSiteInvestigators = healthcareSiteInvestigators;
     }
 
+    /**
+     * Gets the research staffs.
+     * 
+     * @return the research staffs
+     */
     @OneToMany(mappedBy = "healthcareSite", fetch = FetchType.LAZY)
     @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     public List<ResearchStaff> getResearchStaffs() {
         return researchStaffs;
     }
 
+    /**
+     * Sets the research staffs.
+     * 
+     * @param researchStaffs the new research staffs
+     */
     public void setResearchStaffs(List<ResearchStaff> researchStaffs) {
         this.researchStaffs = researchStaffs;
     }
 
+    /**
+     * Adds the research staff.
+     * 
+     * @param rs the rs
+     */
     public void addResearchStaff(ResearchStaff rs) {
         researchStaffs.add(rs);
     }
 
+    /**
+     * Removes the research staff.
+     * 
+     * @param rs the rs
+     */
     public void removeResearchStaff(ResearchStaff rs) {
         researchStaffs.remove(rs);
     }
     
+    /**
+     * Gets the nci institute code.
+     * 
+     * @return the nci institute code
+     */
     @Transient
     public String getNciInstituteCode() {
         return nciInstituteCode;
     }
 
+    /**
+     * Sets the nci institute code.
+     * 
+     * @param nciInstituteCode the new nci institute code
+     */
     public void setNciInstituteCode(String nciInstituteCode) {
         this.nciInstituteCode = nciInstituteCode;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
     public int compareTo(HealthcareSite o) {
         if (this.equals((HealthcareSite) o)) return 0;
         else return 1;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.Organization#hashCode()
+     */
     @Override
     public int hashCode() {
         final int PRIME = 31;
@@ -130,6 +198,9 @@ public abstract class HealthcareSite extends Organization implements Comparable<
         return result;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.Organization#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -143,40 +214,80 @@ public abstract class HealthcareSite extends Organization implements Comparable<
         return true;
     }
 
+    /**
+     * Gets the investigator groups internal.
+     * 
+     * @return the investigator groups internal
+     */
     @OneToMany(mappedBy = "healthcareSite", fetch = FetchType.LAZY)
     @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     public List<InvestigatorGroup> getInvestigatorGroupsInternal() {
         return lazyListHelper.getInternalList(InvestigatorGroup.class);
     }
 
+    /**
+     * Sets the investigator groups internal.
+     * 
+     * @param investigatorGroups the new investigator groups internal
+     */
     public void setInvestigatorGroupsInternal(List<InvestigatorGroup> investigatorGroups) {
         this.lazyListHelper.setInternalList(InvestigatorGroup.class, investigatorGroups);
     }
 
+    /**
+     * Gets the investigator groups.
+     * 
+     * @return the investigator groups
+     */
     @Transient
     public List<InvestigatorGroup> getInvestigatorGroups() {
         return lazyListHelper.getLazyList(InvestigatorGroup.class);
     }
 
+    /**
+     * Sets the investigator groups.
+     * 
+     * @param investigatorGroups the new investigator groups
+     */
     public void setInvestigatorGroups(List<InvestigatorGroup> investigatorGroups) {
     }
     
+    /**
+     * Gets the participants.
+     * 
+     * @return the participants
+     */
     @ManyToMany(mappedBy = "healthcareSites" )
     @Cascade(value = { CascadeType.LOCK})
     public List<Participant> getParticipants() {
         return participants;
     }
 
+    /**
+     * Sets the participants.
+     * 
+     * @param participants the new participants
+     */
     public void setParticipants(List<Participant> participants) {
     	this.participants = participants;
     }
     
+    /**
+     * Gets the external organizations.
+     * 
+     * @return the external organizations
+     */
     @Transient
 	public List<HealthcareSite> getExternalOrganizations() {
 		return externalOrganizations;
 	}
     
-    public void addExternalOganization(HealthcareSite externalHealthcareSite){
+    /**
+     * Adds the external organization.
+     * 
+     * @param externalHealthcareSite the external healthcare site
+     */
+    public void addExternalOrganization(HealthcareSite externalHealthcareSite){
     	this.getExternalOrganizations().add(externalHealthcareSite);
     }
 
