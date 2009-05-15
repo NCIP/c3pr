429739454142ef2554867d3fde2b400cf9d10034
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-05-15 20:31:01 +0000

    Introduced the inv status enum

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/constants/InvestigatorStatusCodeEnum.java
@@ -0,0 +1,33 @@
+package edu.duke.cabig.c3pr.constants;
+
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.getByClassAndCode;
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.register;
+import static gov.nih.nci.cabig.ctms.domain.EnumHelper.sentenceCasedName;
+import gov.nih.nci.cabig.ctms.domain.CodedEnum;
+
+public enum InvestigatorStatusCodeEnum implements CodedEnum<String> {
+    AC("Active"), IN("Inactive");
+
+    private String code;
+
+    private InvestigatorStatusCodeEnum(String code) {
+        this.code = code;
+        register(this);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDisplayName() {
+        return sentenceCasedName(this);
+    }
+
+    public String getName() {
+        return name();
+    }
+
+    public static InvestigatorStatusCodeEnum getByCode(String code) {
+        return getByClassAndCode(InvestigatorStatusCodeEnum.class, code);
+    }
+}
