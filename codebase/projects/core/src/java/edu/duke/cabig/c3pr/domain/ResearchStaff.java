a0639ef5515eec3c756cb01c0948e930ef8bb729
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-05-15 19:12:42 +0000

    added comments for java docs

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/ResearchStaff.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/ResearchStaff.java
@@ -20,7 +20,10 @@ import org.hibernate.annotations.Parameter;
 
 import com.semanticbits.coppa.domain.annotations.RemoteProperty;
 
+// TODO: Auto-generated Javadoc
 /**
+ * The Class ResearchStaff.
+ * 
  * @author Priyatam
  */
 @Entity
@@ -29,50 +32,91 @@ import com.semanticbits.coppa.domain.annotations.RemoteProperty;
 @GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "research_staff_id_seq") })
 public abstract class ResearchStaff extends User {
 
+    /** The study personnels. */
     private List<StudyPersonnel> studyPersonnels = new ArrayList<StudyPersonnel>();
 
+    /** The nci identifier. */
     private String nciIdentifier;
     
+    /** The healthcare site. */
     private HealthcareSite healthcareSite;
     
+    /** The user based recipient. */
     private List<UserBasedRecipient> userBasedRecipient;
     
+    /** The external research staff. */
     protected List<ResearchStaff> externalResearchStaff = new ArrayList<ResearchStaff>();
     
+	/**
+	 * Instantiates a new research staff.
+	 */
 	public ResearchStaff() {
 		super();
 	}
 	
+	/**
+	 * Gets the external research staff.
+	 * 
+	 * @return the external research staff
+	 */
 	@Transient
 	public List<ResearchStaff> getExternalResearchStaff() {
 		return externalResearchStaff;
 	}
 
+	/**
+	 * Sets the external research staff.
+	 * 
+	 * @param externalResearchStaff the new external research staff
+	 */
 	public void setExternalResearchStaff(List<ResearchStaff> externalResearchStaff) {
 		this.externalResearchStaff = externalResearchStaff;
 	}
 	
+	/**
+	 * Adds the external research staff.
+	 * 
+	 * @param externalResearchStaff the external research staff
+	 */
 	public void addExternalResearchStaff(ResearchStaff externalResearchStaff){
 	    	this.getExternalResearchStaff().add(externalResearchStaff);
 	    }
 
 
+	/**
+	 * Adds the study personnel.
+	 * 
+	 * @param studyPersonnel the study personnel
+	 */
 	public void addStudyPersonnel(StudyPersonnel studyPersonnel) {
         getStudyPersonnels().add(studyPersonnel);
     }
 
     // / BEAN METHODS
 
+    /**
+     * Gets the study personnels.
+     * 
+     * @return the study personnels
+     */
     @OneToMany(mappedBy = "researchStaff")
     @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     public List<StudyPersonnel> getStudyPersonnels() {
         return studyPersonnels;
     }
 
+    /**
+     * Sets the study personnels.
+     * 
+     * @param studyPersonnels the new study personnels
+     */
     public void setStudyPersonnels(List<StudyPersonnel> studyPersonnels) {
         this.studyPersonnels = studyPersonnels;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.Person#getContactMechanisms()
+     */
     @OneToMany
     @Cascade(value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     @JoinColumn(name = "RS_ID")
@@ -81,25 +125,48 @@ public abstract class ResearchStaff extends User {
         return contactMechanisms;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.Person#setContactMechanisms(java.util.List)
+     */
     public void setContactMechanisms(List<ContactMechanism> contactMechanisms) {
         this.contactMechanisms = contactMechanisms;
     }
 
+    /**
+     * Gets the healthcare site.
+     * 
+     * @return the healthcare site
+     */
     @ManyToOne
     @JoinColumn(name = "HCS_ID")
     public HealthcareSite getHealthcareSite() {
         return healthcareSite;
     }
 
+    /**
+     * Sets the healthcare site.
+     * 
+     * @param healthcareSite the new healthcare site
+     */
     public void setHealthcareSite(HealthcareSite healthcareSite) {
         this.healthcareSite = healthcareSite;
     }
 
+    /**
+     * Compare to.
+     * 
+     * @param o the o
+     * 
+     * @return the int
+     */
     public int compareTo(Object o) {
         if (this.equals((ResearchStaff) o)) return 0;
         else return 1;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.Person#hashCode()
+     */
     @Override
     public int hashCode() {
         final int PRIME = 31;
@@ -109,6 +176,9 @@ public abstract class ResearchStaff extends User {
         return result;
     }
 
+    /* (non-Javadoc)
+     * @see edu.duke.cabig.c3pr.domain.Person#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -121,15 +191,30 @@ public abstract class ResearchStaff extends User {
         return true;
     }
 
+    /**
+     * Gets the nci identifier.
+     * 
+     * @return the nci identifier
+     */
     @RemoteProperty
     public String getNciIdentifier() {
         return nciIdentifier;
     }
 
+    /**
+     * Sets the nci identifier.
+     * 
+     * @param nciIdentifier the new nci identifier
+     */
     public void setNciIdentifier(String nciIdentifier) {
         this.nciIdentifier = nciIdentifier;
     }
 
+    /**
+     * Gets the user based recipient.
+     * 
+     * @return the user based recipient
+     */
     @OneToMany
     @Cascade(value = { CascadeType.LOCK})
     @JoinColumn(name = "research_staff_id")
@@ -137,6 +222,11 @@ public abstract class ResearchStaff extends User {
 		return userBasedRecipient;
 	}
 
+	/**
+	 * Sets the user based recipient.
+	 * 
+	 * @param userBasedRecipient the new user based recipient
+	 */
 	public void setUserBasedRecipient(List<UserBasedRecipient> userBasedRecipient) {
 		this.userBasedRecipient = userBasedRecipient;
 	}
