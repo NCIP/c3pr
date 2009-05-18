4a9cb25d4ee0d7a31dc9ed8ea3975bcc785f657d
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-05-18 18:40:38 +0000

    Domain layer test coverage work.

---
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/ScheduledNotificationTest.java
@@ -0,0 +1,33 @@
+package edu.duke.cabig.c3pr.domain;
+
+import edu.duke.cabig.c3pr.AbstractTestCase;
+
+/**
+ * The Class ScheduledNotificationTest.
+ */
+public class ScheduledNotificationTest extends AbstractTestCase{
+	
+	/** The Constant MESSAGE_BODY. */
+	public static final String MESSAGE_BODY = "This is the message body!";
+	
+	/**
+	 * Test get html based message used in report generation.
+	 * 
+	 * @throws Exception the exception
+	 */
+	public void testGetHtmlMessage() throws Exception {
+		ScheduledNotification scheduledNotification = new ScheduledNotification();
+		
+		StringBuffer message = new StringBuffer();
+		message.append("<html><body>");
+		message.append(MESSAGE_BODY);
+		message.append("</body></html>");
+		
+		scheduledNotification.setMessage(message.toString());
+		
+		assertEquals(MESSAGE_BODY, scheduledNotification.getHtmlMessage());
+	}
+
+	
+}
+
