fd968a65282325da0d642b017358d1dcde815c1e
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-05-19 15:47:47 +0000

    Domain layer test coverage work.

--- codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/StratumGroupTest.java
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/StratumGroupTest.java
@@ -7,6 +7,12 @@ import edu.duke.cabig.c3pr.AbstractTestCase;
  */
 public class StratumGroupTest extends AbstractTestCase{
 	
+	/** The Constant QUESTION_1. */
+	public static final String QUESTION_1 = "question 1";
+	
+	/** The Constant QUESTION_2. */
+	public static final String QUESTION_2 = "question 2";
+	
 	/** The Constant ANSWER_1. */
 	public static final String ANSWER_1 = "answer_1";
 	
@@ -14,6 +20,44 @@ public class StratumGroupTest extends AbstractTestCase{
 	public static final String ANSWER_2 = "answer_2";
 	
 
+	public void testEquals(){
+		
+		StratificationCriterionAnswerCombination scac1 = new StratificationCriterionAnswerCombination();
+		
+		StratificationCriterion sc1 = new StratificationCriterion();
+		StratificationCriterionPermissibleAnswer scpa1 = new StratificationCriterionPermissibleAnswer();
+		
+		sc1.setQuestionNumber(1);
+		sc1.setQuestionText(QUESTION_1);
+		scpa1.setPermissibleAnswer(ANSWER_1);
+		sc1.getPermissibleAnswers().add(scpa1);
+		
+		scac1.setStratificationCriterion(sc1);
+		scac1.setStratificationCriterionPermissibleAnswer(scpa1);
+		
+		StratificationCriterionAnswerCombination scac2 = new StratificationCriterionAnswerCombination();
+		
+		StratificationCriterion sc2 = new StratificationCriterion();		
+		StratificationCriterionPermissibleAnswer scpa2 = new StratificationCriterionPermissibleAnswer();
+		
+		sc2.setQuestionNumber(1);
+		sc2.setQuestionText(QUESTION_1);
+		scpa2.setPermissibleAnswer(ANSWER_1);
+		sc2.getPermissibleAnswers().add(scpa2);
+		
+		scac2.setStratificationCriterion(sc2);
+		scac2.setStratificationCriterionPermissibleAnswer(scpa2);
+		
+		StratumGroup stratumGroup1 = new StratumGroup();
+		stratumGroup1.getStratificationCriterionAnswerCombination().add(scac1);
+		
+		StratumGroup stratumGroup2 = new StratumGroup();
+		stratumGroup2.getStratificationCriterionAnswerCombination().add(scac2);
+		
+		assertTrue(stratumGroup1.equals(stratumGroup2));
+	}
+	
+	
 	/**
 	 * Test set retired indicator as true.
 	 */
