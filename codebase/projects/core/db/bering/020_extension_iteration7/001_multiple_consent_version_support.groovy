0884b2b75fadf2f285e222f2531289ef7d560842
Himanshu Gupta <himanshu.iit@gmail.com>
2009-05-27 17:35:30 +0000

    multiple consent version

--- codebase/projects/core/db/bering/020_extension_iteration7/001_multiple_consent_version_support.groovy
+++ codebase/projects/core/db/bering/020_extension_iteration7/001_multiple_consent_version_support.groovy
@@ -1,16 +1,16 @@
 class MultipleConsentVersionSupport extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         createTable("consents") { t ->
-        	t.addColumn('retired_indicator', 'string', nullable:true)
-       		t.addColumn('grid_id' , 'string' , nullable:true)
+        	t.addColumn("retired_indicator", "string", nullable:true)
+       		t.addColumn("grid_id" , "string" , nullable:true)
        		t.addVersionColumn()
             t.addColumn("name", "string", nullable:false)
             t.addColumn("study_id", "integer", nullable:false)
         }
         
         createTable("consent_versions") { t ->
-            t.addColumn('retired_indicator', 'string', nullable:true)
-       		t.addColumn('grid_id' , 'string' , nullable:true)
+            t.addColumn("retired_indicator", "string", nullable:true)
+       		t.addColumn("grid_id" , "string" , nullable:true)
        		t.addVersionColumn()
             t.addColumn("name", "string",nullable:false)
             t.addColumn("date", "date" ,nullable:false)
@@ -19,15 +19,15 @@ class MultipleConsentVersionSupport extends edu.northwestern.bioinformatics.beri
         }
         
         createTable("stu_sub_cosnt_vers") { t ->
-        	t.addColumn('retired_indicator', 'string', nullable:true)
-       		t.addColumn('grid_id' , 'string' , nullable:true)
+        	t.addColumn("retired_indicator", "string", nullable:true)
+       		t.addColumn("grid_id" , "string" , nullable:true)
        		t.addVersionColumn()
             t.addColumn("informed_consent_signed_date", "date")
             t.addColumn("stu_sub_id", "integer",nullable:false)
             t.addColumn("consent_version_id", "integer",nullable:false)
         }
         
-          addColumn('studies','consent_validity_period','string');
+          addColumn("studies","consent_validity_period","string");
     }
 
     void down() {
