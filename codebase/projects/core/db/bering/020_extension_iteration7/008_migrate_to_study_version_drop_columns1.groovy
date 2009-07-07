f83402990371ff86d68df8b335c3b7ba359eda82
Himanshu Gupta <himanshu.iit@gmail.com>
2009-07-07 13:44:07 +0000

    study version column drop consent version

---
+++ codebase/projects/core/db/bering/020_extension_iteration7/008_migrate_to_study_version_drop_columns1.groovy
@@ -0,0 +1,9 @@
+class MigrateToStudyVersionFropColumns1 extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+      	dropColumn('studies', 'consent_version');
+    }
+
+	void down() {
+	    
+    }
+} 
