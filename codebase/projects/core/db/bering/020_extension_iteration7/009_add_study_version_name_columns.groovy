39e377513f0cf24218696aee152d8277a128e761
Himanshu Gupta <himanshu.iit@gmail.com>
2009-08-04 15:20:56 +0000

    groovy merged

--- codebase/projects/core/db/bering/020_extension_iteration7/009_add_study_version_name_columns.groovy
+++
@@ -1,9 +0,0 @@
-class MigrateToStudyVersionFropColumns1 extends edu.northwestern.bioinformatics.bering.Migration {
-    void up() {
-      	addColumn('study_versions', 'name', 'string');
-    }
-
-	void down() {
-	    dropColumn('study_versions', 'name');
-    }
-} 
--- codebase/projects/core/db/bering/020_extension_iteration7/010_add_amendment_reasons_column_to_study_version.groovy
+++
@@ -1,9 +0,0 @@
-class addAmendmentReasonColumnToStudyVersion extends edu.northwestern.bioinformatics.bering.Migration {
-    void up() {
-      	addColumn('study_versions', 'amendment_reason', 'string');
-    }
-
-	void down() {
-	    dropColumn('study_versions', 'amendment_reason');
-    }
-}
