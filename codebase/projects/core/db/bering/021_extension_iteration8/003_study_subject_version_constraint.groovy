1d253be54a8536f07cbf05385f1c8da696a2c6f9
Himanshu Gupta <himanshu.iit@gmail.com>
2009-08-04 15:20:22 +0000

    grovvy merged

--- codebase/projects/core/db/bering/021_extension_iteration8/003_study_subject_version_constraint.groovy
+++ codebase/projects/core/db/bering/021_extension_iteration8/003_study_subject_version_constraint.groovy
@@ -4,21 +4,21 @@ class StudySubjecVersionConstraint extends edu.northwestern.bioinformatics.berin
 		dropColumn("scheduled_epochs","spa_id")
 		addColumn('scheduled_epochs','study_subject_ver_id','integer');
 		execute("ALTER TABLE SCHEDULED_EPOCHS ADD CONSTRAINT FK_SCEPH_SS FOREIGN KEY (study_subject_ver_id) REFERENCES study_subject_versions (ID)");
-		
-				
+
+
 		//drop FK study subject to study site
 		execute("alter table study_subjects drop constraint fk_spa_sto")
 		//drop column study site in study subject
 		dropColumn("study_subjects","sto_id")
 		//add FK study site study version to study subject study version
-		execute("ALTER TABLE study_subject_versions ADD CONSTRAINT FK_SSV_SSSV FOREIGN KEY (study_site_ver_id) REFERENCES stu_site_stu_versions (ID)");
+		execute("ALTER TABLE study_subject_versions ADD CONSTRAINT FK_SSV_SSSV FOREIGN KEY (study_site_ver_id) REFERENCES study_site_versions (ID)");
 		//add FK study subject to study subject study version
 		execute("ALTER TABLE study_subject_versions ADD CONSTRAINT FK_SSV_SS FOREIGN KEY (spa_id) REFERENCES study_subjects (ID)");
-		
-		
+
+
 		//add FK study subject version to study subject consent version
 		execute("ALTER TABLE study_subject_consents ADD CONSTRAINT FK_SSC_SSV FOREIGN KEY (study_subject_ver_id) REFERENCES study_subject_versions (ID)");
-		
+
 		//add FK consent version to study subject consent version
 		execute("ALTER TABLE study_subject_consents ADD CONSTRAINT FK_SSC_CV FOREIGN KEY (consent_version_id) REFERENCES consent_versions (ID)");
     }
