998a579c93bed83f0abc5a7cca2cc214ddfd087d
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-09-03 17:53:37 +0000

    oracle compliance

--- codebase/projects/core/db/bering/021_extension_iteration8/008_icd9_disease_sites_migration.groovy
+++ codebase/projects/core/db/bering/021_extension_iteration8/008_icd9_disease_sites_migration.groovy
@@ -1,9 +1,9 @@
 class ICD9DiseaseSitesMigration extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-    	 execute("update disease_history set other_disease_site_code =NULL where other_disease_site_code like '(Begin typing here)' or other_disease_site_code like ''");
-    	 execute("update disease_history set other_disease_site_code = anatomic_sites.name from anatomic_sites where disease_history.anatomic_site_id = anatomic_sites.id");
-    	 execute("alter table disease_history add column icd9_disease_site_id integer");
-    	 execute("alter table disease_history add constraint FK_DHI_ICD9 FOREIGN KEY (icd9_disease_site_id) REFERENCES icd9_disease_sites(ID)");
+    	execute("update disease_history set other_disease_site_code =NULL where other_disease_site_code like '(Begin typing here)' or other_disease_site_code like ''");
+    	execute("update disease_history set other_disease_site_code = (select anatomic_sites.name from anatomic_sites where disease_history.anatomic_site_id = anatomic_sites.id)");
+		execute("alter table disease_history add icd9_disease_site_id integer");
+    	execute("alter table disease_history add constraint FK_DHI_ICD9 FOREIGN KEY (icd9_disease_site_id) REFERENCES icd9_disease_sites(ID)");
     }
 
 	void down() {
