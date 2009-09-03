84b83af9f85b99afae9ad4759cba3e8755a3e717
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-09-03 19:22:26 +0000

    oracle compliance

--- codebase/projects/core/db/bering/021_extension_iteration8/011_alter_icd9_disease_sites_code_to_string_not_nullable.groovy
+++ codebase/projects/core/db/bering/021_extension_iteration8/011_alter_icd9_disease_sites_code_to_string_not_nullable.groovy
@@ -1,7 +1,12 @@
 class ChangeICD9DiseaseSitesCodeToStringNotNullable extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-    	execute("alter table icd9_disease_sites alter column code type text")
-    	execute("alter table icd9_disease_sites alter column code set not null")
+	    if (databaseMatches('oracle')) {
+		   execute("alter table icd9_disease_sites modify (code varchar2(10) not null)")
+	 	}
+	 	if (databaseMatches('postgres')) {
+		   	execute("alter table icd9_disease_sites alter column code type text")
+	    	execute("alter table icd9_disease_sites alter column code set not null")
+	 	}
     }
 
     void down() {
