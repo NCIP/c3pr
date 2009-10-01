47611abc5ff6fe1a0725f57a17df0cd0702dc0ad
kruttik Aggarwal <kruttikagarwal@gmail.com>
2009-10-01 17:56:53 +0000

    added test cases for contact mechanisms. Related to CPR-899.

--- codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/PersonTestCase.java
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/PersonTestCase.java
@@ -284,9 +284,147 @@ public class PersonTestCase extends AbstractTestCase {
 		assertFalse("comparing person object to another person", person.equals(newPerson));
 	}
 
+	/**
+	 * Test set invalid email.
+	 */
+	public void testSetInvalidEmail(){
+		Person newPerson = new PersonSubClass();
+		try {
+			newPerson.setEmail("aaa@bbb");
+			fail("Should have failed");
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Test set email.
+	 */
 	public void testSetEmail(){
 		Person newPerson = new PersonSubClass();
 		newPerson.setEmail("aaa@bbb.com");
+		assertEquals("aaa@bbb.com", newPerson.getEmail());
+		assertEquals(1, newPerson.getContactMechanisms().size());
+	}
+	
+	/**
+	 * Test set invalid phone.
+	 */
+	public void testSetInvalidPhone(){
+		Person newPerson = new PersonSubClass();
+		try {
+			newPerson.setPhone("123456789");
+			fail("Should have failed");
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Test set email.
+	 */
+	public void testSetPhone1(){
+		Person newPerson = new PersonSubClass();
+		newPerson.setPhone("1234567890");
+		assertEquals("1234567890", newPerson.getPhone());
+		assertEquals(1, newPerson.getContactMechanisms().size());
+	}
+	
+	/**
+	 * Test set email.
+	 */
+	public void testSetPhone2(){
+		Person newPerson = new PersonSubClass();
+		newPerson.setPhone("123-456-7890");
+		assertEquals("123-456-7890", newPerson.getPhone());
+		assertEquals(1, newPerson.getContactMechanisms().size());
+	}
+	
+	/**
+	 * Test set invalid phone.
+	 */
+	public void testSetInvalidFax(){
+		Person newPerson = new PersonSubClass();
+		try {
+			newPerson.setFax("123456789");
+			fail("Should have failed");
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Test set email.
+	 */
+	public void testSetFax1(){
+		Person newPerson = new PersonSubClass();
+		newPerson.setFax("1234567890");
+		assertEquals("1234567890", newPerson.getFax());
+		assertEquals(1, newPerson.getContactMechanisms().size());
+	}
+	
+	/**
+	 * Test set email.
+	 */
+	public void testSetFax2(){
+		Person newPerson = new PersonSubClass();
+		newPerson.setFax("123-456-7890");
+		assertEquals("123-456-7890", newPerson.getFax());
+		assertEquals(1, newPerson.getContactMechanisms().size());
+	}
+	
+	/**
+	 * Test contact mechanism size.
+	 */
+	public void testContactMechanismSize(){
+		Person newPerson = new PersonSubClass();
+		newPerson.setFax("123-456-7890");
+		newPerson.setEmail("aaa@bbb.com");
+		newPerson.setPhone("1234567890");
+		assertEquals("123-456-7890", newPerson.getFax());
+		assertEquals("aaa@bbb.com", newPerson.getEmail());
+		assertEquals("1234567890", newPerson.getPhone());
+		assertEquals(3, newPerson.getContactMechanisms().size());
+		
+		newPerson.setFax("123-456-0000");
+		newPerson.setEmail("ppp@qqq.com");
+		newPerson.setPhone("1111111111");
+		assertEquals("123-456-0000", newPerson.getFax());
+		assertEquals("ppp@qqq.com", newPerson.getEmail());
+		assertEquals("1111111111", newPerson.getPhone());
+		assertEquals(3, newPerson.getContactMechanisms().size());
+	}
+	
+	/**
+	 * Test contact mechanism size.
+	 */
+	public void testSetContactMechanismNullValue(){
+		Person newPerson = new PersonSubClass();
+		newPerson.setFax("");
+		newPerson.setEmail(null);
+		newPerson.setPhone(null);
+		assertNull(newPerson.getFax());
+		assertNull(newPerson.getPhone());
+		assertNull(newPerson.getEmail());
+		assertEquals(0, newPerson.getContactMechanisms().size());
+		
+		newPerson.setFax("123-456-7890");
+		newPerson.setEmail("aaa@bbb.com");
+		newPerson.setPhone("1234567890");
+		assertEquals("123-456-7890", newPerson.getFax());
+		assertEquals("aaa@bbb.com", newPerson.getEmail());
+		assertEquals("1234567890", newPerson.getPhone());
+		assertEquals(3, newPerson.getContactMechanisms().size());
+		newPerson.setFax("");
+		newPerson.setEmail(null);
+		newPerson.setPhone(null);
+		assertNull(newPerson.getFax());
+		assertNull(newPerson.getPhone());
+		assertNull(newPerson.getEmail());
+		assertEquals(0, newPerson.getContactMechanisms().size());
 	}
 	
 }
