b8f5ef632c942b27c1e6c65b9e06b1da4a72fce9
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2009-10-08 15:39:42 +0000

    Added comments

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RemoteContactMechanism.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RemoteContactMechanism.java
@@ -4,10 +4,20 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 import com.semanticbits.coppa.domain.annotations.RemoteEntity;
-
+/**
+ * This class was created for testing the collection handling capapbility of the COPPA 
+ * interceptor framework. This functionality will be addressed in a future release. 
+ * 
+ * Although remote contacts will use this contact mechanism, it will not be marked as a remoteEntity(in app*-core settings).
+ * It wont have a resolver or an external Id and the interceptor will not treat it as a remote entity. 
+ * Meaning in order to update it, the parent Resolver (like InvestigatorResolver) will have to set it explicitly.
+ * 
+ * @author Vinay Gangoli
+ */
 @Entity
 @DiscriminatorValue("Remote")
 @RemoteEntity
 public class RemoteContactMechanism extends ContactMechanism {
+
 
 }
