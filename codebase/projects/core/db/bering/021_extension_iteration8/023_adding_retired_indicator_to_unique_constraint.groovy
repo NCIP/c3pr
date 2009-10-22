08aae5e4356dea19d18ba282e7bd1a0ddc653e45
Himanshu Gupta <himanshu.iit@gmail.com>
2009-10-22 19:37:35 +0000

    CPR-1120,  	 CPR-942

---
+++ codebase/projects/core/db/bering/021_extension_iteration8/023_adding_retired_indicator_to_unique_constraint.groovy
@@ -0,0 +1,19 @@
+class AddRtrdIndicatorToUnqConst extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+    	execute("ALTER TABLE epochs drop CONSTRAINT UK_STU_VER_EPH");
+    	execute("ALTER TABLE epochs ADD CONSTRAINT UK_STU_VER_EPH UNIQUE(name,stu_version_id,retired_indicator)");
+    	
+    	execute("ALTER TABLE epochs drop CONSTRAINT uk_eph_arm");
+    	execute("ALTER TABLE epochs ADD CONSTRAINT uk_eph_arm UNIQUE(name,eph_id,retired_indicator)");
+    	
+	}
+
+	void down() {
+		execute("ALTER TABLE epochs drop CONSTRAINT UK_STU_VER_EPH");
+    	execute("ALTER TABLE epochs ADD CONSTRAINT UK_STU_VER_EPH UNIQUE(name,stu_version_id)");
+    	
+    	execute("ALTER TABLE epochs drop CONSTRAINT uk_eph_arm");
+    	execute("ALTER TABLE epochs ADD CONSTRAINT uk_eph_arm UNIQUE(name,eph_id)");
+    	
+    }
+}
