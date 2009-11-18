0e00a997673ff053e19a6c77971bd955d5cc6ae5
kruttik Aggarwal <kruttikagarwal@gmail.com>
2009-11-18 21:35:05 +0000

    replaced the single quotes. Related to CPR-1271.

--- codebase/projects/core/db/bering/021_extension_iteration8/28_update_summary3_disease_sites.groovy
+++ codebase/projects/core/db/bering/021_extension_iteration8/28_update_summary3_disease_sites.groovy
@@ -1,7 +1,7 @@
 class UpdateSummary3DiseaseSites extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-    	execute("update summ3_rep_disease_sites set name='Non-Hodgkin's Lymphoma' where id=35;");
-    	execute("update summ3_rep_disease_sites set name='Hodgkin's Lymphoma' where id=36");
+    	execute("update summ3_rep_disease_sites set name='Non-Hodgkins Lymphoma' where id=35");
+    	execute("update summ3_rep_disease_sites set name='Hodgkins Lymphoma' where id=36");
     }
 
     void down() {
