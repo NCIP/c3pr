60c93fb74219e1836a3303e3ac26899ac94664ef
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-11-18 20:52:38 +0000

    CPR-832 updated

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/Summary3ReportDao.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/Summary3ReportDao.java
@@ -5,15 +5,12 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
-import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.CriteriaSpecification;
 import org.hibernate.criterion.Order;
 
-import edu.duke.cabig.c3pr.constants.OrganizationIdentifierTypeEnum;
-import edu.duke.cabig.c3pr.domain.ICD9DiseaseSite;
 import edu.duke.cabig.c3pr.domain.HealthcareSite;
-import edu.duke.cabig.c3pr.domain.Study;
-import edu.duke.cabig.c3pr.domain.StudySubject;
 import edu.duke.cabig.c3pr.domain.Summary3Report;
+import edu.duke.cabig.c3pr.domain.Summary3ReportDiseaseSite;
 import gov.nih.nci.cabig.ctms.dao.MutableDomainObjectDao;
 
 // TODO: Auto-generated Javadoc
@@ -36,45 +33,47 @@ public class Summary3ReportDao extends GridIdentifiableDao<Summary3Report> imple
 	/**
 	 * Gets the newly enrolled therapeutic study patients for given icd9 disease site.
 	 * 
-	 * @param diseaseSite the disease site
+	 * @param summary3ReportDiseaseSite the disease site
 	 * @param hcs the hcs
 	 * @param startDate the start date
 	 * @param endDate the end date
 	 * 
 	 * @return the newly enrolled therapeutic study patients for given anatomic site
 	 */
-	public int getNewlyEnrolledTherapeuticStudySubjectCountForGivenICD9DiseaseSite(
-			ICD9DiseaseSite diseaseSite, HealthcareSite hcs, Date startDate,
+	public int getNewlyEnrolledTherapeuticStudySubjectCountForGivenSummary3ReportDiseaseSite(
+			Summary3ReportDiseaseSite summary3ReportDiseaseSite, HealthcareSite hcs, Date startDate,
 			Date endDate) {
 		
 		 return  getHibernateTemplate().find("from StudySubject ss,StudySubjectStudyVersion ssv where ssv=any elements(ss.studySubjectStudyVersions) and " +
 		 		"ss.startDate >= ? and ss.startDate <= ? and " +
-		 		"ss.diseaseHistoryInternal.icd9DiseaseSite.name = ? and ssv.studySiteStudyVersion.studySite.studyInternal.type = ? " +
+		 		"ss.diseaseHistoryInternal.icd9DiseaseSite.summary3ReportDiseaseSite.name = ? and ssv.studySiteStudyVersion.studySite.studyInternal.type = ? " +
 		 		"and ssv.studySiteStudyVersion.studySite.healthcareSite.id in " +
 		 		"(select h.id from HealthcareSite h where " +
   			    "h.identifiersAssignedToOrganization.value=? and h.identifiersAssignedToOrganization.primaryIndicator = 'TRUE')",
-                new Object[] {startDate, endDate, diseaseSite.getName(), "Genetic Therapeutic", hcs.getCtepCode()}).size();
+                new Object[] {startDate, endDate, summary3ReportDiseaseSite.getName(), "Genetic Therapeutic", hcs.getCtepCode()}).size();
 	}
 
 	/**
 	 * Gets the newly registered patients for given anatomic site.
 	 * 
-	 * @param diseaseSite the disease site
+	 * @param summary3ReportDiseaseSite the disease site
 	 * @param hcs the hcs
 	 * @param startDate the start date
 	 * @param endDate the end date
 	 * 
 	 * @return the newly registered patients for given anatomic site
 	 */
-	public int getNewlyRegisteredSubjectCountForGivenICD9DiseaseSite(ICD9DiseaseSite diseaseSite,
+	public int getNewlyRegisteredSubjectCountForGivenSummary3ReportDiseaseSite(Summary3ReportDiseaseSite summary3ReportDiseaseSite,
 			HealthcareSite hcs, Date startDate, Date endDate) {
-		
-		 return  getHibernateTemplate().find("from StudySubject ss,StudySubjectStudyVersion ssv where ssv=any elements(ss.studySubjectStudyVersions) and" +
+		//commented out because C3PR is not the source of truth for newly registered patients.
+		 /*return  getHibernateTemplate().find("from StudySubject ss,StudySubjectStudyVersion ssv where ssv=any elements(ss.studySubjectStudyVersions) and" +
 		 		" ss.startDate >= ? and ss.startDate <= ? and " +
 		 		"ss.diseaseHistoryInternal.icd9DiseaseSite.name = ? and ssv.studySiteStudyVersion.studySite.healthcareSite.id in " +
 		 		"(select h.id from HealthcareSite h where " +
 		 		"h.identifiersAssignedToOrganization.value=? and h.identifiersAssignedToOrganization.primaryIndicator = 'TRUE'))",
-                new Object[] {startDate, endDate, diseaseSite.getName(), hcs.getCtepCode()}).size();
+                new Object[] {startDate, endDate, summary3ReportDiseaseSite.getName(), hcs.getCtepCode()}).size();*/
+		
+		return 0;
 	}
 	
 	/**
@@ -122,5 +121,12 @@ public class Summary3ReportDao extends GridIdentifiableDao<Summary3Report> imple
 	public void save(Summary3Report arg0) {
 		// not required
 	}
+	
+	 public List<Summary3ReportDiseaseSite> getAllOrderedByName(){
+	    	Criteria summary3ReportDiseaseSiteCriteria =getSession().createCriteria(Summary3ReportDiseaseSite.class);
+	    	summary3ReportDiseaseSiteCriteria.addOrder(Order.asc("name"));
+	    	summary3ReportDiseaseSiteCriteria.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
+	    	return summary3ReportDiseaseSiteCriteria.list();
+	    }
 
 }
