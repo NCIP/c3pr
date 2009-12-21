068d4c21389b15729d3eaeceb182c20ddbcf60c6
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-12-21 17:00:21 +0000

    cpr-949 moved not null FK constraint to later script until after the study versions are migrated

--- codebase/projects/core/db/bering/020_extension_iteration7/005_migrate_to_study_version.groovy
+++ codebase/projects/core/db/bering/020_extension_iteration7/005_migrate_to_study_version.groovy
@@ -10,7 +10,6 @@ class MigrateToStudyVersion extends edu.northwestern.bioinformatics.bering.Migra
 		execute("alter table epochs drop constraint FK_EPH_STU");
 		execute("ALTER TABLE epochs drop CONSTRAINT UK_STU_EPH");
 		execute("ALTER TABLE epochs ADD CONSTRAINT UK_STU_VER_EPH UNIQUE(name,stu_version_id)");
-		execute("ALTER TABLE epochs ADD CONSTRAINT FK_EPH_STU_VER FOREIGN KEY (stu_version_id) REFERENCES study_versions (ID)");
 		dropColumn('epochs', 'stu_id');
         
         // associating disease to study version
