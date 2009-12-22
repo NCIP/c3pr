30356b6a3ab94c67b31806e947919d731d74e818
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-12-22 19:34:53 +0000

    corrected syntax error

--- codebase/projects/core/db/bering/022_extension_iteration9/001_update_sequences_new_tables_for_study_versioning.groovy
+++ codebase/projects/core/db/bering/022_extension_iteration9/001_update_sequences_new_tables_for_study_versioning.groovy
@@ -1,12 +1,12 @@
 class UpdateSequencesNewTablesForStudyVersioning extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
       if (databaseMatches('postgres')){
-	    	select setVal('consents_id_seq',max(id)) from consents;
-			select setVal('study_versions_id_seq',max(id)) from study_versions;
-			select setVal('study_subject_consents_id_seq',max(id)) from study_subject_consents;
-			select setVal('study_site_versions_id_seq',max(id)) from study_site_versions;
-			select setVal('study_subject_versions_id_seq',max(id)) from study_subject_versions;
-			select setVal('stu_site_status_history_id_seq',max(id)) from stu_site_status_history;
+    	  execute("select setVal('consents_id_seq',max(id)) from consents");
+		  execute("select setVal('study_versions_id_seq',max(id)) from study_versions");
+		  execute("select setVal('study_subject_consents_id_seq',max(id)) from study_subject_consents");
+		  execute("select setVal('study_site_versions_id_seq',max(id)) from study_site_versions");
+		  execute("select setVal('study_subject_versions_id_seq',max(id)) from study_subject_versions");
+		  execute("select setVal('stu_site_status_history_id_seq',max(id)) from stu_site_status_history");
 		}
 	}
 
