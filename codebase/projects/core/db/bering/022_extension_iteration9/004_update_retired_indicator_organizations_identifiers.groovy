8cc0e9affa97edec7a55f3e8516b5e1a12ef0d2e
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-12-23 18:52:00 +0000

    cpr-839 updating missing retired_indicator in organizations and OAI identifiers with missing hcs_id

---
+++ codebase/projects/core/db/bering/022_extension_iteration9/004_update_retired_indicator_organizations_identifiers.groovy
@@ -0,0 +1,9 @@
+class UpdateRetiredIndicatorOrganizationsIdentifiers extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+       	execute("update organizations set retired_indicator='false' where retired_indicator is null or retired_indicator like ''");
+       		execute("update identifiers set hcs_id =16831 where dtype like 'OAI' and hcs_id is null");
+    }
+  
+    void down() {
+    }
+}
