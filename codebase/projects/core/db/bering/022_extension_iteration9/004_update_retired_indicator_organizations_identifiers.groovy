fcf36bcd076f8364fa7b610edf3c6d0798d98a5e
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2009-12-30 21:23:23 +0000

    formatting change

--- codebase/projects/core/db/bering/022_extension_iteration9/004_update_retired_indicator_organizations_identifiers.groovy
+++ codebase/projects/core/db/bering/022_extension_iteration9/004_update_retired_indicator_organizations_identifiers.groovy
@@ -1,7 +1,7 @@
 class UpdateRetiredIndicatorOrganizationsIdentifiers extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
        	execute("update organizations set retired_indicator='false' where retired_indicator is null or retired_indicator like ''");
-       		execute("update identifiers set hcs_id =16831 where dtype like 'OAI' and hcs_id is null");
+       	execute("update identifiers set hcs_id =16831 where dtype like 'OAI' and hcs_id is null");
     }
   
     void down() {
