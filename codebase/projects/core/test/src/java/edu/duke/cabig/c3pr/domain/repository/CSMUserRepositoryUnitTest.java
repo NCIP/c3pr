168e88b281a809690b7a7d80fe35ffad9e66f2b5
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-01-14 22:33:05 +0000

    Added unit test cases. Related to CPR-1497.

---
+++ codebase/projects/core/test/src/java/edu/duke/cabig/c3pr/domain/repository/CSMUserRepositoryUnitTest.java
@@ -0,0 +1,267 @@
+package edu.duke.cabig.c3pr.domain.repository;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.easymock.classextension.EasyMock;
+import org.springframework.mail.MailSender;
+
+import edu.duke.cabig.c3pr.AbstractTestCase;
+import edu.duke.cabig.c3pr.dao.UserDao;
+import edu.duke.cabig.c3pr.domain.repository.impl.CSMUserRepositoryImpl;
+import edu.duke.cabig.c3pr.domain.repository.impl.CSMUserRepositoryImpl.C3PRNoSuchUserException;
+import edu.duke.cabig.c3pr.exception.C3PRBaseRuntimeException;
+import gov.nih.nci.security.UserProvisioningManager;
+import gov.nih.nci.security.authorization.domainobjects.User;
+import gov.nih.nci.security.exceptions.CSObjectNotFoundException;
+import gov.nih.nci.security.exceptions.CSTransactionException;
+import gov.nih.nci.security.util.StringEncrypter;
+import gov.nih.nci.security.util.StringEncrypter.EncryptionException;
+
+public class CSMUserRepositoryUnitTest extends AbstractTestCase {
+
+	private UserProvisioningManager userProvisioningManager;
+	private UserDao userDao;
+	private MailSender mailSender;
+	private CSMUserRepository csmUserRepository;
+	private User csmUser;
+	private edu.duke.cabig.c3pr.domain.User c3prUser;
+	private String invalidUsername = "Invalid";
+	private String validUsername = "Valid";
+	private String invalidEmail = "Invalid@email.com";
+	private String validEmail = "Valid@email.com";
+	private String invalidLoginId = "InvalidLoginId";
+	private String validLoginId = "ValidLoginId";
+	private String oldPassword="old_password";
+	private String newPassword="new_password";
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		csmUser = registerMockFor(User.class);
+		userProvisioningManager = registerMockFor(UserProvisioningManager.class);
+		userDao = registerMockFor(UserDao.class);
+		mailSender = registerMockFor(MailSender.class);
+		CSMUserRepositoryImpl csmUserRepositoryImpl = new CSMUserRepositoryImpl();
+		csmUserRepository = csmUserRepositoryImpl;
+		csmUserRepositoryImpl.setMailSender(mailSender);
+		csmUserRepositoryImpl.setUserDao(userDao);
+		csmUserRepositoryImpl.setUserProvisioningManager(userProvisioningManager);
+		c3prUser = registerMockFor(edu.duke.cabig.c3pr.domain.User.class);
+	}
+	
+	public void testGetUserByNameInvalidUsername(){
+		EasyMock.expect(userProvisioningManager.getUser(invalidUsername)).andReturn(null);		
+		replayMocks();
+		try {
+			csmUserRepository.getUserByName(invalidUsername);
+			fail();
+		} catch (C3PRNoSuchUserException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			fail();
+		}finally{
+			verifyMocks();
+		}
+	}
+	
+	public void testGetUserByNameValidUsernameInvalidResearchStaff(){
+		EasyMock.expect(userProvisioningManager.getUser(validUsername)).andReturn(csmUser);
+		EasyMock.expect(csmUser.getEmailId()).andReturn(invalidEmail);
+		EasyMock.expect(userDao.getByEmailAddress(invalidEmail)).andReturn(null);
+		replayMocks();
+		try {
+			csmUserRepository.getUserByName(validUsername);
+			fail();
+		} catch (C3PRNoSuchUserException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			fail();
+		}finally{
+			verifyMocks();
+		}
+	}
+	
+	public void testGetUserByName(){
+		EasyMock.expect(userProvisioningManager.getUser(validUsername)).andReturn(csmUser);
+		EasyMock.expect(csmUser.getEmailId()).andReturn(validEmail);
+		EasyMock.expect(userDao.getByEmailAddress(validEmail)).andReturn(c3prUser);
+		replayMocks();
+		try {
+			csmUserRepository.getUserByName(validUsername);
+		}catch (Exception e) {
+			fail();
+		}finally{
+			verifyMocks();
+		}
+	}
+	
+	public void testGetUsernameByIdInvalidLoginId(){
+		try {
+			EasyMock.expect(userProvisioningManager.getUserById(invalidLoginId)).andThrow(new CSObjectNotFoundException());
+		} catch (CSObjectNotFoundException e1) {
+			fail();
+		}
+		replayMocks();
+		try {
+			csmUserRepository.getUsernameById(invalidLoginId);
+			fail();
+		} catch (C3PRNoSuchUserException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			fail();
+		}finally{
+			verifyMocks();
+		}
+	}
+	
+	public void testGetUsernameById(){
+		try {
+			EasyMock.expect(userProvisioningManager.getUserById(validLoginId)).andReturn(csmUser);
+		} catch (CSObjectNotFoundException e1) {
+			fail();
+		}
+		EasyMock.expect(csmUser.getLoginName()).andReturn(validUsername);
+		replayMocks();
+		try {
+			assertEquals(validUsername, csmUserRepository.getUsernameById(validLoginId));
+		}catch (Exception e) {
+			fail();
+		}finally{
+			verifyMocks();
+		}
+	}
+	
+	public void testUserChangePasswordException(){
+		EasyMock.expect(c3prUser.getLoginId()).andReturn(validLoginId);
+		try {
+			EasyMock.expect(userProvisioningManager.getUserById(validLoginId)).andReturn(csmUser);
+		} catch (CSObjectNotFoundException e1) {
+			fail();
+		}
+		c3prUser.resetToken();
+		c3prUser.setPasswordLastSet(EasyMock.isA(Timestamp.class));
+		EasyMock.expect(csmUser.getPassword()).andReturn(oldPassword);
+		c3prUser.addPasswordToHistory(oldPassword, 1);
+		EasyMock.expect(c3prUser.getSalt()).andReturn("salt_").times(2);
+		csmUser.setPassword("salt_"+newPassword);
+		userDao.save(c3prUser);
+		try {
+			userProvisioningManager.modifyUser(csmUser);
+			EasyMock.expectLastCall().andThrow(new CSTransactionException());
+		} catch (CSTransactionException e) {
+			fail();
+		}
+		replayMocks();
+		try {
+			csmUserRepository.userChangePassword(c3prUser, newPassword, 1);
+			fail();
+		} catch (C3PRBaseRuntimeException e) {
+			e.printStackTrace();
+		}finally{
+			verifyMocks();
+		}
+	}
+	
+	public void testUserChangePassword(){
+		EasyMock.expect(c3prUser.getLoginId()).andReturn(validLoginId);
+		try {
+			EasyMock.expect(userProvisioningManager.getUserById(validLoginId)).andReturn(csmUser);
+		} catch (CSObjectNotFoundException e1) {
+			fail();
+		}
+		c3prUser.resetToken();
+		c3prUser.setPasswordLastSet(EasyMock.isA(Timestamp.class));
+		EasyMock.expect(csmUser.getPassword()).andReturn(oldPassword);
+		c3prUser.addPasswordToHistory(oldPassword, 1);
+		EasyMock.expect(c3prUser.getSalt()).andReturn("salt_").times(2);
+		csmUser.setPassword("salt_"+newPassword);
+		userDao.save(c3prUser);
+		try {
+			userProvisioningManager.modifyUser(csmUser);
+		} catch (CSTransactionException e) {
+			fail();
+		}
+		replayMocks();
+		try {
+			csmUserRepository.userChangePassword(c3prUser, newPassword, 1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}finally{
+			verifyMocks();
+		}
+	}
+	
+	public void testUserHasPasswordTrue(){
+		EasyMock.expect(c3prUser.getLoginId()).andReturn(validLoginId);
+		try {
+			EasyMock.expect(userProvisioningManager.getUserById(validLoginId)).andReturn(csmUser);
+		} catch (CSObjectNotFoundException e1) {
+			fail();
+		}
+		try {
+			EasyMock.expect(csmUser.getPassword()).andReturn(new StringEncrypter().encrypt(newPassword));
+		} catch (EncryptionException e) {
+			e.printStackTrace();
+			fail();
+		}
+		EasyMock.expect(c3prUser.getSalt()).andReturn("");
+		replayMocks();
+		assertTrue(csmUserRepository.userHasPassword(c3prUser, newPassword));
+		verifyMocks();
+	}
+	
+	public void testUserHasPasswordFalse(){
+		EasyMock.expect(c3prUser.getLoginId()).andReturn(validLoginId);
+		try {
+			EasyMock.expect(userProvisioningManager.getUserById(validLoginId)).andReturn(csmUser);
+		} catch (CSObjectNotFoundException e1) {
+			fail();
+		}
+		try {
+			EasyMock.expect(csmUser.getPassword()).andReturn(new StringEncrypter().encrypt(oldPassword));
+		} catch (EncryptionException e) {
+			e.printStackTrace();
+			fail();
+		}
+		EasyMock.expect(c3prUser.getSalt()).andReturn("");
+		replayMocks();
+		assertFalse(csmUserRepository.userHasPassword(c3prUser, newPassword));
+		verifyMocks();
+	}
+	
+	public void testUserHadPasswordTrue(){
+		List<String> passwordHistory = new ArrayList<String>();
+		try {
+			passwordHistory.add(new StringEncrypter().encrypt(oldPassword));
+			passwordHistory.add(new StringEncrypter().encrypt(newPassword));
+		} catch (EncryptionException e) {
+			e.printStackTrace();
+			fail();
+		}
+		EasyMock.expect(c3prUser.getPasswordHistory()).andReturn(passwordHistory);
+		replayMocks();
+		assertTrue(csmUserRepository.userHadPassword(c3prUser, newPassword));
+		verifyMocks();
+	}
+	
+	public void testUserHadPasswordFalse(){
+		List<String> passwordHistory = new ArrayList<String>();
+		try {
+			passwordHistory.add(new StringEncrypter().encrypt(oldPassword));
+		} catch (EncryptionException e) {
+			e.printStackTrace();
+			fail();
+		}
+		EasyMock.expect(c3prUser.getPasswordHistory()).andReturn(passwordHistory);
+		replayMocks();
+		assertFalse(csmUserRepository.userHadPassword(c3prUser, newPassword));
+		verifyMocks();
+	}
+	
+	public void testSendUserMailException(){
+		
+	}
+}
