8530503da0348e91ace14dca65655884c10ccf00
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-01-22 17:17:39 +0000

    Removed the relaxed constraint on empty username. Related to CPR-1216.

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/AuditInfoFilter.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/AuditInfoFilter.java
@@ -12,10 +12,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.acegisecurity.Authentication;
 import org.acegisecurity.context.SecurityContextHolder;
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import edu.duke.cabig.c3pr.utils.web.filter.PreAuthenticationSetupFilter;
 
 public class AuditInfoFilter extends
                 gov.nih.nci.cabig.ctms.web.filters.ContextRetainingFilterAdapter {
 
+	private Logger log = Logger.getLogger(PreAuthenticationSetupFilter.class);
+	
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response,
                     final FilterChain chain) throws IOException, ServletException {
@@ -25,14 +30,14 @@ public class AuditInfoFilter extends
         if (authentication != null){
         	userName = SecurityContextHolder.getContext().getAuthentication().getName();
         }
-        if(StringUtils.isBlank(userName)){
-        	userName=request.getParameter("userName");
-        }
         if (!StringUtils.isBlank(userName)) {
+        	log.debug("setting audit info for "+ userName);
             gov.nih.nci.cabig.ctms.audit.DataAuditInfo
                             .setLocal(new gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo(
                             		userName, request.getRemoteAddr(), new Date(),
                                             httpReq.getRequestURI()));
+        }else{
+        	log.debug("no authentication found in SecurityContext. Skipping audit info setup");
         }
         chain.doFilter(request, response);
         edu.nwu.bioinformatics.commons.DataAuditInfo.setLocal(null);
