9ca99f31f27f04a7788789a05c10bbe41030e8d8
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-01-22 17:16:54 +0000

    Added a filter to set anonymous audit for setup flow requests. Related to CPR-1216.

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/AnonymousAuditInfoFilter.java
@@ -0,0 +1,33 @@
+package edu.duke.cabig.c3pr.utils.web;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+import edu.duke.cabig.c3pr.utils.web.filter.PreAuthenticationSetupFilter;
+
+public class AnonymousAuditInfoFilter extends
+                gov.nih.nci.cabig.ctms.web.filters.ContextRetainingFilterAdapter {
+
+	private Logger log = Logger.getLogger(PreAuthenticationSetupFilter.class);
+	
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response,
+                    final FilterChain chain) throws IOException, ServletException {
+    	log.debug("setting anonymous audit info");
+    	HttpServletRequest httpReq = (HttpServletRequest) request;
+        gov.nih.nci.cabig.ctms.audit.DataAuditInfo
+        .setLocal(new gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo(
+        		"anonymous", request.getRemoteAddr(), new Date(),
+                        ((HttpServletRequest)request).getRequestURI()));
+        chain.doFilter(request, response);
+        edu.nwu.bioinformatics.commons.DataAuditInfo.setLocal(null);
+    }
+}
