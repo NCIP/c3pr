8779be4090c946e691fdc1b0cfe05fdc85d49971
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-02-08 16:29:28 +0000

    Related to CPR-1508.

--- codebase/projects/core/db/bering/023_extension_iteration10/001_update_nci_identifier_column_investigators.groovy
+++ codebase/projects/core/db/bering/023_extension_iteration10/001_update_nci_identifier_column_investigators.groovy
@@ -1,11 +1,11 @@
 class UpdateNCIIdentidifierInvestigators extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
     	if (databaseMatches('postgres')){
-    	  execute("update investigators set nci_identifier = (select chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || '-' || ceil(random()*1000) || chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || '-' || ceil(random()*1000) from investigators inv where inv.id=investigators.id ) where nci_identifier is null");
+    	  execute("update investigators set nci_identifier = (select chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || '-' || ceil(random()*1000) || chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || chr(mod(CAST(ceil(random()*100) as Integer),26)+65) || '-' || ceil(random()*1000) from investigators inv where inv.id=investigators.id ) where nci_identifier is null or nci_identifier=''");
     	  execute("ALTER TABLE investigators ALTER COLUMN nci_identifier SET NOT NULL");
 		}
 		if (databaseMatches('oracle')){
-    	  execute("update investigators set nci_identifier =  (select dbms_random.string('U', 3) || '-' || cast(dbms_random.value(1,999) as int)  || dbms_random.string('U', 2) || '-' || cast(dbms_random.value(1,999) as int) from investigators inv where inv.id=investigators.id) where nci_identifier is null");
+    	  execute("update investigators set nci_identifier =  (select dbms_random.string('U', 3) || '-' || cast(dbms_random.value(1,999) as int)  || dbms_random.string('U', 2) || '-' || cast(dbms_random.value(1,999) as int) from investigators inv where inv.id=investigators.id) where nci_identifier is null or nci_identifier=''");
     	  execute("ALTER TABLE investigators MODIFY(nci_identifier not null)");
 		}
 		execute("alter table investigators add constraint uq_assignedId_inv unique (nci_identifier)");
