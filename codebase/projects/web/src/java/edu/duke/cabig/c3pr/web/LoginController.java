3413bc0f420cf2458f4032c998461ac925d24bde
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-02-22 20:51:07 +0000

    redirect to dashboard page if user already logged in. Related to CPR-1535.

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/LoginController.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/LoginController.java
@@ -1,12 +1,11 @@
 package edu.duke.cabig.c3pr.web;
 
-import org.springframework.web.servlet.mvc.ParameterizableViewController;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import edu.duke.cabig.c3pr.tools.Configuration;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
 
 /**
  * User: ion
@@ -17,6 +16,9 @@ import edu.duke.cabig.c3pr.tools.Configuration;
 public class LoginController extends ParameterizableViewController {
 
     protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+    	if(SecurityContextHolder.getContext().getAuthentication()!=null){
+    		return new ModelAndView("redirect:/pages/dashboard");
+    	}
         return super.handleRequestInternal(httpServletRequest, httpServletResponse);    //To change body of overridden methods use File | Settings | File Templates.
     }
 
