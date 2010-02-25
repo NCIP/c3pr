1dc8d0bc8a45a42ffbc8010406af8cf12f221152
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2010-02-25 16:57:16 +0000

    cpr-1609

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/repository/OrganizationRepository.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/repository/OrganizationRepository.java
@@ -1,11 +1,11 @@
 package edu.duke.cabig.c3pr.domain.repository;
 
-import edu.duke.cabig.c3pr.domain.ConverterOrganization;
+import edu.duke.cabig.c3pr.domain.BaseOrganizationDataContainer;
 import edu.duke.cabig.c3pr.domain.LocalHealthcareSite;
 import edu.duke.cabig.c3pr.domain.RemoteHealthcareSite;
 
 public interface OrganizationRepository {
 	
-	public ConverterOrganization convertLocalToRemote(LocalHealthcareSite localHealthcareSite,RemoteHealthcareSite remoteHealthcareSite);
+	public BaseOrganizationDataContainer convertLocalToRemote(LocalHealthcareSite localHealthcareSite,RemoteHealthcareSite remoteHealthcareSite);
 
 }
