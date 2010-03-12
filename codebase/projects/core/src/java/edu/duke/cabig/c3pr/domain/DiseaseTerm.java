28aa2fa7ffc9a4e0cb1b3e11933cfcd1d62c97a2
Himanshu Gupta <himanshu.iit@gmail.com>
2010-03-12 13:48:44 +0000

    removed unused imports

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/DiseaseTerm.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/DiseaseTerm.java
@@ -1,7 +1,6 @@
 package edu.duke.cabig.c3pr.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
