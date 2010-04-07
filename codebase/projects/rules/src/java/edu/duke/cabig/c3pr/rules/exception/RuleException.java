848a6c8ad4791c1ffaaddc73ef4bc29378a0c45f
Himanshu Gupta <himanshu.iit@gmail.com>
2010-04-07 15:33:32 +0000

    removed commented code

--- codebase/projects/rules/src/java/edu/duke/cabig/c3pr/rules/exception/RuleException.java
+++ codebase/projects/rules/src/java/edu/duke/cabig/c3pr/rules/exception/RuleException.java
@@ -6,22 +6,6 @@ package edu.duke.cabig.c3pr.rules.exception;
  */
 public class RuleException extends Exception {
 
-    /*public RuleException(String message, Throwable cause) {
-        this("RULE-001", message, cause);
-    }
-
-    public RuleException(Throwable cause) {
-        this("RULE-001", "Rule Exception", cause);
-    }
-
-    public RuleException(String code, String message) {
-        this(code, message, null);
-    }
-
-    public RuleException(String code, String message, Throwable cause) {
-        super(code, message, cause);
-    }*/
-	
 	public RuleException(String message, Throwable cause) {
         super(message, cause);
     }
--- codebase/projects/rules/src/java/edu/duke/cabig/c3pr/rules/exception/RuleSetNotFoundException.java
+++ codebase/projects/rules/src/java/edu/duke/cabig/c3pr/rules/exception/RuleSetNotFoundException.java
@@ -2,18 +2,6 @@ package edu.duke.cabig.c3pr.rules.exception;
 
 public class RuleSetNotFoundException extends RuleException {
 
-    /*public RuleSetNotFoundException(String message, Throwable cause) {
-        super("RULE-100", message, cause);
-    }
-
-    public RuleSetNotFoundException(Throwable t) {
-        super("RULE-100", "Unable to find the deployed rule set", t);
-    }
-
-    public RuleSetNotFoundException(String message) {
-        super("RULE-100", message);
-    }*/
-	
 	public RuleSetNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
