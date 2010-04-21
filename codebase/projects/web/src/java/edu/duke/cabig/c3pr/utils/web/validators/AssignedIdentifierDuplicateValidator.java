76fd30b7d2e4fd5b324984d385aa358d971214c4
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2010-04-21 18:26:14 +0000

    CPR-1841: Added a identifier validator to check for duplicate assigned_identiiers for staff

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/validators/AssignedIdentifierDuplicateValidator.java
@@ -0,0 +1,33 @@
+package edu.duke.cabig.c3pr.utils.web.validators;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import edu.duke.cabig.c3pr.dao.ResearchStaffDao;
+import edu.duke.cabig.c3pr.domain.ResearchStaff;
+
+
+public class AssignedIdentifierDuplicateValidator implements Validator {
+
+	protected ResearchStaffDao researchStaffDao;
+	
+    public boolean supports(Class aClass) {
+    	return ResearchStaff.class.isAssignableFrom(aClass);
+    }
+
+    public void validate(Object object, Errors errors) {
+        ResearchStaff researchStaff = (ResearchStaff) object;
+        ResearchStaff rStaffFromDB = researchStaffDao.getByAssignedIdentifierFromLocal(researchStaff
+													.getAssignedIdentifier());
+		if (rStaffFromDB != null) {
+			errors.reject("RSTAFF_EXISTS","Research Staff with identifier " +researchStaff.getAssignedIdentifier()+ " already exists");
+			return;
+		}
+    }
+
+    @Required
+	public void setResearchStaffDao(ResearchStaffDao researchStaffDao) {
+		this.researchStaffDao = researchStaffDao;
+	}
+}
