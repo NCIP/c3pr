feec1fe73f608ffca2d3d3eb04fab012c2e4706b
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2010-05-18 13:53:08 +0000

    cpr-1861 updated with code for generating identifier parameter string, used by rules

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/IdentifierGenerator.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/IdentifierGenerator.java
@@ -1,6 +1,5 @@
 package edu.duke.cabig.c3pr.utils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.jackrabbit.uuid.UUID;
@@ -8,12 +7,20 @@ import org.apache.jackrabbit.uuid.UUID;
 import edu.duke.cabig.c3pr.constants.OrganizationIdentifierTypeEnum;
 import edu.duke.cabig.c3pr.constants.RegistrationWorkFlowStatus;
 import edu.duke.cabig.c3pr.dao.StudyDao;
+import edu.duke.cabig.c3pr.domain.Identifier;
 import edu.duke.cabig.c3pr.domain.OrganizationAssignedIdentifier;
 import edu.duke.cabig.c3pr.domain.Participant;
 import edu.duke.cabig.c3pr.domain.StudySubject;
 import edu.duke.cabig.c3pr.domain.SystemAssignedIdentifier;
 
 public class IdentifierGenerator {
+	public static final String ASSIGNED_BY_PARAM_NAME="assignedBy";
+	public static final String ASSIGNED_BY_ORG_VALUE="organization";
+	public static final String ASSIGNED_BY_SYS_VALUE="system";
+	public static final String SYSYEM_NAME_PARAM_NAME="systemName";
+	public static final String ORG_NCI_PARAM_NAME="organizationNciId";
+	public static final String IDENTIFIER_VALUE_PARAM_NAME="identifier";
+	public static final String IDENTIFIER_TYPE_PARAM_NAME="identifierType";
 	
 	private StudyDao studyDao ;
 	
@@ -21,12 +28,6 @@ public class IdentifierGenerator {
 		OrganizationAssignedIdentifier orgIdentifier = new OrganizationAssignedIdentifier();
 		orgIdentifier.setHealthcareSite(studySubject.getStudySite().getStudy().getCoordinatingCenterAssignedIdentifier().getHealthcareSite());
 		orgIdentifier.setType(OrganizationIdentifierTypeEnum.COORDINATING_CENTER_ASSIGNED_STUDY_SUBJECT_IDENTIFIER);
-//		List<StudySubject> studySubjects = new ArrayList<StudySubject>();
-//		if(studySubject.getParentStudySubject() == null ){
-//			studySubjects = studyDao.getStudySubjectsForStudy(studySubject.getStudySite().getStudy().getId());
-//		}else{
-//			studySubjects = studyDao.getStudySubjectsForCompanionStudy(studySubject.getStudySite().getStudy().getId());
-//		}
 		List<StudySubject> studySubjects = studyDao.getStudySubjectsForStudy(studySubject.getStudySite().getStudy().getId());
 		int count = 1 ;
 		for(StudySubject s : studySubjects){
@@ -53,6 +54,17 @@ public class IdentifierGenerator {
 		sysIdentifier.setValue(UUID.randomUUID().toString());
 		return sysIdentifier;
 	}
+	
+	 public static String createParameterString(Identifier identifier){
+	    	if (identifier instanceof OrganizationAssignedIdentifier) {
+				OrganizationAssignedIdentifier organizationAssignedIdentifier = (OrganizationAssignedIdentifier) identifier;
+				 return ASSIGNED_BY_PARAM_NAME+"="+ASSIGNED_BY_ORG_VALUE+"&"+ORG_NCI_PARAM_NAME+"="+organizationAssignedIdentifier.getHealthcareSite().getPrimaryIdentifier()
+						+"&"+IDENTIFIER_TYPE_PARAM_NAME+"="+identifier.getTypeInternal()+"&"+IDENTIFIER_VALUE_PARAM_NAME+"="+identifier.getValue();
+			}
+	    	SystemAssignedIdentifier systemAssignedIdentifier=(SystemAssignedIdentifier)identifier;
+	    	return ASSIGNED_BY_PARAM_NAME+"="+ASSIGNED_BY_SYS_VALUE+"&"+SYSYEM_NAME_PARAM_NAME+"="+systemAssignedIdentifier.getSystemName()
+			+"&"+IDENTIFIER_TYPE_PARAM_NAME+"="+identifier.getTypeInternal()+"&"+IDENTIFIER_VALUE_PARAM_NAME+"="+identifier.getValue();
+	    }
 
 	public StudyDao getStudyDao() {
 		return studyDao;
