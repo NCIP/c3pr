be3cfb643a531f7f2e7596c974b8d765262d2bd5
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-05-26 20:22:31 +0000

    Fixed CPR-1915.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/IdentifierGenerator.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/IdentifierGenerator.java
@@ -31,7 +31,8 @@ public class IdentifierGenerator {
 		List<StudySubject> studySubjects = studyDao.getStudySubjectsForStudy(studySubject.getStudySite().getStudy().getId());
 		int count = 1 ;
 		for(StudySubject s : studySubjects){
-			if (s.getRegWorkflowStatus() == RegistrationWorkFlowStatus.ENROLLED) {
+			if (s.getRegWorkflowStatus() == RegistrationWorkFlowStatus.ENROLLED ||
+					s.getRegWorkflowStatus() == RegistrationWorkFlowStatus.OFF_STUDY) {
 				count++;
 			}
 		}
