8b8c7c5c861fe1eb7a724ceb4ba9402ab8dc1a00
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2010-06-01 15:15:56 +0000

    cpr-1894 updated for Oracle

--- codebase/projects/core/db/bering/026_extension_iteration13/003_add_default_system_identifiers_to_subjects_and_snapshots.groovy
+++ codebase/projects/core/db/bering/026_extension_iteration13/003_add_default_system_identifiers_to_subjects_and_snapshots.groovy
@@ -6,9 +6,9 @@ class AddDefaultSystemIdentifiersToSubjectsAndSnapshots extends edu.northwestern
 		 }
 	 
 	 if (databaseMatches('oracle')) {
-	 
+	 		execute("insert into identifiers (id,version,dtype,system_name,type,value,prt_id,retired_indicator) (select identifiers_ID_SEQ.NEXTVAL,0,'SAI','C3PR','SUBJECT_IDENTIFIER',dbms_random.string('U', 3) || '-' || cast(dbms_random.value(1,999) as int)  || dbms_random.string('U', 2) || '-' || cast(dbms_random.value(1,999) as int),id,'false' from participants)");
+			execute("insert into identifiers (id,version,dtype,system_name,type,retired_indicator,value,stu_sub_dmgphcs_id) (select identifiers_ID_SEQ.NEXTVAL,0,'SAI','C3PR','SUBJECT_IDENTIFIER','false',identifiers.value,stu_sub_demographics.id from identifiers join stu_sub_demographics on identifiers.prt_id = stu_sub_demographics.prt_id where identifiers.prt_id is not null and identifiers.type like 'SUBJECT_IDENTIFIER' and identifiers.system_name like 'C3PR')");
 	 	}
-	 	
 	}
 
 	void down(){
