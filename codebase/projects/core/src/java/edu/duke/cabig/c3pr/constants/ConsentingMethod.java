1089086edcdbff16d3c6e9386ad4bcd9d0f9225c
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2010-06-21 13:51:32 +0000

    cpr-1917 adding enum for consenting method

---
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/constants/ConsentingMethod.java
@@ -0,0 +1,29 @@
+package edu.duke.cabig.c3pr.constants;
+
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.getByClassAndCode;
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.register;
+import static gov.nih.nci.cabig.ctms.domain.EnumHelper.sentenceCasedName;
+import gov.nih.nci.cabig.ctms.domain.CodedEnum;
+
+public enum ConsentingMethod implements CodedEnum<String>{
+	WRITTEN("Written"), VERBAL("Verbal");
+
+	private String code;
+
+	private ConsentingMethod(String code) {
+		this.code = code;
+		register(this);
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public static ConsentingMethod getByCode(String code) {
+		return getByClassAndCode(ConsentingMethod.class, code);
+	}
+
+	public String getDisplayName() {
+		return sentenceCasedName(this);
+	}
+}
