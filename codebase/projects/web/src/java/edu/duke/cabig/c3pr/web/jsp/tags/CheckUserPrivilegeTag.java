3a716820a1a5fccfddf76fb3a5462d9743061e4e
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-06-22 18:43:49 +0000

    added a tag for checking privileges. Related to CPR-1946.

---
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/jsp/tags/CheckUserPrivilegeTag.java
@@ -0,0 +1,36 @@
+package edu.duke.cabig.c3pr.web.jsp.tags;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import edu.duke.cabig.c3pr.constants.UserPrivilegeType;
+import edu.duke.cabig.c3pr.utils.SecurityUtils;
+
+public class CheckUserPrivilegeTag extends TagSupport{
+
+	private String hasPrivileges;
+	
+	@Override
+	public int doStartTag() throws JspException {
+		List<UserPrivilegeType> privileges = new ArrayList<UserPrivilegeType>();
+		for(String privilegeString : hasPrivileges.split(",")){
+			privileges.add(UserPrivilegeType.valueOf(privilegeString));
+		}
+		if(SecurityUtils.hasPrivilege(privileges)){
+			return EVAL_BODY_INCLUDE;
+		}
+		return SKIP_BODY;
+	}
+
+	public String getHasPrivileges() {
+		return hasPrivileges;
+	}
+
+	public void setHasPrivileges(String hasPrivileges) {
+		this.hasPrivileges = hasPrivileges;
+	}
+
+}
