ae1538a0bc017c05c3e320f42078ebbd7da79dd9
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2010-06-22 17:24:52 +0000

    CPR-1846: Unified security development.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/SecurityUtils.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/SecurityUtils.java
@@ -7,14 +7,28 @@ import org.acegisecurity.Authentication;
 import org.acegisecurity.GrantedAuthority;
 import org.acegisecurity.userdetails.User;
 import org.apache.commons.collections15.CollectionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+import edu.duke.cabig.c3pr.accesscontrol.AuthorizedUser;
+import edu.duke.cabig.c3pr.constants.C3PRUserGroupType;
 import edu.duke.cabig.c3pr.constants.RoleTypes;
+import edu.duke.cabig.c3pr.dao.ResearchStaffDao;
+import edu.duke.cabig.c3pr.dao.RolePrivilegeDao;
+import gov.nih.nci.cabig.ctms.suite.authorization.ProvisioningSession;
+import gov.nih.nci.cabig.ctms.suite.authorization.SuiteRoleMembership;
 
 /**
  * The Class SecurityUtils.
  */
 public class SecurityUtils {
 
+	public static final String ACEGI_PREFIX = "ROLE_";
+	
+	/** The log. */
+	private static Log log = LogFactory.getLog(SecurityUtils.class);
+
+	
 	/**
 	 * Check if user is a super user based on ROLE_c3pr_admin.
 	 * 
@@ -66,7 +80,148 @@ public class SecurityUtils {
 		}
 		return roleTypes;
 	}
+	
+	/**
+	 * Gets the c3 pr user role types.
+	 *
+	 * @param authentication the authentication
+	 * @return the c3 pr user role types
+	 */
+	public static List<C3PRUserGroupType> getC3PRUserRoleTypes(Authentication authentication){
+		return getC3PRUserRoleTypes(authentication.getAuthorities());
+	}
+	
+	/**
+	 * Gets the c3 pr user role types.
+	 *
+	 * @param authentication the authentication
+	 * @return the c3 pr user role types
+	 */
+	public static List<C3PRUserGroupType> getC3PRUserRoleTypes(GrantedAuthority[] grantedAuthorities){
+		List<C3PRUserGroupType> c3prUserGroupTypes = new ArrayList<C3PRUserGroupType>();
+		for(GrantedAuthority grantedAuthority : grantedAuthorities){
+			c3prUserGroupTypes.add(C3PRUserGroupType.getByCode(grantedAuthority.getAuthority().substring(ACEGI_PREFIX.length())));
+		}
+		return c3prUserGroupTypes;
+	}
+	
+	
+	/**
+	 * Checks for all site acsess.
+	 * Get the provisioningSession from the user and the roles from the authentication object to 
+	 * determine hasAllSiteAccess
+	 *  User has all Site access if he isnt scoped by site or suiteRoleMembership.isAllSites() is true
+	 *
+	 * @param authentication the authentication
+	 * @return true, if successful
+	 */
+	public static boolean hasAllSiteAccess(Authentication authentication){
+		ProvisioningSession provisioningSession = ((AuthorizedUser)authentication.getPrincipal()).getProvisioningSession();
+		List<C3PRUserGroupType> rolesList = SecurityUtils.getC3PRUserRoleTypes(authentication);
+		SuiteRoleMembership suiteRoleMembership; 
+		for(C3PRUserGroupType userRole: rolesList){
+			suiteRoleMembership = provisioningSession.getProvisionableRoleMembership(C3PRUserGroupType.getUnifiedSuiteRole(userRole));
+			if(suiteRoleMembership.isAllSites() || !suiteRoleMembership.hasSiteScope()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Checks for all study acsess.
+	 * Get the provisioningSession from the user and the roles from the authentication object to 
+	 * determine hasAllSiteAccess
+	 * User has all Study access if he isnt scoped by study or suiteRoleMembership.isAllStudies() is true
+	 *
+	 * @param authentication the authentication
+	 * @return true, if successful
+	 */
+	public static boolean hasAllStudyAccess(Authentication authentication){
+		ProvisioningSession provisioningSession = ((AuthorizedUser)authentication.getPrincipal()).getProvisioningSession();
+		List<C3PRUserGroupType> rolesList = SecurityUtils.getC3PRUserRoleTypes(authentication);
+		SuiteRoleMembership suiteRoleMembership; 
+		for(C3PRUserGroupType userRole: rolesList){
+			suiteRoleMembership = provisioningSession.getProvisionableRoleMembership(C3PRUserGroupType.getUnifiedSuiteRole(userRole));
+			if(suiteRoleMembership.isAllStudies() || !suiteRoleMembership.hasStudyScope()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Builds the user accessible organization ids list.
+	 * Get the provisioningSession from the user and the roles from the authentication object to 
+	 * build userAccessibleOrganizationIdsList(provided user doesn't have hasAllSiteAccess).
+	 *
+	 * @param authentication the authentication
+	 * @return the list
+	 */
+	public static List<String> buildUserAccessibleOrganizationIdsList(Authentication authentication){
+		ProvisioningSession provisioningSession = ((AuthorizedUser)authentication.getPrincipal()).getProvisioningSession();
+		List<C3PRUserGroupType> rolesList = SecurityUtils.getC3PRUserRoleTypes(authentication);
+		List<String> userAccessibleOrganizationIdsList = new ArrayList<String>();
+		SuiteRoleMembership suiteRoleMembership; 
+		for(C3PRUserGroupType userRole: rolesList){
+			suiteRoleMembership = provisioningSession.getProvisionableRoleMembership(C3PRUserGroupType.getUnifiedSuiteRole(userRole));
+			if(suiteRoleMembership.isAllSites()){
+				log.error("User has access to all sites. No point in building list");
+			} else {
+				//add NC010 from "HealthcareSite.NC010" to the userAccessibleOrganizationIdsList
+				for(String siteId:suiteRoleMembership.getSiteIdentifiers()){
+					userAccessibleOrganizationIdsList.add(siteId.substring(siteId.lastIndexOf(".") + 1));
+				}
+			}
+		}
+		return userAccessibleOrganizationIdsList;
+	}
 
+	/**
+	 * Builds the user accessible organization ids list.
+	 * Get the provisioningSession from the user and the roles from the authentication object to 
+	 * build userAccessibleOrganizationIdsList(provided user doesn't have hasAllSiteAccess).
+	 *
+	 * @param authentication the authentication
+	 * @return the list
+	 */
+	public static List<String> buildUserAccessibleStudyIdsList(Authentication authentication){
+		ProvisioningSession provisioningSession = ((AuthorizedUser)authentication.getPrincipal()).getProvisioningSession();
+		List<C3PRUserGroupType> rolesList = SecurityUtils.getC3PRUserRoleTypes(authentication);
+		List<String> userAccessibleStudyIdsList = new ArrayList<String>();
+		SuiteRoleMembership suiteRoleMembership;
+		for(C3PRUserGroupType userRole: rolesList){
+			suiteRoleMembership = provisioningSession.getProvisionableRoleMembership(C3PRUserGroupType.getUnifiedSuiteRole(userRole));
+			if(suiteRoleMembership.isAllSites()){
+				log.error("User has access to all sites. No point in building list");
+			} else {
+				//add NC010 from "HealthcareSite.NC010" to the userAccessibleOrganizationIdsList
+				for(String studyId:suiteRoleMembership.getStudyIdentifiers()){
+					userAccessibleStudyIdsList.add(studyId.substring(studyId.lastIndexOf(".") + 1));
+				}
+			}
+		}
+		return userAccessibleStudyIdsList;
+	}
+	
+	
+	/**
+	 * Check privilege, given roles set.
+	 *
+	 * @param rolesSet the roles set
+	 * @param privilege the privilege
+	 * @return true, if successful
+	 */
+	public static boolean checkPrivilegeGivenRoles(RolePrivilegeDao rolePrivilegeDao, List<C3PRUserGroupType> rolesList, String objectId, String privilege) {
+		for(C3PRUserGroupType role: rolesList){
+			if(rolePrivilegeDao.isValidRolePrivilege(objectId, privilege, role.getCode())){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
 	/**
 	 * Gets the role types that can access staff.
 	 *
@@ -78,18 +233,6 @@ public class SecurityUtils {
 			roleTypes.add(RoleTypes.getByCode(role));
 		}
 		return roleTypes;
-	}
-	
-	/**
-	 * Checks for all site access. Does this by refering the csm PG.
-	 * To be implemented.
-	 *
-	 * @param authentication the authentication
-	 * @return true, if successful
-	 */
-	public static boolean hasAllSiteAccess(Authentication authentication) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 	
