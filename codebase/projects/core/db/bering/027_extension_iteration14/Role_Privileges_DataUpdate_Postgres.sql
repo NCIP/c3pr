2d4d0e721d8b6129434b442154ac37c1c7053ced
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-06-28 15:42:34 +0000

    added mappings for study_team_admin. Related to CPR-1946.

--- codebase/projects/core/db/bering/027_extension_iteration14/Role_Privileges_DataUpdate_Postgres.sql
+++ codebase/projects/core/db/bering/027_extension_iteration14/Role_Privileges_DataUpdate_Postgres.sql
@@ -25,3 +25,6 @@ INSERT INTO role_privileges(ROLE_NAME,OBJECT_ID,PRIVILEGE, ID)
 VALUES('registrar','edu.duke.cabig.c3pr.domain.Subject', 'READ', (select nextval('role_privileges_ID_SEQ')));
 INSERT INTO role_privileges(ROLE_NAME,OBJECT_ID,PRIVILEGE, ID) 
 VALUES('registrar','edu.duke.cabig.c3pr.domain.StudySite', 'READ', (select nextval('role_privileges_ID_SEQ')));
+
+INSERT INTO role_privileges(ROLE_NAME,OBJECT_ID,PRIVILEGE, ID) 
+VALUES('study_team_administrator','edu.duke.cabig.c3pr.domain.Study', 'READ', (select nextval('role_privileges_ID_SEQ')));
