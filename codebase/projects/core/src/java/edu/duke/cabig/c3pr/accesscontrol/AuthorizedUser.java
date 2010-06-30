b9d2483d48ec45ae383a78a768449aea436c73c9
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2010-06-30 15:01:00 +0000

    CPR-1846: Unified security development. Code for dynamic provisioning of non-research staff CSM users

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/accesscontrol/AuthorizedUser.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/accesscontrol/AuthorizedUser.java
@@ -1,6 +1,7 @@
 package edu.duke.cabig.c3pr.accesscontrol;
 
 import edu.duke.cabig.c3pr.constants.UserPrivilegeType;
+import edu.duke.cabig.c3pr.domain.ResearchStaff;
 import edu.duke.cabig.c3pr.domain.RolePrivilege;
 import gov.nih.nci.cabig.ctms.suite.authorization.ProvisioningSession;
 
@@ -27,10 +28,13 @@ public class AuthorizedUser extends User {
 	
 	private RolePrivilege rolePrivileges[];
 	
+	private ResearchStaff researchStaff;
+	
     public AuthorizedUser(String string, String string1, boolean b, boolean b1, boolean b2, boolean b3,
-                    GrantedAuthority[] grantedAuthorities, ProvisioningSession provisioningSession, RolePrivilege rolePrivileges[]) throws IllegalArgumentException {
+                    GrantedAuthority[] grantedAuthorities, ProvisioningSession provisioningSession, RolePrivilege rolePrivileges[], ResearchStaff researchStaff) throws IllegalArgumentException {
         super(string, string1, b, b1, b2, b3, grantedAuthorities);
         
+        this.researchStaff = researchStaff;
         this.provisioningSession = provisioningSession;
         this.rolePrivileges = rolePrivileges;
     }
@@ -68,5 +72,10 @@ public class AuthorizedUser extends User {
 		}
 		return filteredRolePrivileges;
 	}
-	
+
+
+	public ResearchStaff getResearchStaff() {
+		return researchStaff;
+	}
+
  }
--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/accesscontrol/C3prUserDetailsService.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/accesscontrol/C3prUserDetailsService.java
@@ -2,20 +2,28 @@ package edu.duke.cabig.c3pr.accesscontrol;
 
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import edu.duke.cabig.c3pr.constants.C3PRUserGroupType;
+import edu.duke.cabig.c3pr.dao.ResearchStaffDao;
 import edu.duke.cabig.c3pr.dao.RolePrivilegeDao;
+import edu.duke.cabig.c3pr.dao.UserDao;
+import edu.duke.cabig.c3pr.domain.LocalResearchStaff;
+import edu.duke.cabig.c3pr.domain.ResearchStaff;
 import edu.duke.cabig.c3pr.domain.RolePrivilege;
+import edu.duke.cabig.c3pr.exception.C3PRBaseException;
 import edu.duke.cabig.c3pr.utils.SecurityUtils;
 import gov.nih.nci.cabig.ctms.suite.authorization.ProvisioningSessionFactory;
 import gov.nih.nci.security.acegi.csm.authorization.CSMUserDetailsService;
+import gov.nih.nci.security.authorization.domainobjects.User;
 
 import org.acegisecurity.GrantedAuthority;
 import org.acegisecurity.userdetails.UserDetails;
 import org.acegisecurity.userdetails.UsernameNotFoundException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.jackrabbit.uuid.UUID;
 import org.springframework.dao.DataAccessException;
 
 /**
@@ -30,7 +38,11 @@ public class C3prUserDetailsService extends CSMUserDetailsService{
 	
 	private ProvisioningSessionFactory provisioningSessionFactory;
 	
-	RolePrivilegeDao rolePrivilegeDao;
+	private RolePrivilegeDao rolePrivilegeDao;
+	
+	private ResearchStaffDao researchStaffDao;
+	
+	private UserDao userDao;
 	
 	private static final Log logger = LogFactory.getLog(C3prUserDetailsService.class);
 	
@@ -47,10 +59,55 @@ public class C3prUserDetailsService extends CSMUserDetailsService{
 		gov.nih.nci.security.authorization.domainobjects.User csmUser = 
 			getCsmUserProvisioningManager().getUser(userDetails.getUsername());
 		AuthorizedUser authorizedUser= new AuthorizedUser(userDetails.getUsername(), userDetails.getPassword(), true, true, true, true, 
-						userDetails.getAuthorities(), provisioningSessionFactory. createSession(csmUser.getUserId()), getAllRolePrivileges(userDetails.getAuthorities()));
+						userDetails.getAuthorities(), provisioningSessionFactory. createSession(csmUser.getUserId()), getAllRolePrivileges(userDetails.getAuthorities()), 
+						getResearchStaff(csmUser));
+		
 		return authorizedUser;
 	}
 	
+	/**
+	 * Gets the research staff. 
+	 * Creates one if the staff does not exist. This is for the suite use case of dynamic provisioning.
+	 *
+	 * @param userId the user id
+	 * @return the research staff
+	 */
+	private ResearchStaff getResearchStaff(gov.nih.nci.security.authorization.domainobjects.User csmUser) {
+		edu.duke.cabig.c3pr.domain.User c3prUser = userDao.getByLoginId(csmUser.getUserId());
+		if(c3prUser == null){
+			ResearchStaff researchStaff = populateResearchStaff(csmUser);
+			try {
+				logger.debug("Attempting to dynamically provision the CSM user with user id: "+ csmUser.getUserId() +" in C3PR as staff.");
+				setAuditInfo();
+				researchStaffDao.createResearchStaff(researchStaff);
+			} catch (C3PRBaseException e) {
+				logger.error("Unable to dynamically provision staff for user: "+csmUser.getUserId());
+				logger.error(e.getMessage());
+			} catch(Exception e){
+				logger.error(e.getMessage());
+			}
+			return researchStaff;
+		}
+		return (ResearchStaff) c3prUser;
+	}
+
+	/**
+	 * Populate research staff.
+	 *
+	 * @param csmUser the csm user
+	 * @return the research staff
+	 */
+	private ResearchStaff populateResearchStaff(User csmUser) {
+		ResearchStaff researchStaff = new LocalResearchStaff();
+		researchStaff.setFirstName(csmUser.getFirstName());
+		researchStaff.setLastName(csmUser.getLastName());
+		researchStaff.setLoginId(csmUser.getUserId().toString());
+		researchStaff.setEmail(csmUser.getEmailId());
+		researchStaff.setPhone(csmUser.getPhoneNumber());
+		researchStaff.setAssignedIdentifier(UUID.randomUUID().toString());
+		return researchStaff;
+	}
+
 	/**
 	 * Gets the all role privileges.
 	 *
@@ -85,4 +142,24 @@ public class C3prUserDetailsService extends CSMUserDetailsService{
 		this.rolePrivilegeDao = rolePrivilegeDao;
 	}
 
+	public ResearchStaffDao getResearchStaffDao() {
+		return researchStaffDao;
+	}
+
+	public void setResearchStaffDao(ResearchStaffDao researchStaffDao) {
+		this.researchStaffDao = researchStaffDao;
+	}
+
+	public UserDao getUserDao() {
+		return userDao;
+	}
+
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	public void setAuditInfo(){
+    	gov.nih.nci.cabig.ctms.audit.DataAuditInfo.setLocal(new gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo(
+        		"C3PR Admin", "C3PR dynamic provisioning of suite user", new Date(), "C3PR dynamic provisioning of suite user"));
+    }
 }
