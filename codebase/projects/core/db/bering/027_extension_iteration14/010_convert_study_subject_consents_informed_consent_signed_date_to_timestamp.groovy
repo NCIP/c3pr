e43b548ae5fb1271b3d61ff5ed87da9b3fdb3a2b
Himanshu Gupta <himanshu.iit@gmail.com>
2010-07-01 15:44:44 +0000

    Oracle migration fix

--- codebase/projects/core/db/bering/027_extension_iteration14/010_convert_study_subject_consents_informed_consent_signed_date_to_timestamp.groovy
+++ codebase/projects/core/db/bering/027_extension_iteration14/010_convert_study_subject_consents_informed_consent_signed_date_to_timestamp.groovy
@@ -1,8 +1,11 @@
 class ConvertStudySubjectConsentsInformedConsentSignedDateToTimestamp extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-    	addColumn('study_subject_consents','informed_consent_signed_tstamp', 'timestamp');
-	    execute("update study_subject_consents set informed_consent_signed_tstamp = informed_consent_signed_date")
-   
+    	if (databaseMatches('oracle')){
+       		addColumn('study_subject_consents','informed_consent_signed_tstamp', 'timestamp');
+        }
+        if (databaseMatches('postgresql')){
+       		addColumn('study_subject_consents','informed_consent_signed_timestamp', 'timestamp');
+        }
    	}
 	void down() {
     }
