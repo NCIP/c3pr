f4a6a90731fc95ab4bd275a67921596487f5a30e
Himanshu Gupta <himanshu.iit@gmail.com>
2010-07-01 13:29:25 +0000

    Oracle migration fix

--- codebase/projects/core/db/bering/027_extension_iteration14/015_UpdateRolePrivilegeTable.groovy
+++ codebase/projects/core/db/bering/027_extension_iteration14/015_UpdateRolePrivilegeTable.groovy
@@ -3,7 +3,6 @@ class CreateRolePrivilegeTable extends edu.northwestern.bioinformatics.bering.Mi
     void up() {
         
         if (databaseMatches('oracle')) {
-		   	execute("RENAME SEQ_role_privileges_ID to role_privileges_ID_SEQ");
             external("Role_Privileges_DataUpdate_Oracle.sql")
         } else if (databaseMatches('postgresql')){
             external("Role_Privileges_DataUpdate_Postgres.sql")
