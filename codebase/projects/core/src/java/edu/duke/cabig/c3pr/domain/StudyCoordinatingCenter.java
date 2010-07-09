9b8c8dcefb00bfc14d2377cb5ee2cacb07d1b21e
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-07-09 19:58:58 +0000

    removed multisite code for study. Related to CPR-1975.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/StudyCoordinatingCenter.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/StudyCoordinatingCenter.java
@@ -1,13 +1,7 @@
 package edu.duke.cabig.c3pr.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Transient;
-
-import edu.duke.cabig.c3pr.constants.APIName;
 
 /**
  * The Class StudyCoordinatingCenter.
@@ -24,19 +18,5 @@ public class StudyCoordinatingCenter extends StudyOrganization {
     public StudyCoordinatingCenter() {
         super();
         this.setHostedMode(true);
-    }
-    
-    /**
-     * Gets the possible endpoints.
-     * 
-     * @return the possible endpoints
-     */
-    @Transient
-    public List<APIName> getPossibleEndpoints(){
-        List<APIName> apiList=new ArrayList<APIName>();
-        if(!isSuccessfullSend(APIName.ACTIVATE_STUDY_SITE)){
-            apiList.add(APIName.ACTIVATE_STUDY_SITE);
-        }
-        return apiList;
     }
 }
