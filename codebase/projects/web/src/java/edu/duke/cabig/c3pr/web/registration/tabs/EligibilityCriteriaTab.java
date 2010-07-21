3cf9a69100b664a631a4f7ec5a8a3f70b83553d4
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-07-21 20:09:12 +0000

    Removed the post process method. Related to CPR-CPR-2010.

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/registration/tabs/EligibilityCriteriaTab.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/registration/tabs/EligibilityCriteriaTab.java
@@ -3,15 +3,6 @@ package edu.duke.cabig.c3pr.web.registration.tabs;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.validation.Errors;
-import org.springframework.web.util.WebUtils;
-
-import edu.duke.cabig.c3pr.constants.EpochType;
-import edu.duke.cabig.c3pr.domain.Epoch;
-import edu.duke.cabig.c3pr.domain.ScheduledEpoch;
-import edu.duke.cabig.c3pr.domain.StudySubject;
 import edu.duke.cabig.c3pr.web.registration.StudySubjectWrapper;
 
 /**
@@ -34,35 +25,4 @@ public class EligibilityCriteriaTab extends RegistrationTab<StudySubjectWrapper>
         return ref;
     }
 
-    @Override
-    public void postProcess(HttpServletRequest request, StudySubjectWrapper command, Errors error) {
-    	StudySubjectWrapper wrapper = (StudySubjectWrapper) command ;
-    	StudySubject studySubject = wrapper.getStudySubject();
-        // The following code for building scheduled epoch after moving subject to a new epoch.
-        if(WebUtils.hasSubmitParameter(request, "epoch")){
-        	Integer id;
-	        try {
-	            id = Integer.parseInt(request.getParameter("epoch"));
-	        }
-	        catch (RuntimeException e) {
-	            return;
-	        }
-	      
-	        Epoch epoch = epochDao.getById(id);
-	        epochDao.initialize(epoch);
-	        ScheduledEpoch scheduledEpoch;
-	        if (epoch.getType() == EpochType.TREATMENT) {
-	            (epoch).getArms().size();
-	            scheduledEpoch = new ScheduledEpoch();
-	        }
-	        else {
-	            scheduledEpoch = new ScheduledEpoch();
-	        }
-	        scheduledEpoch.setEpoch(epoch);
-	        studySubject.getScheduledEpochs().add(scheduledEpoch);
-	        studySiteDao.initialize(studySubject.getStudySite());
-        }
-    }
-
-    
 }
