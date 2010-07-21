83d76c887741a47dc1ad6b321ff72ff763fb62a6
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2010-07-21 13:53:45 +0000

    cpr-1917 added method to determine if consentingMethod exists in list

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/C3PRWebUtils.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/utils/web/C3PRWebUtils.java
@@ -2,9 +2,16 @@ package edu.duke.cabig.c3pr.utils.web;
 
 import java.util.List;
 
+import edu.duke.cabig.c3pr.constants.ConsentingMethod;
+
 public class C3PRWebUtils {
 	public static boolean contains(List list, Object o) {
 	      return list.contains(o);
+	   }
+	
+	public static boolean containsConsentingMethod(List list, String consentMethod) {
+		ConsentingMethod consentingMethod = ConsentingMethod.getByCode(consentMethod);
+	      return list.contains(consentingMethod);
 	   }
 
 }
