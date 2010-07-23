b2990e4115233066635ed6c0aaaa4a26a5dc97f0
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2010-07-23 20:07:57 +0000

    reverted

--- codebase/projects/web/test/src/java/edu/duke/cabig/c3pr/web/CreateParticipantControllerTest.java
+++ codebase/projects/web/test/src/java/edu/duke/cabig/c3pr/web/CreateParticipantControllerTest.java
@@ -42,7 +42,7 @@ public class CreateParticipantControllerTest extends ControllerTestCase {
 
     protected void setUp() throws Exception {
         super.setUp();
-        context = getDeployedApplicationContext();
+        context = ContextTools.createDeployedApplicationContext();
         participant = registerMockFor(Participant.class);
         participantDao = registerMockFor(ParticipantDao.class);
         controller.setParticipantDao(participantDao);
