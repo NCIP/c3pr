0f899c8bc2cb6c1d59862ccc554715371504f384
Denis Krylov <denis.krylov@semanticbits.com>
2010-07-27 16:03:05 +0000

    CPR-1857: Subject Management service implementation: fixed Bering migration script to run on Oracle.

--- codebase/projects/core/db/bering/028_extension_iteration15/006_add_death_related_field_to_participant.groovy
+++ codebase/projects/core/db/bering/028_extension_iteration15/006_add_death_related_field_to_participant.groovy
@@ -1,10 +1,10 @@
 class AddDeathRelatedColumnsToParticipant extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
     	addColumn('participants', 'death_date', 'timestamp');    	
-    	setNullable('participants','death_date', true);
+    	//setNullable('participants','death_date', true);
     	
     	addColumn('participants', 'death_indicator', 'boolean');    	
-    	setNullable('participants','death_indicator', true);
+    	//setNullable('participants','death_indicator', true);
     	
     	
 	}
