d6b2ef8cf74626dcf5db7606f657a1b046c91d3d
Himanshu Gupta <himanshu.iit@gmail.com>
2010-07-28 18:20:45 +0000

    unused imports removed

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/report/tabs/Summary3ReportDetailsTab.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/report/tabs/Summary3ReportDetailsTab.java
@@ -1,23 +1,7 @@
 package edu.duke.cabig.c3pr.web.report.tabs;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.validation.Errors;
-
-import edu.duke.cabig.c3pr.constants.CoordinatingCenterStudyStatus;
-import edu.duke.cabig.c3pr.constants.InvestigatorStatusCodeEnum;
-import edu.duke.cabig.c3pr.dao.HealthcareSiteInvestigatorDao;
-import edu.duke.cabig.c3pr.domain.HealthcareSiteInvestigator;
-import edu.duke.cabig.c3pr.domain.Study;
-import edu.duke.cabig.c3pr.domain.StudyInvestigator;
-import edu.duke.cabig.c3pr.domain.StudySite;
 import edu.duke.cabig.c3pr.domain.Summary3Report;
-import edu.duke.cabig.c3pr.domain.validator.StudyValidator;
-import edu.duke.cabig.c3pr.utils.StringUtils;
 import edu.duke.cabig.c3pr.utils.web.spring.tabbedflow.WorkFlowTab;
-import edu.duke.cabig.c3pr.web.study.StudyWrapper;
 
 /**
  * Created by IntelliJ IDEA. User: kherm Date: Jun 13, 2007 Time: 7:27:09 PM To change this template
