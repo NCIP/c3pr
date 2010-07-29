addb2fc5fe46b1cadf8e35e16b135c53fcab0417
Denis Krylov <denis.krylov@semanticbits.com>
2010-07-29 14:42:48 +0000

    CPR-1857: Subject Management service implementation: fixed 010_set_def_values_for_new_partic_fields.groovy: would not run on Oracle.

--- codebase/projects/core/db/bering/028_extension_iteration15/010_set_def_values_for_new_partic_fields.groovy
+++ codebase/projects/core/db/bering/028_extension_iteration15/010_set_def_values_for_new_partic_fields.groovy
@@ -1,6 +1,14 @@
 class SetDefaultValuesForNewParticipantFields extends edu.northwestern.bioinformatics.bering.Migration {
-    void up() {    	
-    	execute("update participants set death_indicator = false, state_code='ACTIVE'");
+    void up() {
+       
+        if (databaseMatches('postgres')){ 	
+    		execute("update participants set death_indicator = false, state_code='ACTIVE'");
+    	}
+    	
+     	if (databaseMatches('oracle')){
+       		execute("update participants set death_indicator = 0, state_code='ACTIVE'");
+       	}   	
+    	
 	}
 	void down() {		
     }
