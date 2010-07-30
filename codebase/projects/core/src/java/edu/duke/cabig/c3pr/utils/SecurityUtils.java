1fedc0ac65220529921b591d5d6e108886be0f64
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2010-07-30 21:53:19 +0000

    CPR-2029: not displaying non c3pr study scoped roles for perf reasons

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/SecurityUtils.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/utils/SecurityUtils.java
@@ -432,15 +432,15 @@ public class SecurityUtils {
 		studyScopedRoles.add(C3PRUserGroupType.DATA_ANALYST);
 		studyScopedRoles.add(C3PRUserGroupType.DATA_READER);
 		
-		studyScopedRoles.add(C3PRUserGroupType.AE_STUDY_DATA_REVIEWER);
-		studyScopedRoles.add(C3PRUserGroupType.AE_EXPEDITED_REPORT_REVIEWER);
-		studyScopedRoles.add(C3PRUserGroupType.AE_REPORTER);
-		
-		studyScopedRoles.add(C3PRUserGroupType.LAB_DATA_USER);
-		studyScopedRoles.add(C3PRUserGroupType.LAB_IMPACT_CALENDAR_NOTIFIER);
-		
-		studyScopedRoles.add(C3PRUserGroupType.STUDY_CALENDAR_TEMPLATE_BUILDER);
-		studyScopedRoles.add(C3PRUserGroupType.STUDY_SUBJECT_CALENDAR_MANAGER);
+//		studyScopedRoles.add(C3PRUserGroupType.AE_STUDY_DATA_REVIEWER);
+//		studyScopedRoles.add(C3PRUserGroupType.AE_EXPEDITED_REPORT_REVIEWER);
+//		studyScopedRoles.add(C3PRUserGroupType.AE_REPORTER);
+//		
+//		studyScopedRoles.add(C3PRUserGroupType.LAB_DATA_USER);
+//		studyScopedRoles.add(C3PRUserGroupType.LAB_IMPACT_CALENDAR_NOTIFIER);
+//		
+//		studyScopedRoles.add(C3PRUserGroupType.STUDY_CALENDAR_TEMPLATE_BUILDER);
+//		studyScopedRoles.add(C3PRUserGroupType.STUDY_SUBJECT_CALENDAR_MANAGER);
 		
 		return studyScopedRoles;
 	}
