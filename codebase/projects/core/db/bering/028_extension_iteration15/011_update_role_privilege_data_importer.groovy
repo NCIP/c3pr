b6ebb558747273adf0cefdc776bf572eacf89038
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2010-08-23 21:54:41 +0000

    CPR:2047:Added the privs to the data importer in the role_privileges table.

--- codebase/projects/core/db/bering/028_extension_iteration15/011_update_role_privilege_data_importer.groovy
+++ codebase/projects/core/db/bering/028_extension_iteration15/011_update_role_privilege_data_importer.groovy
@@ -2,9 +2,23 @@ class UpdateRolePrivilegeDataImporter extends edu.northwestern.bioinformatics.be
     void up() {
     	if (databaseMatches('postgres')){
        		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(nextval('role_privileges_ID_SEQ'),0,'data_importer','edu.duke.cabig.c3pr.domain.Study','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(nextval('role_privileges_ID_SEQ'),0,'data_importer','edu.duke.cabig.c3pr.domain.HealthcareSite','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(nextval('role_privileges_ID_SEQ'),0,'data_importer','edu.duke.cabig.c3pr.domain.HealthcareSite','CREATE')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(nextval('role_privileges_ID_SEQ'),0,'data_importer','edu.duke.cabig.c3pr.domain.Investigator','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(nextval('role_privileges_ID_SEQ'),0,'data_importer','edu.duke.cabig.c3pr.domain.Investigator','CREATE')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(nextval('role_privileges_ID_SEQ'),0,'data_importer','edu.duke.cabig.c3pr.domain.StudySubject','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(nextval('role_privileges_ID_SEQ'),0,'data_importer','edu.duke.cabig.c3pr.domain.Subject','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(nextval('role_privileges_ID_SEQ'),0,'data_importer','edu.duke.cabig.c3pr.domain.Subject','CREATE')");
        	}
      	if (databaseMatches('oracle')){
        		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(role_privileges_ID_SEQ.NEXTVAL,0,'data_importer','edu.duke.cabig.c3pr.domain.Study','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(role_privileges_ID_SEQ.NEXTVAL,0,'data_importer','edu.duke.cabig.c3pr.domain.HealthcareSite','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(role_privileges_ID_SEQ.NEXTVAL,0,'data_importer','edu.duke.cabig.c3pr.domain.HealthcareSite','CREATE')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(role_privileges_ID_SEQ.NEXTVAL,0,'data_importer','edu.duke.cabig.c3pr.domain.Investigator','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(role_privileges_ID_SEQ.NEXTVAL,0,'data_importer','edu.duke.cabig.c3pr.domain.Investigator','CREATE')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(role_privileges_ID_SEQ.NEXTVAL,0,'data_importer','edu.duke.cabig.c3pr.domain.StudySubject','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(role_privileges_ID_SEQ.NEXTVAL,0,'data_importer','edu.duke.cabig.c3pr.domain.Subject','READ')");
+       		execute("insert into role_privileges (id,version,role_name,object_id,privilege) values(role_privileges_ID_SEQ.NEXTVAL,0,'data_importer','edu.duke.cabig.c3pr.domain.Subject','CREATE')");
        	}
 	}
 	
