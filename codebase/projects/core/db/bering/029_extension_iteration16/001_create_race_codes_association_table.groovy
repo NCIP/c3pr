d14c4710a0442a4543bb48a475ca692ac1507887
Himanshu Gupta <himanshu.iit@gmail.com>
2010-08-24 14:23:01 +0000

    race code related changes

--- codebase/projects/core/db/bering/029_extension_iteration16/001_create_race_codes_association_table.groovy
+++ codebase/projects/core/db/bering/029_extension_iteration16/001_create_race_codes_association_table.groovy
@@ -2,9 +2,9 @@ class CreateRaceCodeAssociationsTable extends edu.northwestern.bioinformatics.be
     void up() {
     	//creating race_codes table
     	createTable("race_codes") { t ->
-	       	t.addVersionColumn()
-            t.addColumn("grid_id", "string")
             t.addColumn("race_code", "string", nullable: false)
+               t.addVersionColumn()
+            t.addColumn("grid_id", "string")
          }
          
          // inserting data in race_codes table
