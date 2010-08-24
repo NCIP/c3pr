a70b57d1416a0df80475a93f99a9589b31a41865
Denis Krylov <denis.krylov@semanticbits.com>
2010-08-24 13:52:06 +0000

    CPR-2085 Create automated unit and integration tests for Subject Management NES: fixed RaceCode.java to remove duplicate field that prevented the app from start up.

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RaceCode.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RaceCode.java
@@ -1,28 +1,20 @@
 package edu.duke.cabig.c3pr.domain;
 
+import edu.duke.cabig.c3pr.constants.RaceCodeEnum;
+import gov.nih.nci.cabig.ctms.domain.AbstractMutableDomainObject;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
-import edu.duke.cabig.c3pr.constants.RaceCodeEnum;
-import gov.nih.nci.cabig.ctms.domain.AbstractMutableDomainObject;
-
 @Entity
 @Table(name = "race_codes")
 @GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "race_codes_id_seq") })
-public class RaceCode extends AbstractMutableDomainObject{
-	private Integer id ;
-	public Integer getId() {
-		return id;
-	}
+public class RaceCode extends AbstractMutableDomainObject {
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	private RaceCodeEnum raceCode ;
+	private RaceCodeEnum raceCode;
 
 	public void setRaceCode(RaceCodeEnum raceCode) {
 		this.raceCode = raceCode;
@@ -32,5 +24,4 @@ public class RaceCode extends AbstractMutableDomainObject{
 		return raceCode;
 	}
 
-	
 }
