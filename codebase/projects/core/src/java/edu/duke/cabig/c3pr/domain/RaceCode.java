f9e43ea95537d85d79a572051796ab48105890cd
Himanshu Gupta <himanshu.iit@gmail.com>
2010-08-24 17:29:08 +0000

    race code related changes

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RaceCode.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/domain/RaceCode.java
@@ -14,7 +14,7 @@ import org.hibernate.annotations.Parameter;
 @Entity
 @Table(name = "race_codes")
 @GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "race_codes_id_seq") })
-public class RaceCode extends AbstractMutableDomainObject{
+public class RaceCode extends AbstractMutableDomainObject {
 	private RaceCodeEnum raceCode;
 	
 	public void setRaceCode(RaceCodeEnum raceCode) {
@@ -25,5 +25,15 @@ public class RaceCode extends AbstractMutableDomainObject{
 	public RaceCodeEnum getRaceCode() {
 		return raceCode;
 	}
+	
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final RaceCode that = (RaceCode) o;
+
+        if (raceCode != null ? !raceCode.equals(that.raceCode) : that.raceCode != null) return false;
+        return true;
+    }
 
 }
