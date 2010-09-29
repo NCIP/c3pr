3294bd0e1cc414ddd4883abb315ea5046c99bb23
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-09-29 21:26:40 +0000

    added subject registry test code. Related to CPR-2092.

--- codebase/projects/web/test/src/java/edu/duke/cabig/c3pr/webservice/WebServiceTestSuite.java
+++ codebase/projects/web/test/src/java/edu/duke/cabig/c3pr/webservice/WebServiceTestSuite.java
@@ -3,7 +3,9 @@ package edu.duke.cabig.c3pr.webservice;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import edu.duke.cabig.c3pr.webservice.converters.JAXBToDomainObjectConverterImplTest;
+import edu.duke.cabig.c3pr.webservice.converters.SubjectRegistryJAXBToDomainObjectConverterImplTest;
 import edu.duke.cabig.c3pr.webservice.subjectmanagement.SubjectManagementImplTest;
+import edu.duke.cabig.c3pr.webservice.subjectregistry.SubjectRegistryImplTest;
 
 public class WebServiceTestSuite {
 
@@ -13,6 +15,8 @@ public class WebServiceTestSuite {
 		//$JUnit-BEGIN$
 		suite.addTestSuite(JAXBToDomainObjectConverterImplTest.class);
 		suite.addTestSuite(SubjectManagementImplTest.class);
+		suite.addTestSuite(SubjectRegistryJAXBToDomainObjectConverterImplTest.class);
+		suite.addTestSuite(SubjectRegistryImplTest.class);
 		//$JUnit-END$
 		return suite;
 	}
