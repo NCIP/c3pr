89216c3a1869ea9d70b7008f4f26f9d534696d80
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2010-10-22 20:09:05 +0000

    cpr-2163

--- codebase/projects/core/db/bering/026_extension_iteration13/007_migrate_research_staff_with_text_login_ids.groovy
+++ codebase/projects/core/db/bering/026_extension_iteration13/007_migrate_research_staff_with_text_login_ids.groovy
@@ -6,14 +6,18 @@ class MigrateResearchStaffWithTextLoginIDs extends edu.northwestern.bioinformati
             t.addColumn("login_name", "string")
          }
          
-         //  insert all user_ids and login_names that are in text 
-         	execute("insert into user_research_staff_migration (user_id,login_name) (select user_id,login_name from csm_user where login_name in (select research_staff.login_id from research_staff where research_staff.login_id not in (select research_staff.login_id from research_staff,csm_user where research_staff.login_id = csm_user.user_id)))");
-		
-		// update the research_staff login_id
 		if (databaseMatches('postgres')){
+			//  insert all user_ids and login_names that are in text 
+         	execute("insert into user_research_staff_migration (user_id,login_name) (select user_id,login_name from csm_user where login_name in (select research_staff.login_id from research_staff where research_staff.login_id not in (select research_staff.login_id from research_staff,csm_user where research_staff.login_id = csm_user.user_id::text)))");
+       		
+       		// update the research_staff login_id
        		execute("update research_staff set login_id = (user_research_staff_migration.user_id)from user_research_staff_migration where user_research_staff_migration.login_name = research_staff.login_id");
        	}
      	if (databaseMatches('oracle')){
+     		//  insert all user_ids and login_names that are in text 
+         	execute("insert into user_research_staff_migration (user_id,login_name) (select user_id,login_name from csm_user where login_name in (select research_staff.login_id from research_staff where research_staff.login_id not in (select research_staff.login_id from research_staff,csm_user where research_staff.login_id = csm_user.user_id)))");
+       		
+       		// update the research_staff login_id
        		execute("update research_staff set login_id = (select user_research_staff_migration.user_id from user_research_staff_migration where user_research_staff_migration.login_name = research_staff.login_id)");
        	}
 		 
