6933ab74345e49a084edb8259d7cb5e0c90680d9
kruttik Aggarwal <kruttikagarwal@gmail.com>
2010-11-10 17:53:59 +0000

    reverted to revision 13290.

--- codebase/projects/web/test/src/java/edu/duke/cabig/c3pr/webservice/WebServiceTestSuite.java
+++ codebase/projects/web/test/src/java/edu/duke/cabig/c3pr/webservice/WebServiceTestSuite.java
@@ -4,7 +4,6 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import edu.duke.cabig.c3pr.webservice.converters.JAXBToDomainObjectConverterImplTest;
 import edu.duke.cabig.c3pr.webservice.converters.SubjectRegistryJAXBToDomainObjectConverterImplTest;
-import edu.duke.cabig.c3pr.webservice.integration.SubjectRegistryMayoUseCaseIntegrationTest;
 import edu.duke.cabig.c3pr.webservice.studyutility.StudyUtilityImplTest;
 import edu.duke.cabig.c3pr.webservice.subjectmanagement.SubjectManagementImplTest;
 import edu.duke.cabig.c3pr.webservice.subjectregistry.SubjectRegistryImplTest;
@@ -18,9 +17,8 @@ public class WebServiceTestSuite {
 		suite.addTestSuite(JAXBToDomainObjectConverterImplTest.class);
 		suite.addTestSuite(SubjectManagementImplTest.class);
 		suite.addTestSuite(SubjectRegistryJAXBToDomainObjectConverterImplTest.class);
-		suite.addTestSuite(StudyUtilityImplTest.class);
 		suite.addTestSuite(SubjectRegistryImplTest.class);
-		suite.addTestSuite(SubjectRegistryMayoUseCaseIntegrationTest.class);
+		suite.addTestSuite(StudyUtilityImplTest.class);
 		//$JUnit-END$
 		return suite;
 	}
