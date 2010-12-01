b0e59ceb29a0e16e73b3d59d3c42fe236ff3a50c
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2010-12-01 21:43:06 +0000

    cpr-1917

--- codebase/projects/core/db/bering/027_extension_iteration14/010_convert_study_subject_consents_informed_consent_signed_date_to_timestamp.groovy
+++ codebase/projects/core/db/bering/027_extension_iteration14/010_convert_study_subject_consents_informed_consent_signed_date_to_timestamp.groovy
@@ -2,10 +2,13 @@ class ConvertStudySubjectConsentsInformedConsentSignedDateToTimestamp extends ed
     void up() {
     	if (databaseMatches('oracle')){
        		addColumn('study_subject_consents','informed_consent_signed_tstamp', 'timestamp');
+       		execute("update study_subject_consents set informed_consent_signed_tstamp = informed_consent_signed_date");
         }
         if (databaseMatches('postgresql')){
        		addColumn('study_subject_consents','informed_consent_signed_timestamp', 'timestamp');
+       		execute("update study_subject_consents set informed_consent_signed_timestamp = informed_consent_signed_date");
         }
+       
    	}
 	void down() {
     }
