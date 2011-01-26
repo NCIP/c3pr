ef31fad9d5e89d6dac51a26ac68a2e7adfb2df43
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2011-01-26 15:54:39 +0000

    changed tab title to 'Study Subject Timeline'

--- codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/registration/tabs/RegistrationHistoryChartTab.java
+++ codebase/projects/web/src/java/edu/duke/cabig/c3pr/web/registration/tabs/RegistrationHistoryChartTab.java
@@ -2,15 +2,11 @@ package edu.duke.cabig.c3pr.web.registration.tabs;
 
 import edu.duke.cabig.c3pr.web.registration.StudySubjectWrapper;
 
-/**
- * Created by IntelliJ IDEA. User: kherm Date: Jun 15, 2007 Time: 3:30:05 PM To
- * change this template use File | Settings | File Templates.
- */
 public class RegistrationHistoryChartTab<C extends StudySubjectWrapper> extends
 		RegistrationTab<C> {
 	
 	public RegistrationHistoryChartTab() {
-		super("Registration Timeline", "Registration Timeline",
+		super("Study Subject Timeline", "Study Subject Timeline",
 				"registration/reg_history_timeline");
 	}
 	
