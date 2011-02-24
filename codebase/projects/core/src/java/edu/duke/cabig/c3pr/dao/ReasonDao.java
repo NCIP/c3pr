4e8d9fdf8f0d49feeb241f8441fe50ddf566caae
Vinay Gangoli <vinay.gangoli@semanticbits.com>
2011-02-24 17:24:42 +0000

    Fixed the getByCodeAndType query

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/ReasonDao.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/dao/ReasonDao.java
@@ -2,12 +2,16 @@ package edu.duke.cabig.c3pr.dao;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import edu.duke.cabig.c3pr.domain.OffFollowupReason;
 import edu.duke.cabig.c3pr.domain.OffReservingReason;
 import edu.duke.cabig.c3pr.domain.OffScreeningReason;
 import edu.duke.cabig.c3pr.domain.OffStudyReason;
 import edu.duke.cabig.c3pr.domain.OffTreatmentReason;
 import edu.duke.cabig.c3pr.domain.Reason;
+import gov.nih.nci.logging.api.util.StringUtils;
 
 
 /**
@@ -15,6 +19,10 @@ import edu.duke.cabig.c3pr.domain.Reason;
  */
 public class ReasonDao extends GridIdentifiableDao<Reason> {
 
+   /** The log. */
+    private static Log log = LogFactory.getLog(ReasonDao.class);
+ 
+    
     @Override
     public Class<Reason> domainClass() {
         return Reason.class;
@@ -44,8 +52,33 @@ public class ReasonDao extends GridIdentifiableDao<Reason> {
     	return (Reason)getHibernateTemplate().find("from Reason where code = ?",code).get(0);
     }
     
-    public Reason getReasonByCodeAndType(String code, String classname){
-    	return (Reason)getHibernateTemplate().find("from Reason where code = ? and dtype = ?",new Object[] {code, classname}).get(0);
+    /**
+     * Gets the reason by code and type. Returns null if no match is found.
+     *
+     * @param code the code
+     * @param classname the classname
+     * @return the reason by code and type
+     */
+    public Reason getReasonByCodeAndType(String code, String className){
+    	if(StringUtils.isBlank(className)){
+    		return null;
+    	}
+    	String query = getClassConditionPartOfSearchQuery(className);
+    	List<Reason> reasonList = getHibernateTemplate().find("from Reason r where code = ?" + query,
+    															new Object[] {code});
+    	if(reasonList.size() > 0){
+    		if(reasonList.size() > 1){
+    			log.error("Reason table has more then one record with code: "+code+ " and dtype: "+className);
+    		}
+    		return reasonList.get(0);
+    	}
+    	return null;
     }
+
+	private String getClassConditionPartOfSearchQuery(String className) {
+		String modifiedClassName = className.substring(className.lastIndexOf(".")+1);
+		return "and r.class = "+modifiedClassName;
+	}
     
 }
+
