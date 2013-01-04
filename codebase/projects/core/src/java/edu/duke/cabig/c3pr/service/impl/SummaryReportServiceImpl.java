cc054c7249ee7180cf197ea58267864c11b721b6
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2013-01-04 15:57:29 -0500

    add copyright header to java, xml and properties files

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/impl/SummaryReportServiceImpl.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/impl/SummaryReportServiceImpl.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright Duke Comprehensive Cancer Center and SemanticBits
+ * 
+ *   Distributed under the OSI-approved BSD 3-Clause License.
+ *   See https://github.com/NCIP/c3pr/LICENSE.txt for details.
+ ******************************************************************************/
 package edu.duke.cabig.c3pr.service.impl;
 
 import java.util.Date;
--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/impl/passwordpolicy/PasswordPolicyServiceImpl.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/service/impl/passwordpolicy/PasswordPolicyServiceImpl.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright Duke Comprehensive Cancer Center and SemanticBits
+ * 
+ *   Distributed under the OSI-approved BSD 3-Clause License.
+ *   See https://github.com/NCIP/c3pr/LICENSE.txt for details.
+ ******************************************************************************/
 package edu.duke.cabig.c3pr.service.impl.passwordpolicy;
 
 import org.springframework.beans.factory.annotation.Required;
