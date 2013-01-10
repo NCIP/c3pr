21f7d422664b902401df51fb09e758e11c850b43
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2013-01-10 11:31:53 -0500

    add copyright header to java, jsp, xml and properties files

--- codebase/projects/core/src/java/edu/duke/cabig/c3pr/security/DomainObjectPrivilegeAndObjectIdGenerator.java
+++ codebase/projects/core/src/java/edu/duke/cabig/c3pr/security/DomainObjectPrivilegeAndObjectIdGenerator.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+* Copyright Duke Comprehensive Cancer Center and SemanticBits
+* 
+* Distributed under the OSI-approved BSD 3-Clause License.
+* See https://github.com/NCIP/c3pr/LICENSE.txt for details.
+*******************************************************************************/
 package edu.duke.cabig.c3pr.security;
 
 import edu.duke.cabig.c3pr.domain.AbstractMutableDeletableDomainObject;
--- codebase/projects/web/web/WEB-INF/views/registration/asynchronous/checkCeiling.jsp
+++ codebase/projects/web/web/WEB-INF/views/registration/asynchronous/checkCeiling.jsp
@@ -1,3 +1,9 @@
+<%--
+ Copyright Duke Comprehensive Cancer Center and SemanticBits
+ 
+ Distributed under the OSI-approved BSD 3-Clause License.
+ See https://github.com/NCIP/c3pr/LICENSE.txt for details.
+--%>
 <script>
 if(${alertForCeiling}){
 	alert("Accrual Ceiling for this Epoch alreay reached. Cannot enroll any more subjects on this Epoch.");
