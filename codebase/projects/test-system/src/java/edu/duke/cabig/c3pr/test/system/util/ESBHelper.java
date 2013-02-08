91edbe7971a91758346f7fc5542c2752949a8d26
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2013-02-08 13:49:38 -0500

    apply copyright header

--- codebase/projects/test-system/src/java/edu/duke/cabig/c3pr/test/system/util/ESBHelper.java
+++ codebase/projects/test-system/src/java/edu/duke/cabig/c3pr/test/system/util/ESBHelper.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+* Copyright Duke Comprehensive Cancer Center and SemanticBits
+* 
+* Distributed under the OSI-approved BSD 3-Clause License.
+* See http://ncip.github.com/c3pr/LICENSE.txt for details.
+*******************************************************************************/
 package edu.duke.cabig.c3pr.test.system.util;
 
 import gov.nci.nih.cagrid.tests.core.util.AntUtils;
--- codebase/projects/ws-client/src/edu/duke/cabig/c3pr/webservice/iso21090/package-info.java
+++ codebase/projects/ws-client/src/edu/duke/cabig/c3pr/webservice/iso21090/package-info.java
@@ -1,2 +1,8 @@
+/*******************************************************************************
+* Copyright Duke Comprehensive Cancer Center and SemanticBits
+* 
+* Distributed under the OSI-approved BSD 3-Clause License.
+* See http://ncip.github.com/c3pr/LICENSE.txt for details.
+*******************************************************************************/
 @javax.xml.bind.annotation.XmlSchema(namespace = "uri:iso.org:21090", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package edu.duke.cabig.c3pr.webservice.iso21090;
