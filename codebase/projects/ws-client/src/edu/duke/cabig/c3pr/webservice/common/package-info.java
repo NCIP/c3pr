209665ce814811384f949e7e6c441d7210937db1
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2013-02-08 13:33:59 -0500

    update copyright header

--- codebase/projects/ws-client/src/edu/duke/cabig/c3pr/webservice/common/package-info.java
+++ codebase/projects/ws-client/src/edu/duke/cabig/c3pr/webservice/common/package-info.java
@@ -1,2 +1,8 @@
+/*******************************************************************************
+* Copyright Duke Comprehensive Cancer Center and SemanticBits
+* 
+* Distributed under the OSI-approved BSD 3-Clause License.
+* See http://ncip.github.com/c3pr/LICENSE.txt for details.
+*******************************************************************************/
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://enterpriseservices.nci.nih.gov/Common", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package edu.duke.cabig.c3pr.webservice.common;
